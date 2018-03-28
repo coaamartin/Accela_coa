f3e36cad49477234e5adce5d1b8fd9e744218704
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-03-28 10:06:10 -0600

    added missing code
    ADMIN had code in Accela but not checked into Github

--- EVENT/WTUA;Building!~!~!~.js
+++ EVENT/WTUA;Building!~!~!~.js
@@ -1,3 +1,18 @@
 // WTUA:Building/*/*/*
 
+// following block of code was in Accela but not Github... it was added by "ADMIN" 12-22-2016
+if (wfTask == "Permit Issuance" &amp;&amp; wfStatus == "Issued")
+editAppSpecific("Permit Issued Date", sysDateMMDDYYYY);
+if (wfTask == "Permit Issuance" &amp;&amp; wfStatus == "Issued")
+editAppSpecific("Permit Expiration Date", dateAdd(null, 180));
+if (wfTask == "Permit Status" &amp;&amp; wfStatus == "Permit Issued")
+editAppSpecific("Permit Issued Date", sysDateMMDDYYYY);
+if (wfTask == "Permit Status" &amp;&amp; wfStatus == "Permit Issued")
+editAppSpecific("Permit Expiration Date", dateAdd(null, 180));
+if (wfTask == "Permit Issuance" &amp;&amp; wfStatus == "Issued")
+licEditExpInfo(null, AInfo["Permit Expiration Date"]);
+if (wfTask == "Permit Status" &amp;&amp; wfStatus == "Permit Issued")
+licEditExpInfo(null, AInfo["Permit Expiration Date"]);
+// end ADMIN code
+
 script205_DeactivateSpecInsp();
