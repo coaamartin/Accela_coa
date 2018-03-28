6eb9ba5fa4c3ea681112d8f7eda725719fae812d
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-03-28 11:32:16 -0600

    script 295

---
+++ INCLUDES_CUSTOM/script295_UpdateCompletenessCheckPRA.js
@@ -0,0 +1,23 @@
+//Script 295	Update completeness check to complete when payment made
+//Record Types:	PublicWorks/Real Property/Easement/NA
+//Event: 		PaymentReceivedAfter (PRA)
+//Desc:			When a payment is made and the balance is 0 
+//				then update the workflow task Completeness Check 
+//				with the workflow status to Complete.
+//Created By: Silver Lining Solutions
+
+function script295_UpdateCompletenessCheckPRA() {
+	logDebug("script295_UpdateCompletenessCheckPRA started.");
+	try{
+		if ( balanceDue <= 0 ) {
+			updateTask("Completeness Check","Complete","updated via script when balance is zero","updated via script when balance is zero");
+			logDebug("script295: updated completeness check to complete!");
+		}
+	}
+	catch(err){
+		showMessage = true;
+		comment("script295: Error on custom function script295_UpdateCompletenessCheckPRA(). Please contact administrator. Err: " + err);
+		logDebug("script295: Error on custom function script295_UpdateCompletenessCheckPRA(). Please contact administrator. Err: " + err);
+	}
+	logDebug("script295_UpdateCompletenessCheckPRA() ended.");
+};//END script295_UpdateCompletenessCheckPRA();
