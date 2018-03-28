2b3457596038c86fa02d39903cbd277eba37db3a
Suhail Wakil <suhailwakil@gmail.com>
2018-03-28 13:50:45 -0700

    script40

---
+++ INCLUDES_SCRIPTS/40_backFlowPreventerEmail.js
@@ -0,0 +1,17 @@
+//Supporting Functions
+function script40_backFlowPreventerEmail(){
+	if (wfTask == "Backflow Preventor" && wfStatus == "Final") {
+		var applicant = getContactByType("Applicant", capId);
+		if (!applicant || !applicant.getEmail()) {
+			logDebug("**WARN no applicant found on or no email capId=" + capId);
+			return false;
+		}
+		var email = applicant.getEmail();
+		var emailtemplatename = "BACKFLOW PREVENTER NOTIFICATION";
+		var vEParams = aa.util.newHashtable();
+		var emailparams = addStdVarsToEmail(vEParams, capId);
+		emailparams.put("$$wfDate$$", wfDate);
+		emailAsync(email, emailtemplatename, emailparams, "", "", "", "");
+
+	}
+}
