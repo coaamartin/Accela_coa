3d86d148aa1c0383effb215513542244be623127
Main <jmain@auroragov.org>
2018-03-29 10:20:50 -0600

    more 40

---
+++ INCLUDES_SCRIPTS/40_backFlowPreventerEmail.js
@@ -0,0 +1,22 @@
+//script40_backFlowPreventerEmail Functions
+
+result = script40_backFlowPreventerEmail();
+logDebug("Started 40_backFlowPreventerEmail.."+ result);
+function script40_backFlowPreventerEmail(){
+	logDebug("Started 40_backFlowPreventerEmail..");
+	if (wfTask == "Backflow Preventor" && wfStatus == "Final") {
+		var applicant = getContactByType("Applicant", capId);
+		if (!applicant || !applicant.getEmail()) {
+			logDebug("**WARN no applicant found on or no email capId=" + capId);
+			return false;
+		}
+		var email = applicant.getEmail();
+		var emailtemplatename = "BACKFLOW PREVENTER NOTIFICATION";
+		var vEParams = aa.util.newHashtable();
+		var emailparams = aa.util.newHashtable();
+		emailparams = addStdVarsToEmail(vEParams, capId);
+		emailparams.put("$$wfDate$$", wfDate);
+		emailAsync(email, emailtemplatename, emailparams, "", "", "", "");
+	}
+	logDebug("Ended 40_backFlowPreventerEmail..");
+}
