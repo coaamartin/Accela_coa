9ca96b0cf49e8c25380dfbbca64ef83fbc54fc14
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-04-02 11:39:09 -0600

    Add files via upload

---
+++ INCLUDES_CUSTOM/script281_AssignScheduledFireInspection.js
@@ -0,0 +1,31 @@
+//Script 281
+//Record Types:	Fire/*/*/*
+//Event: 		ISA
+//Desc:			Event InspectionScheduleAfter 
+//
+//              Criteria: Any Fire Inspection being scheduled 
+//
+//              Action: When an Inspection is Scheduled update the Inspector 
+//              to the assigned user on the record detail
+//
+//Created By: Silver Lining Solutions
+
+function script281_AssignScheduledFireInspection(){
+	logDebug("script281_AssignScheduledFireInspection() started.");
+	try
+	{
+		// first get the user that has been assigned to the Record
+		// if there is no user this may need to be an error
+		
+		// get this inspection number
+		// inspId = curResult.inspId;
+		// we have the user, assign this user to the inspection being scheduled
+		// assignInspection(inspNum,userID);
+		
+	}
+	catch(err)
+	{
+		logDebug("Error on script281_AssignScheduledFireInspection(). Please contact administrator. Err: " + err);
+	}
+	logDebug("script281_AssignScheduledFireInspection() ended.");
+};//END script281_AssignScheduledFireInspection
