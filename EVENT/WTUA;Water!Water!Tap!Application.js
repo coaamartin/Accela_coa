a0af245a7655d88a0bd724a415d7c3e831d3e53f
Main <jmain@auroragov.org>
2018-04-09 16:06:10 -0600

    Adding script 33 - water tap application

--- EVENT/WTUA;Water!Water!Tap!Application.js
+++ EVENT/WTUA;Water!Water!Tap!Application.js
@@ -1,3 +1,6 @@
 //COA Script - Suhail
 include("32_recStatusUpdateWaterTap");
 include("31_1_EmailWithFee");
+
+//COA Script - JMAIN
+include("33_WaterTapApplicationFee.js");
---
+++ INCLUDES_SCRIPTS/33_WaterTapApplicationFee.js
@@ -0,0 +1,14 @@
+/*
+When wf step "Application Submittal" status = "Specific Contract Fees - Accepted" then 
+status wf step "Fee Processing" =  "Ready to Pay with Specific Contract Fees"
+
+written by JMAIN
+*/
+
+
+if (wfTask == "Application Submittal" && wfStatus == "Specific Contract Fees - Accepted")
+{
+	logDebug("Activating Task Fee Processing and Setting Task Status to Specific Contract Fees - Accepted");
+	activateTask("Fee Processing");
+	updateTask("Fee Processing", "Specific Contract Fees - Accepted", "updated by script", "updated by script");
+}
