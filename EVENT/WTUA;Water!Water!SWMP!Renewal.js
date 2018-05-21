eb3175e379867d9fe0ea7b7857c6a24cbb5a7f7e
Charlie MUnzer <cvm@byrnesoftware.com>
2018-05-21 13:06:42 -0500

    testing script 85 - added function to event & custom file

--- EVENT/WTUA;Water!Water!SWMP!Renewal.js
+++ EVENT/WTUA;Water!Water!SWMP!Renewal.js
@@ -1,3 +1,5 @@
 //WTUA:Water/Water/SWMP/Renewal
 
 script84_SendRenewalEmailWhenPermitIssuedComplete();
+
+script85_UpdateSwmpParent();
---
+++ INCLUDES_CUSTOM/script85_UpdateSwmpParent.js
@@ -0,0 +1,43 @@
+/*Script 85
+ * Record Types:	Water/Water/SWMP/Renewal
+ * Event: 		WorkflowTaskUpdateAfter (WTUA)
+ * 
+ * Desc:			
+ * If the workflow task “Permit Issuance” has a status of “Completed” then automatically 
+ * update the parent permit (Water/Water/SWMP/Permit) with data from the current renewal 
+ * record(Water/Water/SWMP/Renewal) including contacts, custom fields, application name, 
+ * address and parcel.
+ * 
+*/
+
+function script85_UpdateSwmpParent() {
+	logDebug("script85_UpdateSwmpParent() started.");
+	try{
+		// if wfTask = Permit Issued & wfStatus = Completed”
+	//	if ( wfTask == "Permit Issued" && wfStatus == "Completed" ) 
+	//	{
+            //get parent
+            var parentCapid = getParent();
+            if(parentCapid) {
+                //make sure parent is a permit (Water/Water/SWMP/Permit)
+                parentCap = aa.cap.getCap(parentCapid).getOutput();
+                parentCapTypeString = childCap.getCapType().toString();
+                logDebug("parentCapTypeString = " + parentCapTypeString);
+                if(parentCapTypeString == 'Water/Water/SWMP/Permit') {
+                    // copy data from renewal to parent application
+                    copyContacts(capId,parentCapid);
+                    copyASIFields(capId,parentCapid)
+                    copyAddresses(capId, parentCapid);
+                    copyParcels(capId, parentCapid);
+                    parentCapid.setSpecialText(capId.getSpecialText());
+                }
+            }
+ 	//	}
+	}
+	catch(err){
+		showMessage = true;
+		comment("Error on custom function “Permit Issuance(). Please contact administrator. Err: " + err);
+		logDebug("Error on custom function “Permit Issuance(). Please contact administrator. Err: " + err);
+	}
+	logDebug("script85_UpdateSwmpParent() ended.");
+};//END script85_UpdateSwmpParent();
