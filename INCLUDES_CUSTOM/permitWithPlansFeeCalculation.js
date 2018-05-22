594d66cca8e32598f253614f83627319c580359a
Ledezma <jal@wbai.com>
2018-05-21 21:31:32 -0500

    updated setCodeReference() updated permitWithPlansFeeCalcualtion() to use the correct fee items for fee schedule BLD_PNP

--- INCLUDES_CUSTOM/permitWithPlansFeeCalculation.js
+++ INCLUDES_CUSTOM/permitWithPlansFeeCalculation.js
@@ -55,12 +55,12 @@ function permitWithPlansFeeCalculation(workFlowTask, workflowStatusArray, permit
 		feeCodesAry["BUILDING_DRIVEWAY_FEE"] = "BLD_PWP_11";
 	} else if (appTypeArray && String(appTypeArray[2]).equalsIgnoreCase("No Plans")) {
 		feeSched = "BLD_PNP";
-		feeCodesAry["BUILDING_FEE_FLAT"] = "BLD_PWP_06";
-		feeCodesAry["BUILDING_FEE_VALUATION"] = "BLD_PWP_01";
-		feeCodesAry["ARAPAHOE_FEE_1"] = "BLD_PWP_03";
-		feeCodesAry["ARAPAHOE_FEE_2"] = "BLD_PWP_04";
-		feeCodesAry["BUILDING_USE_TAX_FEE"] = "BLD_PWP_02";
-		feeCodesAry["BUILDING_DRIVEWAY_FEE"] = "BLD_PWP_11";
+		feeCodesAry["BUILDING_FEE_FLAT"] = "BLD_PNP_06";
+		feeCodesAry["BUILDING_FEE_VALUATION"] = "BLD_PNP_01";
+		feeCodesAry["ARAPAHOE_FEE_1"] = "BLD_PNP_03";
+		feeCodesAry["ARAPAHOE_FEE_2"] = "BLD_PNP_04";
+		feeCodesAry["BUILDING_USE_TAX_FEE"] = "BLD_PNP_02";
+		feeCodesAry["BUILDING_DRIVEWAY_FEE"] = "BLD_PNP_11";
 	}
 
 	if (feeSched == null) {
