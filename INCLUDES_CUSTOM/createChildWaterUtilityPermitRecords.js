9fff095957021ee905aef8c5f6ce9c94bac488a3
jantled <jal@wbai.com>
2018-05-23 13:18:42 -0500

    Updated createChildWaterUtilityPermitRecords() for script 337

--- INCLUDES_CUSTOM/createChildWaterUtilityPermitRecords.js
+++ INCLUDES_CUSTOM/createChildWaterUtilityPermitRecords.js
@@ -1,37 +1,39 @@
+/* Script 337 
+ *
+ * Create water permit child.
+ *
+ */
 function createChildWaterUtilityPermitRecords() {
-	try {
-		if (wfTask == "Fire Life Safety Review" && wfStatus == "Approved") {
-			var tsiArray = new Array();
-			loadTaskSpecific(tsiArray);
-			var pFireLine = tsiArray["Private Fire Line"];
-			var NoOfFireLines = tsiArray["Number of Private Fire Lines"];
-			aa.print("pFireLine "+ pFireLine);
-			aa.print("NoOfFireLines " + NoOfFireLines);
-			
-			if (pFireLine == "Yes"
-					&& (NoOfFireLines != null && parseInt(NoOfFireLines) > 0)) {
+    logDebug("createChildWaterUtilityPermitRecords() started");
+    try {
+        var $iTrc = ifTracer2;
+        if ($iTrc(wfTask == "Fire Life Safety Review" && wfStatus == "Approved", 'wfTask == "Fire Life Safety Review" && wfStatus == "Approved"')) {
+            var tsiArray = new Array();
+            loadTaskSpecific(tsiArray);
+            var pFireLine = tsiArray["Private Fire Line"];
+            var NoOfFireLines = tsiArray["Number of Private Fire Lines"];
+            aa.print("pFireLine: "+ pFireLine);
+            aa.print("NoOfFireLines: " + NoOfFireLines);
+            
+            if (pFireLine == "Yes" && (NoOfFireLines != null && parseInt(NoOfFireLines) > 0)) {
 
-				for (var i = 0; i < parseInt(NoOfFireLines); i++) {
-					var cCapId = createChild("Water", "Utility", "Permit",
-							"NA", "Water Utility Permit"); // this function
-															// copies address,
-															// parcel,and
-															// contact
-															// information
-					if (cCapId != null) {
-						// copy Owner
-						copyOwner(capId, cCapId);
-					}
+                for (var i = 0; i < parseInt(NoOfFireLines); i++) {
+                    var cCapId = createChild("Water", "Utility", "Permit","NA", "Water Utility Permit"); // this function
+                                                            // copies address,
+                                                            // parcel,and
+                                                            // contact
+                                                            // information
+                    if (cCapId != null) {
+                        // copy Owner
+                        copyOwner(capId, cCapId);
+                    }
 
-				}
-
-				editAppSpecific("Utility Permit Type", "Private Fire Lines",
-						cCapId);
-			}
-
-		}
-
-	} catch (e) {
-		logDebug("****ERROR IN WTUA:BUILDING/PERMIT/NEW BUILDING/NA:**** " + e);
-	}
-}
+                }
+                editAppSpecific("Utility Permit Type", "Private Fire Lines",cCapId);
+            }
+        }
+    } catch (e) {
+        logDebug("****ERROR IN WTUA:BUILDING/PERMIT/NEW BUILDING/NA:**** " + e);
+    }
+    logDebug("createChildWaterUtilityPermitRecords() ended");
+}//END createChildWaterUtilityPermitRecords()
