ea4f6fc6ab2ceaae699fbdb2f6ee2871d758af86
Ledezma <jal@wbai.com>
2018-05-23 11:18:02 -0500

    updated script6

--- INCLUDES_CUSTOM/bldScript6_FinalInspComplete.js
+++ INCLUDES_CUSTOM/bldScript6_FinalInspComplete.js
@@ -1,7 +1,7 @@
 function bldScript6_FinalInspComplete(){
     logDebug("bldScript6_FinalInspComplete() started")
     try{
-        var $iTrc = ifTracer2,
+        var $iTrc = ifTracer,
             pendOrSched = inspectionsByStatus(capId, "scheduled") && inspectionsByStatus(capId, "pending"),
             lettersReceived = checkReqLettersReceived(capId),
             certOfOccup = AInfo["Certificate of Occupancy"] == "CHECKED";
