0303d9004c65b9fb36584e054861b242eb0c1958
James Main <jmain@auroragov.org>
2018-05-23 12:43:29 -0600

    working on COA #5

--- INCLUDES_SCRIPTS/5_fireCompleteOrNoViolations.js
+++ INCLUDES_SCRIPTS/5_fireCompleteOrNoViolations.js
@@ -6,7 +6,7 @@ if (inspResult == "Complete")
 {
 	{	
 		logDebug("inspResult is complete so setting num failed insp to 0 and closing Inspection task");
-		editAppSpecific("Number of Failed Inspections", 0);
+		editAppSpecific("Number of Failed Inspections", "0");
 		closeTask("Inspection","Compliance/Complete","Updated via script","Updated via script");
 		updateAppStatus("Complete","updated by script");
 	}
@@ -14,7 +14,7 @@ if (inspResult == "Complete")
 if (inspResult =="No Violations Found")
 	{
 		logDebug("inspResult is no violations so setting num failed insp to 0 and closing Inspection task");
-		editAppSpecific("Number of Failed Inspections", 0);
+		editAppSpecific("Number of Failed Inspections", "0");
 		closeTask("Inspection","No Violations Found","Updated via script","Updated via script");
 		updateAppStatus("Complete","updated by script");
 	}	
