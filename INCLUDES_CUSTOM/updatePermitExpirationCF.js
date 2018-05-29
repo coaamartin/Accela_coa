1c16d6e2197cf24fee171cf76b9bb279be6d492e
jantled <jal@wbai.com>
2018-05-29 18:15:16 -0500

    updated updatePermitExpirationCF()

--- INCLUDES_CUSTOM/updatePermitExpirationCF.js
+++ INCLUDES_CUSTOM/updatePermitExpirationCF.js
@@ -2,7 +2,7 @@ function updatePermitExpirationCF(workflowTaskName, workflowStatus,cfName) {
 	for ( var i in workflowTaskName) {
 		var wft = workflowTaskName[i];
 		if (wfTask == wft && wfStatus == workflowStatus) {
-			var statusDate = dateAdd(null,180);
+			var statusDate = dateAdd(wfDateMMDDYYYY,180);
 			useAppSpecificGroupName = false;
 			editAppSpecific(cfName,statusDate);
 		}
