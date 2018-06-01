c63a2b99443aa6ee665962ba228d9aefb3748b87
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-06-01 08:48:13 -0600

    Add files via upload

--- INCLUDES_CUSTOM/script208_UpdatePermitFields.js
+++ INCLUDES_CUSTOM/script208_UpdatePermitFields.js
@@ -14,8 +14,8 @@ function script208_UpdatePermitFields() {
 			if (wfTask==("Permit Issuance") && wfStatus ==("Issued")) {
 				logDebug("script208_correct Task/Status.");
 				var sysDateYYYYMMDD = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"YYYY-MM-DD");
-				editAppSpecific("permit issue date",sysDateYYYYMMDD);
-				editAppSpecific("permit expiration date",dateAdd(null,180));
+				editAppSpecific("Permit Issue Date",sysDateYYYYMMDD);
+				editAppSpecific("Permit Expiration Date",dateAdd(null,180));
 				logDebug("script208_Updating asi fields.");
 			}
 		}
