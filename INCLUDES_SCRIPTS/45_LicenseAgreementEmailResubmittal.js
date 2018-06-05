c4bfdb8ffb5f3e57f74e6eb1bb463801d7d715ed
Suhail Wakil <suhailwakil@gmail.com>
2018-06-04 22:08:56 -0700

    script 45

---
+++ INCLUDES_SCRIPTS/45_LicenseAgreementEmailResubmittal.js
@@ -0,0 +1,41 @@
+if(isTaskActive("Plans Coordination")){
+  var checkTasks = ["Engineering Review", "Real Property Review", "Water Dept Review", "Life Safety Review", "Traffic Review", "Parks Review", "Risk Management Review"];
+  var statusCheck = "Resubmittal Requested";
+
+  var isActive = isListActive(checkTasks);
+  if(!isActive){
+    var listHasResubmittal = isResubmittal(checkTasks);
+    if(listHasResubmittal){
+      sendEmailToApplicant();
+    }
+  }
+}
+
+function isListActive(checkTasks){
+  for(i in checkTasks){
+    active = isTaskActive(checkTasks[i]);
+    if(active){
+      return true;
+    }
+  }
+  return false;
+}
+
+function isResubmittal(){
+  for(i in checkTasks){
+    status = taskStatus(checkTasks[i]);
+    if(status.equals(statusCheck)){
+      return true;
+    }
+  }
+  return false;
+}
+
+function sendEmailToApplicant(){
+  var contact = "Applicant";
+  var template = "JD_TEST_TEMPLATE";
+  var joke = "Where there's a will, there's a relative.";
+  var emailparams = aa.util.newHashtable();
+  emailparams.put("$$Joke$$", joke);
+  emailContacts(contact, template, emailparams, "", "", "N", "");
+}
