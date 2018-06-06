a7c1ac20dd1bd8963082254a4d02e61cfddcc326
jantled <jal@wbai.com>
2018-06-06 17:56:43 -0500

    script2

--- INCLUDES_CUSTOM/bldScript2_noContractorCheck4WTUA.js
+++ INCLUDES_CUSTOM/bldScript2_noContractorCheck4WTUA.js
@@ -27,13 +27,37 @@ function bldScript2_noContractorCheck4WTUA(){
             if(getPrimaryOwnerFullName().toUpperCase() == applicantName.toUpperCase()) ownerApplicantMatch = true;
             
             if($iTrc((!ownerIsContractor && !lpOnFile) || (ownerIsContractor && !ownerApplicantMatch), '(!ownerIsContractor && !lpOnFile) || (ownerIsContractor && !ownerApplicantMatch)'))
-                cancelMsg = "Contractor is not attached to Permit. Please send an email through Communications tab to the Applicant that the Permit cannot be issued without a Licensed Contractor";
+                cancelMsg = "UNABLE to issue permit without a Contractor attached.  Email has been sent to applicant. Task has been updated to In Progress";
             
             cancel = showMessage = cancelMsg.length > 0;
             
             if(cancel) {
 				comment(cancelMsg);
 				updateTask("Permit Issuance", "In Progress", "Updated via script.", "Updated via script 2. A contractor is needed before issuing permit.");
+				updateAppStatus("In Progress","");
+				
+				adResult = aa.address.getAddressByCapId(capId).getOutput(); 
+			    for(x in adResult)
+			    {
+			    	var adType = adResult[x].getAddressType(); 
+			    	var stNum = adResult[x].getHouseNumberStart();
+			    	var preDir =adResult[x].getStreetDirection();
+			    	var stName = adResult[x].getStreetName(); 
+			    	var stType = adResult[x].getStreetSuffix();
+			    	var city = adResult[x].getCity();
+			    	var state = adResult[x].getState();
+			    	var zip = adResult[x].getZip();
+			    }
+	
+	            var primaryAddress = stNum + " " + preDir + " " + stName + " " + stType + " " + "," + city + " " + state + " " + zip;
+				
+				var emailTemplate = 'BLD PERMIT REQUIRES LP # 2',
+                contactTypes = 'Applicant',
+                emailparams = aa.util.newHashtable();
+				emailparams.put("$$altID$$", capId.getCustomID());
+	            emailparams.put("$$FullAddress$$", primaryAddress);
+				
+                emailContacts(contactTypes, emailTemplate, emailparams, "", "", "N", "");
 			}
             
         }
