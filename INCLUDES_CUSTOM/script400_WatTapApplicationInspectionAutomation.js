1466e5a9966923083f52871fcae782061c57acec
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-06 10:45:00 -0500

    changed script400_WatTapApplicationInspectionAutomation()

--- INCLUDES_CUSTOM/script400_WatTapApplicationInspectionAutomation.js
+++ INCLUDES_CUSTOM/script400_WatTapApplicationInspectionAutomation.js
@@ -31,11 +31,11 @@ function script400_WatTapApplicationInspectionAutomation() {
 
             
         // printObjProps(guideSheetObjects);
-        // if (guideSheetObjects &&  guideSheetObjects.length > 0) {
-        //      for (idx in guideSheetObjects) {
-        //         printObjProps(guideSheetObjects[idx]);
+         if (guideSheetObjects &&  guideSheetObjects.length > 0) {
+              for (idx in guideSheetObjects) {
+                 printObjProps(guideSheetObjects[idx]);
 
-        //         printObjProps(guideSheetObjects[idx].item);
+                 printObjProps(guideSheetObjects[idx].item);
         //          for (idx2 in guideSheetObjects[idx].item.getItemASISubgroupList()) { 
         //         //     printObjProps(guideSheetObjects[idx].item.getItemASISubgroupList()[idx2]);
         //         //     logDebug(guideSheetObjects[idx].item.getItemASISubgroupList()[idx2].fields.length);
@@ -48,18 +48,9 @@ function script400_WatTapApplicationInspectionAutomation() {
         //         // if (guideSheetsAry[idx].gsType == "FORESTRY INSPECTOR" && guideSheetsAry[g].text == checkListItemName) {
         //         //     resultMatched = (String(guideSheetsAry[g].status).toLowerCase() == "yes");
         //        //  }
-        //     }
-        // }  
+             }
+         }  
        
-    //    var guidesheetModel = aa.env.getValue("GuidesheetModel")
-    //  //  logDebug(guidesheetModel);
-    //  //  printObjProps(guidesheetModel);
-    //    var guidesheetModelItems = guidesheetModel.getItems().toArray();
-    //    for (var i in guidesheetModelItems) {
-    //         logDebug(guidesheetModelItems[i]);
-    //         printObjProps(guidesheetModelItems[i]);
-    //     } 
-
 
 		if (ifTracer(eventName.indexOf("InspectionResultSubmitBefore") > -1, 'eventName.indexOf(InspectionResultSubmitBefore) > -1')) {
             if (ifTracer(inspType == 'Meter Set Inspection', 'inspType == Meter Set Inspection')) {
