0f19185e2ccf950816a29c5e588f9b6eca390778
John Schomp <john@grayquarter.com>
2018-06-07 16:14:26 -0700

    144 null check

--- PAGEFLOW/ACA_144_BEFORE_REQ_PARCEL_OR_ADDR.js
+++ PAGEFLOW/ACA_144_BEFORE_REQ_PARCEL_OR_ADDR.js
@@ -18,7 +18,7 @@
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
 var showMessage = false; // Set to true to see results in popup window
-var showDebug = true; // Set to true to see debug messages in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = true;
@@ -99,7 +99,7 @@ if (address) {
 	logDebug('streetName = '+streetName);
 }
 
-if (!parcelNum && !streetName) {
+if ((!parcelNum || parcelNum == "") && (!streetName || streetName =="")) {
 	cancel = true;
 	showMessage = true;
 	comment("Address or Parcel is required");
