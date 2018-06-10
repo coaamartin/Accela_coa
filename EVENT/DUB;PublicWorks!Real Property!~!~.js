f01c3ce5608f4d91b9793eb888e7ebfb1d694de2
Suhail Wakil <suhailwakil@gmail.com>
2018-06-10 11:59:18 -0700

    script 44

--- EVENT/DUB;PublicWorks!Real Property!~!~.js
+++ EVENT/DUB;PublicWorks!Real Property!~!~.js
@@ -1,11 +1,11 @@
-var message = "AAAAA";
-cancel = true;
-showMessage=true;
-showDebug=true;
 
 //if (isPublicUser) {
+if (appStatus.equals("In Review")) {
+	var message = "AAAAA";
+	cancel = true;
+	showMessage=true;
+	showDebug=true;
 
-if (cancel) {
 	aa.env.setValue("ScriptReturnCode", "1");
 	if (showMessage) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
 	if (showDebug) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>");
