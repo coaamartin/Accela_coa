4a224279437b110a1a05edf2372706160b3828e5
Suhail Wakil <suhailwakil@gmail.com>
2018-06-10 11:41:22 -0700

    script 44

--- EVENT/DUB;PublicWorks!Real Property!~!~.js
+++ EVENT/DUB;PublicWorks!Real Property!~!~.js
@@ -5,11 +5,9 @@ cancel = true;
 
 //if (isPublicUser) {
 if (cancel) {
-	aa.env.setValue("ScriptReturnCode", "1"); 
-	aa.env.setValue("ScriptReturnMessage", "WorkflowTaskUpdateAfter for Renewal process.");
-/*
+	aa.env.setValue("ScriptReturnCode", "1");
 	if (showMessage) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
 	if (showDebug) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>");
-	if (showMessage) aa.env.setValue("ErrorMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);*/
+	if (showMessage) aa.env.setValue("ErrorMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
 }
 //} 
