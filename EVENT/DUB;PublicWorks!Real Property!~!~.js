ece2c1f23b4f79e9b7295f71c6ca51cfa4647ab0
Suhail Wakil <suhailwakil@gmail.com>
2018-06-10 22:38:18 -0700

    script 44

--- EVENT/DUB;PublicWorks!Real Property!~!~.js
+++ EVENT/DUB;PublicWorks!Real Property!~!~.js
@@ -11,13 +11,14 @@ capStatus = aa.cap.getCap(capId).getOutput().getCapStatus();
 
 
 if (isPublicUser && capStatus.equals("In Review")) {
-	var message = "AAAAA";
+	var message = "Can not upload a document while record status is In Review";
 	cancel = true;
 	showMessage=true;
-	showDebug=true;
 
-	aa.env.setValue("ScriptReturnCode", "1");
-	if (showMessage) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
-	if (showDebug) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>");
-	if (showMessage) aa.env.setValue("ErrorMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
+	if (cancel) {
+		aa.env.setValue("ScriptReturnCode", "1");
+		if (showMessage)
+			aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled </b></font><br><br>" + message);
+		
+	}
 }
