3cd905cfce92738c57bad51562f4a39060ac3664
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-11 07:55:07 -0500

    broke out code to include file 260_EmailExcelEnergyOnInspectionResult

--- EVENT/IRSA;Building!Permit!~!~.js
+++ EVENT/IRSA;Building!Permit!~!~.js
@@ -7,38 +7,6 @@ if (matchARecordType([
     "Building/Permit/No Plans/NA"
 ], appTypeString)) {
     if (ifTracer(inspType == "Electrical Rough" || inspType  == "Mechanical Rough", "inspType == Electrical Rough or Mechanical Rough")) {
-        var guideSheetObjects = getGuideSheetObjects(inspId);
-            guideSheetObject,
-            sendEmail = false,
-            tempMeterReleaseComment='',
-            finalMeterReleaseComment=''
-            emailTemplate = 'BLD EXCEL ENERGY # 260',
-            emailParams = aa.util.newHashtable();
-
-        if (ifTracer(guideSheetObjects &&  guideSheetObjects.length > 0, "GuideSheet(s) Exists")) {
-            for (idx in guideSheetObjects) {
-                guideSheetObject = guideSheetObjects[idx];
-                if(ifTracer(guideSheetObject.gsType == 'Electrical Meter Release' || guideSheetObject.gsType == 'Mechanical Meter Release', "Guidesheet Type = Electrical Meter Release or Mechanical Meter Release")) {
-                    if(ifTracer(guideSheetObject.text == 'Temporary Meter Release', "guideSheetObject.text == 'Temporary Meter Release'")) {
-                        if(ifTracer(guideSheetObject.status == 'Yes', "guideSheetObject.text == 'Yes'")) {
-                            sendEmail = true;
-                         }
-                         tempMeterReleaseComment = guideSheetObject.comment;
-                    }
-                    if(ifTracer(guideSheetObject.text == 'Final Meter Release', "guideSheetObject.text == 'Temporary Meter Release'")) {
-                        if(ifTracer(guideSheetObject.status == 'Yes', "guideSheetObject.text == 'Yes'")) {
-                            sendEmail = true;
-                         }
-                         finalMeterReleaseComment = guideSheetObject.comment;
-                    }
-                    printObjProps(guideSheetObject);
-                }
-            }
-        }
-        if (ifTracer(sendEmail, "sendEmail is truthy")) {
-            emailParams.put("$$checkListItem1$$",tempMeterReleaseComment);
-            emailParams.put("$$checkListItem2$$",finalMeterReleaseComment);
-            emailAsync("", emailTemplate, emailParams);
-        }
+        include("260_EmailExcelEnergyOnInspectionResult");
     }
 }
