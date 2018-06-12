de187b75c0881f0f14231e2299379c0d4983168d
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-12 11:08:53 -0500

    changed script 376

---
+++ BATCH/BATCH_SCRIPT376_SWMP_RENEWAL_NOTICE.js
@@ -0,0 +1,55 @@
+/* 
+* SCRIPT 376
+*/
+var batchJobName = "" + aa.env.getValue("BatchJobName");	// Name of the batch job
+
+var SCRIPT_VERSION = 3.0;
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
+eval(getScriptText("INCLUDES_CUSTOM", null, true));
+
+function getScriptText(e) {
+	var t = aa.getServiceProviderCode();
+	if (arguments.length > 1)
+		t = arguments[1];
+	e = e.toUpperCase();
+	var n = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		var r = n.getScriptByPK(t, e, "ADMIN");
+		return r.getScriptText() + ""
+	} catch (i) {
+		return ""
+	}
+}
+
+
+notifyOverdueIssuedPermits("Water", "Water", "SWMP", "Permit");
+
+
+function notifyOverdueIssuedPermits(grp, typ, stype, cat){
+    var idx,
+        expDate,
+        expSince,
+        capScript,
+        maxExpDaysAllowed = 29,
+        capScriptList = aa.cap.getByAppType(grp, typ, stype, cat).getOutput();
+    
+    for(idx in capScriptList) {
+        capScript = capScriptList[idx];
+        if(ifTracer(capScript.getCapStatus() == "Issued", "Record status = Issued" )) {
+            capScript.capModel.expDate = new Date(2018,4,3);
+            aa.print("Exp Date: " + capScript.capModel.getExpDate());
+            expDate = capScript.capModel.getExpDate();
+            if(ifTracer(expDate, "Record expDate is truthy")) {
+                expSince = dateDiff(expDate, new Date());
+                aa.print(expSince);
+                if (expSince > maxExpDaysAllowed) {
+                    capId=capScript.getCapID();
+                    aa.print("sending email, cap id = " + capId.getCustomID());
+                    emailContacts("Applicant", "WAT RENEWAL OF SWMP PERMIT # 376");
+                    updateAppStatus("About to Expire","Updated via Batch Job : " + batchJobName, capScript.getCapID());
+                }
+            }
+        }
+    }
+}
--- EVENT/ASA;Water!Water!SWMP!~.js
+++ EVENT/ASA;Water!Water!SWMP!~.js
@@ -1,8 +1,8 @@
 /*
-* Script 424
+* Script 376
 */
 if (matchARecordType([
-    "Water/Water/SWMP/Permit",
+  //  "Water/Water/SWMP/Permit",
     "Water/Water/SWMP/Renewal"
 ], appTypeString)) {
     (function() {
@@ -12,11 +12,11 @@ if (matchARecordType([
             feePeriod = "FINAL" ;
 
         if (ifTracer(nmbrOfDisturbedAcreas  > 5, "nmbrOfDisturbedAcreas > 5")) {
-            addFee(feeItem, feeSched, feePeriod, 2250, "Y");
+            addFee(feeItem, feeSched, feePeriod, 750, "Y");
         } else if(ifTracer(nmbrOfDisturbedAcreas  > 1, "nmbrOfDisturbedAcreas > 1")) {
-            addFee(feeItem, feeSched, feePeriod, 1200, "Y");
+            addFee(feeItem, feeSched, feePeriod, 400, "Y");
         } else if(ifTracer(nmbrOfDisturbedAcreas  > 0, "nmbrOfDisturbedAcreas > 0")) {
-            addFee(feeItem, feeSched, feePeriod, 450, "Y");
+            addFee(feeItem, feeSched, feePeriod, 150, "Y");
         }
     })(); 
 }
