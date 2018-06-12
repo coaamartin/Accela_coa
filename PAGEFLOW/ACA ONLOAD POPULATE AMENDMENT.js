1962344791a8e4ae4988ce0c79e52b87da3acfe0
grayquarter <john@grayquarter.com>
2018-06-12 12:09:35 -0700

    36

--- PAGEFLOW/ACA ONLOAD POPULATE AMENDMENT.js
+++ PAGEFLOW/ACA ONLOAD POPULATE AMENDMENT.js
@@ -86,6 +86,26 @@ try {
 	logDebug("Error " + err.message + " at " + err.lineNumber + "Stack: " + err.stack);
 
 }
+
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+} else {
+	if (cancel) {
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	} else {
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	}
+}
+
 ////////////////////////////////////// Functions below this point ////////////////////////////////////////////////////////////////
 function copyAppSpecific4ACA(capFrom) { // copy all App Specific info into new Cap
 	var i = capFrom.getAppSpecificInfoGroups().iterator();
