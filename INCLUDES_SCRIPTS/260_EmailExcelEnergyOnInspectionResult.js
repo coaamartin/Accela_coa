742d45782a709f2d3456a947dba76bf9587a170f
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-13 08:16:33 -0500

    changed 260_EmailExcelEnergyOnInspectionResult()

--- INCLUDES_SCRIPTS/260_EmailExcelEnergyOnInspectionResult.js
+++ INCLUDES_SCRIPTS/260_EmailExcelEnergyOnInspectionResult.js
@@ -3,35 +3,52 @@ script260_EmailExcelEnergyOnInspectionResult();
 function script260_EmailExcelEnergyOnInspectionResult() {
     var guideSheetObjects = getGuideSheetObjects(inspId);
         guideSheetObject,
+        gasMeterSelected = false,
+        electricalMeterSelected = false,
         sendEmail = false,
-        tempMeterReleaseComment='',
-        finalMeterReleaseComment=''
         emailTemplate = 'BLD EXCEL ENERGY # 260',
         emailParams = aa.util.newHashtable();
 
     if (ifTracer(guideSheetObjects &&  guideSheetObjects.length > 0, "GuideSheet(s) Exists")) {
         for (idx in guideSheetObjects) {
             guideSheetObject = guideSheetObjects[idx];
-            if(ifTracer(guideSheetObject.gsType == 'Electrical Meter Release' || guideSheetObject.gsType == 'Mechanical Meter Release', "Guidesheet Type = Electrical Meter Release or Mechanical Meter Release")) {
+            if(ifTracer(guideSheetObject.gsType == 'Electrical Meter Release' || guideSheetObject.gsType == 'Gas Meter Release', "Guidesheet Type = Electrical Meter Release or Gas Meter Release")) {
                 if(ifTracer(guideSheetObject.text == 'Temporary Meter Release', "guideSheetObject.text == 'Temporary Meter Release'")) {
                     if(ifTracer(guideSheetObject.status == 'Yes', "guideSheetObject.text == 'Yes'")) {
                         sendEmail = true;
+                        setChecklistItemFlag()
                     }
-                    tempMeterReleaseComment = guideSheetObject.comment;
                 }
                 if(ifTracer(guideSheetObject.text == 'Final Meter Release', "guideSheetObject.text == 'Temporary Meter Release'")) {
                     if(ifTracer(guideSheetObject.status == 'Yes', "guideSheetObject.text == 'Yes'")) {
                         sendEmail = true;
+                        setChecklistItemFlag();
                     }
-                    finalMeterReleaseComment = guideSheetObject.comment;
                 }
-                printObjProps(guideSheetObject);
             }
         }
     }
     if (ifTracer(sendEmail, "sendEmail is truthy")) {
-        emailParams.put("$$checkListItem1$$",tempMeterReleaseComment);
-        emailParams.put("$$checkListItem2$$",finalMeterReleaseComment);
+        setChecklistItemText();
         emailAsync("Inspection-co@xcelenergy.com", emailTemplate, emailParams);
     }
+
+    function setChecklistItemFlag() {
+        if(ifTracer(guideSheetObject.gsType == 'Electrical Meter Release', "Guidesheet Type = Electrical Meter Release")) {
+            electricalMeterSelected = true;
+        } else if(ifTracer(guideSheetObject.gsType == 'Gas Meter Release', "Guidesheet Type = Gas Meter Release")) {
+            gasMeterSelected = true;
+        }
+    }
+
+    function setChecklistItemText() {
+        if(ifTracer(electricalMeterSelected && gasMeterSelected, "Both electricalMeterSelected && gasMeterSelected flags are true")) {
+            emailParams.put("$$checkListItem1$$","Gas and Electrical Meter");
+        } else if(ifTracer(electricalMeterSelected, "electricalMeterSelected is true")) {
+            emailParams.put("$$checkListItem1$$","Electrical Meter");
+        } else if(ifTracer(gasMeterSelected, "gasMeterSelected is true")) {
+            emailParams.put("$$checkListItem1$$","Gas Meter");
+        }
+    }
+
 }
