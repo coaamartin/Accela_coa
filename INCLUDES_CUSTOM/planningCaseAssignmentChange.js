4581394f812636e50f8ea9601f25f6540380668d
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-14 08:38:36 -0500

    working on script 255

--- INCLUDES_CUSTOM/planningCaseAssignmentChange.js
+++ INCLUDES_CUSTOM/planningCaseAssignmentChange.js
@@ -2,16 +2,16 @@
  * main function
  * 
  */
+         
 function planningCaseAssignmentChange(){
-	// try {
-	 	logDegug("a");
-	// 	   var assignedStaff=getAssignedStaff(capId);
-	// 	logDegug("assignedStaff:" + assignedStaff);
-   	// 	if (typeof(assignedStaff)!="undefined" && assignedStaff!=null && assignedStaff!=""){
-   	// 		assignWfTask(assignedStaff,capId);
-	// 	}
+	try {
+		   var assignedStaff=getAssignedStaff(capId);
+		logDegug("assignedStaff:" + assignedStaff);
+   		if (typeof(assignedStaff)!="undefined" && assignedStaff!=null && assignedStaff!=""){
+   			assignWfTask(assignedStaff,capId);
+		}
 		
-	// }catch(err){
-	// 	aa.debug("planningCaseAssignmentChange " , err);
-	// }
+	}catch(err){
+		aa.debug("planningCaseAssignmentChange " , err);
+	}
 }
