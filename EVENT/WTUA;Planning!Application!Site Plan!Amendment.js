923ceaec31b3cc85ae64c6539d0d04bc9f18ab17
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-06-14 17:14:51 -0600

    Update WTUA;Planning!Application!Site Plan!Amendment.js
    commented out updateReviewCommentsDueDate - not working.

--- EVENT/WTUA;Planning!Application!Site Plan!Amendment.js
+++ EVENT/WTUA;Planning!Application!Site Plan!Amendment.js
@@ -32,6 +32,8 @@ Sample Call:
 		"Applicant 2nd Submission Date", "Applicant 3rd Submission Date", "MESSAGE_NOTICE_PUBLIC WORKS", recordURL);
 
 */
+/****************** Per Don Bates we are commenting out this code - if you need it, please correct it ******/
+/*  ************* start of code commented out!
 var workFlowTask = "Review Distribution";
 var workFlowStatus = "In Review";
 var firstReviewDateASI = "1st Review Comments Due Date";
@@ -48,7 +50,7 @@ var recordURL = getACARecordURL(acaURLDefault);
 
 updateReviewCommentsDueDate(workFlowTask, workFlowStatus, firstReviewDateASI, secondReviewDateASI, thirdReviewDateASI, meetingType, planningCommissionDateASI,
 		applicant2ndSubmissionDateASI, applicant3rdSubmissionDateASI, emailTemplate, recordURL);
-
+******************** end commented out code!  */
 /*
 Script 273
 Record Types:		Planning/Application/Site Plan/Amendment
