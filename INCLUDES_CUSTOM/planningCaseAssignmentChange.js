e7aba646a83a5638a2ad887ccf12b838f55224b6
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-14 07:50:43 -0500

    chenges to script 255

--- INCLUDES_CUSTOM/planningCaseAssignmentChange.js
+++ INCLUDES_CUSTOM/planningCaseAssignmentChange.js
@@ -4,7 +4,9 @@
  */
 function planningCaseAssignmentChange(){
 	try {
-   		var assignedStaff=getAssignedStaff(capId);
+		logDegug("a");
+		   var assignedStaff=getAssignedStaff(capId);
+		logDegug("assignedStaff:" + assignedStaff);
    		if (typeof(assignedStaff)!="undefined" && assignedStaff!=null && assignedStaff!=""){
    			assignWfTask(assignedStaff,capId);
 		}
