a2e94b79a913b7d84844c5e532a0b85efb37a062
jantled <jal@wbai.com>
2018-06-15 15:57:49 -0500

    add new conf for version control

---
+++ TEMPLATES/CONFIGURABLES/CONF_BUILDING_INSPECTION_AUTOMATION.js
@@ -0,0 +1,461 @@
+{
+  "Building/Enforcement/Notice of Violation/NA": {
+    "InspectionResultSubmitAfter": [
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 328",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Pre court investigation"
+          ],
+          "inspectionResult": [
+            "Compliance"
+          ]
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "Closed",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType": "",
+          "inspectionCopyComment": true,
+          "rangeType": "",
+          "range": 0,
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Pre Hearing Inspection",
+          "taskStatus": "Compliance",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 328",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Pre court investigation"
+          ],
+          "inspectionResult": ["Non compliance"
+          ]
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType": "",
+          "inspectionCopyComment": true,
+          "rangeType": "",
+          "range": 0,
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Pre Hearing Inspection",
+          "taskStatus": "Non Compliance",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 331a",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": ["Investigation"],
+          "inspectionResult": ["Reassign to another Division"]
+        },
+        "action": {
+          "newAppStatus": "Closed",
+          "caseCopyComments": false,
+          "rangeType": "",
+          "range": 0,
+          "sameInspector": true,
+          "taskName": "Investigation",
+          "taskStatus": "Reassign to another Division"
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 331b",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": ["Investigation"],
+          "inspectionResult": ["No Violation Observed"]
+        },
+        "action": {
+          "newAppStatus": "Closed",
+          "caseCopyComments": false,
+          "rangeType": "",
+          "range": 0,
+          "sameInspector": true,
+          "taskName": "Investigation",
+          "taskStatus": "No Violation Observed"
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - 331d",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Investigation"
+          ],
+          "inspectionResult": [
+            "Notice of Violation"
+          ]
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": true,
+          "inspectionType": "Notice of Violation Inspection",
+          "inspectionCopyComment": true,
+          "rangeType": "Days",
+          "range": 7,
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Investigation",
+          "taskStatus": "Notice of Violation",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 331c",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Investigation"
+          ],
+          "inspectionResult": [
+            "In Progress"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType": "Investigation",
+          "inspectionCopyComment": true,
+          "rangeType": "Days",
+          "range": "7",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Investigation",
+          "taskStatus": "In Progress",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 332a",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Notice of Violation Inspection"
+          ],
+          "inspectionResult": [
+                "First Notice"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": true,
+          "inspectionType":"Notice of Violation Inspection",
+          "inspectionCopyComment": true,
+          "rangeType": "Days",
+          "range": "7",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Notice of Violation",
+          "taskStatus": "First Notice",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 332b",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Notice of Violation Inspection"
+          ],
+          "inspectionResult": [
+                "Second Notice"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType":"Notice of Violation Inspection",
+          "inspectionCopyComment": true,
+          "rangeType": "Days",
+          "range": "7",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Notice of Violation",
+          "taskStatus": "Second Notice",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 332c",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Notice of Violation Inspection"
+          ],
+          "inspectionResult": [
+                "Third Notice"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType":"Notice of Violation Inspection",
+          "inspectionCopyComment": true,
+          "rangeType": "Days",
+          "range": "7",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Notice of Violation",
+          "taskStatus": "Third Notice",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 332d",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Notice of Violation Inspection"
+          ],
+          "inspectionResult": [
+                "Issue Summons"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType":"",
+          "inspectionCopyComment": true,
+          "rangeType": "",
+          "range": "",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Notice of Violation",
+          "taskStatus": "Issue Summons",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules - Script 332e",
+          "operators": {}
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "Notice of Violation Inspection"
+          ],
+          "inspectionResult": [
+                "Compliance"
+          ],
+          "isCreatedByACA": false,
+          "isACAEvent": false
+
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "newAppStatus": "Closed",
+          "caseCreationType": "",
+          "caseFailureStatus": [],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType":"",
+          "inspectionCopyComment": true,
+          "rangeType": "",
+          "range": "",
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "Notice of Violation",
+          "taskStatus": "Compliance",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      }
+      ]
+  }
+}
