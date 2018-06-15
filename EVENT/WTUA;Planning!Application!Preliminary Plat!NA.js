5fccc83804321eaa54538c43da7ec13184847291
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-06-14 19:37:25 -0600

    Add files via upload

--- EVENT/WTUA;Planning!Application!Preliminary Plat!NA.js
+++ EVENT/WTUA;Planning!Application!Preliminary Plat!NA.js
@@ -29,3 +29,148 @@ if($iTrc(wfTask == "Generate Hearing Results" && wfStatus == "Technical Submitta
 
 setEAgendaDueDate("Generate Hearing Results", [ "Review Complete" ], "Complete E-Agenda", "City Council");
 
+/*
+Script 274
+Record Types:	Planning/Application/Preliminary Plat/NA
+				Planning/Application/Site Plan/Major
+Desc:			Spec:  (from spec: 274) and tracker comments
+Created By: Silver Lining Solutions
+*/
+
+logDebug("script274_WTUA_CalcReviewDueDatesAndPotentialPCHearingSchedule2 START.");
+var appSpecRevComments = "";
+var appSpecValRevComments = "";
+var appSpecProjCommHearingDate = "";
+var appSpecSubmissionDate = "";
+var appSpecValSubmissionDate = "";
+
+if (wfTask == "Review Distribution" && wfStatus == "In Review") {
+	logDebug("*******TASK AND STATUS MATCH*****");
+	if ( !(AInfo["1st Review Comments Due Date"]) ) {
+		// Set up the 'target' date we want to search for meetings
+		var dToday = new Date();
+		var lookForPlanningMtgDate	= aa.date.parseDate(XdateAddHC(dToday,(7*12)));
+		var lookForMMDDYYYY = ("0" + lookForPlanningMtgDate.getMonth()).slice(-2) + "/" 
+								+ ("0" + lookForPlanningMtgDate.getDayOfMonth()).slice(-2) + "/" 
+								+ lookForPlanningMtgDate.getYear();
+
+		//Set up the 'look back' from the target date for searching
+		var lookForStartDate		= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY,0));
+		
+		//Set up the 'look forward' from the target date for searching
+		var lookForEndDate			= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY,+45));
+		
+		//Find the closest meeting to lookForPlanningMtgDate between lookForStartDate and lookForEndDate 
+		var newPlnMtg = getClosestAvailableMeeting("Planning Commission", lookForPlanningMtgDate, lookForStartDate, lookForEndDate, "PLANNING COMMISSION");
+
+		// update review comments
+		var revdDate = aa.date.parseDate(XdateAddHC("",15, true));
+		var revdDateStr = ("0" + revdDate.getMonth()).slice(-2) + "/" 
+							+ ("0" + revdDate.getDayOfMonth()).slice(-2) + "/" 
+							+ revdDate.getYear();
+		editAppSpecific("1st Review Comments Due date",revdDateStr);
+		
+		// update planning commission date if found
+		if (newPlnMtg != null) {
+			var newHearingDate = (""+ newPlnMtg.startDate).slice(5,7)+"/" 
+								+(""+ newPlnMtg.startDate).slice(8,10)+"/"
+								+(""+ newPlnMtg.startDate).slice(0,4);
+			editAppSpecific("Projected Planning Commission Date",newHearingDate);
+		} else {
+			logDebug("Script 274: WARNING - there is no planning commission date within 45 days of your target date!");
+			comment("<B><Font Color=RED>WARNING - there is no planning commission date within 45 days of your target date!</Font></B>");
+		}
+	}
+	else if ( !(AInfo["2nd Review Comments Due Date"]) ) {
+		// Set up the 'target' date we want to search for meetings
+		logDebug("1st Review Comments is Populated--Looking at 2nd Review Comments");
+		var dToday = new Date();
+		var lookForPlanningMtgDate	= aa.date.parseDate(XdateAddHC(dToday,(7*8)));
+		var lookForMMDDYYYY = ("0" + lookForPlanningMtgDate.getMonth()).slice(-2) + "/" 
+								+ ("0" + lookForPlanningMtgDate.getDayOfMonth()).slice(-2) + "/" 
+								+ lookForPlanningMtgDate.getYear();
+		
+		//Set up the 'look back' from the target date for searching
+		var lookForStartDate		= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY,0));
+		
+		//Set up the 'look forward' from the target date for searching
+		var lookForEndDate			= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY,+45));
+		
+		//Find the closest meeting to lookForPlanningMtgDate between lookForStartDate and lookForEndDate 
+		var newPlnMtg = getClosestAvailableMeeting("Planning Commission", lookForPlanningMtgDate, lookForStartDate, lookForEndDate, "PLANNING COMMISSION");
+
+		// update review comments
+		var revdDate = aa.date.parseDate(XdateAddHC("",20, true));
+		var revdDateStr = ("0" + revdDate.getMonth()).slice(-2) + "/" 
+							+ ("0" + revdDate.getDayOfMonth()).slice(-2) + "/" 
+							+ revdDate.getYear();
+		editAppSpecific("2nd Review Comments Due date",revdDateStr);
+		logDebug("*******2nd Review Date = " +revdDateStr);
+
+/*		// update submission date
+		var subdDate = aa.date.parseDate(XdateAddHC("",20, true));
+		var subdDateStr = ("0" + subdDate.getMonth()).slice(-2) + "/" 
+							+ ("0" + subdDate.getDayOfMonth()).slice(-2) + "/" 
+							+ subdDate.getYear();
+		editAppSpecific("Applicant 2nd Submission Date",subdDateStr); 
+*/
+		// update planning commission date if found
+		if (newPlnMtg != null) {
+			var newHearingDate = (""+ newPlnMtg.startDate).slice(5,7)+"/" 
+								+(""+ newPlnMtg.startDate).slice(8,10)+"/"
+								+(""+ newPlnMtg.startDate).slice(0,4);
+			editAppSpecific("Projected Planning Commission Date",newHearingDate);
+		} else {
+			logDebug("Script 274: WARNING - there is no planning commission date within 45 days of your target date!");
+			comment("<B><Font Color=RED>WARNING - there is no planning commission date within 45 days of your target date!</Font></B>");
+		}
+		
+	} else {
+		// Set up the 'target' date we want to search for meetings
+		var dToday = new Date();
+		var lookForPlanningMtgDate	= aa.date.parseDate(XdateAddHC(dToday,(7*4)));
+		var lookForMMDDYYYY = ("0" + lookForPlanningMtgDate.getMonth()).slice(-2) + "/" 
+								+ ("0" + lookForPlanningMtgDate.getDayOfMonth()).slice(-2) + "/" 
+								+ lookForPlanningMtgDate.getYear();
+				
+		//Set up the 'look back' from the target date for searching
+		var lookForStartDate		= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY,0));
+		
+		//Set up the 'look forward' from the target date for searching
+		var lookForEndDate			= aa.date.parseDate(aa.date.addDate(lookForMMDDYYYY, +45));
+		
+		//Find the closest meeting to lookForPlanningMtgDate between lookForStartDate and lookForEndDate 
+		var newPlnMtg = getClosestAvailableMeeting("Planning Commission", lookForPlanningMtgDate, lookForStartDate, lookForEndDate, "PLANNING COMMISSION");
+
+		if (!(AInfo["3rd Review Comments Due Date"])) {
+			// update review comments
+			var revdDate = aa.date.parseDate(XdateAddHC("",10, true));
+			var revdDateStr = ("0" + revdDate.getMonth()).slice(-2) + "/" 
+								+ ("0" + revdDate.getDayOfMonth()).slice(-2) + "/" 
+								+ revdDate.getYear();
+			editAppSpecific("3rd Review Comments Due date",revdDateStr);
+		
+			// update submission date
+			var subdDate = aa.date.parseDate(XdateAddHC("",15, true));
+			var subdDateStr = ("0" + subdDate.getMonth()).slice(-2) + "/" 
+								+ ("0" + subdDate.getDayOfMonth()).slice(-2) + "/" 
+								+ subdDate.getYear();
+			editAppSpecific("Applicant 3rd Submission Date",subdDateStr); 
+		}
+
+		// update planning commission date if found
+		if (newPlnMtg != null) {
+			var newHearingDate = (""+ newPlnMtg.startDate).slice(5,7)+"/" 
+								+(""+ newPlnMtg.startDate).slice(8,10)+"/"
+								+(""+ newPlnMtg.startDate).slice(0,4);
+			editAppSpecific("Projected Planning Commission Date",newHearingDate);
+		} else {
+			logDebug("Script 274: WARNING - there is no planning commission date within 45 days of your target date!");
+			comment("<B><Font Color=RED>WARNING - there is no planning commission date within 45 days of your target date!</Font></B>");
+		}
+	}
+	
+// send an email to the applicant - we're waiting on the actual template here.
+	sendEmail("TEST_FOR_SCRIPTS");
+}
+logDebug("script274_WTUA_CalcReviewDueDatesAndPotentialPCHearingSchedule2 end.");
