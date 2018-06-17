9b015c568ec62bf349de8536642cdc82c0895cce
Suhail Wakil <suhailwakil@gmail.com>
2018-06-16 17:10:25 -0700

    18

--- PAGEFLOW/ACA_18_BEFORE_SWMP_BOND.js
+++ PAGEFLOW/ACA_18_BEFORE_SWMP_BOND.js
@@ -1,6 +1,6 @@
 var cap = aa.env.getValue("CapModel");
 var useAppSpecificGroupName = false;	
-var message = " 1-- ";							
+var message = " -- ";							
 var debug = "";								
 var br = "<BR>";							
 var cap = aa.env.getValue("CapModel");
@@ -14,14 +14,22 @@ var ASIValue = AInfo["PAYING WITH BOND"];
 
 if(ASIValue.equals("Yes"))
 {
-	if(appTypeResult == "Water/Water/SWMP/Application")
+	loadACAASITables();
+	//if (typeof(BONDINFORMATION) == "object") {
+		
+		message = message +'---'+BONDINFORMATION.length
+	//}
+
+	logMessage("**ERROR ");
+
+	/*if(appTypeResult == "Water/Water/SWMP/Application")
   	{
   		loadASITable("BOND INFORMATION");
 		//if(TotalASITRows("BOND INFORMATION",capId) == "0") 
 		//{
 			logMessage("**ERROR ");
 		//}
-	}
+	}*/
 }
 
 if (message.indexOf("**ERROR") > 0)
@@ -30,70 +38,58 @@ if (message.indexOf("**ERROR") > 0)
 	aa.env.setValue("ErrorMessage", message);
 }
 
-function loadASITable(tname) {
-
- 	//
- 	// Returns a single ASI Table array of arrays
-	// Optional parameter, cap ID to load from
-	//
-
-	var itemCap = capId;
-	message = message + " ---- " +itemCap+ " ---- " ;
-	if (arguments.length == 2) itemCap = arguments[1]; // use cap ID specified in args
-
-	var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput();
-	var ta = gm.getTablesArray()
-	var tai = ta.iterator();
-
-	while (tai.hasNext())
-	  {
-	  var tsm = tai.next();
-	  var tn = tsm.getTableName();
-	  message = message + " ---- " +tn+ " ---- " ;
-      if (!tn.equals(tname)) continue;
-
-	  if (tsm.rowIndex.isEmpty())
-	  	{
-	  		message = message + " ---- " +"EMPTY"+ " ---- " ;
-			logDebug("Couldn't load ASI Table " + tname + " it is empty");
-			return false;
-		}
-
-   	  var tempObject = new Array();
-	  var tempArray = new Array();
-
-  	  var tsmfldi = tsm.getTableField().iterator();
-	  var tsmcoli = tsm.getColumns().iterator();
-      var readOnlyi = tsm.getAppSpecificTableModel().getReadonlyField().iterator(); // get Readonly filed
-	  var numrows = 1;
-
-	  while (tsmfldi.hasNext())  // cycle through fields
-		{
-		if (!tsmcoli.hasNext())  // cycle through columns
-			{
-			var tsmcoli = tsm.getColumns().iterator();
-			tempArray.push(tempObject);  // end of record
-			var tempObject = new Array();  // clear the temp obj
-			numrows++;
-			}
-		var tcol = tsmcoli.next();
-		var tval = tsmfldi.next();
-		var readOnly = 'N';
-		if (readOnlyi.hasNext()) {
-			readOnly = readOnlyi.next();
-		}
-		var fieldInfo = new asiTableValObj(tcol.getColumnName(), tval, readOnly);
-		tempObject[tcol.getColumnName()] = fieldInfo;
-
-		}
-		tempArray.push(tempObject);  // end of record
-	  }
-	  return tempArray;
+function loadACAASITables() {
+	//logDebugWithThreadID("Loading ASIT for Venue NOC:"+count);
+	var gm = cap.getAppSpecificTableGroupModel();
+	if(gm == null)
+	{
+		return;
 	}
+	var ta = gm.getTablesMap();
+	if(ta == null)
+	{
+		return;
+	}
+	var tai = ta.values().iterator();
+	while (tai.hasNext()) {
+		var tsm = tai.next();
 
+		if (tsm.rowIndex.isEmpty())
+			continue; // empty table
 
-function loadAppSpecific4ACA(thisArr) 
-{
+		var tempObject = new Array();
+		var tempArray = new Array();
+		var tn = tsm.getTableName();
+
+		tn = String(tn).replace(/[^a-zA-Z0-9]+/g, '');
+
+		if (!isNaN(tn.substring(0, 1)))
+			tn = "TBL" + tn // prepend with TBL if it starts with a number
+
+				var tsmfldi = tsm.getTableField().iterator();
+		var tsmcoli = tsm.getColumns().iterator();
+		var numrows = 1;
+		while (tsmfldi.hasNext()) // cycle through fields
+		{
+			if (!tsmcoli.hasNext()) // cycle through columns
+			{
+
+				var tsmcoli = tsm.getColumns().iterator();
+				tempArray.push(tempObject); // end of record
+				var tempObject = new Array(); // clear the temp obj
+				numrows++;
+			}
+			var tcol = tsmcoli.next();
+			var tval = tsmfldi.next();
+			tempObject[tcol.getColumnName()] = tval;
+		}
+		tempArray.push(tempObject); // end of record
+		var copyStr = "" + tn + " = tempArray";
+		eval(copyStr); // move to table name
+	}
+}
+
+function loadAppSpecific4ACA(thisArr){
 	//
 	// Returns an associative array of App Specific Info
 	// Optional second parameter, cap ID to load from
