e2da822299f968e59986c29081cd63ab92f51b7b
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-18 10:50:30 -0500

    changed 22

--- INCLUDES_SCRIPTS/22_activateCompletenessCheck.js
+++ INCLUDES_SCRIPTS/22_activateCompletenessCheck.js
@@ -1,16 +1,39 @@
 (function() {
-    var idx,
+    var idxDocs,
         docListArray,
         doc,
-        docCat;
+        docCat,
+        asiGroups,
+        idxAsiGrps;
 
 
-logDebug("one");
-    docListArray = getDocumentList();
-    for (var idx in docListArray) {
-        doc = docListArray[idx];
-        docCat = doc.docCategory;
-        logDebug(docCat);
-        printObjProperties(doc);
+//     docListArray = getDocumentList();
+//     for (idxDocs in docListArray) {
+//         doc = docListArray[idxDocs];
+//         docCat = doc.docCategory;
+//         logDebug(docCat);
+//  //       printObjProps(doc);
+//     }
+
+//printObjProps(capId);
+// printObjProps(cap);
+// printObjProps(cap.capModel);
+
+//var capModel =  aa.cap.getCap(capId).getOutput().getCapModel();
+
+    asiGroups = cap.capModel.getAppSpecificInfoGroups()
+    for (idxAsiGrps in asiGroups) {
+       // doc = docListArray[idxAsiGrps];
+      //  docCat = doc.docCategory;
+    //    logDebug(docListArray[idxAsiGrps]);
+        printObjProps(docListArray[idxAsiGrps]);
+    }
+
+    var idxAsiGrps= cap.capModel.getAppSpecificInfoGroups().iterator();
+    while (i.hasNext())
+    {
+        var group = i.next();
+        printObjProps(group);
+
     }
 })(); 
