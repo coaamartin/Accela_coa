b2dd13ac7c9180e230368b0849f99a919fe2d72b
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-19 14:21:59 -0500

    updated 250

--- INCLUDES_SCRIPTS/250_updateAssignedUserForTrafficEngRequest.js
+++ INCLUDES_SCRIPTS/250_updateAssignedUserForTrafficEngRequest.js
@@ -1,24 +1,15 @@
 script250_updateAssignedUserForTrafficEngRequest();
 
 function script250_updateAssignedUserForTrafficEngRequest(){
-var assignToUserName = AInfo['Updated.Assigned To']
-
 	useTaskSpecificGroupName=false;
-	var tsiArray = new Array(); 
-		
-	loadTaskSpecific(tsiArray);
-	for (i in tsiArray){
-		if (ifTracer(i=="Assigned To", "i == Assigned To")) {
-			var assignedTo=tsiArray[i];
-			if (ifTracer(assignedTo!=null && assignedTo!="", "assignedTo is valid")) {
-				logDebug(assignedTo);
-				var userName=assignedTo.split(" ");
-				var userObj = aa.person.getUser(userName[0],null,userName[1]).getOutput();
-		//		printObjProps(userObj);
-				assignTask("Traffic Investigation",userObj.getUserID());
-				updateTaskDepartment("Traffic Investigation",userObj.getDeptOfUser());		
-			}	
-		}
+
+	var assignedTo = getTaskSpecific(wfTask, "Assigned To");
+	logDebug(assignedTo);
+	if(assignedTo != false) {
+		var userName=assignedTo.split(" ");
+		var userObj = aa.person.getUser(userName[0],null,userName[1]).getOutput();
+		assignTask("Traffic Investigation",userObj.getUserID());
+		updateTaskDepartment("Traffic Investigation",userObj.getDeptOfUser());		
 	}
 	 	
-}
+}	
