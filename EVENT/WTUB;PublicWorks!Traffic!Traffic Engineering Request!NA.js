17dc9f4b381162e9b4e3b6266f185ddcaa9cdae0
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-20 08:56:30 -0500

    removed invalid " from wtub

--- EVENT/WTUB;PublicWorks!Traffic!Traffic Engineering Request!NA.js
+++ EVENT/WTUB;PublicWorks!Traffic!Traffic Engineering Request!NA.js
@@ -1,6 +1,6 @@
 /* Title :  Work order fields must have a value (WorkflowTaskUpdateBefore)
 
-Purpose :   If workflow task = "Draft Workorder" and workflow status = "Drafted workorder" and if any of the following custom fields do
+Purpose :   If workflow task = 'Draft Workorder' and workflow status = 'Drafted workorder' and if any of the following custom fields do
 NOT have a value (Location, Description, and Priority) then prevent the workflow from moving forward and raise the error
 message “Content incomplete please populate workflow information to use this status”
 
@@ -9,7 +9,7 @@ Author :   Israa Ismail
 Functional Area : Records 
 
 Sample Call : validateWOFields()
-Notes : The name of the record is changed to “Traffic Engineering Request” (PublicWorks/Traffic/Traffic Engineering Request/NA). 
+Notes : The name of the record is changed to 'Traffic Engineering Request (PublicWorks/Traffic/Traffic Engineering Request/NA). 
 */
 
 validateWOFields();
--- INCLUDES_SCRIPTS/173_RequireFinalResponseSent.js
+++ INCLUDES_SCRIPTS/173_RequireFinalResponseSent.js
@@ -1,13 +1,14 @@
-script173_RequireFinalResponseSent()
+script173_RequireFinalResponseSent();
 
 function script173_RequireFinalResponseSent() {
     var errMsg = '',
         finalRespReq=getAppSpecific("Final Response Required", capId),
         tasks = aa.workflow.getHistory(capId).getOutput();
+        
     if (ifTracer(finalRespReq=="CHECKED", 'finalRespReq==CHECKED')) {
         for (var t in tasks) {
             var task = tasks[t];
-            if (ifTracer(task.getTaskDescription() == 'Final Response Required' ||  task.getTaskDescription() == 'Final Request Sent', 'one of the adhoc workflows are in history - must be done')) {
+            if (ifTracer(task.getTaskDescription() == 'Final Response Required' ||  task.getTaskDescription() == 'Final Request Sent', 'workflows description matches')) {
              //   if(task.disposition == 'Complete')
             //    printObjProps(task);  
                 return true;
