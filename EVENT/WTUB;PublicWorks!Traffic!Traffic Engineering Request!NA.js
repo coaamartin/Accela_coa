4656778253044db6a2219d36cf3ba5bff81c2301
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-20 09:07:45 -0500

    removed invalid " from wtub

--- EVENT/WTUB;PublicWorks!Traffic!Traffic Engineering Request!NA.js
+++ EVENT/WTUB;PublicWorks!Traffic!Traffic Engineering Request!NA.js
@@ -2,7 +2,7 @@
 
 Purpose :   If workflow task = 'Draft Workorder' and workflow status = 'Drafted workorder' and if any of the following custom fields do
 NOT have a value (Location, Description, and Priority) then prevent the workflow from moving forward and raise the error
-message “Content incomplete please populate workflow information to use this status”
+message 'Content incomplete please populate workflow information to use this status'
 
 Author :   Israa Ismail
 
