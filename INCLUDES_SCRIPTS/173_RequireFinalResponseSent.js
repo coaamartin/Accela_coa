aa73b83b8c59c8845f657f3f35ebcc94963c68e9
Charlie MUnzer <cvm@byrnesoftware.com>
2018-06-20 08:15:19 -0500

    Changed 173

--- INCLUDES_SCRIPTS/173_RequireFinalResponseSent.js
+++ INCLUDES_SCRIPTS/173_RequireFinalResponseSent.js
@@ -4,19 +4,18 @@ function script173_RequireFinalResponseSent() {
     var errMsg = '',
         finalRespReq=getAppSpecific("Final Response Required", capId),
         tasks = aa.workflow.getHistory(capId).getOutput();
-
-    if (finalRespReq=="CHECKED"){
+    if (ifTracer(finalRespReq=="CHECKED", 'finalRespReq==CHECKED')) {
         for (var t in tasks) {
             var task = tasks[t];
-            if (task.getTaskDescription() == 'Final Response Required' ||  task.getTaskDescription() == 'Final Request Sent') {
-                printObjProps(task);  
-           //     actionBy = task.getTaskItem().getSysUser();
-           //     aa.print(actionBy);
-            //    var userObj = aa.person.getUser(actionBy.firstName,null,actionBy.lastName).getOutput();
-           //     assignTask("Generate Work Order",userObj.getUserID());
+            if (ifTracer(task.getTaskDescription() == 'Final Response Required' ||  task.getTaskDescription() == 'Final Request Sent', 'one of the adhoc workflows are in history - must be done')) {
+             //   if(task.disposition == 'Complete')
+            //    printObjProps(task);  
+                return true;
             }
         }
     }
-
+	showMessage = true;
+	comment("Final Response Sent needs to be completed");
+	cancel = true;
 
 }
