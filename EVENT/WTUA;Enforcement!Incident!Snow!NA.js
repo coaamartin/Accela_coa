b47df819c119c7a27e7a5de5178ac2db75bb50d2
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-06-21 14:26:53 -0600

    Add files via upload

---
+++ EVENT/WTUA;Enforcement!Incident!Snow!NA.js
@@ -0,0 +1,19 @@
+//Script 73
+//Record Types:	​Enforcement/Incident/Snow/NA
+//				Enforcement/Incident/Snow/NA
+//				Enforcement/Incident/Snow/NA
+//				Forestry/Request/Citizen/NA
+
+//Event: 		TBD
+//Desc:			TBD
+//Created By: Silver Lining Solutions
+
+logDebug("START: Script 77");
+
+if ( wfTask == "Initial Investigation" && wfStatus == "Skip to City Abatement")
+{
+	logDebug("task/status criteria met");
+}
+
+
+logDebug("END: Script 77");
