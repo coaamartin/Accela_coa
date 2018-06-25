f5a98a906d31e29074db2fe4177f73fd613ce663
jantled <jal@wbai.com>
2018-06-25 17:09:39 -0500

    updated for script 332

--- EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
+++ EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
@@ -1,4 +1,6 @@
 var $iTrc = ifTracer;
 //Script 332
+//processNotOfViolInsp(inspectionType to check for, Inspection result to check for, createNewInspection, updateworflow, task2update, status2updateto);
 processNotOfViolInsp("Notice of Violation Inspection", "First Notice", true, true, "Notice of Violation", "First Notice");
-
+processNotOfViolInsp("Notice of Violation Inspection", "Second Notice", true, true, "Notice of Violation", "Second Notice");
+processNotOfViolInsp("Notice of Violation Inspection", "Third Notice", true, true, "Notice of Violation", "Third Notice");
