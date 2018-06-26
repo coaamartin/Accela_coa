861681f0148c298308da038442f4d7971369540b
Charles Munzer <cvm@byrnesoftware.com>
2018-06-26 15:40:30 -0500

    IUpdated 397

--- EVENT/WTUA;Water!Water!SWMP!Application.js
+++ EVENT/WTUA;Water!Water!SWMP!Application.js
@@ -36,14 +36,7 @@ if (ifTracer(wfTask=="Fee Processing" &&
 	);
 } 
 
-//script 397
-logDebug("Script 397 Starting");
-if(ifTracer(wfTask == "Closure" && wfStatus == "Closed")) {
-	include("397_createPpbmpRecordBasedOnCustomListData");
-}
-
 
 createPPBMPRecord("Re Certification", [ "Accepted" ], "POND TYPES");
 
 script399_BondEmailAndAwaitingBondTaskStatus();
-	
--- EVENT/WTUA;Water!Water!SWMP!Permit.js
+++ EVENT/WTUA;Water!Water!SWMP!Permit.js
@@ -1,1 +1,7 @@
 script398_ScheduleFinalInspection();
+
+//script 397
+logDebug("Script 397 Starting");
+if(ifTracer(wfTask == "Closure" && wfStatus == "Closed")) {
+	include("397_createPpbmpRecordBasedOnCustomListData");
+}
