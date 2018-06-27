d94e297b8b5c4cfdce644926a497abe071979fd0
darrenkoontz <32111889+darrenkoontz@users.noreply.github.com>
2018-06-27 15:17:42 -0600

    Add files via upload

---
+++ EVENT/WTUA;Enforcement!Incident!Abatement!NA.js
@@ -0,0 +1,22 @@
+//Script 229
+//Name: 		Issued Retail Product Manufacturer License
+//Record Types:	Enforcement/Incident/Abatement/NA
+//Event: 		EMSE
+//Desc:			Criteria; wfTask = License Issuance and status = Issued Action; Generate email to Responsible Party and cc: Applicant (Contacts) Subject: 
+//				“Congratulations your Retail Product Manufacturer License $$Record_ID$$ has been issued” and attached license as a pdf. 
+//				(Dusty to provide email template and report name for attachment). ). Include a deep link of the record within the email body. 
+//				Create child Licenses/Marijuana/ Retail Product Manufacturer/License Record and copy APO, Contacts, Values in Custom Fields. 
+//				auto schedule Quarterly Inspections Assign all to user Steve Clark on the child license 
+//				record; MJ AMED Inspection, MJ Building Inspection - Electrical, MJ Building Inspection - Life Safety, MJ Building Inspection - Mechanical, 
+//				MJ Building Inspection – Plumbing, MJ Building Inspection – Structural, MJ Security Inspection - 3rd Party , MJ Zoning Inspection , 
+//				schedule date 3 months minus a week.
+//
+//Created By: Silver Lining Solutions
+
+logDebug ("script94 START");
+
+
+if (wfTask == "License Issuance" && wfStatus== "Issued Action)
+{
+
+logDebug ("script94 END");
