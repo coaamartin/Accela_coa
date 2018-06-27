89b2886750cdb600e3a5bdd203eaf846c129ebba
Charles Munzer <cvm@byrnesoftware.com>
2018-06-27 09:54:49 -0500

    updated 397

--- EVENT/WTUA;Water!Water!SWMP!Permit.js
+++ EVENT/WTUA;Water!Water!SWMP!Permit.js
@@ -2,6 +2,6 @@ script398_ScheduleFinalInspection();
 
 //script 397
 logDebug("Script 397 Starting");
-if(ifTracer(wfTask == "Closure" && wfStatus == "Closed", "wfTask == Closure && wfStatus == Closed")) {
+if(ifTracer(wfTask == "Closure" && wfStatus == "Closed Pond", "wfTask == Closure && wfStatus == Closed Pond")) {
 	include("397_createPpbmpRecordBasedOnCustomListData");
 }
