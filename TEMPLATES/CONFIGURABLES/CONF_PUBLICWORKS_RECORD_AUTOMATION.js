b7a53b9319a45ec6def10e58a01b5525324c691c
jantled <jal@wbai.com>
2018-06-28 14:11:05 -0500

    updated conf file for tracking

--- TEMPLATES/CONFIGURABLES/CONF_PUBLICWORKS_RECORD_AUTOMATION.js
+++ TEMPLATES/CONFIGURABLES/CONF_PUBLICWORKS_RECORD_AUTOMATION.js
@@ -152,13 +152,13 @@
       }
     ]
   },
-  "PublicWorks/Real Property/License Agreement/NA": {
+ "PublicWorks/Real Property/License Agreement/NA": {
     "WorkflowTaskUpdateAfter": [
       {
         "preScript": "",
         "postScript": "",
         "metadata": {
-          "description": "To run automated script based on JSON rules",
+          "description": "Script 333",
           "operators": {}
         },
         "criteria": {
@@ -166,10 +166,11 @@
             "Completeness Check"
           ],
           "status": [
-            "Complete"
+            "Ready to Pay"
           ],
           "customFields": {
-            "Review Fee?": "Yes"
+            "Review Fee?": "Yes",
+			"License Type": "License Agreement"
           },
           "isCreatedByACA": "",
           "balanceAllowed": "",
@@ -219,7 +220,10 @@
           "status": [
             "Ready to Pay"
           ],
-          "customFields": {},
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Master License Agreement"
+		  },
           "isCreatedByACA": "",
           "balanceAllowed": "",
           "recordStatus": []
@@ -247,6 +251,581 @@
             {
               "feeSchedule": "PW_LIC_AGR",
               "feeCode": "PW_AGR_01",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Addendum"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_04",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Assignment of a License"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_04",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Pre-License Agreement"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_02",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Revocable License (Commercial)"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_03",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Revocable License (Residential)"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_03",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "333",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Completeness Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Fee?": "Yes",
+			"License Type": "Supplemental Site License"
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_LIC_AGR",
+              "feeCode": "PW_AGR_04",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      }
+    ]
+ },
+  "PublicWorks/Drainage/NA/NA": {
+    "WorkflowTaskUpdateAfter": [
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "123",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Quality Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Type": "Master Drainage Report"		  
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_CIVIL_03",
+              "feeCode": "PW_CIVIL_05",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "123",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Quality Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Type": "Preliminary Drainage Letter"		  
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_CIVIL_03",
+              "feeCode": "PW_CIVIL_04",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      },
+	  {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "123",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Quality Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {
+            "Review Type": "Preliminary Drainage Report"		  
+		  },
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_CIVIL_03",
+              "feeCode": "PW_CIVIL_03",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      }
+    ]
+  },
+  "PublicWorks/Civil Plan/Review/NA": {
+    "WorkflowTaskUpdateAfter": [
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "123",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Quality Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {},
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_CIVIL_01",
+              "feeCode": "PW_CIVIL_01",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "FINAL"
+            }
+          ]
+        }
+      }
+    ]
+  },
+  "PublicWorks/Civil Plan/Revision/NA": {
+    "WorkflowTaskUpdateAfter": [
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "123",
+          "operators": {}
+        },
+        "criteria": {
+          "task": [
+            "Quality Check"
+          ],
+          "status": [
+            "Ready to Pay"
+          ],
+          "customFields": {},
+          "isCreatedByACA": "",
+          "balanceAllowed": "",
+          "recordStatus": []
+        },
+        "action": {
+          "prescript": "",
+          "activateTask": [],
+          "daysOut": "",
+          "deactivateTask": [],
+          "deleteTask": [],
+          "updateTask": [
+            {
+              "task": "",
+              "status": ""
+            }
+          ],
+          "invoiceFees": "Y",
+          "createChild": "",
+          "createParent": "",
+          "addCondition": "",
+          "removeCondition": "",
+          "addComment": "",
+          "newStatus": "",
+          "assessFees": [
+            {
+              "feeSchedule": "PW_CIVIL_02",
+              "feeCode": "PW_CIVIL_02",
               "feeQuantity": 1,
               "feeInvoice": "Y",
               "feePeriod": "FINAL"
