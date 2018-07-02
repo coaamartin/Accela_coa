dbb5284af33fdfbcb20759050833ffa42666e0d7
jantled <jal@wbai.com>
2018-07-02 10:14:23 -0500

    moved 331 from configurable to custom

--- EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
+++ EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
@@ -20,10 +20,32 @@ if(inspType == "Notice of Violation Inspection"){
         logDebug("Record is Closed.  Closing all active tasks for process code: " + wfProcess);
         if(wfProcess) deactivateActiveTasks(wfProcess);
     }
-	
-	if(inspResult == "Issue Summons"){
-		closeTask("Notice of Violation", "Issue Summons", "via Script 332", "via Script 332");
-		activateTask("Pre Hearing Inspection");
-	}
+    
+    if(inspResult == "Issue Summons"){
+        closeTask("Notice of Violation", "Issue Summons", "via Script 332", "via Script 332");
+        activateTask("Pre Hearing Inspection");
+    }
     //Script 332 end
+}
+
+
+if(inspType == "Investigation"){
+    //Script 331 Begin
+      //331a
+    if(inspResult == "Reassign to another Division"){
+        closeTask("Investigation", "Reassign to another Division", "Closed via 331", "Closed via 331");
+        updateAppStatus("Closed", "Updated via IRSA 331");
+        closeAllTasks(capId, "");
+    }
+      //331b
+    if(inspResult == "No Violation Observed"){
+        closeTask("Investigation", "No Violation Observed", "Closed vis 331", "Closed via 331");
+        updateAppStatus("Closed", "Updated via IRSA 331");
+        closeAllTasks(capId, "");
+    }
+      //331c
+    processNotOfViolInsp("Investigation", "In Progress", true, "Investigation", true, "Investigation", "In Progress");
+      //331d
+    processNotOfViolInsp("Investigation", "Notice of Violation", true, "Notice of Violation Inspection", true, "Investigation", "Notice of Violation");
+    //Script 331 End
 }
