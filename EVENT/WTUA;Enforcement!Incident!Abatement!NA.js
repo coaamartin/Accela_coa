117eb1173867e9b4b8b937cab7d63c2e108fff9a
darrenkoontz <32111889+darrenkoontz@users.noreply.github.com>
2018-07-02 19:36:33 -0600

    Add files via upload

--- EVENT/WTUA;Enforcement!Incident!Abatement!NA.js
+++ EVENT/WTUA;Enforcement!Incident!Abatement!NA.js
@@ -20,11 +20,19 @@ if (ifTracer(wfTask == "Invoicing" && wfStatus == "Invoiced", "wfTask == Invoici
 //
 //Created By: Silver Lining Solutions
 
-logDebug ("script94 START");
+logDebug ("script229 START");
 
 
 if (wfTask == "License Issuance" && wfStatus== "Issued Action")
-{
+{ 	// Get the Applicant's email
+	var recordApplicant = getContactByType("Applicant", capId);
+	var applicantEmail = null;
+	if (!recordApplicant || recordApplicant.getEmail() == null || recordApplicant.getEmail() == "") {
+		logDebug("**WARN no applicant or applicant has no email, capId=" + capId);
+	} else {
+		applicantEmail = recordApplicant.getEmail();
 
-logDebug ("script94 END");
+
 }
+
+logDebug ("script229 END");
