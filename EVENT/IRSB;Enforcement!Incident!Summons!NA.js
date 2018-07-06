c7bb45e4d7363eab4b34895e12f7aec6f3982aa9
grayquarter <john@grayquarter.com>
2018-07-05 17:28:19 -0700

    424

--- EVENT/IRSB;Enforcement!Incident!Summons!NA.js
+++ EVENT/IRSB;Enforcement!Incident!Summons!NA.js
@@ -4,8 +4,8 @@ if ("Summons Issuance".equals(inspType)) {
 	if (matches(inspResult,"Visit/Attempted Contact","Letter to be Sent","Personal Service","Compliance","Cancelled")) {
 		if (inspId && "Scheduled".equals(String(aa.inspection.getInspection(capId,inspId).getOutput().getInspectionStatus()))) {
 			cancel = true;
-			comment("Inspection can't be resulted with this status.   Initial status must be 'Taken and Stored - Summons' or 'Taken and Stored - Citation");
 			showMessage = true;
+			comment("Inspection can't be resulted with this status.   Initial status must be 'Taken and Stored - Summons' or 'Taken and Stored - Citation");
 		}
 	}
 }
