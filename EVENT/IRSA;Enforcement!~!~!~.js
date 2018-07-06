6f1a62396dee210d280a21843b7422c9a0b159f2
jantled <jal@wbai.com>
2018-07-06 12:57:06 -0500

    added files for script 346

---
+++ EVENT/IRSA;Enforcement!~!~!~.js
@@ -0,0 +1,5 @@
+
+if(inspType == "Zoning Initial Inspection"){
+    //Script 346
+	enfProcessInspResult("Zoning Initial Inspection", "Notice Posted", "Zoning Follow-Up Inspection", "Initial Investigation Re-Inspection Days", false, "Initial Investigation", "Notice Posted");
+}
