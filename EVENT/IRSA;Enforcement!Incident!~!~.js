0962fc9c92949c1c9d017185f2a39a89dc075c7e
Charles Munzer <cvm@byrnesoftware.com>
2018-07-06 14:02:47 -0500

    updated script 426

--- EVENT/IRSA;Enforcement!Incident!~!~.js
+++ EVENT/IRSA;Enforcement!Incident!~!~.js
@@ -1,8 +1,18 @@
-//script 432
+//script 423
 logDebug("Script 423 Starting");
 if (matchARecordType([
     "Enforcement/Incident/Snow/NA",
     "Enforcement/Incident/Abatement/NA"
 ], appTypeString)) {
     include("423_AccessInvoiceSnowAndAbatementFees");
+}
+
+//script 426
+logDebug("Script 426 Starting");
+if (matchARecordType([
+    "Enforcement/Incident/Abatement/NA",
+    "Enforcement/Incident/Summons/NA",
+    "Enforcement/Incident/Record with County/NA"
+], appTypeString)) {
+    include("426_UpdateParentEnfCaseCustomListAndStatus");
 }
