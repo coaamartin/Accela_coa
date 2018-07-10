1a70970eaed18b30ef752d2c2fc014e74a583642
jantled <jal@wbai.com>
2018-07-10 16:15:53 -0500

    .

--- EVENT/WTUA;PublicWorks!Traffic!Traffic Engineering Request!NA.js
+++ EVENT/WTUA;PublicWorks!Traffic!Traffic Engineering Request!NA.js
@@ -6,13 +6,15 @@ if (ifTracer((wfTask=="Initial Review" || wfTask=="Initial Supervisor Review") &
 
 if(ifTracer(wfTask == "Initial Review" && wfStatus == "Assigned to Supervisor", 'wfTask == "Initial Review" && wfStatus == "Assigned to Supervisor"')){
     var assignedTo = getTaskSpecific(wfTask, "Assigned to Supervisor");
-	logDebug(assignedTo);
-	if(assignedTo != false) {
-		var userName=assignedTo.split(" ");
-		var userObj = aa.person.getUser(userName[0],null,userName[1]).getOutput();
-		assignTask("Initial Supervisor Review",userObj.getUserID());
-		updateTaskDepartment("Initial Supervisor Review",userObj.getDeptOfUser());		
-	}
+    logDebug(assignedTo);
+    if(assignedTo != false) {
+        var userName=assignedTo.split(" ");
+        var userObj = aa.person.getUser(userName[0],null,userName[1]).getOutput();
+        if(userObj){
+            assignTask("Initial Supervisor Review",userObj.getUserID());
+            updateTaskDepartment("Initial Supervisor Review",userObj.getDeptOfUser());
+        }
+    }
 }
 
 logDebug('Script 250 Ending')
@@ -22,11 +24,11 @@ logDebug('Script 269 Starting')
 if (ifTracer(wfTask=="Manager Review" && wfStatus=="Approved",'wfTask & wfStatus match')) {
     include("269_GenerateWorkOrderTrafficInvestigation");
 
-	//Script 174 Begin
-	if (AInfo["Final Response Required"]=="CHECKED"){
-	 	var res=addAdHocTask("ADHOC_WORKFLOW","Final Request Sent", "");
-	}
-	//Script 174 End
+    //Script 174 Begin
+    if (AInfo["Final Response Required"]=="CHECKED"){
+        var res=addAdHocTask("ADHOC_WORKFLOW","Final Request Sent", "");
+    }
+    //Script 174 End
 }
 
 //script 171
@@ -37,11 +39,11 @@ if (ifTracer(wfTask=="Manager Review" && wfStatus=="Request Complete",'wfTask & 
 }
 
 if(iTracer(wfTask == "Draft Workorder" && wfStatus == "Workorder Drafted", 'wf:Draft Workorder/Workorder Drafted')){
-	script265_ManagerReviewToSupervisor();
+    script265_ManagerReviewToSupervisor();
 }
 
 if(iTracer(wfTask == "Traffic Investigation" && matches(wfStatus, "No Change Warranted", "Refer to Forestry", "Refer to Code Enforcement"), 'wf:Traffic Investigation/No Change Warranted or Refer to Forestry or Refer to Code Enforcement')){
-	script265_ManagerReviewToSupervisor();
+    script265_ManagerReviewToSupervisor();
 }
 
 script270_GenerateWorkOrderNumber();
@@ -53,8 +55,8 @@ Purpose : If the workflow task = 'Manager Review' and the workflow status = 'App
 number(Format TBD by Aurora) and update the custom field "Work Order Number" and send a notification email(Template
 TBD by Aurora) with an attached Work Order Report(Report name TBD) and email Chris Carnihan(ccarnihan@aurora.gov)
 
-Author: Yazan Barghouth 
- 
+Author: Yazan Barghouth
+
 Functional Area : Records
 
 Sample Call:
