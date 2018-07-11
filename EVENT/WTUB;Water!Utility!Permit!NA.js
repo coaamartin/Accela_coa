433deb700f478f63514b6ca24d5a72a5bb05b3d5
John Schomp <john@grayquarter.com>
2018-07-11 13:07:35 -0700

    304

--- EVENT/WTUB;Water!Utility!Permit!NA.js
+++ EVENT/WTUB;Water!Utility!Permit!NA.js
@@ -1,6 +1,37 @@
 /*
 This event created by JMAIN 03-15-2018
-*/
+ */
 
 include("15_waterCheckFeesInvoicedBeforeSendingEmail");
 script89_PreventInspStatus();
+
+// begin script 304
+/*
+If workflow task = Final Acceptance Inspection and
+Status = Warranty Work Required and Document
+Type of Warranty Work Punch List does not exist
+block progression of the workflow and
+raise the error message “Warranty
+Work Punch List is required for
+Warranty Work Required status
+ */
+
+if ("Final Acceptance Inspection".equals(wfTask) && "Warranty Work Required".equals(wfStatus)) {
+	var docType = "Warrant Work Punch List";
+	var docList = getDocumentList();
+	var filteredDocs;
+
+	if (docList) {
+		var filteredDocs = docList.filter(function (o) {
+				return docType.equalsIgnoreCase(o.getDocCategory().toString());
+			});
+	}
+
+	if (!filteredDocs || filteredDocs.length == 0) {
+		comment("Warranty Work Punch List is required for Warranty Work Required status");
+		showMessage = true;
+		cancel = true;
+	}
+}
+
+// end script 304
