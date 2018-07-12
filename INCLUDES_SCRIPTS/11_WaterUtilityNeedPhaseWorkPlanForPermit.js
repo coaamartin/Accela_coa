a743da3d2307c83d66eeeba9d4028985ee808964
John Schomp <john@grayquarter.com>
2018-07-11 23:17:06 -0700

    debug 281

--- INCLUDES_SCRIPTS/11_WaterUtilityNeedPhaseWorkPlanForPermit.js
+++ INCLUDES_SCRIPTS/11_WaterUtilityNeedPhaseWorkPlanForPermit.js
@@ -9,7 +9,7 @@ var neededdocument = "Phase Work Plan";
 //has the needed document been uploaded?
 var docuploaded = false;
 var capIdobject = aa.cap.getCapID(capId).getOutput();
-var documentsobject = aa.document.getCapDocumentList(capIdobject, myUserId);
+var documentsobject = aa.document.getCapDocumentList(capIdobject, "ADMIN");
 var listofdocuments = documentsobject.getOutput();
 if (documentsobject.getSuccess)
 {
