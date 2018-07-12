711a11d4b535837add78bf77a06d803d5b85ee60
John Schomp <john@grayquarter.com>
2018-07-11 23:31:27 -0700

    Refactor 11_WaterUtilityNeedPhaseWorkPlanForPermit.js

--- EVENT/WTUA;Water!Utility!Permit!NA.js
+++ EVENT/WTUA;Water!Utility!Permit!NA.js
@@ -40,8 +40,10 @@ Supported Email Parameters:
 
 checkNoFeeAndUpdateTask(capId, "Fee Processing", [ "No Fees Required" ], "MESSAGE_NOTICE_PUBLIC WORKS", "Permit Issuance", "Issued","Issued");
 
+
 //JMAIN - show message if no phase plan
-include("11_WaterUtilityNeedPhaseWorkPlanForPermit");
+//disabled the script below as it was causing errors and will not work as designed
+//include("11_WaterUtilityNeedPhaseWorkPlanForPermit");
 
 //SWAKIL - Email
 include("41_EmailWaterUtilityPermit");
