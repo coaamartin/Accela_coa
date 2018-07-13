50228b656253748e8252ae0f1098dd160efbc2f6
Charles Munzer <cvm@byrnesoftware.com>
2018-07-13 18:02:05 -0500

    UPDATED 426

--- INCLUDES_CUSTOM/getContacts.js
+++ INCLUDES_CUSTOM/getContacts.js
@@ -5,12 +5,12 @@ function getContacts(options) {
     };
     for (var attr in options) { settings[attr] = options[attr]; } //optional params - overriding default settings
 
-    
+
     var contactsByType = [];
 	var contactArray = getPeople(settings.capId);
 
 	for (thisContact in contactArray) {
-        if (settings.contactType == null || (contactArray[thisContact].getPeople().contactType).toUpperCase() == conType.toUpperCase()) {
+        if (settings.contactType == null || (contactArray[thisContact].getPeople().contactType).toUpperCase() == settings.contactType.toUpperCase()) {
             contactsByType.push(contactArray[thisContact].getPeople())
         }
     }
