2d0ec4d12739a854f2ed16245f070dd6a377d268
James Main <jmain@auroragov.org>
2018-07-17 08:37:50 -0600

    updated WTUA Water Water Tap Application event

--- EVENT/WTUA;Water!Water!Tap!Application.js
+++ EVENT/WTUA;Water!Water!Tap!Application.js
@@ -2,5 +2,6 @@
 include("32_recStatusUpdateWaterTap");
 include("31_1_EmailWithFee");
 
-//COA Script - JMAIN
-include("33_WaterTapApplicationFee.js");
+//COA Script - JMAIN 
+include("33_WaterTapApplicationFee");
+
