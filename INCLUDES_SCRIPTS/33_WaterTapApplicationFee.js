de34c65bc26bf96e97f5b9be0b873ad510038e5c
James Main <jmain@auroragov.org>
2018-07-17 08:25:16 -0600

    fixed water tap application #33 again - updated debug

--- INCLUDES_SCRIPTS/33_WaterTapApplicationFee.js
+++ INCLUDES_SCRIPTS/33_WaterTapApplicationFee.js
@@ -8,7 +8,7 @@ written by COA JMAIN
 
 if (wfTask == "Application Submittal" && wfStatus == "Specific Contract Fees - Accepted")
 {
-	logDebug("Activating Task Fee Processing and Setting Task Status to Specific Contract Fees - Accepted");
+	logDebug("Activating Task Fee Processing and Setting Task Status to Ready to Pay with Specific Contract Fees");
 	activateTask("Fee Processing");
 	updateTask("Fee Processing", "Ready to Pay with Specific Contract Fees", "updated by script COA 33", "updated by script COA 33");
 }
