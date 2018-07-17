b7f23b7bda3c2f7cfbe00a3e95aca4e54be1ea02
jantled <jal@wbai.com>
2018-07-17 08:46:49 -0500

    .

--- INCLUDES_CUSTOM/setCodeReference.js
+++ INCLUDES_CUSTOM/setCodeReference.js
@@ -6,13 +6,19 @@
  */
 
 function setCodeReference(wfStatusCompare) {
-	var $iTrc = ifTracer;
-	logDebug("setCodeReference() started");
+    var $iTrc = ifTracer;
+    logDebug("setCodeReference() started");
+    //In case the permit is issued from other event, we hard code the task and status.
+    if(vEventName != "WorkflowTaskUpdateAfter"){
+        logDebug("Event name is not WTUA");
+        wfStatus = "Issued";
+    }
+	
     if ($iTrc(wfStatus == wfStatusCompare, 'wfStatus == ' + wfStatusCompare)) {
         var codeRefVal = getAppSpecific("Code Reference");
         if ($iTrc(isEmpty(codeRefVal), 'isEmpty(codeRefVal)')) {
             editAppSpecific("Code Reference", "2015 I-Codes/Aurora Muni Codes/2017-NEC");
         }
     }
-	logDebug("setCodeReference() ended.");
+    logDebug("setCodeReference() ended.");
 }
