eb7a13e53fa06fc8e72a78b7efd3273b87bbd147
jantled <jal@wbai.com>
2018-07-17 17:26:33 -0500

    added file

---
+++ TEMPLATES/CONFIGURABLES/CONF_PUBLICWORKS_INSPECTION_AUTOMATION.js
@@ -0,0 +1,52 @@
+{
+  "PublicWorks/Public Improvement/Permit/NA": {
+    "InspectionResultSubmitAfter": [
+      {
+        "preScript": "",
+        "postScript": "",
+        "metadata": {
+          "description": "To run automated script based on JSON rules",
+          "operators": {
+            
+          }
+        },
+        "criteria": {
+          "inspectionTypePerformed": [
+            "PI Inspection"
+          ],
+          "inspectionResult": [
+            "Final"
+          ]
+        },
+        "action": {
+          "costRangeType": "",
+          "costRange": 0,
+          "costFeeType": "",
+          "costFeeSchedule": "",
+          "costFeeName": "",
+          "costFeeAmount": 0,
+          "caseCreationType": "",
+          "caseFailureStatus": [
+            
+          ],
+          "caseType": "",
+          "caseCopyComments": false,
+          "inspectionType": "",
+          "rangeType": "",
+          "range": 0,
+          "sameInspector": true,
+          "createCondition": "",
+          "createConditionType": "",
+          "createConditionSeverity": "",
+          "feeSchedule": "",
+          "feeName": "",
+          "feeAmount": 0,
+          "taskName": "PI Inspection",
+          "taskStatus": "Completed",
+          "removeCondition": "",
+          "removeConditionType": ""
+        }
+      }
+    ]
+  }
+}
