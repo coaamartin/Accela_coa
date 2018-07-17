959555ac917f465820c9c24d7953f35c9c0fc889
James Main <jmain@auroragov.org>
2018-07-17 10:52:33 -0600

    Updated the email template code on #34 again - first time didn't work.

--- INCLUDES_SCRIPTS/34_emailResubmittalPlanReview.js
+++ INCLUDES_SCRIPTS/34_emailResubmittalPlanReview.js
@@ -1,12 +1,14 @@
 //created by swakil
 //edited by JMAIN - altered email template 07/17/2018
 
-logDebug("Starting Script...");
+logDebug("Starting Script... #34");
 
 if(wfTask=="Plan Review" && wfStatus=="Resubmittal Requested"){
 	var contact = "Applicant";
 	var template = "WAT_IRR_PLAN_RESUB";
 	//must inlcude an emailparams hashtable even if we don't use it.
+	//wfComment is NOT included as a built-in so we must add it if the email template requires this variable
 	var emailparams = aa.util.newHashtable();
+	emailparams.put(wfComment);
 	emailContacts(contact, template, emailparams, "", "", "N", "");
 }
