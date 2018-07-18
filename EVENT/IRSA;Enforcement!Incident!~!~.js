122689247cb2a758250a2d6315860d4ddaaa52f7
Charles Munzer <cvm@byrnesoftware.com>
2018-07-18 17:53:55 -0500

    up-dated 426 and 423

--- EVENT/IRSA;Enforcement!Incident!~!~.js
+++ EVENT/IRSA;Enforcement!Incident!~!~.js
@@ -1,15 +1,3 @@
-//script 432
-logDebug("Script 423 Starting");
-if (matchARecordType([
-    "Enforcement/Incident/Abatement/NA",
-], appTypeString)) {
-    if (ifTracer(wfTask == "Invoicing" && wfStatus == "Invoiced", "wfTask == Invoicing && wfStatus == Invoiced")) {
-        include("423_AccessInvoiceSnowAndAbatementFees");
-    }
-}
-
-
-
 
 //script 423
 logDebug("Script 423 Starting");
--- EVENT/WTUA;Enforcement!Incident!Abatement!NA.js
+++ EVENT/WTUA;Enforcement!Incident!Abatement!NA.js
@@ -1,8 +1,4 @@
-//script 432
-logDebug("Script 423 Starting");
-if (ifTracer(wfTask == "Invoicing" && wfStatus == "Invoiced", "wfTask == Invoicing && wfStatus == Invoiced")) {
-    include("423_AccessInvoiceSnowAndAbatementFees");
-}
+
 
 
 //Script 229	
