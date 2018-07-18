6dc02a88a7ba539061d4dee2e304c2ad058157e6
Charles Munzer <cvm@byrnesoftware.com>
2018-07-18 15:44:50 -0500

    updated 426

--- INCLUDES_CUSTOM/getAsiTableRows.js
+++ INCLUDES_CUSTOM/getAsiTableRows.js
@@ -8,29 +8,35 @@
 function getAsiTableRows(tableName, options) {
     var settings = {
         capId: capId,
-        colFilters: null //array of column values to filter by or null
+        colFilters: null //array of column values to filter by... null = all rows
     };
     //optional params - overriding default settings
     for (var attr in options) { settings[attr] = options[attr]; }
 
     var rows = loadASITable(tableName, settings.capId),
-        filter;
+        filter,
+        matched,
+        rtn = [];
 
     if (rows == undefined || rows == null) {
         return null;    //no table or rows
     }
 
-    //printObjProps(rows);
-
-    // if(settings.colFilters != null && settings.colFilters.length > 0) {
-    //     for(var idxRows in rows) {
-    //         printObjProps(rows[idxRows]);
-    //         // for(var idxFilter in settings.colFilters) {
-    //         //     filter = settings.colFilters[idxFilter];
-    //         //    // if(filter.colName == rows[ ])
-    //         // }
-    //     }
-    // } 
+    if(settings.colFilters != null && settings.colFilters.length > 0) {
+        for(var idxRows in rows) {
+            matched = true;
+            for(var idxFilter in settings.colFilters) {
+                filter = settings.colFilters[idxFilter];
+                if(filter.colValue != rows[idxRows][filter.colName]) {
+                    matched = false;
+                }
+            }
+            if(matched) {
+                rtn.push(rows[idxRows]);
+            }
+        }
+        return rtn
+    } 
 
     return rows;
 }
