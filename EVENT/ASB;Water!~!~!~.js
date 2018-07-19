f6be36f961dd7405b687105e30f1ee992489c182
John Schomp <john@grayquarter.com>
2018-07-19 13:04:31 -0700

    exclude renewals since ASB fires before the data is copied

--- EVENT/ASB;Water!~!~!~.js
+++ EVENT/ASB;Water!~!~!~.js
@@ -1,14 +1,17 @@
 /* Title :  Require Parcel or Address on Record (ApplicationSubmitBefore)
 
-Purpose :  If No Address or Parcel has been provided on the record (Water/*//*) then block submission and raise error ”Address or Parcel is
+Purpose :  If No Address or Parcel has been provided on the record (Water/*/
+/*) then block submission and raise error ”Address or Parcel is
 required”.
 
 Author :   Israa Ismail
 
-Functional Area : Records 
+Functional Area : Records
 
 Sample Call : checkIfAddressOrParcelExists();
 
-*/
+ */
 
-checkIfAddressOrParcelExists();
+if (!appMatch("*/*/*/Renewal") {
+	checkIfAddressOrParcelExists();
+}
