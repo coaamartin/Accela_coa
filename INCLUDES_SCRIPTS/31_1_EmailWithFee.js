d97305ea5dc8bbd0f0b7716efef9083b02e4a8ec
Suhail Wakil <suhailwakil@gmail.com>
2018-07-20 12:02:36 -0700

    updated email template and parameters

--- INCLUDES_SCRIPTS/31_1_EmailWithFee.js
+++ INCLUDES_SCRIPTS/31_1_EmailWithFee.js
@@ -10,6 +10,7 @@ if ("Fee Processing".equals(wfTask) && "Ready to Pay".equals(wfStatus))
 	var iFees = new Array();
 	var feesString = "";
 	iFees = getInvoicedFees();
+	var feesTotal = 0;
 
 	if (iFees.length > 0)
 	{
@@ -17,12 +18,28 @@ if ("Fee Processing".equals(wfTask) && "Ready to Pay".equals(wfStatus))
 		{
 			//Create string to include in email
 			feesString += iFees[f].getFeeCod() + " " + iFees[f].getFeeDescription() + ": " + iFees[f].getFee() + "\n";
+			feesTotal += parseInt(iFees[f].getFee());
 
 		}
 		var contact = "Applicant";
-		var template = "JD_TEST_TEMPLATE";
+		var template = "TAP_FFES";
+		//get contact
+		var aContact = getContactByType(contact, capId);
+		if (aContact) fullName = aContact.getFullName() || aContact.getFirstName() + " " + aContact.getLastName();
+
+		//build ACA URL
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));  
+		var recURL = acaSite + getACAUrl();		
+
 		var emailparams = aa.util.newHashtable();
-		emailparams.put("$$invoideFees$$", feesString);
+		emailparams.put("$$date$$", sysDateMMDDYYYY);
+		emailparams.put("$$altID$$", capId.getCustomID());
+		emailparams.put("$$recAlias$$", appTypeString);
+		emailparams.put("$$contactName$$", fullName);
+		emailparams.put("$$feesTotal$$", feesTotal);
+		emailparams.put("$$balDue$$", balanceDue);
+		emailparams.put("$$recLink$$", recURL);
 		emailContacts(contact, template, emailparams, "", "", "N", "");
 	}
 }
