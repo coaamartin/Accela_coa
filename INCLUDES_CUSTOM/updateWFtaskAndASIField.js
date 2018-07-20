76ab650559182042e8aeed4146b3dc993037fd85
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-20 14:53:18 -0600

    Add files via upload

--- INCLUDES_CUSTOM/updateWFtaskAndASIField.js
+++ INCLUDES_CUSTOM/updateWFtaskAndASIField.js
@@ -11,9 +11,12 @@
  */
 function updateWFtaskAndASIField(parentWorkflowTasktoBechecked, parentworkFlowStatustoBeChecked, workFlowTaskTobechecked, workFlowStatusTobeChecked, workflowTasktobeActivated,
 		ASIFieldNametoBeUpdated, ASIFieldValue) {
+							logDebug("Script 200 Starting");
 	if (wfTask == workFlowTaskTobechecked && wfStatus == workFlowStatusTobeChecked) {
+						logDebug("Script 200 pass task and status " + wfTask + " " + wfStatus);
 		var parentTask = aa.workflow.getTask(capId, parentWorkflowTasktoBechecked).getOutput();
 		if (parentTask != null && parentTask != "") {
+						logDebug("Script 200 pass task and status " + parentTask);
 			if (parentTask.getDisposition() == parentworkFlowStatustoBeChecked) {
 				logDebug("Script 200 Starting");
 				activateTask(workflowTasktobeActivated);
