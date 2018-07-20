3ec7a0cfe142539f8362a328f3dc6335cdaca7ad
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-20 15:29:53 -0600

    Add files via upload

--- EVENT/WTUA;Forestry!Request!Citizen!~.js
+++ EVENT/WTUA;Forestry!Request!Citizen!~.js
@@ -11,7 +11,7 @@ Functional Area : Records
 Sample Call:
 updateWFtaskAndASIField("Tree Request Intake", "Crew Work", "Removal", "Stump Grind", "Area Number",2)
  */
- updateWFtaskAndASIField("Tree Request Intake", "No Plant", "Crew Work", "Removal", "Stump Grind Priority",2);
+ updateWFtaskAndASIField("Tree Request Intake", "No Plant", "Crew Work", "Removal", "Area Number","2");
 /*
 if(wfTask == "Crew Work" && wfStatus =="Removal"){
 	
