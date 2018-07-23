a808dcc1777968f4e8c558f88ba2c6abf21b0add
jantled <jal@wbai.com>
2018-07-23 13:58:37 -0500

    .

--- EVENT/WTUA;ODA!Pre App!NA!NA.js
+++ EVENT/WTUA;ODA!Pre App!NA!NA.js
@@ -14,12 +14,17 @@ Purpose : check if workflow task/status matches, send email to multiple parties.
 Author: Yazan Barghouth  
 Functional Area : Records
 Sample Call:
-	sendMeetingConfirmationEmail("Finalize Agenda", [ "Complete" ], "MESSAGE_NOTICE_PUBLIC WORKS");
+    sendMeetingConfirmationEmail("Finalize Agenda", [ "Complete" ], "MESSAGE_NOTICE_PUBLIC WORKS");
 */
 
 sendMeetingConfirmationEmail("Finalize Agenda", [ "Complete" ], "MESSAGE_NOTICE_PUBLIC WORKS");
 
-//Script 225
 if(ifTracer(wfTask == "Prepare Final Letter" && wfStatus == "Complete", 'wf:Prepare Final Letter/Complete')){
+    //Script 225
     odaScript225_emailMeetingNotes();
+}
+
+if(ifTracer(wfTask == "Pre App Meeting Request" && wfStatus == "Scheduled", 'wf:Pre App Meeting Request/Scheduled')){
+    //Script 221
+    activateTask("Pre Application Meeting");
 }
