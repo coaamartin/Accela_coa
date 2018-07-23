086428da48052a5e27a62dd259f990d7276d35af
James Main <jmain@auroragov.org>
2018-07-23 10:20:55 -0600

    edited COa #9 and changed the AND to an OR in the script logic

--- INCLUDES_SCRIPTS/9_WaterPermitCheckInspectionsComplete.js
+++ INCLUDES_SCRIPTS/9_WaterPermitCheckInspectionsComplete.js
@@ -1,46 +1,67 @@
+/*
+COA #9
+Event InspectionResultSubmitAfter Criteria When all scheduled inspections
+have a status of Passed or Canceled or Complete and Initial Acceptance Inspection
+has a status of Complete Action Move the workflow forward to Request Materials Testing,
+by inserting status = Completed on wftask = Utility Inspection. And Email Applicant that
+Installation is complete will now need the Materials testing uploaded.
+Using WUP Request for Testing Template attached example.
+written by swakil 05/2018
+edited by jmain 07/23/2018
+*/
+
 var inspnsComplete = inspectionsComplete();
 var inspnComplete = inspectionComplete("Initial Acceptance");
 
-if(inspnsComplete && inspnComplete){
-    closeTask("Utility Inspection", "Completed", "EMSE ID 9", "EMSE ID 9");
-    activateTask("Request Materials Testing", "EMSE ID 9", "EMSE ID 9");  
-    sendEmailToApplicant();
+if(inspnsComplete || inspnComplete)
+{
+	closeTask("Utility Inspection", "Completed", "EMSE ID 9", "EMSE ID 9");
+	activateTask("Request Materials Testing", "EMSE ID 9", "EMSE ID 9");  
+	sendEmailToApplicant();
 }
 
-function inspectionsComplete() {
-  var t = aa.inspection.getInspections(capId);
-  if (t.getSuccess()) {
-    var n = t.getOutput();
-    if(n.length > 0){
-      for (xx in n){
-          inspStatus = n[xx].getInspectionStatus().toUpperCase();
-          if (!( inspStatus.equals("PASSED") || inspStatus.equals("CANCELED") || inspStatus.equals("COMPLETE")) ) 
-          {
-              return false;
-          }
-      }
-      return true; 
-    } 
-  } 
-  return false;
+function inspectionsComplete()
+{
+	var t = aa.inspection.getInspections(capId);
+	if (t.getSuccess()) 
+	{
+		var n = t.getOutput();
+		if(n.length > 0)
+		{
+			for (xx in n)
+			{
+				inspStatus = n[xx].getInspectionStatus().toUpperCase();
+				if (!( inspStatus.equals("PASSED") || inspStatus.equals("CANCELED") || inspStatus.equals("COMPLETE")) ) 
+				{
+				  return false;
+				}
+			}
+			return true; 
+		} 
+	} 
+	return false;
 }
 
-function inspectionComplete(type) {
-  var t = aa.inspection.getInspections(capId);
-  if (t.getSuccess()) {
-    var n = t.getOutput();
-    if(n.length > 0){
-      for (xx in n){
-          inspType = n[xx].getInspectionType();
-          inspStatus = n[xx].getInspectionStatus().toUpperCase();
-          if ( inspType.equals(type) && inspStatus.equals("COMPLETE"))  
-          {
-              return true;
-          }
-      }
-    } 
-  } 
-  return false;
+function inspectionComplete(type) 
+{
+	var t = aa.inspection.getInspections(capId);
+	if (t.getSuccess()) 
+	{
+		var n = t.getOutput();
+		if(n.length > 0)
+		{
+			for (xx in n)
+			{
+				inspType = n[xx].getInspectionType();
+				inspStatus = n[xx].getInspectionStatus().toUpperCase();
+				if ( inspType.equals(type) && inspStatus.equals("COMPLETE"))  
+				{
+				  return true;
+				}
+			}
+		} 
+	} 
+	return false;
 }
 
 function sendEmailToApplicant(){
