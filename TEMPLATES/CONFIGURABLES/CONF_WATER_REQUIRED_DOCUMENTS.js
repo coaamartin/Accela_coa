3b77e3235b9e513628262fa0f6207e559873c353
grayquarter <john@grayquarter.com>
2018-07-22 22:42:56 -0700

    191

--- TEMPLATES/CONFIGURABLES/CONF_WATER_REQUIRED_DOCUMENTS.js
+++ TEMPLATES/CONFIGURABLES/CONF_WATER_REQUIRED_DOCUMENTS.js
@@ -1,34 +1,4 @@
-{
-  "Water/Water/Irrigation Plan Review/NA": {
-    "WorkflowTaskUpdateBefore": [
-      {
-        "preScript": "",
-        "postScript": "",
-        "metadata": {
-          "description": "To run automated script based on JSON rules",
-          "operators": {
-            
-          }
-        },
-        "criteria": {
-          "task": [
-            "Application Submittal"
-          ],
-          "status": [
-            "Plans Required"
-          ]
-        },
-        "action": {
-          "requiredDocuments": [
-            "Irrigation Plan"
-          ],
-          "requirementType": "STANDARD",
-          "validationMessage": "You must Upload the Irrigation Plan to complete your submission"
-        }
-      }
-    ]
-  },
-  "Water/Utility/Permit/NA": {
+{  "Water/Utility/Permit/NA": {
     "WorkflowTaskUpdateBefore": [
       {
         "preScript": "",
