cf3670aa375714276ea3b8e3fd4069effcef1249
Charles Munzer <cvm@byrnesoftware.com>
2018-07-25 09:38:26 -0500

    updated 148

--- BATCH/BATCH_SCRIPT148_ARBORIST_LICENSE_RENEWAL.js
+++ BATCH/BATCH_SCRIPT148_ARBORIST_LICENSE_RENEWAL.js
@@ -43,7 +43,7 @@ function AboristLicenseRenewal(grp, typ, stype, cat){
     if(aa.env.getValue("RunDate") != null) {
         runDate = new Date(aa.env.getValue("RunDate"));
     }
-    if(runDate.getMonth() != 2 || runDate.getMonth() != 11) {
+    if(runDate.getMonth() != 1 || runDate.getMonth() != 11) {
         aa.print('You can only run this in December or Februrary OR pass in parameter RunDate (mm/dd/yyyy)');
     }
     for(idx in capScriptList) {
