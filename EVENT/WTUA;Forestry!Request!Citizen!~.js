9b36d8c47481ae96ae4d52b6f00ac9c8c6e7911d
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-25 17:33:09 -0600

    Add files via upload

--- EVENT/WTUA;Forestry!Request!Citizen!~.js
+++ EVENT/WTUA;Forestry!Request!Citizen!~.js
@@ -1,11 +1,12 @@
-//Script 200
-//Record Types:	Forestry/Request/Citizen
+/*
+Script 200
+Record Types:	Forestry/Request/Citizen
 
-//Event: 	WTUA
-//Desc:		If the workflow task = "Crew Work" and the workflow status = "Removal" then activate the workflow task "Stump Grind" and
-//				set the custom field "Stump Grind Priority" to the value of 2 if the workflow task "Tree Request Intake" has a status of "No
-//				Plant".
-//Created By: Silver Lining Solutions
+Event: 	WTUA
+Desc:		If the workflow task = "Crew Work" and the workflow status = "Removal" then activate the workflow task "Stump Grind" and
+				set the custom field "Stump Grind Priority" to the value of 2 if the workflow task "Tree Request Intake" has a status of "No
+				Plant".
+Created By: Silver Lining Solutions
 */
 if(wfTask == "Crew Work" && wfStatus =="Removal"){
 	
