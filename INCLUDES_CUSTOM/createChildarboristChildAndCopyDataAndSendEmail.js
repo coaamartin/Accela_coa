04ad3c8e4a6fdab7bf5b04c7012636cbacc50827
jantled <jal@wbai.com>
2018-07-25 10:58:20 -0500

    .

--- INCLUDES_CUSTOM/createChildarboristChildAndCopyDataAndSendEmail.js
+++ INCLUDES_CUSTOM/createChildarboristChildAndCopyDataAndSendEmail.js
@@ -9,51 +9,59 @@
  * @param rptParams report param if exists
  */
 function createChildarboristChildAndCopyDataAndSendEmail(LicenseType, emailTemplate, reportName, rptParams) {
-    var applicantEmail = "";
-    var licTypeArray = LicenseType.split("/");
-    var appName = cap.getSpecialText();
-    var createdApp = aa.cap.createApp(licTypeArray[0], licTypeArray[1], licTypeArray[2], licTypeArray[3], appName);
-    
-    if (!createdApp.getSuccess()) {
-        logDebug("**ERROR creating app failed, error: " + createdApp.getErrorMessage());
-    }
-    
-    createdApp = createdApp.getOutput();
-    logDebug("Creating Parent License : " + createdApp.getCustomID());
-    //add as parent:
-    var related = aa.cap.createAppHierarchy(createdApp, capId);
-    if (!related.getSuccess()) {
-        logDebug("**ERROR createAppHierarchy failed, error: " + related.getErrorMessage());
-    }
-    if (createdApp != null) {
-        copyContacts(capId, createdApp);
-        copyAppSpecific(createdApp);
-        var rNewLicIdString = createdApp.getCustomID();
-        createRefLP4Lookup(rNewLicIdString, "Business", "Arborist Applicant", null);
-        var rNewLP = aa.licenseScript.getRefLicensesProfByLicNbr(aa.serviceProvider, rNewLicIdString).getOutput();
-        if(rNewLP) aa.licenseScript.associateLpWithCap(capId, rNewLP[0]);
+    logDebug("createChildarboristChildAndCopyDataAndSendEmail() Started");
+    try{
+        var applicantEmail = "";
+        var licTypeArray = LicenseType.split("/");
+        var appName = cap.getSpecialText();
+        var createdApp = aa.cap.createApp(licTypeArray[0], licTypeArray[1], licTypeArray[2], licTypeArray[3], appName);
         
-        var recordApplicant = getContactByType("Arborist Applicant", capId);
-        if (recordApplicant) {
-            applicantEmail = recordApplicant.getEmail();
+        if (!createdApp.getSuccess()) {
+            logDebug("**ERROR creating app failed, error: " + createdApp.getErrorMessage());
         }
-        if (applicantEmail == null || applicantEmail == "") {
-            logDebug("**WARN Applicant on record " + capId + " has no email");
-
-        } else {
-
-            var emailParams = aa.util.newHashtable();
-            addParameter(emailParams, "$$altID$$", cap.getCapModel().getAltID());
-            //addParameter(emailParams, "$$recordAlias$$", cap.getCapModel().getCapType().getAlias());
-            //addParameter(emailParams, "$$recordStatus$$", cap.getCapModel().getCapStatus());
-            //addParameter(emailParams, "$$wfComment$$", wfComment);
-            //addParameter(emailParams, "$$wfTask$$", wfTask);
-            //addParameter(emailParams, "$$wfStatus$$", wfStatus);
-
-            //sendEmailWithReport(applicantEmail, "", emailTemplate, reportName, rptParams, emailParams)
-            emailContacts("Arborist Applicant",emailTemplate, emailParams, reportName,rptParams);
-
+        
+        createdApp = createdApp.getOutput();
+        logDebug("Creating Parent License : " + createdApp.getCustomID());
+        //add as parent:
+        var related = aa.cap.createAppHierarchy(createdApp, capId);
+        if (!related.getSuccess()) {
+            logDebug("**ERROR createAppHierarchy failed, error: " + related.getErrorMessage());
         }
-
+        if (createdApp != null) {
+            copyContacts(capId, createdApp);
+            copyAppSpecific(createdApp);
+            var rNewLicIdString = createdApp.getCustomID();
+            createRefLP4Lookup(rNewLicIdString, "Business", "Arborist Applicant", null);
+            var rNewLP = aa.licenseScript.getRefLicensesProfByLicNbr(aa.serviceProvider, rNewLicIdString).getOutput();
+            if(rNewLP) aa.licenseScript.associateLpWithCap(capId, rNewLP[0]);
+            
+            var recordApplicant = getContactByType("Arborist Applicant", capId);
+            if (recordApplicant) {
+                applicantEmail = recordApplicant.getEmail();
+            }
+            if (applicantEmail == null || applicantEmail == "") {
+                logDebug("**WARN Applicant on record " + capId + " has no email");
+        
+            } else {
+        
+                var emailParams = aa.util.newHashtable();
+                addParameter(emailParams, "$$altID$$", cap.getCapModel().getAltID());
+                //addParameter(emailParams, "$$recordAlias$$", cap.getCapModel().getCapType().getAlias());
+                //addParameter(emailParams, "$$recordStatus$$", cap.getCapModel().getCapStatus());
+                //addParameter(emailParams, "$$wfComment$$", wfComment);
+                //addParameter(emailParams, "$$wfTask$$", wfTask);
+                //addParameter(emailParams, "$$wfStatus$$", wfStatus);
+        
+                //sendEmailWithReport(applicantEmail, "", emailTemplate, reportName, rptParams, emailParams)
+                emailContacts("Arborist Applicant",emailTemplate, emailParams, reportName,rptParams);
+        
+            }
+        }
     }
-}
+    catch(err){
+        showMessage = true;
+        comment("Error on custom function createChildarboristChildAndCopyDataAndSendEmail(). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber);
+        logDebug("Error on custom function createChildarboristChildAndCopyDataAndSendEmail(). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber + ". Stack: " + err.stack);
+    }
+    logDebug("createChildarboristChildAndCopyDataAndSendEmail() ended");
+}//END
