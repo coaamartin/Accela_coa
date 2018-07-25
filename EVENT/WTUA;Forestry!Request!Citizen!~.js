19dc29f458ce16239354acf3fd2a7d6eaffc0b10
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-25 17:05:30 -0600

    Add files via upload

--- EVENT/WTUA;Forestry!Request!Citizen!~.js
+++ EVENT/WTUA;Forestry!Request!Citizen!~.js
@@ -3,17 +3,15 @@ Title : Activate Stump Grind workflow task (WorkflowTaskUpdateAfter)
 Purpose : If the workflow task = "Crew Work" and the workflow status = "Removal" then activate the workflow task "Stump Grind" and
 set the custom field "Stump Grind Priority" to the value of 2 if the workflow task "Tree Request Intake" has a status of "No
 Plant".
-
-Author: Haitham Eleisah
-
-Functional Area : Records
-
-Sample Call:
-updateWFtaskAndASIField("Tree Request Intake", "Crew Work", "Removal", "Stump Grind", "Area Number",2)
-
- updateWFtaskAndASIField("Tree Request Intake", "No Plant", "Crew Work", "Removal", "Area Number","2");
 */
 if(wfTask == "Crew Work" && wfStatus =="Removal"){
 	
-	activateTask("Stump Grind");
+		activateTask("Stump Grind");
+		var parentTask = aa.workflow.getTask(capId, "Tree Request Intake").getOutput();
+		if (parentTask != null && parentTask != "") {
+			if (parentTask.getDisposition() == "No Plant") {
+	
+				editAppSpecific("Stump Grind Priority", 2);
+			}
+		}
 }
