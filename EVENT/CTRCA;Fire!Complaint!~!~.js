16cbbc266b6b7b186abfd6d32198733b5785d56d
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-25 17:27:44 -0600

    Add files via upload

---
+++ EVENT/CTRCA;Fire!Complaint!~!~.js
@@ -0,0 +1,39 @@
+//*********************************************************************************************************
+//script 186 		Schedule Fire Inspection on submission
+//
+//Record Types:		Fire/Complaint/*/*
+//Event: 			CTRCA
+//Desc:				
+//IRSA
+//	 	criteria:	If the record is submitted from ACA
+//		Action:		Schedule the inspection type “Fire Complaint” for the date that the record is submitted 
+//					and assign to the proper inspector based on the inspection map settings using GIS for 
+//					the districts. 
+//
+//Created By: 		Silver Lining Solutions
+//*********************************************************************************************************
+logDebug("Script 186 START");
+
+    // get the inspector from GIS and assign the rec to this user
+    inspUserObj = null;
+    x = getGISBufferInfo("AURORACO","Fire Response Zones Run Order","0.01","BATTALION_FIRSTDUE")
+    logDebug(x[0]["BATTALION_FIRSTDUE"])
+   
+    var offFullName = x[0]["OFFICER_NAME"];
+    
+    var offFname = offFullName.substr(0,offFullName.indexOf(' '));
+    logDebug("first name = " + offFname);
+    
+    var offLname = offFullName.substr(offFullName.indexOf(' ')+1);
+    logDebug("last name = " + offLname);
+    
+    inspUserObj = aa.person.getUser(offFname,null,offLname).getOutput();
+	
+	if(inspUserObj != null)
+		{scheduleInspection("Summons Issuance",0, inspUserID);}
+	else{
+		comment("Inspector not found via GIS.  Inspection scheduled but not assigned to Inspector.");
+		scheduleInspection("Summons Issuance",0, inspUserID);
+		}
+
+logDebug("Script 186 END");
