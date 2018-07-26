37cd1eca771c0d12bce4c73ad7cef27fcc7d54cc
Main <jmain@auroragov.org>
2018-07-26 13:11:18 -0600

    Updated COA Script #34 to stop including built in variables for email template

--- INCLUDES_SCRIPTS/34_emailResubmittalPlanReview.js
+++ INCLUDES_SCRIPTS/34_emailResubmittalPlanReview.js
@@ -4,7 +4,7 @@
 logDebug("Starting Script... #34");
 
 if(wfTask=="Plan Review" && wfStatus=="Resubmittal Requested"){
-	var contact = "Applicant";
+	var contacttypes = "Applicant";
 	var template = "WAT_IRR_PLAN_RESUB";
 	//must inlcude an emailparams hashtable even if we don't use it.
 	//wfComment is NOT included as a built-in so we must add it if the email template requires this variable
@@ -14,10 +14,10 @@ if(wfTask=="Plan Review" && wfStatus=="Resubmittal Requested"){
 	if (aContact) fullName = aContact.getFullName() || aContact.getFirstName() + " " + aContact.getLastName();	
 	
 	var eParams = aa.util.newHashtable();
-	addParameter(eParams, "$$altid$$", cap.getCapModel().getAltID());
-	addParameter(eParams, "$$capAlias$$", cap.getCapType().getAlias());
-	addParameter(eParams, "$$FullName$$", cap.getCapStatus());
+	//addParameter(eParams, "$$altid$$", cap.getCapModel().getAltID());
+	//addParameter(eParams, "$$capAlias$$", cap.getCapType().getAlias());
+	//addParameter(eParams, "$$FullName$$", cap.getCapStatus());
 	addParameter(eParams, "$$wfComment$$", wfComment);
-	addParameter(eParams, "$$todayDate$$", sysDateMMDDYYYY);
-	emailContacts(contact, template, eParams, "", "", "N", "");
+	//addParameter(eParams, "$$todayDate$$", sysDateMMDDYYYY);
+	emailContacts(contacttypes, template, eParams, "", "", "N", "");
 }
