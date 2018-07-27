3689658aee90feab8a513f42d04dfae967e7f12e
Charles Munzer <cvm@byrnesoftware.com>
2018-07-27 15:23:42 -0500

    updated 250

--- INCLUDES_SCRIPTS/250_updateAssignedUserForTrafficEngRequest.js
+++ INCLUDES_SCRIPTS/250_updateAssignedUserForTrafficEngRequest.js
@@ -9,6 +9,7 @@ function script250_updateAssignedUserForTrafficEngRequest(){
 		var userName=assignedTo.split(" ");
 		var userObj = aa.person.getUser(userName[0],null,userName[1]).getOutput();
 		assignTask("Traffic Investigation",userObj.getUserID());
-		updateTaskDepartment("Traffic Investigation",userObj.getDeptOfUser());		
+		updateTaskDepartment("Traffic Investigation",userObj.getDeptOfUser());
+		editTaskSpecific("Traffic Investigation", "Assigned to Investigator", userObj.getUserID());	
 	}
 }	
