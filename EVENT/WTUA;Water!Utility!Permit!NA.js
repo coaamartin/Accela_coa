3223453e9c5bb668db124162963591b6ae177163
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-07-26 19:18:04 -0600

    Update WTUA;Water!Utility!Permit!NA.js
    script 401 added

--- EVENT/WTUA;Water!Utility!Permit!NA.js
+++ EVENT/WTUA;Water!Utility!Permit!NA.js
@@ -52,3 +52,8 @@ if(ifTracer(wfTask == "Final Acceptance Inspection" && wfStatus == "Warranty Wor
 	//Script 302
 	wtrScript302_warrantyReqdNotification();
 }
+
+/* Script 401 - moved from ASIUA to WTUA */
+if ("Water Main Utility Permit".equals(AInfo["Utility Permit Type"]) || "Private Fire Lines".equals(AInfo["Utility Permit Type"])) {
+	createTempWaterWetTapCopyDataAndSendEmail("WATER CREATE WET TAP TEMP RECORD #401");
+}
