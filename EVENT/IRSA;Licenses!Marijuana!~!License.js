eff4457fd294eec6c40e0e76cca584ee4259844a
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-07-31 15:39:20 -0700

    updated for scripts 231 and 232

--- EVENT/IRSA;Licenses!Marijuana!~!License.js
+++ EVENT/IRSA;Licenses!Marijuana!~!License.js
@@ -17,19 +17,8 @@ Sample Call:
 
 */
 
-//pre-existing code, probably needs to be removed once testing is concluded
-/*
-autoScheduleFailedInspectionsOrgScheduleDatePlusDays([ "MJ AMED Inspection", "MJ Building Inspection - Electrical", "MJ Building Inspection - Life Safety",
-		"MJ Building Inspection - Mechanical", "MJ Building Inspection - Plumbing", "MJ Building Inspection - Structural", "MJ Security Inspection - 3rd Party",
-		"MJ Zoning Inspection" ], "Failed", 7, "LIC MJ INSPECTION CORRECTION REPORT # 231", "WorkFlowTasksOverdue", rptParams);
-*/		
-
-//var rptParams = aa.util.newHashtable();
-//rptParams.put("altID", cap.getCapModel().getAltID());
-		
-failedMJInspectionAutomation([ "MJ AMED Inspection", "MJ Building Inspection - Electrical", "MJ Building Inspection - Life Safety",
-		"MJ Building Inspection - Mechanical", "MJ Building Inspection - Plumbing", "MJ Building Inspection - Structural", "MJ Security Inspection - 3rd Party",
-		"MJ Zoning Inspection" ], "Failed", 7, "LIC MJ INSPECTION CORRECTION REPORT # 231", "WorkFlowTasksOverdue");
+//check for failed MJ inspections
+failedMJInspectionAutomation();
 
 /* comment out for testing 		
 passedMJInspectionAutomation([ "MJ AMED Inspection", "MJ Building Inspection - Electrical", "MJ Building Inspection - Life Safety",
