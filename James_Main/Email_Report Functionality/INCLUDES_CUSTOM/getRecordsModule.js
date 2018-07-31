9f6006a820f62d50f22c56c19e497bd850fd882d
James Main <jmain@auroragov.org>
2018-07-31 16:21:22 -0600

    removed the james_main folder emmet put there

--- James_Main/Email_Report Functionality/INCLUDES_CUSTOM/getRecordsModule.js
+++
@@ -1,9 +0,0 @@
-function getRecordsModule(vCapId) {
-	var vCap = aa.cap.getCap(vCapId);
-	var vModule = "";
-	if (vCap.getSuccess()) {
-		vCap = vCap.getOutput();
-		vModule = vCap.getCapModel().getModuleName();
-	}
-	return vModule;
-}
