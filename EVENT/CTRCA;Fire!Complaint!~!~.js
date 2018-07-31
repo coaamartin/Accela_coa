aa150343f3140adb2c31e8137df7794516210911
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-07-31 13:50:22 -0600

    Add files via upload

--- EVENT/CTRCA;Fire!Complaint!~!~.js
+++ EVENT/CTRCA;Fire!Complaint!~!~.js
@@ -25,8 +25,9 @@ if (x && x.length > 0) {
 	var user = lookup("FIRE STATION", refUser);
 	if (user != null && user != "")
 	{
-		scheduleInspection("Fire Complaint",0, user);}
+		scheduleInspection("Fire Complaint",0, user);
 		assignCap(user);
+	}
 	else{
 		comment("Inspector not found via GIS.  Inspection scheduled but not assigned to Inspector.");
 		scheduleInspection("Fire Complaint",0);
