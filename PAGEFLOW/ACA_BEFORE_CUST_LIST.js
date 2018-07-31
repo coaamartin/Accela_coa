b2ab089fb29514fda5a7ba295c6c2abffab11fae
jantled <jal@wbai.com>
2018-07-31 14:52:15 -0500

    .

--- PAGEFLOW/ACA_BEFORE_CUST_LIST.js
+++ PAGEFLOW/ACA_BEFORE_CUST_LIST.js
@@ -79,7 +79,7 @@ var appTypeString = appTypeResult.toString();               // Convert applicati
 var appTypeArray = appTypeString.split("/");                // Array of application type string
 var AInfo = new Array();                        // Create array for tokenized variables
 var capId = null; // needed for next call
-//loadAppSpecific4ACA(AInfo);                       // Add AppSpecific Info
+loadAppSpecific4ACA(AInfo);                       // Add AppSpecific Info
 
 // page flow custom code begin
 
@@ -91,40 +91,42 @@ parcel = cap.getParcelModel();
 address = cap.getAddressModel();
 
 //Do any pageflow validation scripting for custom lists here for water
-if(appTypeArray[0] == 'Water'){
-    //Scripting for Water/Utility/Permit/NA
-    if(appTypeString == "Water/Utility/Permit/NA"){
-        var permitType = AInfo["Utility Permit Type"];
-        if(permitType == "Water Main Utility Permit"){
-            loadASITables4ACA();
-            if (typeof (WATERMATERIAL) == "object") {
-                for(x in WATERMATERIAL){
-                    var col1 = WATERMATERIAL[x]["Size of Pipe"];
-                    var col2 = WATERMATERIAL[x]["Pipe Material"];       
-                    var col3 = WATERMATERIAL[x]["Length in Lineal Feet"];
-                    
-                    logDebug("col1:" + col1 + ";col1.length():" + col1.length());
-                    logDebug("col2:" + col2 + ";col2.length():" + col2.length());
-                    logDebug("col3:" + col3 + ";col3.length():" + col3.length());
-                    if((col1.length() != 0) || (col2.length()!=0) || (col3.length()!=0)){
-                        cancel = false;
-                    }
-                    else
-                        message += "You must add at least 1 row in  WATER MATERIAL.";
-                }
-            }
-            else
-                message += "You must add at least 1 row in  WATER MATERIAL.";
-        }
-    }
-    //end Scripting for Water/Utility/Permit/NA
-    
-    showMessage = cancel = message.length ? true : false;
-    
-}//END scriting for water module
-
-// page flow custom code end
-
+//if(appTypeArray[0] == 'Water'){
+//    //Scripting for Water/Utility/Permit/NA
+//    if(appTypeString == "Water/Utility/Permit/NA"){
+//        var permitType = AInfo["Utility Permit Type"];
+//        if(permitType == "Water Main Utility Permit"){
+//            loadASITables4ACA();
+//            //if (typeof (WATERMATERIAL) == "object") {
+//            //    for(x in WATERMATERIAL){
+//            //        var col1 = WATERMATERIAL[x]["Size of Pipe"];
+//            //        var col2 = WATERMATERIAL[x]["Pipe Material"];       
+//            //        var col3 = WATERMATERIAL[x]["Length in Lineal Feet"];
+//            //        
+//            //        logDebug("col1:" + col1 + ";col1.length():" + col1.length());
+//            //        logDebug("col2:" + col2 + ";col2.length():" + col2.length());
+//            //        logDebug("col3:" + col3 + ";col3.length():" + col3.length());
+//            //        if((col1.length() != 0) || (col2.length()!=0) || (col3.length()!=0)){
+//            //            cancel = false;
+//            //        }
+//            //        else
+//            //            message += "You must add at least 1 row in  WATER MATERIAL.";
+//            //    }
+//            //}
+//            //else
+//            //    message += "You must add at least 1 row in  WATER MATERIAL.";
+//        }
+//    }
+//    //end Scripting for Water/Utility/Permit/NA
+//    message += "Test";
+//	showDebug = true;
+//    showMessage = cancel = message.length ? true : false;
+//    
+//}//END scriting for water module
+  message += "Test";
+  showDebug = true;// page flow custom code end
+  showMessage = cancel = message.length ? true : false;
+  aa.sendMail("jal@byrnesoftware.com", "jal@byrnesoftware.com", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 
 if (debug.indexOf("**ERROR") > 0) {
     aa.env.setValue("ErrorCode", "1");
