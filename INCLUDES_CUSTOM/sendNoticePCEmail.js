edf03970d790cbe102cd6f50a56e96c68c251177
John Schomp <john@grayquarter.com>
2018-08-03 14:15:45 -0700

    290

--- INCLUDES_CUSTOM/sendNoticePCEmail.js
+++ INCLUDES_CUSTOM/sendNoticePCEmail.js
@@ -1,6 +1,6 @@
 function sendNoticePCEmail(){
 //use the correct wftask name
-if (wfTask=="Prepare Signs and Notice - PC" && wfStatus=="Complete"){
+if (wfTask.indexOf("Prepare Signs and Notice") == 0 && wfStatus=="Complete"){
 	//Please use the correct email template name & report name
 	var emailTemplate="PLN PREPARE SIGNS #290";
 	var reportName="WorkFlowTasksOverdue";
