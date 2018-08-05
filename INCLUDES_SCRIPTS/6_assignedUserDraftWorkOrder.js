47f2e0dcc4d4155bca9e2abee826765e33c7c4d8
Suhail Wakil <suhailwakil@gmail.com>
2018-08-05 11:38:42 -0700

    s6

--- INCLUDES_SCRIPTS/6_assignedUserDraftWorkOrder.js
+++ INCLUDES_SCRIPTS/6_assignedUserDraftWorkOrder.js
@@ -2,7 +2,7 @@
 if ("Traffic Investigation".equals(wfTask) && "Draft Work Order".equals(wfStatus))
 {	
 	var userID = aa.env.getValue("CurrentUserID");
-	assignTask("Draft Workorder", userID);
+	assignTask("Draft Workorder", "SWAKIL");
 }
 
 
