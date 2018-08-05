f718738716cf405fafa1e685c60d2e354f3e5939
Suhail Wakil <suhailwakil@gmail.com>
2018-08-05 11:16:36 -0700

    script 6

--- INCLUDES_SCRIPTS/6_assignedUserDraftWorkOrder.js
+++ INCLUDES_SCRIPTS/6_assignedUserDraftWorkOrder.js
@@ -1,9 +1,5 @@
 //SWAKIL
-var uid = aa.env.getValue("CurrentUserID");
 if ("Traffic Investigation".equals(wfTask) && "Draft Work Order".equals(wfStatus))
 {	
-	assignTask("Draft Workorder",uid);
+	assignTask("Draft Workorder", currentUserID +"");
 }
-
-
-
