25d886cf930bcbba8a99d3ce52a9f27cefaaab9a
Charlie MUnzer <cvm@byrnesoftware.com>
2018-08-06 09:43:32 -0500

    updated 55

--- EVENT/IRSA;Forestry!~!~!~.js
+++ EVENT/IRSA;Forestry!~!~!~.js
@@ -1,6 +1,6 @@
 logDebug("Script 55 Starting");
 if (ifTracer(matchARecordType([
-      "​Forestry/Permit/NA/NA",
+      "Forestry/Permit/NA/NA",
       "Forestry/Request/Citizen/NA​"
   ], appTypeString), 'Record type matches.')) {
         include("55_AfterForestryFieldCrewInspection");
