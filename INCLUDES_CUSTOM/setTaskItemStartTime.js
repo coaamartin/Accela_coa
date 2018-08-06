c4d73266411a561bb244269a3f2f4185e213d223
jantled <jal@wbai.com>
2018-08-06 17:02:32 -0500

    .

--- INCLUDES_CUSTOM/setTaskItemStartTime.js
+++ INCLUDES_CUSTOM/setTaskItemStartTime.js
@@ -32,8 +32,7 @@ function setTaskItemStartTime(wfstr, dateStr){
             var processID = fTask.getProcessID();
             var completeFlag = fTask.getCompleteFlag();
 
-			//fTask.setStartTime(aa.util.now());
-			fTask.setStartTime(aa.util.parseDate(dateStr));
+			fTask.setStartTime(aa.util.now());
             /*if (useProcess) {
                 aa.workflow.adjustTask(itemCap, stepnumber, processID, "Y", "N", null, null)
             } else {
