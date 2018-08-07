451909b1abd77514a259ef48ee3bb172925ceebb
Charlie MUnzer <cvm@byrnesoftware.com>
2018-08-07 08:30:13 -0500

    update 24

---
+++ INCLUDES_CUSTOM/getGuideSheetItems.js
@@ -0,0 +1,48 @@
+ /* RETURNS AN ARRAY OF [ {com.accela.aa.inspection.guidesheet.GGuideSheetItemModel } ]
+     * OPTIONAL FILTERS SUCH AS inspId, guidesheetName, guideTypeName, guideItemName, guideItemValue
+    */
+function getGuideSheetItems(options) {
+    var settings = {
+        capId: capId,
+        inspId: null,    // filter by InspId - null for no filtering
+        guideTypeName: null,    // filter by guideTypeName - null for no filtering
+        guideItemName: null,  // filter by guideItemName (guideItemText) - null for no filtering
+        guideItemValue: null  // filter by itemValue (guideItemStatus) - null for no filtering
+    };
+    //optional params - overriding default settings
+    for (var attr in options) { settings[attr] = options[attr]; } 
+  
+    var returnItems = [],
+        inspections = [],
+        inspModel,
+        inspGuidesheets,
+        inspGuidesheetsArray = [],
+        inspGuidesheetItemsArray = [];
+
+    inspections = aa.inspection.getInspections(settings.capId).getOutput();
+
+    for (var inspKey in inspections) {
+        if (inspections[inspKey].getIdNumber() == settings.inspId || settings.inspId == null) {
+            inspModel = inspections[inspKey].getInspection();
+            inspGuidesheets = inspModel.getGuideSheets();
+
+            if (inspGuidesheets) {
+                inspGuidesheetsArray = inspGuidesheets.toArray();
+                for (var gsKey in inspGuidesheetsArray) {
+                    if(inspGuidesheetsArray[gsKey] == settings.guideTypeName || settings.guideTypeName == null) {
+                        var inspGuidesheetItemsArray = inspGuidesheetsArray[gsKey].getItems().toArray();
+                        for (var idx in inspGuidesheetItemsArray) {
+                            if(settings.guideItemName == inspGuidesheetItemsArray[idx].guideItemText
+                                || settings.guideItemValue == inspGuidesheetItemsArray[idx].guideItemStatus
+                                || (settings.guideItemName == null && settings.guideItemValue == null)
+                            ) {
+                                returnItems.push( inspGuidesheetItemsArray[idx]);
+                            }
+                        }
+                    }
+                } 
+            }
+        } 
+    }
+    return returnItems;
+}
