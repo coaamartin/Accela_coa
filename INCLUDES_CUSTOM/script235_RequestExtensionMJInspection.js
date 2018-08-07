9b3a066a7742ef36d9186dfbef0a5ccac84cb713
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-08-07 16:24:34 -0700

    added script 235

---
+++ INCLUDES_CUSTOM/script235_RequestExtensionMJInspection.js
@@ -0,0 +1,23 @@
+
+function requestExtensionMJInspection() {
+
+	// list MJ inspection types
+	var inspectionTypesAry = [ "MJ AMED Inspection", "MJ Building Inspection - Electrical", "MJ Building Inspection - Life Safety",
+		"MJ Building Inspection - Mechanical", "MJ Building Inspection - Plumbing", "MJ Building Inspection - Structural", "MJ Security Inspection - 3rd Party",
+		"MJ Zoning Inspection" ];
+		
+	//define number of days to schedule next inspection
+	var daysToAdd = 7;
+	
+	//check for extension request and schedule new inspection 7 days out
+	for (s in inspectionTypesAry) {
+		if (inspType == inspectionTypesAry[s] && inspResult == "Request for Extension") {	
+		
+		var inspComments = capId.getInspectionComments();		
+		var newInspSchedDate = dateAdd(inspResultDate, daysToAdd);
+		
+		scheduleInspectDate(inspType, newInspSchedDate, null, null, inspComments);	
+		
+		}	
+	}
+}
