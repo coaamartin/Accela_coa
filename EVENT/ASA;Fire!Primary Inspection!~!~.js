7eb38944968ed1763831ac1afe98f349390aa382
jbschillo <jbschillo@gmail.com>
2018-08-07 10:42:11 -0700

    Adding Event for Script 188

---
+++ EVENT/ASA;Fire!Primary Inspection!~!~.js
@@ -0,0 +1,5 @@
+
+
+// Begin script to set the Inspection Month to same month as submitted date Script 188
+include("188_FireUpdateInspectionMonth");
+// End script to set the Inspection Month to same month as submitted date Script 188
