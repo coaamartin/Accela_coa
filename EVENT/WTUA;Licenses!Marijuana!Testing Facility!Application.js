63c724f0567d19367acc298e7014064653a8c689
jbschillo <jbschillo@gmail.com>
2018-08-08 11:52:28 -0700

    first commit

---
+++ EVENT/WTUA;Licenses!Marijuana!Testing Facility!Application.js
@@ -0,0 +1,9 @@
+
+
+//create MJ Testing Facility License from Issued MJ Testing Facility application  script 228
+
+if (wfStatus.equals("Issued")){
+	
+	include("226_CreateMJTestingFacilityLicense");
+	
+}
