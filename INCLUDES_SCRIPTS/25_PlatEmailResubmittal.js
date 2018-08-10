f78efd2ad86cabeffe3d8dc346d4c5e4a96aaf17
James Main <jmain@auroragov.org>
2018-08-10 13:08:53 -0600

    updated swakil script #25 - made no real changes - just formatting and comment

--- INCLUDES_SCRIPTS/25_PlatEmailResubmittal.js
+++ INCLUDES_SCRIPTS/25_PlatEmailResubmittal.js
@@ -1,38 +1,50 @@
-if(isTaskActive("Plans Coordination")){
+//written by swakil
+
+if(isTaskActive("Plans Coordination"))
+{
   var checkTasks = ["Water Revenue Review","Parks Review","Water Dept Review","Easement Review","Real Property Review","Addressing Review","Engineering Review",
   "Life Safety Review","Planning Review","Traffic Review"];
   var statusCheck = "Resubmittal Requested";
 
   var isActive = isListActive(checkTasks);
-  if(!isActive){
+  if(!isActive)
+  {
     var listHasResubmittal = isResubmittal(checkTasks);
-    if(listHasResubmittal){
+    if(listHasResubmittal)
+	{
       sendEmailToApplicant();
     }
   }
 }
 
-function isListActive(checkTasks){
-  for(i in checkTasks){
+function isListActive(checkTasks)
+{
+  for(i in checkTasks)
+  {
     active = isTaskActive(checkTasks[i]);
-    if(active){
+    if(active)
+	{
       return true;
     }
   }
   return false;
 }
 
-function isResubmittal(){
-  for(i in checkTasks){
+function isResubmittal()
+{
+  for(i in checkTasks)
+  {
     status = taskStatus(checkTasks[i]);
-    if(status.equals(statusCheck)){
+    if(status.equals(statusCheck))
+	{
       return true;
     }
   }
   return false;
 }
 
-function sendEmailToApplicant(){
+function sendEmailToApplicant()
+{
   var contact = "Applicant";
   var template = "JD_TEST_TEMPLATE";
   var joke = "Where there's a will, there's a relative.";
