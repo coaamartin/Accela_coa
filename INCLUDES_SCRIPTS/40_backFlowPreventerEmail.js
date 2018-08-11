1d08086afd60a68c480d0ca0d9b8a28d6f8facf0
Suhail Wakil <suhailwakil@gmail.com>
2018-08-10 17:16:52 -0700

    updated script in INCLUDES_SCRIPTS and deleted same function in INCLUDES_CUSTOM as it was overriding it

--- INCLUDES_SCRIPTS/40_backFlowPreventerEmail.js
+++ INCLUDES_SCRIPTS/40_backFlowPreventerEmail.js
@@ -7,19 +7,12 @@ function script40_backFlowPreventerEmail()
 	logDebug("Started script40_backFlowPreventerEmail..");
 	if (wfTask == "Backflow Preventor" && wfStatus == "Final")
 	{
-		var applicant = getContactByType("Applicant", capId);
-		if (!applicant || !applicant.getEmail())
-		{
-			logDebug("**WARN no applicant found on or no email capId=" + capId);
-			return false;
-		}
-		var email = applicant.getEmail();
 		var emailtemplatename = "BACKFLOW PREVENTER NOTIFICATION";
 		var vEParams = aa.util.newHashtable();
 		var emailparams = aa.util.newHashtable();
 		emailparams = addStdVarsToEmail(vEParams, capId);
 		emailparams.put("$$wfDate$$", wfDate);
-		emailAsync(email, emailtemplatename, emailparams, "", "", "", "");
+		emailAsync("backflow@auroragov.org", emailtemplatename, emailparams, "", "", "", "");
 	}
 	logDebug("Ended #40_backFlowPreventerEmail..");
 };
