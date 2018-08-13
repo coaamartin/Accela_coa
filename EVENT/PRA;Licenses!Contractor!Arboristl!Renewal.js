36fb121a0c81009053e7862e4b4a2337d4f8d77c
John Schomp <john@grayquarter.com>
2018-08-12 21:00:27 -0700

    146 arborist licenses

---
+++ EVENT/PRA;Licenses!Contractor!Arboristl!Renewal.js
@@ -0,0 +1,64 @@
+/*
+Script xx
+JHS 8/12/2018
+
+update the workflow task "Renewal Intake" with a
+status of "Fees Paid" and update the workflow task
+"Licensing Issuance" with a status of "Renewed" and
+update the License record with a Status of Issued and
+new Expiration date (12/31 of current year)
+
+ */
+
+if (balanceDue == 0) {
+
+	closeTask("Renewal Intake", "Fees Paid", "Updated by PRA;Licenses!Arborist!General!Renewal", "");
+
+	closeTask("License Issuance", "Renewed", "Updated by PRA;Licenses!Arborist!General!Renewal", "");
+
+	// Begin script to complete the renewal and send notifications
+	var vLicenseID;
+	var vIDArray;
+	var renewalCapProject;
+	var vExpDate;
+	var vNewExpDate;
+	var vLicenseObj;
+
+	// Get the parent license
+	vLicenseID = getParentLicenseCapID(capId);
+	vIDArray = String(vLicenseID).split("-");
+	vLicenseID = aa.cap.getCapID(vIDArray[0], vIDArray[1], vIDArray[2]).getOutput();
+
+	if (vLicenseID != null) {
+		// Get current expiration date.
+		vLicenseObj = new licenseObject(null, vLicenseID);
+		vExpDate = new Date();
+		// All licenses expire on 12/31
+		vNewExpDate = new Date(vExpDate.getFullYear(), 11, 31);
+
+		// Update license expiration date
+		logDebug("Updating Expiration Date to: " + vNewExpDate);
+		vLicenseObj.setExpiration(dateAdd(vNewExpDate, 0));
+		// Set license record expiration to active
+		vLicenseObj.setStatus("Active");
+		// set parent record status to Issued
+		updateAppStatus("Issued", "Updated by PRA;Licenses!Contractor!Arborist!Renewal", vLicenseID);
+
+		//Set renewal to complete, used to prevent more than one renewal record for the same cycle
+		renewalCapProject = getRenewalCapByParentCapIDForIncomplete(vLicenseID);
+		if (renewalCapProject != null) {
+			renewalCapProject.setStatus("Complete");
+			aa.cap.updateProject(renewalCapProject);
+		}
+
+		var vEmailTemplate = "TBD LICENSE RENEWAL EMAIL";
+		var vEParams = aa.util.newHashtable();
+		addParameter(vEParams, "$$LicenseType$$", "Arborist Contractor License");
+		addParameter(vEParams, "$$ExpirationDate$$", dateAdd(vNewExpDate, 0));
+		addParameter(vEParams, "$$ApplicationID$$", vLicenseID.getCustomID());
+		addParameter(vEParams, "$$altID$$", vLicenseID.getCustomID());
+
+		emailContacts("All", vEmailTemplate, vEParams, null, null);
+
+	}
+}
--- EVENT/WTUA;Licenses!Contractor!Arborist!Application.js
+++ EVENT/WTUA;Licenses!Contractor!Arborist!Application.js
@@ -1,27 +1,4 @@
-/*
-Title : Create child arborist and business license (WorkflowTaskUpdateAfter)
-Purpose : When workflow task = "Issuance" and the workflow status = "Issued" then create child Arborist License, copy record Detail,
-create licensed profession(from Applicant data), copy any duplicate custom fields and email the applicant with the issued
-notification with the attached license report. (Email details will be provided by Aurora). In addition if there is no Business
-License listed as a child record then auto create the business license application as a temp record as well and copy the
-applicable data.
-(Report template & Email details will be provided by Aurora).
-Author: Haitham Eleisah
-
-Functional Area : Records
-
-Sample Call:
-createArboristLicenseAndCopyDataAndSendEmail("License Issuance", "Issued", "Licenses/Contractor/Arborist/License", "MESSAGE_NOTICE_PUBLIC WORKS", "WorkFlowTasksOverdue", rptParams);
-
- */
-
-
-
-if(ifTracer(wfTask == "License Issuance" && wfStatus == "Issued", 'wf:License Issuance/Issued')){
-	var emailTemplate = "FT ARBORIST LICENSE ISSUANCE #146";
-    var reportName = "JD_TEST_SSRS";
-    var LicenseType = "Licenses/Contractor/Arborist/License";
-    var rptParams = aa.util.newHashtable();
-    rptParams.put("Record_ID", cap.getCapModel().getAltID());
-    createArboristLicenseAndCopyDataAndSendEmail(LicenseType, emailTemplate, reportName, rptParams);
+// Script 146
+if ("License Issuance".equals(wfTask) && "Issued".equals(wfStatus)) {
+	include("146_CreateArboristLicenseAndLP");
 }
