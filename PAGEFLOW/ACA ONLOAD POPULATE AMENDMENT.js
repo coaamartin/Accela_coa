7d42d7c31332ecb83a2b480553d5ac90e0f596f0
grayquarter <john@grayquarter.com>
2018-08-15 00:34:25 -0700

    36

--- PAGEFLOW/ACA ONLOAD POPULATE AMENDMENT.js
+++ PAGEFLOW/ACA ONLOAD POPULATE AMENDMENT.js
@@ -311,24 +311,20 @@ function logDebug(dstr) {
 
 }
 
-function appMatch(ats) // optional capId or CapID string
+function appMatch(ats, matchCapParm) // optional capId or CapID string
 {
-	var matchArray = appTypeArray //default to current app
-		if (arguments.length == 2) {
-			matchCapParm = arguments[1]
-				if (typeof(matchCapParm) == "string")
-					matchCapId = aa.cap.getCapID(matchCapParm).getOutput(); // Cap ID to check
-				else
-					matchCapId = matchCapParm;
-				if (!matchCapId) {
-					logDebug("**WARNING: CapId passed to appMatch was not valid: " + arguments[1]);
-					return false
-				}
-				matchCap = aa.cap.getCap(matchCapId).getOutput();
-			matchArray = matchCap.getCapType().toString().split("/");
-		}
+	if (typeof(matchCapParm) == "string")
+		matchCapId = aa.cap.getCapID(matchCapParm).getOutput(); // Cap ID to check
+	else
+		matchCapId = matchCapParm;
+	if (!matchCapId) {
+		logDebug("**WARNING: CapId passed to appMatch was not valid: " + arguments[1]);
+		return false
+	}
+	matchCap = aa.cap.getCap(matchCapId).getOutput();
+	matchArray = matchCap.getCapType().toString().split("/");
 
-		var isMatch = true;
+	var isMatch = true;
 	var ata = ats.split("/");
 	if (ata.length != 4)
 		logDebug("**ERROR in appMatch.  The following Application Type String is incorrectly formatted: " + ats);
@@ -338,7 +334,6 @@ function appMatch(ats) // optional capId or CapID string
 				isMatch = false;
 	return isMatch;
 }
-
 function copyAdditionalInfo(srcCapId, targetCapId) {
 	//1. Get Additional Information with source CAPID.  (BValuatnScriptModel)
 	var additionalInfo = getAdditionalInfo(srcCapId);
