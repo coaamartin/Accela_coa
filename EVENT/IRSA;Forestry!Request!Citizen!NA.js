07e13ac7a1cc2a31a09495fee023c67615f60823
Charlie M <cvm@byrnesoftware.com>
2018-08-16 15:29:53 -0500

    updated 62

--- EVENT/IRSA;Forestry!Request!Citizen!NA.js
+++ EVENT/IRSA;Forestry!Request!Citizen!NA.js
@@ -1,2 +1,9 @@
 logDebug("Script 27 Starting");
 include("24_ForestryInspectionResultAutomation");
+
+
+logDebug("Script 62 Starting");
+if (ifTracer(inspType == "Forestry Inspection" && matches(inspResult, "PR1", "PR2", "PR20"), 'inspType == "Forestry Inspection" && matches(inspResult, "PR1", "PR2", "PR20"')) { 
+    closeTask("Inspection Phase", "Complete", "", "");      
+    activateTask("Crew Work");
+}
