26fb23877975559b17b4e0b1d97cf24666a8bf83
grayquarter <john@grayquarter.com>
2018-08-16 22:54:34 -0700

    36

--- EVENT/CTRCA;P​ublicWorks!~!~!~.js
+++
@@ -1,9 +0,0 @@
-//Begin script to link amendment to application when user Defers Payment in ACA, see script 36
-var vParentId;
-vParentId = "" + aa.env.getValue("ParentCapID");
-if (vParentId == false || vParentId == null || vParentId == "") {
-	var vAppId = getAppSpecific("Application ID");
-	if (vAppId != "" && vAppId != null) {
-		addParent(vAppId);
-	}
-}
