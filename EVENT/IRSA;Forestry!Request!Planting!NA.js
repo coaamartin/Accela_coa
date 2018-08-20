af6d488c0123e732234fe0a44b2e42a16b5f8f0a
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-08-20 10:49:23 -0700

    Updates for Script 379

--- EVENT/IRSA;Forestry!Request!Planting!NA.js
+++ EVENT/IRSA;Forestry!Request!Planting!NA.js
@@ -11,7 +11,7 @@ Author: Yazan Barghouth
 Functional Area : Records
 
 Sample Call:
-	stumpGrindInspectionScheduling("Non-warranty Inspection", [ "Passed" ], "Grind Stump", "Priority 1 Stump Grind", "Priority 1 Stump Grind", "Priority 2 Stump Grind","FORESTRY>NA>NA>NA>NA>FT_FC");
+	stumpGrindInspectionScheduling("Non-warranty Inspection", [ "Passed" ], "Grind Stump", "Priority 1 Stump Grind?", "Priority 1 Stump Grind", "Priority 2 Stump Grind","FORESTRY>NA>NA>NA>NA>FT_FC");
 	
 Notes:
 	- Inspection correct result is Passed (Complete not found)
