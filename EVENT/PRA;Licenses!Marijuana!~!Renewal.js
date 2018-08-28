ebef11261871d973ef6a64f69394bf6a9306e34f
Don Bates <39094801+dbates99@users.noreply.github.com>
2018-08-28 16:34:10 -0300

    Add files via upload

---
+++ EVENT/PRA;Licenses!Marijuana!~!Renewal.js
@@ -0,0 +1,42 @@
+
+// Renewal script
+if (balanceDue == 0) {
+
+
+	closeTask("License Issuance", "Renewed", "Updated by PRA;Licenses!Professional!General!Renewal", "");
+
+	// Begin script to complete the renewal and send notifications
+	var vLicenseID;
+	var vIDArray;
+	var renewalCapProject;
+	var vExpDate;
+	var vNewExpDate;
+	var vLicenseObj;
+
+	// Get the parent license
+	vLicenseID = getParentLicenseCapID(capId);
+	vIDArray = String(vLicenseID).split("-");
+	vLicenseID = aa.cap.getCapID(vIDArray[0], vIDArray[1], vIDArray[2]).getOutput();
+
+	if (vLicenseID != null) {
+		//Activate the license records expiration cycle
+		vLicenseObj = new licenseObject(null, vLicenseID);
+		vLicenseObj.setStatus("Active");
+		thisLicExpOb = vLicenseObj.b1Exp
+		expUnit = thisLicExpOb.getExpUnit()
+		expInt = thisLicExpOb.getExpInterval()
+		if (expUnit == "MONTHS") {
+			newExpDate = dateAddMonths(null, expInt);
+			} 
+		vLicenseObj.setExpiration(newExpDate);
+		updateAppStatus("Issued","Updated by Script",vLicenseID);
+		
+		//Set renewal to complete, used to prevent more than one renewal record for the same cycle
+		renewalCapProject = getRenewalCapByParentCapIDForIncomplete(vLicenseID);
+		if (renewalCapProject != null) {
+			renewalCapProject.setStatus("Complete");
+			aa.cap.updateProject(renewalCapProject);
+		}
+
+	}
+}
