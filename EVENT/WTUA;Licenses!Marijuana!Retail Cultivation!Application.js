f0a61ea04a9db2e71489b813bb725c0b0c2b946a
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-08-29 13:55:57 -0700

    Updates to Scripts 226 - 230

--- EVENT/WTUA;Licenses!Marijuana!Retail Cultivation!Application.js
+++ EVENT/WTUA;Licenses!Marijuana!Retail Cultivation!Application.js
@@ -4,14 +4,14 @@ if(ifTracer(wfTask == "License Issuance" && wfStatus == "Issued", 'wf:License Is
     if(newLicCapId){
         sendMJLicEmail(newLicCapId);
         
-        scheduleInspectionWithCapIdBusinessDays("MJ AMED Inspection", 55, "DALLEN", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Plumbing", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Electrical", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Mechanical", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Life Safety", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Security Inspections - 3rd Party", 55, "DALLEN", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Structural", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Zoning Inspections", 55, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ AMED Inspection", 84, "DALLEN", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Plumbing", 84, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Electrical", 84, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Mechanical", 84, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Life Safety", 84, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Security Inspections - 3rd Party", 84, "DALLEN", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Structural", 84, "SLCLARK", " ", "Scheduled by Script 229", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Zoning Inspections", 84, "DALLEN", " ", "Scheduled by Script 229", newLicCapId);
     }
     //END Script 230
 }
--- EVENT/WTUA;Licenses!Marijuana!Retail Store!Application.js
+++ EVENT/WTUA;Licenses!Marijuana!Retail Store!Application.js
@@ -4,13 +4,14 @@ if(ifTracer(wfTask == "License Issuance" && wfStatus == "Issued", 'wf:License Is
     if(newLicCapId){
         sendMJLicEmail(newLicCapId);
         
-        scheduleInspectionWithCapIdBusinessDays("MJ AMED Inspection", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Plumbing", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Electrical", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Mechanical", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Life Safety", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Security Inspections - 3rd Party", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
-        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Structural", 55, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ AMED Inspection", 84, "DALLEN", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Plumbing", 84, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Electrical", 84, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Mechanical", 84, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Life Safety", 84, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Security Inspections - 3rd Party", 84, "DALLEN", " ", "Scheduled by Script 228", newLicCapId);
+        scheduleInspectionWithCapIdBusinessDays("MJ Building Inspections - Structural", 84, "SLCLARK", " ", "Scheduled by Script 228", newLicCapId);
+		scheduleInspectionWithCapIdBusinessDays("MJ Zoning Inspections", 84, "DALLEN", " ", "Scheduled by Script 228", newLicCapId);
     }
     //END Script 228
 }
