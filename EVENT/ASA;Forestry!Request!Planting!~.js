e86a69d65f7be62e2f90c8bb744469b8088a0a52
Don Bates <39094801+dbates99@users.noreply.github.com>
2018-08-30 13:29:09 -0300

    x

--- EVENT/ASA;Forestry!Request!Planting!~.js
+++ EVENT/ASA;Forestry!Request!Planting!~.js
@@ -1,11 +1,1 @@
-//Populate GIS objects
-var gisIdForLayer = "";
-var gisObjResult = aa.gis.getCapGISObjects(capId); // get gis objects on the cap
-if (gisObjResult.getSuccess()) 	
-	var fGisObj = gisObjResult.getOutput();
-else
-	{ logDebug("**WARNING: Getting GIS objects for CAP.  Reason is: " + gisObjResult.getErrorType() + ":" + gisObjResult.getErrorMessage()); }
-
-//Script 198
-showDebug = true
-treeInventoryPopulate()
+//
