5426b67f6ada185090853691174f19e452ece74b
Don Bates <39094801+dbates99@users.noreply.github.com>
2018-08-30 12:35:00 -0300

    x

--- INCLUDES_CUSTOM/treeInventoryPopulate.js
+++ INCLUDES_CUSTOM/treeInventoryPopulate.js
@@ -18,6 +18,7 @@ function treeInventoryPopulate()
 		arrGIS = getGISBufferInfo("AURORACO","Trees","100","TREE_ID_NO","ADDRESS")
 		logDebug("arrGIS Length " + arrGIS.length)
 		for(x in arrGIS){
+			newRow = new Array();
 			var thisGIS = arrGIS[x];
 			var treeIdNo = thisGIS["TREE_ID_NO"];
 			var manUnit = thisGIS["MAN_UNIT"];
@@ -27,27 +28,20 @@ function treeInventoryPopulate()
 			if(TreeAdd == capAddress)
 				{
 				logDebug("Tree " + treeIdNo + " has an address match TreeAdd " + TreeAdd + " = capAddress " + capAddress)
-				//if (!doesASITRowExist("TREE INFORMATION", "Tree ID", treeIdNo)) {
-					newRow = new Array();
-					newRow["Tree ID"] = new asiTableValObj("Tree ID", treeIdNo, "N");	
-					if (manUnit && manUnit != "")
-						newRow["Management Unit"] = new asiTableValObj("Management Unit", manUnit, "N");
-					else 
-						newRow["Management Unit"] = new asiTableValObj("Management Unit", "", "N");
-					if (diameter && diameter != "")
-						newRow["Existing Diameter"] = new asiTableValObj("Existing Diameter", diameter, "N");
-					else 
-						newRow["Existing Diameter"] = new asiTableValObj("Existing Diameter", "", "N");
-					if (species && species != "")
-						newRow["Species"] = new asiTableValObj("Species", species, "N");
-					else 
-						newRow["Species"] = new asiTableValObj("Species", "", "N");
-					addToASITable("TREE INFORMATION", newRow);
-					//}
-				}
-			else
-				{
-				logDebug("Tree " + treeIdNo + " has NO address match TreeAdd " + TreeAdd + " = capAddress " + capAddress)
+				newRow["Tree ID"] = new asiTableValObj("Tree ID", treeIdNo, "N");	
+				if (manUnit && manUnit != "")
+					{newRow["Management Unit"] = new asiTableValObj("Management Unit", manUnit, "N");}
+				else 
+					{newRow["Management Unit"] = new asiTableValObj("Management Unit", "", "N");}
+				if (diameter && diameter != "")
+					{newRow["Existing Diameter"] = new asiTableValObj("Existing Diameter", diameter, "N");}
+				else 
+					{newRow["Existing Diameter"] = new asiTableValObj("Existing Diameter", "", "N");}
+				if (species && species != "")
+					{newRow["Species"] = new asiTableValObj("Species", species, "N");}
+				else 
+					{newRow["Species"] = new asiTableValObj("Species", "", "N");}
+				addToASITable("TREE INFORMATION", newRow);
 				}
 			}
 		}
