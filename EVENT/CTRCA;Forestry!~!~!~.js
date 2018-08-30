ca146f2e09038da6f0a4b20162ec595496f1308c
Don Bates <39094801+dbates99@users.noreply.github.com>
2018-08-30 13:34:40 -0300

    x

--- EVENT/CTRCA;Forestry!~!~!~.js
+++ EVENT/CTRCA;Forestry!~!~!~.js
@@ -60,3 +60,13 @@ if (publicUser) {
 }
 //End Script 60 - User story 3
 
+//Populate GIS objects
+var gisIdForLayer = "";
+var gisObjResult = aa.gis.getCapGISObjects(capId); // get gis objects on the cap
+if (gisObjResult.getSuccess()) 	
+	var fGisObj = gisObjResult.getOutput();
+else
+	{ logDebug("**WARNING: Getting GIS objects for CAP.  Reason is: " + gisObjResult.getErrorType() + ":" + gisObjResult.getErrorMessage()); }
+
+//Script 198
+treeInventoryPopulate()
