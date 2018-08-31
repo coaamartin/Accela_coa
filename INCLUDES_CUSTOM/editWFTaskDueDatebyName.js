0562d61aaa31c447abe52de82e7a5f6ff36c637d
jantled <jal@wbai.com>
2018-08-31 14:15:38 -0500

    script 192

--- INCLUDES_CUSTOM/editWFTaskDueDatebyName.js
+++ INCLUDES_CUSTOM/editWFTaskDueDatebyName.js
@@ -11,11 +11,11 @@ function editWFTaskDueDatebyName(sName,stdTimeline){
 			var fTask = wfObj[i];
 			if ((fTask.getTaskDescription().toUpperCase().indexOf(sName.toUpperCase())> 0)) {
 				// the new due date value could be changed according to the returned value from the standard choice, for now assuming the value type "Date"
-				wfObj[i].setDueDate(aa.date.parseDate(dateAdd(null,stdTimeline)));
+				wfObj[i].setDueDate(aa.date.parseDate(dateAdd(null,stdTimeline, true)));
 				var fTaskModel = wfObj[i].getTaskItem();
 				var tResult = aa.workflow.adjustTaskWithNoAudit(fTaskModel);
 				if (tResult.getSuccess())
-					logDebug("Set Workflow Task: " + fTask.getTaskDescription() + " due Date " + aa.date.parseDate(dateAdd(null,stdTimeline)));
+					logDebug("Set Workflow Task: " + fTask.getTaskDescription() + " due Date " + aa.date.parseDate(dateAdd(null,stdTimeline, true)));
 				else {
 					logMessage("**ERROR: Failed to update due date on workflow: " + tResult.getErrorMessage());
 					return false;
