50f793a4b0b4b64fc8908ac8c5219cb6daf4f419
Don Bates <39094801+dbates99@users.noreply.github.com>
2018-08-31 14:51:43 -0300

    x

--- EVENT/IRSA;Forestry!Request!Citizen!NA.js
+++ EVENT/IRSA;Forestry!Request!Citizen!NA.js
@@ -9,27 +9,7 @@ if (ifTracer(inspType == "Forestry Inspection" && matches(inspResult, "PR1", "PR
 
 // Start Script 27 Create Planting Record
 if ("Forestry Inspection".equals(inspType) && matches(inspResult, "Other", "PR1", "PR2", "PR20")) {
-	var createPlanting = false;
-	var fgs = getGuideSheetObjects(inspId);
-	if (fgs) {
-		for (var gsitems in fgs) {
-			var fgsi = fgs[gsitems]; // guidesheet item
-			if ("Removal".equals(fgsi.text) && "Yes".equals(fgsi.status)) {
-				createPlanting = true;
-				break;
-			}
-		}
-	}
-	if (createPlanting) {
-		vPlantRecId = createChild("Forestry", "Request", "Planting", "NA");
-		copyAddress(capId, vPlantRecId);
-		copyParcels(capId, vPlantRecId);
-		copyASIInfo(capId, vPlantRecId);
-		copyASITables(capId, vPlantRecId);
-		copyContacts3_0(capId, vPlantRecId);
-		aa.cap.copyCapWorkDesInfo(capId, vPlantRecId);
-		editAppName(getAppName(capId), vPlantRecId);
-	}
+	treeCreatePlantingRecordReplantFromInsp(inspId)
 }
 // End Script 27 Create Planting Record
 
