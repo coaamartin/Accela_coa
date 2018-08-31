45c8a4f223aa40ac2a9127d306577a5d786eada3
jantled <jal@wbai.com>
2018-08-31 16:35:15 -0500

    .

--- INCLUDES_CUSTOM/editWFTaskDueDateOnPlanReviewTimelines.js
+++ INCLUDES_CUSTOM/editWFTaskDueDateOnPlanReviewTimelines.js
@@ -5,34 +5,34 @@ function editWFTaskDueDateOnPlanReviewTimelines() {
     var submittalNum = AInfo["Submittal Number"];
     var sigPlanSheets = AInfo["Signature Plan Sheets"];
     var verticalConst = AInfo["Vertical Construction/Short Review"];
-    if(civilPlanSheets) civilPlanSheets = parseInt(civilPlanSheets);
-    else civilPlanSheets = 0;
-        
-    if(sigPlanSheets) sigPlanSheets = parseInt(sigPlanSheets);
-    else sigPlanSheets = 0;
+    //if(civilPlanSheets) civilPlanSheets = parseInt(civilPlanSheets);
+    //else civilPlanSheets = 0;
+    //    
+    //if(sigPlanSheets) sigPlanSheets = parseInt(sigPlanSheets);
+    //else sigPlanSheets = 0;
     
-    if(appMatch("PublicWorks/Civil Plan/Review/NA")){
-        //if(submittalNum){
-        //    submittalNum = parseInt(submittalNum);
-        //    if(ifTracer(submittalNum == 1, 'submittalNum == 1')){
-        //        if(ifTracer(civilPlanSheets <= 39, 'civilPlanSheets <= 39')) stdTimeline = 15;
-        //        if(ifTracer(civilPlanSheets >= 40 && civilPlanSheets <= 59, 'civilPlanSheets >= 40 && civilPlanSheets <= 59')) stdTimeline = 20;
-        //        if(ifTracer(civilPlanSheets >= 60, 'civilPlanSheets >= 60')) stdTimeline = 25;
-        //    }
-        //    if(ifTracer(submittalNum == 2, 'submittalNum == 2')){
-        //        if(ifTracer(civilPlanSheets <= 39, 'civilPlanSheets <= 39')) stdTimeline = 10;
-        //        if(ifTracer(civilPlanSheets >= 40 && civilPlanSheets <= 59, 'civilPlanSheets >= 40 && civilPlanSheets <= 59')) stdTimeline = 15;
-        //        if(ifTracer(civilPlanSheets >= 60, 'civilPlanSheets >= 60')) stdTimeline = 20;
-        //    
-        //    }
-        //    if(ifTracer(submittalNum >= 3, 'submittalNum == 3')){
-        //        if(ifTracer(sigPlanSheets <= 39, 'sigPlanSheets <= 39')) stdTimeline = 5;
-        //        if(ifTracer(sigPlanSheets >= 40 && sigPlanSheets <= 59, 'sigPlanSheets >= 40 && sigPlanSheets <= 59')) stdTimeline = 10;
-        //        if(ifTracer(sigPlanSheets >= 60, 'sigPlanSheets >= 60')) stdTimeline = 10;
-        //    }
-        //}
+    /*if(ifTracer(appMatch("PublicWorks/Civil Plan/Review/NA"), 'Civil Plan Review')){
+        if(submittalNum){
+            submittalNum = parseInt(submittalNum);
+            if(ifTracer(submittalNum == 1, 'submittalNum == 1')){
+                if(ifTracer(civilPlanSheets <= 39, 'civilPlanSheets <= 39')) stdTimeline = 15;
+                if(ifTracer(civilPlanSheets >= 40 && civilPlanSheets <= 59, 'civilPlanSheets >= 40 && civilPlanSheets <= 59')) stdTimeline = 20;
+                if(ifTracer(civilPlanSheets >= 60, 'civilPlanSheets >= 60')) stdTimeline = 25;
+            }
+            if(ifTracer(submittalNum == 2, 'submittalNum == 2')){
+                if(ifTracer(civilPlanSheets <= 39, 'civilPlanSheets <= 39')) stdTimeline = 10;
+                if(ifTracer(civilPlanSheets >= 40 && civilPlanSheets <= 59, 'civilPlanSheets >= 40 && civilPlanSheets <= 59')) stdTimeline = 15;
+                if(ifTracer(civilPlanSheets >= 60, 'civilPlanSheets >= 60')) stdTimeline = 20;
+            
+            }
+            if(ifTracer(submittalNum >= 3, 'submittalNum == 3')){
+                if(ifTracer(sigPlanSheets <= 39, 'sigPlanSheets <= 39')) stdTimeline = 5;
+                if(ifTracer(sigPlanSheets >= 40 && sigPlanSheets <= 59, 'sigPlanSheets >= 40 && sigPlanSheets <= 59')) stdTimeline = 10;
+                if(ifTracer(sigPlanSheets >= 60, 'sigPlanSheets >= 60')) stdTimeline = 10;
+            }
+        }
     }
-    /*if(ifTracer(appMatch("PublicWorks/Civil Plan/Revision/NA"), 'Civil Plan Revision')){
+    if(ifTracer(appMatch("PublicWorks/Civil Plan/Revision/NA"), 'Civil Plan Revision')){
         if(submittalNum){
             submittalNum = parseInt(submittalNum);
             if(ifTracer(submittalNum == 1, 'submittalNum == 1')){
