714fb9dda41121cb7577f6365a6e58c7d3f3b9b3
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-05 13:43:16 -0700

    Updates to Script 232

--- INCLUDES_CUSTOM/script232_PassedMJInspectionAutomation.js
+++ INCLUDES_CUSTOM/script232_PassedMJInspectionAutomation.js
@@ -72,7 +72,7 @@ function passedMJInspectionAutomation() {
 				addParameter(reportParams, "InspActNumber", inspId);
 				
 				//send email with report attachment		
-				emailContactsWithReportLinkASync("Inspection Contact", emailTemplate, eParams, "", "", "N", "");
+				emailContactsWithReportLinkASync("Inspection Contact", emailTemplate, eParams, null, "", "N", "");
 				return true;
 			}			
 		}
