ed0cc544d911d7da648c2c8bb27acd4bbd2e70b8
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-05 12:20:52 -0700

    Updates for Script 60

--- EVENT/ASA;Forestry!~!~!~.js
+++ EVENT/ASA;Forestry!~!~!~.js
@@ -70,6 +70,14 @@ include("60_ForestryInspectionAssignment");
 
 //End Script 60 - User story 3
 
+//Populate GIS objects
+var gisIdForLayer = "";
+var gisObjResult = aa.gis.getCapGISObjects(capId); // get gis objects on the cap
+if (gisObjResult.getSuccess()) 	
+	var fGisObj = gisObjResult.getOutput();
+else
+	{ logDebug("**WARNING: Getting GIS objects for CAP.  Reason is: " + gisObjResult.getErrorType() + ":" + gisObjResult.getErrorMessage()); }
+
 
 //Script 198
 treeInventoryPopulate()
