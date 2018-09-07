c5e3ebd03c257985c581061a2f450e1c00ab995d
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-07 09:05:48 -0700

    Updates for Script 60

--- INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
+++ INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
@@ -15,32 +15,30 @@ function getPrimaryParcelAttributesAndUpdateCustomField(stdForestryInspectorAssi
 		editAppSpecific("Area Number", vArea); 
 	}
 */
-			
-    // Get GIS Information
-	var vArea = getGISInfoArray("AURORACO", "Forestry Index Mapbook Poly", "TRS_NO");
-	logDebug("Area: " + vArea);
-	//Save to ASI field
-	editAppSpecific("Area Number", vArea);
-	
 	var capParcelResult = aa.parcel.getParcelByCapId(capId, null);
     if (capParcelResult.getSuccess()) {
         var arrParcels = capParcelResult.getOutput().toArray();
         for (var p in arrParcels) {
             var isPrimaryParcel = arrParcels[p].isPrimaryParcel();
             if (isPrimaryParcel) {
-                var township = arrParcels[p].getTownship();
-                var range = arrParcels[p].getRange();
-                var section = arrParcels[p].getSection();
-                var areaNumberValue = township + range +  "0" + section ;
+                //var township = arrParcels[p].getTownship();
+                //var range = arrParcels[p].getRange();
+                //var section = arrParcels[p].getSection();
+                //var areaNumberValue = township + range +  "0" + section ;
                
                 var areaNo = arrParcels[p].getInspectionDistrict();
                 var trs = township + range + section;
-                if ((typeof (areaNo) != "undefined" && areaNo != null && areaNo != "") || (typeof (trs) != "undefined" && trs != null && trs != ""))
+                if ((typeof (areaNo) != "undefined" && areaNo != null && areaNo != "") || (typeof (trs) != "undefined" && trs != null && trs != "")) {
                     mapAreaTRSAssignmentInspectors(areaNo, trs, stdForestryInspectorAssignments);
+					
+					// Get GIS Information
+					var vArea = getGISInfo("AURORACO", "Forestry Index Mapbook Poly", "TRS_NO");
+					logDebug("Area: " + vArea);
+					//Save to ASI field
+					editAppSpecific("Area Number", vArea);
+				}
             }
         }
     }
 }
-
-      
    
