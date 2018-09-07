7f46dc090a5fd0dda33d06a413b19b15e01dbcf8
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-07 09:21:42 -0700

    Updates for Script 60

--- INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
+++ INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
@@ -32,10 +32,10 @@ function getPrimaryParcelAttributesAndUpdateCustomField(stdForestryInspectorAssi
                     mapAreaTRSAssignmentInspectors(areaNo, trs, stdForestryInspectorAssignments);
 					
 					// Get GIS Information
-					var vArea = getGISInfo("AURORACO", "Forestry Index Mapbook Poly", "TRS_NO");
-					logDebug("Area: " + vArea);
+					//var vArea = getGISInfo("AURORACO", "Forestry Index Mapbook Poly", "TRS_NO");
+					logDebug("Area: " + areaNo);
 					//Save to ASI field
-					editAppSpecific("Area Number", vArea);
+					editAppSpecific("Area Number", areaNo);
 				}
             }
         }
