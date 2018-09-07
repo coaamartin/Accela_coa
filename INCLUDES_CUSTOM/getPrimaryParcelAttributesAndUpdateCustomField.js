ecdf4aa96e14df76a13aab8f18a80d49980674b0
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-07 08:57:38 -0700

    Updates for Script 60

--- INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
+++ INCLUDES_CUSTOM/getPrimaryParcelAttributesAndUpdateCustomField.js
@@ -1,20 +1,28 @@
 
 function getPrimaryParcelAttributesAndUpdateCustomField(stdForestryInspectorAssignments) {
     
+/* commented out, vArea is not as complex
+	var vTownship = getGISInfoArray("AURORACO", "Parcels", "PARCEL_JURISDICTION");
+	var vRange = getGISInfoArray("AURORACO", "Parcels", "SHAPE.area");
+	var vSection = getGISInfoArray("AURORACO", "Parcels", "SHAPE.len");
+	var vArea;
+	//  Assume only one return
+	if (vTownship.length > 0 && vRange.length > 0 && vSection.length > 0) {
+		// Format Data
+		vArea = vTownship[0] + " " + vRange[0] + " " + "0" + " " + vSection[0];
+		logDebug("Area: " + vArea);
+		//Save to ASI field
+		editAppSpecific("Area Number", vArea); 
+	}
+*/
+			
     // Get GIS Information
-            var vTownship = getGISInfoArray("AURORACO", "Parcels", "PARCEL_JURISDICTION");
-            var vRange = getGISInfoArray("AURORACO", "Parcels", "SHAPE.area");
-            var vSection = getGISInfoArray("AURORACO", "Parcels", "SHAPE.len");
-            var vArea;
-            //  Assume only one return
-            if (vTownship.length > 0 && vRange.length > 0 && vSection.length > 0) {
-                // Format Data
-                vArea = vTownship[0] + " " + vRange[0] + " " + "0" + " " + vSection[0];
-                logDebug("Area: " + vArea);
-                //Save to ASI field
-                editAppSpecific("Area Number", vArea); 
-            }
-    var capParcelResult = aa.parcel.getParcelByCapId(capId, null);
+	var vArea = getGISInfoArray("AURORACO", "Forestry Index Mapbook Poly", "TRS_NO");
+	logDebug("Area: " + vArea);
+	//Save to ASI field
+	editAppSpecific("Area Number", vArea);
+	
+	var capParcelResult = aa.parcel.getParcelByCapId(capId, null);
     if (capParcelResult.getSuccess()) {
         var arrParcels = capParcelResult.getOutput().toArray();
         for (var p in arrParcels) {
