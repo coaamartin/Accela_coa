9746655a463b3949625265dfcd7015e1de8e405c
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-10 15:14:43 -0700

    Updates to Core Renewal ASA

--- INCLUDES_SCRIPTS/427_Core_Renewal_ASA.js
+++ INCLUDES_SCRIPTS/427_Core_Renewal_ASA.js
@@ -35,17 +35,14 @@ if (parentCapId != null && vGoodToRenew) {
 	aa.cap.copyCapWorkDesInfo(parentCapId,capId);
 
 	//Copy application name from license to renewal
-	//editAppName(getAppName(parentCapId),capId);
+	editAppName(getAppName(parentCapId),capId);
 	
-	var vAppName = getAppName(parentCapId);
-	editAppName(vAppName, capId);
+	//var vAppName = getAppName(parentCapId);
+	//editAppName(vAppName, capId);
 	
-	if(appMatch("Licenses/Marijuana/*/License") == true); {
-		var vStateLicNum = getAppSpecific("State License Number", parentCapId);
-		editAppSpecific("State License Number", vStateLicNum, capId);
-	}
-	
-	//Copy Cap Detail Info from license to renewal
-	aa.cap.copyCapDetailInfo(parentCapId, capId);
+	//if(appMatch("Licenses/Marijuana/*/License") == true); {
+	//	var vStateLicNum = getAppSpecific("State License Number", parentCapId);
+	//	editAppSpecific("State License Number", vStateLicNum, capId);
+	//}
 }
 //End Core Renewal Functionality
