e6d2414f05aef3e62d26b9636b734dd469d87d29
evontrapp <36080657+evontrapp@users.noreply.github.com>
2018-09-10 15:12:52 -0700

    Updates to Core Renewal ASA

---
+++ 427_Core_Renewal_ASA.js
@@ -0,0 +1,48 @@
+//Start Core Renewal Functionality
+if (parentCapId == "undefined" || parentCapId == null) {
+	parentCapId = aa.env.getValue("ParentCapID");
+}
+
+var vGoodToRenew;
+var vOrgCapId;
+
+//Setup/Check renewal
+vGoodToRenew = prepareRenewal();
+if (parentCapId != null && vGoodToRenew) {
+		
+	//Copy Parcels from license to renewal
+	copyParcels(parentCapId,capId);
+	
+	//Copy addresses from license to renewal
+	copyAddress(parentCapId,capId);
+	
+	//copy ASI Info from license to renewal
+	copyASIInfo(parentCapId,capId);
+
+	//Copy ASIT from license to renewal
+	copyASITables(parentCapId,capId);
+
+	// Copy LP
+	copyLicensedProf(parentCapId, capId);
+	
+	//Copy Contacts from license to renewal
+	copyContacts3_0(parentCapId,capId);
+	
+	//Copy Owner from license to renewal
+	copyOwner(parentCapId,capId);
+	
+	//Copy Work Description from license to renewal
+	aa.cap.copyCapWorkDesInfo(parentCapId,capId);
+
+	//Copy application name from license to renewal
+	editAppName(getAppName(parentCapId),capId);
+	
+	//var vAppName = getAppName(parentCapId);
+	//editAppName(vAppName, capId);
+	
+	//if(appMatch("Licenses/Marijuana/*/License") == true); {
+	//	var vStateLicNum = getAppSpecific("State License Number", parentCapId);
+	//	editAppSpecific("State License Number", vStateLicNum, capId);
+	//}
+}
+//End Core Renewal Functionality
