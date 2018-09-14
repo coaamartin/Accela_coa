e84ecd7eacd2e44a0094008cb692da5b2267b1f6
jantled <jal@wbai.com>
2018-09-14 15:09:01 -0500

    .

--- INCLUDES_CUSTOM/script83_TapAppFees.js
+++ INCLUDES_CUSTOM/script83_TapAppFees.js
@@ -31,16 +31,21 @@ function script83_TapAppFees() {
         var nonWtrSqFt   = AInfo["Non-water conserving sq ft"];
         var wtrSqFt      = AInfo["Water conserving sq ft"];
         
+		var feeArray = ["WAT_TA_01","WAT_TA_02","WAT_TA_03","WAT_TA_04","WAT_TA_05","WAT_TA_06","WAT_TA_09","WAT_TA_10","WAT_TA_11","WAT_TA_12","WAT_TA_16","WAT_TA_17","WAT_TA_18","WAT_TA_19","WAT_TA_20","WAT_TA_21","WAT_TA_22","WAT_TA_23","WAT_TA_24","WAT_TA_25","WAT_TA_26","WAT_TA_27","WAT_TA_28","WAT_TA_29","WAT_TA_30","WAT_TA_31","WAT_TA_32","WAT_TA_33","WAT_TA_36","WAT_TA_37","WAT_TA_39","WAT_TA_40","WAT_TA_41","WAT_TA_42","WAT_TA_44"];
+        var feeArray2 = ["WAT_TA2_01","WAT_TA2_02","WAT_TA2_03","WAT_TA2_04","WAT_TA2_05","WAT_TA2_06","WAT_TA2_09","WAT_TA2_10","WAT_TA2_11","WAT_TA2_12","WAT_TA2_16","WAT_TA2_17","WAT_TA2_18","WAT_TA2_19","WAT_TA2_20","WAT_TA2_21","WAT_TA2_22","WAT_TA2_23","WAT_TA2_24","WAT_TA2_25","WAT_TA2_26","WAT_TA2_27","WAT_TA2_28","WAT_TA2_29","WAT_TA2_30","WAT_TA2_31","WAT_TA2_32","WAT_TA2_33","WAT_TA2_36","WAT_TA2_37","WAT_TA2_39","WAT_TA2_40","WAT_TA2_41","WAT_TA2_42","WAT_TA2_44"];
+        
+		feeArray.concat(feeArray2);
+		
+		for(j in feeArray){
+            var aFee = feeArray[j];
+            if(feeExists(aFee)) removeFee(aFee, feePeriod);
+        }
+		
         //use WAT_TA fee schedule if platted after 2017
         if (ifTracer(plattedAfter == "Yes", 'Platted Prior 2017')) {
             feeSched = "WAT_TA";
             
-            var feeArray = ["WAT_TA_01","WAT_TA_02","WAT_TA_03","WAT_TA_04","WAT_TA_05","WAT_TA_06","WAT_TA_09","WAT_TA_10","WAT_TA_11","WAT_TA_12","WAT_TA_16","WAT_TA_17","WAT_TA_18","WAT_TA_19","WAT_TA_20","WAT_TA_21","WAT_TA_22","WAT_TA_23","WAT_TA_24","WAT_TA_25","WAT_TA_26","WAT_TA_27","WAT_TA_28","WAT_TA_29","WAT_TA_30","WAT_TA_31","WAT_TA_32","WAT_TA_33","WAT_TA_36","WAT_TA_37","WAT_TA_39","WAT_TA_40","WAT_TA_41","WAT_TA_42","WAT_TA_44"];
             
-            for(j in feeArray){
-                var aFee = feeArray[j];
-                if(feeExists(aFee)) removeFee(aFee, feePeriod);
-            }
             
             if(ifTracer(type == "Single Family Detached", "Single Family Detached")){
                 if(waterClosets){
@@ -148,13 +153,6 @@ function script83_TapAppFees() {
             }
         } else if (ifTracer(plattedAfter == "No", 'Platted After 2017')) { //use WAT_TA2 fee schedule if platted before 2017
                 feeSched = "WAT_TA2";
-            
-                var feeArray = ["WAT_TA2_01","WAT_TA2_02","WAT_TA2_03","WAT_TA2_04","WAT_TA2_05","WAT_TA2_06","WAT_TA2_09","WAT_TA2_10","WAT_TA2_11","WAT_TA2_12","WAT_TA2_16","WAT_TA2_17","WAT_TA2_18","WAT_TA2_19","WAT_TA2_20","WAT_TA2_21","WAT_TA2_22","WAT_TA2_23","WAT_TA2_24","WAT_TA2_25","WAT_TA2_26","WAT_TA2_27","WAT_TA2_28","WAT_TA2_29","WAT_TA2_30","WAT_TA2_31","WAT_TA2_32","WAT_TA2_33","WAT_TA2_36","WAT_TA2_37","WAT_TA2_39","WAT_TA2_40","WAT_TA2_41","WAT_TA2_42","WAT_TA2_44"];
-                
-                for(j in feeArray){
-                    var aFee = feeArray[j];
-                    if(feeExists(aFee)) removeFee(aFee, feePeriod);
-                }
                 
                 if(ifTracer(type == "Single Family Detached", "Single Family Detached")){
                     if(waterClosets){
