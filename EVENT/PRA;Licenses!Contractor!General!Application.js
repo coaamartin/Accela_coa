c18dbdcd50ec6db17525fb4ef5c9c40032a4217a
jbschillo <jbschillo@gmail.com>
2018-09-20 07:39:44 -0700

    115

--- EVENT/PRA;Licenses!Contractor!General!Application.js
+++ EVENT/PRA;Licenses!Contractor!General!Application.js
@@ -1,6 +1,6 @@
 // Script 111
 if (balanceDue == 0) {
 	include("111_CreateContractorLicenseAndLP");
-	updateTask("License Issuance","Issued","Updated by PRA;Licenses!Contractor!General!Application","");
+	closeTask("License Issuance","Issued","Updated by PRA;Licenses!Contractor!General!Application","");
 }
 
