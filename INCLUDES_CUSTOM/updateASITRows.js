4fe0cea71d099bc3b4c6da028414aaaeb19a2a02
jantled <jal@wbai.com>
2018-09-28 10:25:52 -0500

    debugging 426

--- INCLUDES_CUSTOM/updateASITRows.js
+++ INCLUDES_CUSTOM/updateASITRows.js
@@ -3,16 +3,23 @@
  */
 function updateASITRows(tableName, colName, colValue, updateArray){
 	logDebug("Updating Asi Rows");
+	aa.print("Updating:");
+	aa.print("tableName:" + tableName);
+	aa.print("colName:" + colName);
+	aa.print("colValue:" + colValue);
 	var itemCap = capId
     if (arguments.length > 4)
         itemCap = arguments[4]; // use cap ID specified in args
+	
+	
+	aa.print("itemCap:" + itemCap);
 	
 	var rowsUpdated = 0;
 	var tRows = loadASITable(tableName, itemCap);
 	
 	for(eachRow in tRows){
 		var aRow = tRows[eachRow];
-		
+		aa.print("aRow[colName]:" + aRow[colName]);
 		if(aRow[colName] == colValue){
 			for(i in updateArray){
 				aRow[i] = updateArray[i];		
--- INCLUDES_SCRIPTS/426_UpdateParentEnfCaseCustomListAndStatus.js
+++ INCLUDES_SCRIPTS/426_UpdateParentEnfCaseCustomListAndStatus.js
@@ -322,12 +322,13 @@ function script426_UpdateParentEnfCaseCustomListAndStatus() {
 function updateOrCreateValueInASITable(tableName, colKeyName,fieldName, value, readonly) {
     var colsToUpdate = [];
     colsToUpdate[fieldName] = value;
+	aa.print("capIDString:" + capIDString)
     aa.print("ParentCapId:" + parentCapId );
 	aa.print("tableName:" + tableName);
 	aa.print("colKeyName:" + colKeyName);
 	aa.print("fieldName:" + fieldName);
 	aa.print("value:" + value);
-	aa.print("colsToUpdate:" + colsToUpdate);
+	aa.print("colsToUpdate:" + colsToUpdate[fieldName]);
     if(!updateASITRows(tableName, colKeyName, capIDString, colsToUpdate, parentCapId))
     {
         addAsiTableRow(tableName, [
