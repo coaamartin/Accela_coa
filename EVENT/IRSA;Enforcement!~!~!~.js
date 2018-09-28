2de09064c2c649a35774dc293a8abcbab896e119
Martin <amartin@auroragov.org>
2018-09-28 07:47:58 -0600

    ajm again

--- EVENT/IRSA;Enforcement!~!~!~.js
+++ EVENT/IRSA;Enforcement!~!~!~.js
@@ -1,5 +1,43 @@
 //ajm added for testing
-include("ajm_test");
+deleteCadRows();
+
+function deleteCadRows()
+{
+	 var cadQuery = getRemovedCADAddresses();
+
+	aa.print('---------------------------------------------------------------');
+	aa.print(cadQuery);
+	aa.print('---------------------------------------------------------------');
+
+        var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
+        var ds = initialContext.lookup("java:/AA");
+        var conn = ds.getConnection();
+        var sStmt = conn.prepareStatement(cadQuery);
+		//sStmt.setString(1, 'APD Caution');
+        var rSet = sStmt.executeQuery();
+        var counter = 0;
+        while (rSet.next()) {
+			counter = counter + 1;
+			var refAddrid = rSet.getString("b1_contact_type");
+			var conId = rSet.getString("Organizationname");
+
+			LogDebug("The Contact is: " + refAddrid);			
+			LogDebug("The Organization is: " + conId);
+			//aa.addressCondition.removeAddressCondition(refAddrid, conId);
+        }
+        sStmt.close();
+        conn.close();
+	    LogDebug("Done with this:" + counter);
+}
+
+//Get addresses to be remove(It was removed from CAD).
+function getRemovedCADAddresses()
+{
+	var altId = capId.getCustomID();
+	var cadQuery = "exec spreport_ch_people_buildingmanager_subreport " + altId;
+	return cadQuery;
+}
+//end ajm add
 
 logDebug("DONB 101 " + capId)
 
