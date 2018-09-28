77edb601d2bf74f883614edbe4f010583029eb7e
jantled <jal@wbai.com>
2018-09-28 11:54:27 -0500

    .updated 426

--- INCLUDES_SCRIPTS/426_UpdateParentEnfCaseCustomListAndStatus.js
+++ INCLUDES_SCRIPTS/426_UpdateParentEnfCaseCustomListAndStatus.js
@@ -317,31 +317,32 @@ function script426_UpdateParentEnfCaseCustomListAndStatus() {
     
 }
 
-function updateOrCreateValueInASITable(tableName, colKeyName,fieldName, value, readonly) {
-    if(!updateAsiTableRow(tableName, fieldName, value, { 
-        capId: parentCapId,
-        colFilters: [
-            { colName: colKeyName, colValue: capIDString}
-        ]}) 
-    ) {
-        addAsiTableRow(tableName, [
-            { colName: fieldName, colValue: value }
-        ], { capId: parentCapId });
-    }
-}
-
-
 //function updateOrCreateValueInASITable(tableName, colKeyName,fieldName, value, readonly) {
-//    var colsToUpdate = [];
-//    colsToUpdate[fieldName] = value;
-//    
-//    if(!updateASITRows(tableName, colKeyName, capIDString, colsToUpdate, parentCapId))
-//    {
+//    if(!updateAsiTableRow(tableName, fieldName, value, { 
+//        capId: parentCapId,
+//        colFilters: [
+//            { colName: colKeyName, colValue: capIDString}
+//        ]}) 
+//    ) {
 //        addAsiTableRow(tableName, [
 //            { colName: fieldName, colValue: value }
 //        ], { capId: parentCapId });
 //    }
 //}
+
+
+function updateOrCreateValueInASITable(tableName, colKeyName,fieldName, value, readonly) {
+    var colsToUpdate = [];
+    colsToUpdate[fieldName] = value;
+    
+    if(!updateASITRows(tableName, colKeyName, capIDString, colsToUpdate, parentCapId))
+    {
+        addAsiTableRow(tableName, [
+            { colName: fieldName, colValue: value },
+			{ colName: 'Abatement #', colValue: capIDString }
+        ], { capId: parentCapId });
+    }
+}
 
 function getChildrenWithActiveTasks() {
   //  var parentCapId = getParent(),
