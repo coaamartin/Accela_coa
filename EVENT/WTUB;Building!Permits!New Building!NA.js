cb776115db8d1f7056e913c326d4a9b161b64f2e
Porter <jmporter@auroragov.org>
2018-10-01 15:53:47 -0600

    JMP

--- EVENT/WTUB;Building!Permits!New Building!NA.js
+++ EVENT/WTUB;Building!Permits!New Building!NA.js
@@ -16,5 +16,6 @@ Notes :
 	-The WTUA could not be used to prevent the workflow from proceeding , alternatively we use WTUB
 */
 
+logDebug("Entering JMP TEST");
 requireDataInSpecialInspections();
 
