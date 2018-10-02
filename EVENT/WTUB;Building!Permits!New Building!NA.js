b52765855dc803f5192aad22a71977a6cc0bba67
Porter <jmporter@auroragov.org>
2018-10-02 16:02:41 -0600

    JMP

--- EVENT/WTUB;Building!Permits!New Building!NA.js
+++ EVENT/WTUB;Building!Permits!New Building!NA.js
@@ -1,6 +1,7 @@
 /* JMP - 10/2/2018 - Script Item #71 - Certificate of Occupancy checked
   If Inspection Phase workflow task has the status of ‘Ready for CO’ verify the Info Field ‘Certificate of Occupancy’ is checked, 
   if it is unchecked then stop the workflow progression and give an error message that says “There is not a Certificate of Occupancy required on this record.
+    if it is unchecked then stop the workflow progression and give an error message that says “There is not a Certificate of Occupancy required on this record.
 */
    
 include("71_Certificate_Occpancy_Checked");	
