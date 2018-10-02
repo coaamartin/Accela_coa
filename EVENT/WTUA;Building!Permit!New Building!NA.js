363d075df35fc2fbe421bdb0d1900142ba0e573c
Porter <jmporter@auroragov.org>
2018-10-02 13:04:39 -0600

    JMP

--- EVENT/WTUA;Building!Permit!New Building!NA.js
+++ EVENT/WTUA;Building!Permit!New Building!NA.js
@@ -19,11 +19,12 @@ Sample Call : createChildWaterUtilityPermitRecords()
 
 */
 
-// JMP - 10/2/2018 - Script Item #71 - Certificate of Occupancy checked
-// If Inspection Phase workflow task has the status of ‘Ready for CO’ verify the Info Field ‘Certificate of Occupancy’ is checked, 
-//   if it is unchecked then stop the workflow progression and give an error message that says “There is not a Certificate of Occupancy required on this record.
+/* JMP - 10/2/2018 - Script Item #71 - Certificate of Occupancy checked
+  If Inspection Phase workflow task has the status of ‘Ready for CO’ verify the Info Field ‘Certificate of Occupancy’ is checked, 
+  if it is unchecked then stop the workflow progression and give an error message that says “There is not a Certificate of Occupancy required on this record.
+*/
    
-include("71-Certificate_Occpancy_Checked");	
+include("71_Certificate_Occpancy_Checked");	
 logDebug("JMP JMP ------------------------>> JMP JUST INCLUDED 71-Certificate");
 
 createChildWaterUtilityPermitRecords();
