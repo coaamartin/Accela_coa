9c5808848f6e9dfc8a4f8ca880b2c23d268f48d6
Porter <jmporter@auroragov.org>
2018-10-02 13:38:12 -0600

    JMP

--- INCLUDES_SCRIPTS/71_Certificate_Occpancy_Checked.js
+++ INCLUDES_SCRIPTS/71_Certificate_Occpancy_Checked.js
@@ -3,18 +3,23 @@
 //   if it is unchecked then stop the workflow progression and give an error message that says “There is not a Certificate of Occupancy required on this record.” 
 */
 
-logDebug("JMP JMP ------------------------>> Testing logDebug JMP ----------------->  ");
-
-if(wfTask =="Certificate of Occupancy" && wfStatus== "Final CO Issued"){
+if(wfTask =="Certificate of Occupancy" && wfStatus== "Final CO Issued")
+{
   
   var tmpUASGN = useAppSpecificGroupName;
+  
   useAppSpecificGroupName=false;
-  var cOO = getAppSpecific("Certificate of Occupancy",capId) + "";
+  
+  var cOO = getAppSpecific("Certificate of Occupancy") + "";
   useAppSpecificGroupName = tmpUASGN;
   
   logDebug("JMP JMP ------------------------>> Testing logDebug JMP ----------------->  About to test logic: c00 = " + cOO);  
   
-  if (cOO != "CHECKED" ){
-	comment(<h2 style="background-color:rgb(255, 0, 0)">WARNING - There is not a Certificate of Occupancy required on this record.</h2>);
-    deactivateTask("Certificate of Occupancy");}
+  if (cOO != "CHECKED" )
+    {
+
+		showMessage = true;
+		comment("<h2 style='background-color:rgb(255, 0, 0);'>WARNING - There is not a Certificate of Occupancy required on this record.</h2>");
+		deactivateTask("Certificate of Occupancy");
+	}
 }
