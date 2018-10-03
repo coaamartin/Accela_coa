cfc77a85fe75723c02ccde23e49fe48ad48282f0
Porter <jmporter@auroragov.org>
2018-10-03 14:13:08 -0600

    JMP

---
+++ INCLUDES_SCRIPTS/70_Inspection_Phase_Letters.js
@@ -0,0 +1,11 @@
+/* JMP - 10/3/2018 - Script Item #70 - 70_Inspection_Phase_Letters - Inspection Phase has the status of Ready for CO
+// ​If Inspection Phase has the status of ‘Ready for CO’ check that Letters have all been received; for any Checked Custom Field under Subgroup (ENGINEER LETTERS:
+// ILC Letter Required, Foundation Letter Required, Footing - Pier - Cassion Letter Required, Drain Letter Required, Waterproofing Letter Required) 
+// then see if the matching received field is also checked under Subgroup (ENGINEER LETTERS: ILC Letter Received, Foundation Letter Received, Footing - Pier - Cassion Letter Received, 
+// Drain Letter Received, Waterproofing Letter Received) if all matching fields are checked then allow the workflow to progress 
+*/
+
+logDebug("JMP JMP Alert: ------------------------>> Script Item #70 - 70_Inspection_Phase_Letters");
+
+
+}
