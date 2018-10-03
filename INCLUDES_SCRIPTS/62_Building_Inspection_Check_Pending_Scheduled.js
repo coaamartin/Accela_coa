650cc5afdd4d199df1374bd64cfef4fe4fc7ef8b
Porter <jmporter@auroragov.org>
2018-10-03 08:56:38 -0600

    JMP

--- INCLUDES_SCRIPTS/62_Building_Inspection_Check_Pending_Scheduled.js
+++ INCLUDES_SCRIPTS/62_Building_Inspection_Check_Pending_Scheduled.js
@@ -1,4 +1,4 @@
-/* JMP - 10/2/2018 - Script Item #62 - Prevent Ready for CO if inspections pending or scheduled or active other wf tasks
+/* JMP - 10/3/2018 - Script Item #62 - Prevent Ready for CO if inspections pending or scheduled or active other wf tasks
 // ​If status 'Ready For CO' is selected on Inspection Phase wf task, verify there are no Pending or Scheduled inspections and verify that there are no other workflow tasks active, 
 // if there are then prevent the wf task from proceeding and present a message stating "There are pending or scheduled inspections or workflow tasks active, Inspection Phase workflow can't proceed.  
 */
@@ -8,11 +8,14 @@ logDebug("JMP JMP Alert: ------------------------>> Called Script Item #62 - 62_
 if(wfTask =="Inspection Phase" && wfStatus== "Ready For CO") 
 {
   
-  if ("Pending".equals(inspResult)) || ("Scheduled".equals(inspResult)) 
+/*  if ("Pending".equals(inspResult)) || ("Scheduled".equals(inspResult)) 
   {
+*/	  
 	showMessage = true;
 	comment("<h2 style='background-color:rgb(255, 0, 0);'>WARNING - There are pending or scheduled inspections or workflow tasks active, Inspection Phase workflow can't proceed. </h2>");
 	deactivateTask("Inspection Phase");
 	cancel = true;
-  }
+	
+//  }
+  
 }
