84b16521ef14a8e61dbb35a654d385c0ba36829f
Porter <jmporter@auroragov.org>
2018-10-05 11:55:50 -0600

    JMP

--- INCLUDES_SCRIPTS/70_Inspection_Phase_Letters.js
+++ INCLUDES_SCRIPTS/70_Inspection_Phase_Letters.js
@@ -24,30 +24,10 @@ if(wfTask =="Inspection Phase" && wfStatus== "Ready for CO")
     var Let4a = getAppSpecific("Footing - Pier - Cassion Letter Received") + "";
     var Let5a = getAppSpecific("Drain Letter Received") + "";
     
-    if Let1 !== Let1a 
+    if (Let1 !== Let1a) 
     {
       OhNoBadMatch = true;
-    }  
-    
-    if Let2 !== Let2a 
-    {
-      OhNoBadMatch = true;
-    }   
-    
-    if Let3 !== Let3a 
-    {
-      OhNoBadMatch = true;
-    }  
-
-    if Let4 !== Let4a 
-    {
-      OhNoBadMatch = true;
-    }   
-    
-    if Let5 !== Let5a 
-    {
-      OhNoBadMatch = true;
-    }       
+    }     
 
 
 }
