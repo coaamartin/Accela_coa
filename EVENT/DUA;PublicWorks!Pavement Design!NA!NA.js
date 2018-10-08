b66e03197ee585dcb68f4b6a2bad4b08aa81138c
jantled <jal@wbai.com>
2018-10-08 16:22:34 -0500

    corrected the status

--- EVENT/DUA;PublicWorks!Pavement Design!NA!NA.js
+++ EVENT/DUA;PublicWorks!Pavement Design!NA!NA.js
@@ -17,7 +17,7 @@
 logDebug ("script22 () started")
 var docCategory = checkIfDocUploaded("Outside Agency");
 //logDebug("Doc Category " + docCategory);
-if( publicUser && (capStatus=="Waiting on Documents" || capStatus=="Upload Signature Set" ) && docCategory == "Outside Agency") 
+if( publicUser && (capStatus=="Waiting  on Documents" || capStatus=="Waiting on Documents" || capStatus=="Upload Signature Set" ) && docCategory == "Outside Agency") 
  {  
 	activateTask("Completeness Check");
 	editTaskDueDate("Completeness Check",dateAdd(null, 0));
