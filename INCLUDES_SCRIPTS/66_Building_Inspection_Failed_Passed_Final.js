e42e32e079bfad70efa46e8eeff6298484930ee2
Porter <jmporter@auroragov.org>
2018-10-09 12:51:34 -0600

    JMP

--- INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
+++ INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
@@ -3,20 +3,11 @@
 // expiration Date needs to change to 180 days from the current date
 */
 
-logDebug("JMP JMP Alert: ------------------------>> Script Item #66_Building_Inspection_Failed_Passed_Final");
+logDebug("Alert: ------------------------>> Script Item #66_Building_Inspection_Failed_Passed_Final");
 
 if (matches(inspType.toUpperCase(),"FAILED","PASSED","FINAL"))
 {
   editAppSpecific("Permit Expiration Date",dateAdd(null,180));  
 }
 
-/*if (inspType.toUpperCase() = "PASSED")
-{
-  //editAppSpecific("Permit Expiration Date",dateAdd(null,180));  
-}
 
-if (inspType.toUpperCase() = "FINAL")
-{
-  //editAppSpecific("Permit Expiration Date",dateAdd(null,180));  
-}
-*/
