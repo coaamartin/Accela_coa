611579950559b4f523805af54ae0828362c126e4
Porter <jmporter@auroragov.org>
2018-10-09 12:19:48 -0600

    JMP

--- INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
+++ INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
@@ -3,29 +3,13 @@
 // expiration Date needs to change to 180 days from the current date
 */
 
+//scheduleInspectDate(INSPECTION_NAME, nextInspectionDate)
+
 logDebug("JMP JMP Alert: ------------------------>> Script Item #66_Building_Inspection_Failed_Passed_Final");
-logDebug(" Oh mercy .. is the name here " + inspType + "");
 
-var inspResultObj = aa.inspection.getInspections(capId);
-		if (inspResultObj.getSuccess()) 
-      {
-			var inspList = inspResultObj.getOutput();
-			for (index in inspList) 
-         {
-            
-				//if (matches(inspList[index].getInspectionStatus().toUpperCase(), "FAILED", "PASSED", "FINAL")) {
-  		   //var InspContents = inspList[index];
-         var GetName = inspList[index];
-         var ShowInspName = GetName.getOutput();
-         
-         //
+if matches(inspType.toUpperCase(),"FAILED", "PASSED", "FINAL"))
+{
+  editAppSpecific("Permit Expiration Date",dateAdd(null,180));
+  
+}
 
-         //logDebug("Hello JP - Print Object = " + printObject(inspList[index]));
-         
-               //scheduleInspectDate(INSPECTION_NAME, nextInspectionDate)
-               
-         logDebug("JMP JMP Alert: ------------------------>> Script Item #66 = " + ShowInspName.inspType);
-				
-
-         }
-      }
