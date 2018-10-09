c0f6c0065eed0607417f1f0b53036a7eda9998e8
Porter <jmporter@auroragov.org>
2018-10-09 08:54:09 -0600

    JMP

--- INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
+++ INCLUDES_SCRIPTS/66_Building_Inspection_Failed_Passed_Final.js
@@ -13,7 +13,7 @@ var inspResultObj = aa.inspection.getInspections(capId);
          {
             
 				//if (matches(inspList[index].getInspectionStatus().toUpperCase(), "FAILED", "PASSED", "FINAL")) {
-  		   var ShowInspName = inspList[index].getElementsByTagName("INSPECTION_NAME");
+  		   var ShowInspName = inspList[index].getElementsByName("INSPECTION_NAME");
                //scheduleInspectDate(INSPECTION_NAME, nextInspectionDate)
                
          logDebug("JMP JMP Alert: ------------------------>> Script Item #66 = " + ShowInspName);
