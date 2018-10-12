5bd712dabdadccf3286d6d2b742b1f3144bab37f
jantled <jal@wbai.com>
2018-10-12 11:57:55 -0500

    added script to prevent balance of zero for Abatement if there is a balance on snow record

--- EVENT/PRB;Enforcement!Incident!Abatement!NA.js
+++ EVENT/PRB;Enforcement!Incident!Abatement!NA.js
@@ -16,3 +16,25 @@ Notes:
 */
 //Script 95
 preventPaymentDueToSpecialAssessment("Special Assessment");
+
+//If there is a balance on a snow abatement record ask user to pay that first.
+var parentCapId,
+parentCapScriptModel,
+parentCapTypeString,
+parentBalance = 0;
+
+parentCapId = getParent();
+if(ifTracer(parentCapId, 'parent found')) {
+    parentCapScriptModel = aa.cap.getCap(parentCapId).getOutput();
+    parentCapTypeString = parentCapScriptModel.getCapType().toString();
+    if(ifTracer(parentCapTypeString.indexOf('Enforcement/Incident/Snow') > -1, 'parent = Snow Violation Case')) {
+        //parent is Snow Violation Case
+        parentBalance = getBalanceByCapId("", "", true, parentCapId);
+		
+		if(parentBalance > 0 && balanceDue == PaymentTotalPaidAmount){
+			cancel = true
+			showMessage = true;
+			comment("Please pay the balance on the Snow Violation parent first.");
+		}
+    }
+}
