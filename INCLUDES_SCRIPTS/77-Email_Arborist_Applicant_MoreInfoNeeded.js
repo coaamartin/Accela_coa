5d8c1e73e3dd7344a00311785f4211a19549a231
Porter <jmporter@auroragov.org>
2018-10-23 13:26:52 -0600

    JMP

--- INCLUDES_SCRIPTS/77-Email_Arborist_Applicant_MoreInfoNeeded.js
+++ INCLUDES_SCRIPTS/77-Email_Arborist_Applicant_MoreInfoNeeded.js
@@ -15,16 +15,19 @@ if ("Application Intake".equals(wfTask) && "Additional Info Needed".equals(wfSta
       
       //Get the capId type needed for the email function
 		var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+      var altId = capId.getCustomID();
       
       var eParams = aa.util.newHashtable();
       var contacts = "jmporter@auroragov.org";
       var emailtemplate = "JMP EMAIL TEMPLATE";
-      // var urlstring = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBOK9ZF.img?h=488&w=799&m=6&q=60&o=f&l=f&x=916&y=734"
+      var urlstring = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBOK9ZF.img?h=488&w=799&m=6&q=60&o=f&l=f&x=916&y=734"
       
       addParameter(eParams, "$$wfComment$$", wfComment);
+      addParameter(eParams, "$$altID$$", altId);
       addParameter(eParams, "$$todayDate$$", dateAdd(null, 0));
       addParameter(eParams, "$$scriptid$$", "77");
-      //addParameter(eParams, "$$testpic$$", urlstring);
+      addParameter(eParams, "$$signature$$", "James M. Porter");
+      addParameter(eParams, "$$testpic$$, urlstring);
       
       //emailContacts(contacts, emailtemplate, eParams, "", "", "N", "");
       logDebug("Email Sent: " + aa.document.sendEmailAndSaveAsDocument("noreply@aurora.gov", contacts, "", emailtemplate, eParams, capId4Email, null).getSuccess());
