173829bd249d14768910533b5b5c5dbbd2c90261
Porter <jmporter@auroragov.org>
2018-10-23 08:18:10 -0600

    JMP

--- EVENT/WTUA;Licenses!Contractor!Arborist!Application.js
+++ EVENT/WTUA;Licenses!Contractor!Arborist!Application.js
@@ -2,3 +2,9 @@
 if ("License Issuance".equals(wfTask) && "Issued".equals(wfStatus)) {
 	include("146_CreateArboristLicenseAndLP");
 }
+
+//JMP Added 10/22/18 per Script #77 request
+if ("Application Intake".equals(wfTask) && "Additional Info Needed".equals(wfStatus)) 
+{
+	include("77-Email_Arborist_Applicant_MoreInfoNeeded");
+}
---
+++ EVENT/WTUA;Licenses!Contractor!Arborist!Renewal.js
@@ -0,0 +1,7 @@
+// Script #77 
+// JMP - Created 10/22/2018
+
+if ("Application Intake".equals(wfTask) && "Additional Info Needed".equals(wfStatus)) {
+	include("77-Email_Arborist_Applicant_MoreInfoNeeded");
+}
+
