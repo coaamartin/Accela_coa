3933141c00da91d68b697cf6b796784ff7305c2c
Porter <jmporter@auroragov.org>
2018-10-23 08:58:23 -0600

    JMP

--- INCLUDES_SCRIPTS/77-Email_Arborist_Applicant_MoreInfoNeeded.js
+++ INCLUDES_SCRIPTS/77-Email_Arborist_Applicant_MoreInfoNeeded.js
@@ -11,7 +11,7 @@ if ("Application Intake".equals(wfTask) && "Additional Info Needed".equals(wfSta
    if (wfComment != null && typeof wfComment !== 'undefined') 
       
    {
-       //var thisCap = aa.cap.getCap(capId).getOutput();
+      //var thisCap = aa.cap.getCap(capId).getOutput();
       
       var eParams = aa.util.newHashtable();
       var contacts = "All";
@@ -19,7 +19,7 @@ if ("Application Intake".equals(wfTask) && "Additional Info Needed".equals(wfSta
       var urlstring = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBOK9ZF.img?h=488&w=799&m=6&q=60&o=f&l=f&x=916&y=734"
       
       addParameter(eParams, "$$wfComment$$", wfComment);
-      addParameter(eParams, "$$altID$$", altId);
+      //addParameter(eParams, "$$altID$$", altId);
       addParameter(eParams, "$$todayDate$$", dateAdd(null, 0));
       addParameter(eParams, "$$scriptid$$", "77");
       addParameter(eParams, "$$testpic$$", urlstring);
