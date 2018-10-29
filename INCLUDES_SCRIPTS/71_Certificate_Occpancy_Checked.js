f146cdc73406175846ede9dff7fae25b9a4a4134
Porter <jmporter@auroragov.org>
2018-10-29 12:54:17 -0600

    JMP

--- INCLUDES_SCRIPTS/71_Certificate_Occpancy_Checked.js
+++ INCLUDES_SCRIPTS/71_Certificate_Occpancy_Checked.js
@@ -15,7 +15,9 @@ if(wfTask == "Inspection Phase" && wfStatus == "Ready for CO")     // "Final CO 
   var doesCheckExist = getAppSpecific("Certificate of Occupancy") + "";
   useAppSpecificGroupName = tmpUASGN;
   
-  if (doesCheckExist = "")
+  logDebug("JMP JMP Alert: ------------------------>>" + doesCheckExist + "");
+  
+  if (!doesCheckExist = "Y")
    {
 		showMessage = true;
 		comment("<h2 style='background-color:rgb(255, 0, 0);'>WARNING - There is not a Certificate of Occupancy required on this record.</h2>");
