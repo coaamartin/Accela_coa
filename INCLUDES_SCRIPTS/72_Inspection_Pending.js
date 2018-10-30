273829af1a0c5bdf1f66e52116811fa73f19df02
Porter <jmporter@auroragov.org>
2018-10-30 14:21:08 -0600

    JMP

---
+++ INCLUDES_SCRIPTS/72_Inspection_Pending.js
@@ -0,0 +1,33 @@
+/* JMP - 10/26/2018 - Script Item #73 - Inspection_Leftover 
+
+// If status of 'Leftover' on any scheduled inspection, then automatically schedule the same inspection for the next business day.
+ 
+*/
+
+logDebug("JMP JMP Alert: ------------------------>> Script Item #73 - Inspection_Leftover");
+
+
+if (inspResult == "Left Over")
+{
+  aa.inspection.scheduleInspection(capId, null, aa.date.parseDate(dateAdd(null, 1, true)), null, inspType + "", null); 
+  logDebug("JMP JMP Alert: ------------------------>> Script Item #73 - OK working with current Leftover ... Scheduled New Inspection type and not replicate = " + inspType + "");  
+}  
+ 
+/* 
+var inspections = aa.inspection.getInspections(capId);
+if (inspections.getSuccess()) 
+{
+   inspections = inspections.getOutput();
+	for (i in inspections) 
+   {
+	   var thisInspection = inspections[i];
+  	   if (thisInspection.getInspectionStatus() == "Left Over") 
+      {
+        logDebug("JMP JMP Alert: ------------------------>> Script Item #73 - OK I found a Leftover");
+        //createInspection(thisInspection.getInspectionType() + "",  aa.date.parseDate(dateAdd(null, 1, true)));
+        aa.inspection.scheduleInspection(capId, null, aa.date.parseDate(dateAdd(null, 1, true)), null, thisInspection.getInspectionType() + "", null);
+      }
+   }
+}
+*/
+
--- INCLUDES_SCRIPTS/76_Building_Inspection_Update.js
+++ INCLUDES_SCRIPTS/76_Building_Inspection_Update.js
@@ -4,16 +4,7 @@
 // are to automatically move to ‘Pending’ status in the Inspection tab. 
 */
 
-function createInspection(type, date) // JMP
-{
-  aa.inspection.scheduleInspection(capId, null, date, null, type, null);
-
-}
-
 logDebug("JMP JMP Alert: ------------------------>> Script Item #76 - 76_Building_Inspection_Update ... Need to see if createInspecion still not working ");
-
-//var inspectionArray = ["Roofing", "Engineering Inspection", "Grade Inspection", "Zoning Inspection", "Water Service/Sanitary Service Inspection", "Irrigation Inspection"];
-//var foundInspecionArray = [0, 0, 0, 0, 0];
 
 var foundRoofingInspecion = false;
 var foundEngineerInspecion = false;
@@ -100,38 +91,5 @@ if(wfTask =="Permit Issuance" && wfStatus== "Issued")
    createPendingInspection(inspGroup,"Irrigation Inspection");
   }  
      
-     /*    
-     
-     createInspection("Roofing",  aa.date.parseDate(dateAdd(null, 0, true))); 
-     createInspection("Engineering Inspection",  aa.date.parseDate(dateAdd(null, 0, true))); 
-     createInspection("Grade Inspection",  aa.date.parseDate(dateAdd(null, 0, true))); 
-     createInspection("Zoning Inspection",  aa.date.parseDate(dateAdd(null, 0, true))); 
-     createInspection("Water Service/Sanitary Service Inspection",  aa.date.parseDate(dateAdd(null, 0, true))); 
-     createInspection("Irrigation Inspection",  aa.date.parseDate(dateAdd(null, 0, true)));      
-      
-  } 
-  
-  var inspResultObj = aa.inspection.getInspections(capId);
-  if (inspResultObj.getSuccess()) 
-  {      
-		var inspList = inspResultObj.getOutput();
-      var showInspName = ''; 
-      
-      for (index in inspList)          
-      {            
-         showInspName = inspList[index].getInspectionType() + "";
-         
-         if ((showInspName == "Roofing") || (showInspName == "Engineering Inspection") || (showInspName == "Grade Inspection") || (showInspName == "Zoning Inspection") || (showInspName == "Water Service/Sanitary Service Inspection") || (showInspName == "Irrigation Inspection"))
-         { 
-            
-          foundInspecion = true;    
-
-           //inspList[index].setInspectionStatus("Pending");
-           //aa.inspection.editInspection(inspList[index]);                  
-         } 
-      
-      }
-  }  
-  
-  */
+    
 }
