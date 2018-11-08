bf0555bce3e05c554f640bd2a9a8a21db8da9c89
Porter <jmporter@auroragov.org>
2018-11-08 10:40:31 -0700

    JMP

---
+++ INCLUDES_SCRIPTS/81-Add Fees and Invoice may not have happened.js
@@ -0,0 +1,29 @@
+/* #81 - Add Fees and Invoice may not have happened
+
+11/8/2018 Event: WorkflowTaskUpdateBefore
+
+If Task = Application Acceptance and Status is Ready To Pay (maybe Accepted as well - confirm with Planning) 
+
+Check to see if there is at least one fee and that all Fees are invoiced. 
+
+If not, then raise message "Fees must be added and invoiced to Accept the Application Acceptance" 
+  
+
+*/
+
+logDebug("JMP JMP Alert: ------------------------>> Script Item #81 - Add Fees and Invoice may not have happened");
+
+if (((wfTask == "Application Acceptance") && (wfStatus == "Ready To Pay")) || ((wfTask == "Application Acceptance") && (wfStatus == "Accepted")))
+{   
+
+   if (balanceDue > 0)
+   {
+      showMessage = true;
+	   comment("<h2 style='background-color:rgb(255, 0, 0);'>Fees must be added and invoiced to Accept the Application Acceptance.</h2>");
+      cancel = true;     
+      
+   }
+} 
+ 
+
+
