7e156bdf51038ed6644655f234a99ac882d86eab
Porter <jmporter@auroragov.org>
2018-11-09 15:29:30 -0700

    JMP

--- INCLUDES_SCRIPTS/68_Workflow_Traffic_Review_Assign_People.js
+++ INCLUDES_SCRIPTS/68_Workflow_Traffic_Review_Assign_People.js
@@ -63,34 +63,26 @@ function getAssignedUser()
 
 function getAssignedDept() 
 {
-    try 
-    {
-      var asgnDept = "";
-      var cdScriptObjResult = aa.cap.getCapDetail(capId);
-      if (!cdScriptObjResult.getSuccess()) 
-      {
-        aa.debug("**ERROR: No cap detail script object : ", cdScriptObjResult.getErrorMessage());
-        return "";
-      }
-    }
+   var asgnDept = "";
+   var cdScriptObjResult = aa.cap.getCapDetail(capId);
+   if (!cdScriptObjResult.getSuccess()) 
+   {
+     aa.debug("**ERROR: No cap detail script object : ", cdScriptObjResult.getErrorMessage());
+     return "";
+   }
     
-    var cdScriptObj = cdScriptObjResult.getOutput();
-    if (!cdScriptObj) 
-    {
-      aa.debug("**ERROR: No cap detail script object", "");
-      return "";
-    }
+   var cdScriptObj = cdScriptObjResult.getOutput();
+   if (!cdScriptObj) 
+   {
+     aa.debug("**ERROR: No cap detail script object", "");
+     return "";
+   }
       
-    cd = cdScriptObj.getCapDetailModel();
-    var asgnDept = cd.getAsgnDept();
+   cd = cdScriptObj.getCapDetailModel();
+   var asgnDept = cd.getAsgnDept();
 
-    return asgnDept;
+   return asgnDept;
 
-    catch (e) 
-    {
-      aa.debug("getAssignedDept ", e);
-      return null;
-    }
 }
 
 }
