dcfc7b6a98eae05f5ab59e9c308a8b9fa25b7729
Porter <jmporter@auroragov.org>
2018-11-16 10:02:56 -0700

    JMP

--- INCLUDES_SCRIPTS/58_Rescheduled SWMP.js
+++ INCLUDES_SCRIPTS/58_Rescheduled SWMP.js
@@ -17,6 +17,8 @@ if ("Final Inspection".equals(inspType))  //JMP
   
   var now = new Date();
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  logDebug(now.getMonth() + "");
   var todayDateLongForm = months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear()
   
   var capIDString = capId.getCustomID();	
