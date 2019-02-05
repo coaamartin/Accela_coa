4a3e1184b85f1f3b6aa7a0f64cce37dbc9bf4ebb
Martin <amartin@auroragov.org>
2019-02-05 15:22:18 -0700

    ajm

---
+++ EVENT/CTRCA;MiscServices!~!~!RENEWAL.js
@@ -0,0 +1,9 @@
+// Renewal processing - testing to see if this works for CTRCA
+
+logDebug ("About to prepareAppForRenewal");
+
+prepareAppForRenewal();
+
+//Begin script to assess late fees delinquent MJ renewal
+//include("313_AddExpiredRenewalLateFeeMJ");
+//End script to assess late fees delinquent MJ renewal
