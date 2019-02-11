1fd5a65ce3db08302152ba73c22f9891d3292522
Martin <amartin@auroragov.org>
2019-02-11 13:11:31 -0700

    ajm

--- INCLUDES_SCRIPTS/5085_HandleMiscServicesMasterWTUA.js
+++ INCLUDES_SCRIPTS/5085_HandleMiscServicesMasterWTUA.js
@@ -13,6 +13,29 @@ logDebug("5085 sending Email to GIS");
 }
 
 function sendEmailToGIS(){
-emailAsync2("", "MISC NA MASTER GIS REFERRAL", "");
+		//generate email notices
+		var emailTemplate = "MISC NA MASTER GIS REFERRAL";
+		var todayDate = new Date();
+		if (emailTemplate != null && emailTemplate != "") {
+			eParams = aa.util.newHashtable();
+			eParams.put("$$todayDate$$", todayDate);
+			eParams.put("$$altid$$",capId.getCustomID());
+			eParams.put("$$capAlias$$",cap.getCapType().getAlias(););
+			if (reportName != null && reportName != "") {
+				var rParams = aa.util.newHashtable();
+				addParameter(rParams, "prmRecordID", altId);
+				addParameter(rParams, "prmReportType", reportType);
+				addParameter(rParams, "prmFromDate", "");
+				addParameter(rParams, "prmToDate", "");
+
+				logDebug('Attempting to send email with report: ' + emailTemplate + " : " + reportName + " : " + capId.getCustomID());
+				//logDebug('Attempting to send email with report: ' + emailTemplate + " : " + reportName + " : " + capId.getCustomID());
+				emailContacts(sendEmailToContactTypes, emailTemplate, eParams, reportName, rParams, "Y");
+			} else {
+				logDebug('Attempting to send email: ' + emailTemplate + " : " + capId.getCustomID());
+				//logDebug('Attempting to send email: ' + emailTemplate + " : " + capId.getCustomID());
+				emailContacts(sendEmailToContactTypes, emailTemplate, eParams, null, null, "Y");
+			}
+		}
 }
 	logDebug("5085_HandleMiscServicesMasterWTUA.js ended.");
