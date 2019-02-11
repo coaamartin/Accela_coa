419c438cb5d9b11f3ce0ef77d4aedbf9bb08a6d5
Martin <amartin@auroragov.org>
2019-02-11 14:28:09 -0700

    ajm

--- EVENT/WTUA;MiscServices!Neighborhood!Association!Master.js
+++ EVENT/WTUA;MiscServices!Neighborhood!Association!Master.js
@@ -6,3 +6,18 @@
 // CHANGELOG: 
 logDebug("Calling 5085 script");
 include("5085_HandleMiscServicesMasterWTUA");
+
+		var emailTemplate = "MISC NA MASTER GIS REFERRAL";
+		var todayDate = new Date();
+		if (emailTemplate != null && emailTemplate != "") {
+			logDebug("5085 sending Email to GIS");	
+			eParams = aa.util.newHashtable();
+			eParams.put("$$ContactEmail$$", "amartin@auroragov.org");			
+			eParams.put("$$todayDate$$", todayDate);
+			eParams.put("$$altid$$",capId.getCustomID());
+			eParams.put("$$capAlias$$",cap.getCapType().getAlias());
+			logDebug('Attempting to send email: ' + emailTemplate + " : " + capId.getCustomID());
+			//emailContacts("Individual", emailTemplate, eParams, null, null, "Y");
+		    emailAsync2("", emailTemplate, eParams);	
+			emailAsync("amartin@auroragov.org", emailTemplate, eParams, "", "", "", "");			
+		}
