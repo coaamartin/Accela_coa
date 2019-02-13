00831d09aaa96656d1e88dbe7f4db297d1f6e606
Martin <amartin@auroragov.org>
2019-02-13 08:34:34 -0700

    ajm

--- EVENT/ASA;MiscServices!Neighborhood!Association!Renewal.js
+++ EVENT/ASA;MiscServices!Neighborhood!Association!Renewal.js
@@ -1,15 +1,2 @@
 // Begin script to update the Multiple Parcels table with actual parcels
- if(!publicUser){
-  if (typeof(MULTIPLEPARCELS) == "object")
-  {
-   createMPTableFromParcels();
-  }
- }
-// End script to update the Multiple Parcels table with actual parcels
 
-
-if (matches(currentUserID, "ADMIN")) {
-showDebug = false;
-showMessage = false;
-}
-include("EMSE:SetContactRelationshipToContactType");
--- EVENT/WTUA;MiscServices!Neighborhood!Association!Master.js
+++ EVENT/WTUA;MiscServices!Neighborhood!Association!Master.js
@@ -1,8 +1,8 @@
-// SCRIPTNUMBER: 5084
+// SCRIPTNUMBER: 
 // SCRIPTFILENAME: WTUA;MiscServices!Neighborhood!Association!Master.js
 // PURPOSE: The event called by Accela when a Master task update is taking place.
 // DATECREATED: 02/11/2019
 // BY: amartin
 // CHANGELOG: 
-logDebug("Calling 5085 script");
-include("5085_HandleMiscServicesMasterWTUA");
+logDebug("Calling 5083 script");
+include("5083_HandleMiscServicesMasterWTUA");
