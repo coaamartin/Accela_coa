56338f6073917f6821ef0b673026dd03c28236c6
Porter <jmporter@auroragov.org>
2019-02-19 10:29:58 -0700

    JMP

--- EVENT/ASA;Water!Water!Lawn Irrigation!Permit.js
+++ EVENT/ASA;Water!Water!Lawn Irrigation!Permit.js
@@ -1,5 +1,5 @@
 //Record Types:	Water/Water/Lawn Irrigation/Permit
 //Event: ASA - ApplicationSubmitAfter
 
-5084_Irrigation_Inspection_Pending();
-script193_WatIrrigationAddInspFee();
+include("5084_Irrigation_Inspection_Pending");
+include("script193_WatIrrigationAddInspFee");
