d2a7a2b9cd47926774c8e0b4791ef74b70adef00
Porter <jmporter@auroragov.org>
2019-02-19 10:26:02 -0700

    JMP

--- EVENT/ASA;Water!Water!Lawn Irrigation!Permit.js
+++ EVENT/ASA;Water!Water!Lawn Irrigation!Permit.js
@@ -1,5 +1,5 @@
 //Record Types:	Water/Water/Lawn Irrigation/Permit
 //Event: ASA - ApplicationSubmitAfter
 
-
+5084_Irrigation_Inspection_Pending();
 script193_WatIrrigationAddInspFee();
---
+++ INCLUDES_SCRIPTS/5084_Irrigation_Inspection_Pending.js
@@ -0,0 +1,25 @@
+// SCRIPTNUMBER: 5084
+// SCRIPTFILENAME: 5084_Irrigation_Inspection_Pending.js
+// PURPOSE: Called when custom field "Type of Property" = "Other" OR "Single Family Residential"
+// DATECREATED: 02/19/2019
+// BY: JMP 
+// CHANGELOG: 
+
+logDebug("Script 5084_Irrigation_Inspection_Pending - Started");
+
+var inspGroup = "WAT_LI";
+var inspType = "Single Family Res Lawn/Irrigation Inspection";
+
+if (appTypeResult == "Water/Water/Lawn Irrigation/Permit") {
+   
+   logDebug (AInfo["Type of Property"] + "");   
+  	
+}
+  
+  // createPendingInspection(inspGroup, inspType + "");
+  
+  // logDebug("JMP JMP Alert: ------------------------>> Script Item #72 - OK working with Inspection type Failed/Passed/Cancelled " + inspResult + "");  
+
+ 
+
+
