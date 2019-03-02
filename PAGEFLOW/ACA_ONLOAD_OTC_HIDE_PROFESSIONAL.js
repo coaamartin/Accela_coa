d4122a86d81db6b4be791e96ffd4f6dee9f56349
khobday <khobday@truepointsolutions.com>
2019-03-02 11:25:06 -0800

    khobday

--- PAGEFLOW/ACA_ONLOAD_OTC_HIDE_PROFESSIONAL.js
+++ PAGEFLOW/ACA_ONLOAD_OTC_HIDE_PROFESSIONAL.js
@@ -1,82 +1,189 @@
-//2000 - Void Fee After - Code from TruePoint for better way to handle NSF 
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA_ONLOAD_OTC_HIDE_PROFESSIONAL
+| Event   : ACA Page Flow onload
+|
+| Usage   : The pageflow script will Hide/Skip the "Licensed Professional" Page based on an ASI field on a previous page
+|         : ASI Fireld is "Homeowner acting as Contractor" with a value of Yes or No
+|
+| Client  : N/A
+| Action# : N/A
+|
+| Notes   :
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+var useCustomScriptFile = true; // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
 
-var showMessage = false;				// Set to true to see results in popup window
-var showDebug = false;				// Set to true to see debug messages in popup window
-var message = "";				// Message String
-var debug = "";					// Debug String
-var br = "<BR>";				// Break Tag
-
-var capId = getCapId();				// CapId object
-var cap = aa.cap.getCap(capId).getOutput();	// Cap object
-var capStatus = cap.getCapStatus();		// Cap status
-var capType = cap.getCapType();     // Cap type
-
-var feeSeqListString = aa.env.getValue("FeeItemsSeqNbrArray");	// invoicing fee item list in string type
-var feeSeqList = new Array();					// fee item list in number type
-for(xx in feeSeqListString)
-{
-	feeSeqList.push(Number(feeSeqListString[xx])); 	// convert the string type array to number type array
-}
-
-var paymentPeriodList = new Array();	// payment periods, system need not this parameter for daily side
-
-// The fee item should not belong to a POS before set the fee item status to "CREDITED".
-if (feeSeqList.length && !(capStatus == '#POS' && capType == '_PER_GROUP/_PER_TYPE/_PER_SUB_TYPE/_PER_CATEGORY'))
-{
-	// the following method will set the fee item status from 'VOIDED' to 'CREDITED' after void the fee item;
-	invoiceResult = aa.finance.createInvoice(capId, feeSeqList, paymentPeriodList);
-	if (invoiceResult.getSuccess())
-	{
-		logMessage("Invoicing assessed fee items is successful.");
-	}
-	else
-	{
-		logDebug("ERROR: Invoicing the fee items assessed to app # " + capId + " was not successful.  Reason: " +  invoiceResult.getErrorMessage());
-	}
-}
-
-if (debug.indexOf("ERROR") > 0)
-{
-	aa.env.setValue("ScriptReturnCode", "1");
-	aa.env.setValue("ScriptReturnMessage", debug);
-}
-else
-{
-	aa.env.setValue("ScriptReturnCode", "0");
-	if (showMessage) 
-	{
-		aa.env.setValue("ScriptReturnMessage", message);
-	}
-	if (showDebug)
-	{
-		aa.env.setValue("ScriptReturnMessage", debug);
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
 	}
 }
-	
-function getCapId()  {
 
-    var s_id1 = aa.env.getValue("PermitId1");
-    var s_id2 = aa.env.getValue("PermitId2");
-    var s_id3 = aa.env.getValue("PermitId3");
-
-    var s_capResult = aa.cap.getCapID(s_id1, s_id2, s_id3);
-    if(s_capResult.getSuccess())
-    {
-      return s_capResult.getOutput();
-    }
-    else
-    {
-      logDebug("ERROR: Failed to get capId: " + s_capResult.getErrorMessage());
-      return null;
-    }
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
 }
 
-function logDebug(dstr)
-{
-	debug += dstr + br;
+eval(getScriptText("INCLUDES_CUSTOM", null, useCustomScriptFile));
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+	if (!servProvCode)
+		servProvCode = aa.getServiceProviderCode();
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScripts) {
+			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
 }
-	
-function logMessage(dstr)
-{
-	message += dstr + br;
-}	
+
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+var servProvCode = capId.getServiceProviderCode() // Service Provider Code
+	var publicUser = false;
+var currentUserID = aa.env.getValue("CurrentUserID");
+var publicUserID = aa.env.getValue("CurrentUserID");
+if (currentUserID.indexOf("PUBLICUSER") == 0) {
+	currentUserID = "ADMIN";
+	publicUser = true
+} // ignore public users
+var capIDString = capId.getCustomID(); // alternate cap id string
+var systemUserObj = aa.person.getUser(currentUserID).getOutput(); // Current User Object
+var appTypeResult = cap.getCapType();
+var appTypeString = appTypeResult.toString(); // Convert application type to string ("Building/A/B/C")
+var appTypeArray = appTypeString.split("/"); // Array of application type string
+var currentUserGroup;
+var currentUserGroupObj = aa.userright.getUserRight(appTypeArray[0], currentUserID).getOutput()
+	if (currentUserGroupObj)
+		currentUserGroup = currentUserGroupObj.getGroupName();
+	var capName = cap.getSpecialText();
+var capStatus = cap.getCapStatus();
+var sysDate = aa.date.getCurrentDate();
+var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(), sysDate.getDayOfMonth(), sysDate.getYear(), "");
+var parcelArea = 0;
+
+var estValue = 0;
+var calcValue = 0;
+var feeFactor // Init Valuations
+var valobj = aa.finance.getContractorSuppliedValuation(capId, null).getOutput(); // Calculated valuation
+if (valobj.length) {
+	estValue = valobj[0].getEstimatedValue();
+	calcValue = valobj[0].getCalculatedValue();
+	feeFactor = valobj[0].getbValuatn().getFeeFactorFlag();
+}
+
+var balanceDue = 0;
+var houseCount = 0;
+feesInvoicedTotal = 0; // Init detail Data
+var capDetail = "";
+var capDetailObjResult = aa.cap.getCapDetail(capId); // Detail
+if (capDetailObjResult.getSuccess()) {
+	capDetail = capDetailObjResult.getOutput();
+	var houseCount = capDetail.getHouseCount();
+	var feesInvoicedTotal = capDetail.getTotalFee();
+	var balanceDue = capDetail.getBalance();
+}
+
+var AInfo = new Array(); // Create array for tokenized variables
+loadAppSpecific4ACA(AInfo); // Add AppSpecific Info
+//loadTaskSpecific(AInfo);						// Add task specific info
+//loadParcelAttributes(AInfo);						// Add parcel attributes
+//loadASITables();
+
+logDebug("<B>EMSE Script Results for " + capIDString + "</B>");
+logDebug("capId = " + capId.getClass());
+logDebug("cap = " + cap.getClass());
+logDebug("currentUserID = " + currentUserID);
+logDebug("currentUserGroup = " + currentUserGroup);
+logDebug("systemUserObj = " + systemUserObj.getClass());
+logDebug("appTypeString = " + appTypeString);
+logDebug("capName = " + capName);
+logDebug("capStatus = " + capStatus);
+logDebug("sysDate = " + sysDate.getClass());
+logDebug("sysDateMMDDYYYY = " + sysDateMMDDYYYY);
+logDebug("parcelArea = " + parcelArea);
+logDebug("estValue = " + estValue);
+logDebug("calcValue = " + calcValue);
+logDebug("feeFactor = " + feeFactor);
+
+logDebug("houseCount = " + houseCount);
+logDebug("feesInvoicedTotal = " + feesInvoicedTotal);
+logDebug("balanceDue = " + balanceDue);
+
+// page flow custom code begin
+
+try {
+
+ 
+//The ASI Field is - Homeowner acting as Contractor
+
+
+	var thisPageValue = "No";  // change per page
+	var showPage = (thisPageValue.equals(AInfo["Homeowner acting as Contractor"]));
+
+	if (!showPage) {    
+		aa.env.setValue("ReturnData", "{'PageFlow': {'HidePage' : 'Y'}}"); 
+	}
+
+} catch (err) {
+
+	logDebug(err);
+
+}
+
+// page flow custom code end
+
+
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+} else {
+	if (cancel) {
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	} else {
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	}
+}
