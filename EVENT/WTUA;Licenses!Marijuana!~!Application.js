695f5c172f022e30d85127223caf94ca94d9ad0b
Porter <jmporter@auroragov.org>
2019-03-14 09:10:51 -0600

    JMP

--- EVENT/WTUA;Licenses!Marijuana!~!Application.js
+++ EVENT/WTUA;Licenses!Marijuana!~!Application.js
@@ -9,6 +9,7 @@
 | Created at	: 05/02/2018 08:28:24
 |
 /------------------------------------------------------------------------------------------------------*/
-if ( (wfTask=="City Application Intake" || wfTask=="State Application Intake" || wfTask=="Application Review") && wfStatus=="Additional Info Required"){
-	210_SendMJEmail();
+if ( (wfTask=="City Application Intake" || wfTask=="State Application Intake" || wfTask=="Application Review") && wfStatus=="Additional Info Required")
+{
+	include("210_SendMJEmail");
 }
