de8ad51489caf2e7c2889cfefabd87c649ebe98e
khobday <khobday@truepointsolutions.com>
2019-04-08 22:26:10 -0700

    khobday

--- EVENT/IRSA;Building!Permit!~!~.js
+++ EVENT/IRSA;Building!Permit!~!~.js
@@ -4,8 +4,9 @@
 if (matchARecordType([
     "Building/Permit/New Building/NA",
     "Building/Permit/Plans/NA",
-    "Building/Permit/No Plans/NA"
-], appTypeString)) {
+    "Building/Permit/No Plans/NA",
+    "Building/Permit/OTC/NA"
+	], appTypeString)) {
     if (ifTracer(inspType == "Electrical Rough" || inspType  == "Mechanical Rough", "inspType == Electrical Rough or Mechanical Rough")) {
         include("260_EmailExcelEnergyOnInspectionResult");
     }
