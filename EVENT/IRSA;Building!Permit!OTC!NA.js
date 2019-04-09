a6db71fb5ae60c56edafd573070c11d3dea1caeb
khobday <khobday@truepointsolutions.com>
2019-04-08 22:12:02 -0700

    khobday

---
+++ EVENT/IRSA;Building!Permit!OTC!NA.js
@@ -0,0 +1,8 @@
+//
+bldScript6_FinalInspComplete();
+
+include("66_Building_Inspection_Failed_Passed_Final");
+
+//  10-26-18  JMP
+include("72_Inspection_Pending");
+include("73_Inspection_Leftover");
