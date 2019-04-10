2f575536801274c814fd144fd0ce5547782875b4
Porter <jmporter@auroragov.org>
2019-04-10 09:15:59 -0600

    JMP

--- INCLUDES_SCRIPTS/5092_RequireNoteOnFail.js
+++ INCLUDES_SCRIPTS/5092_RequireNoteOnFail.js
@@ -4,7 +4,7 @@
 // DATECREATED: 04/09/2019
 // BY: JMP
 
-var wfComment = "";
+var wfComment = "Hello JP";
 
 logDebug('Started script 5092_RequireNoteOnFail');
 
@@ -12,19 +12,21 @@ if (wfStatus == "Fail") //There is only 1 WFStep so no reason to check
 {
    logDebug(wfComment.text); 
    
-   if (wfComment == null || typeof wfComment == 'undefined') 
-   {   
-
-   logDebug('Comments are required when a status of Fail has been indicated');
-   //jmp
-   comment("<B><Font Color=RED>Comments are required when a status of Fail has been indicated </Font></B>");
-   cancel = true;
+   if (wfComment != null && typeof wfComment !== 'undefined')       
+   {
+     logDebug('Script#5092 Comments have been provided');
    
    }
    // JMP
+   
    else
       
    {
+      
+   logDebug('Script#5092 - Comments were not provided when the status of Fail had been indicated');
+   //jmp
+   comment("<B><Font Color=RED>Comments are required when a status of Fail has been indicated </Font></B>");
+   cancel = true;
       
     logDebug(wfComment);  
       
