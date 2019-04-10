c3ff4a1a3bf8389857686b07055ae0eae61dccc6
Porter <jmporter@auroragov.org>
2019-04-10 10:16:25 -0600

    JMP

---
+++ EVENT/WTUB;Water!Utility!Service!NA.js
@@ -0,0 +1,8 @@
+// SCRIPTEVENT: WTUB:Water!Utility!Service!NA
+// PURPOSE: Created to handle new status of 'Fail' - Issue log #12
+// DATECREATED: 4/9/2019
+// BY: JMP
+
+//include("5092_RequireNoteOnFail");
+
+logDebug('Removing call for SCRIPT#5092');
