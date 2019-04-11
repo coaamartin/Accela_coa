b5b82f47bcf38b69de9db92e65093b4ab04126ab
Porter <jmporter@auroragov.org>
2019-04-11 10:43:34 -0600

    JMP

--- INCLUDES_SCRIPTS/5092_RequireNoteOnFail.js
+++ INCLUDES_SCRIPTS/5092_RequireNoteOnFail.js
@@ -9,7 +9,7 @@ logDebug('Started script 5092_RequireNoteOnFail');
 if (wfStatus == "Fail" && wfTask == "Inspection") //There is only 1 WFStep so no reason to check wfTask however included for future changes
 {
     
-   if (typeof wfComment !== 'undefined' || wfComment == null)      // Ojbect will not exist if blank  
+   if (typeof wfComment !== 'undefined')      // Ojbect will not exist if blank  
    {
 
      logDebug('Script#5092 - Comments were not provided when the status of Fail had been indicated');
@@ -17,13 +17,22 @@ if (wfStatus == "Fail" && wfTask == "Inspection") //There is only 1 WFStep so no
      cancel = true;
    }
    
-   else        
+   else 
       
-   {
+     if (typeof wfComment == null)      // Ojbect will not exist if blank  
+     {      
+   
+       logDebug('Script#5092 - Comments were not provided when the status of Fail had been indicated');
+       comment("<B><Font Color=RED>Comments are required when a status of Fail has been indicated </Font></B>");
+       cancel = true;
+   
+     }   
+     else
+     {
        
-     closeTask("Inspection", "Completed", "EMSE ID 5092", "EMSE ID 5092");
+       closeTask("Inspection", "Completed", "EMSE ID 5092", "EMSE ID 5092");
      
-   }  
+     }  
 }  
 
 logDebug('Ended script 5092_RequireNoteOnFail');
