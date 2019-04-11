9ce58e4fc3d1103744648d7ec88e93ab5101187d
Porter <jmporter@auroragov.org>
2019-04-11 09:13:11 -0600

    JMP

--- INCLUDES_SCRIPTS/32_recStatusUpdateWaterTap.js
+++ INCLUDES_SCRIPTS/32_recStatusUpdateWaterTap.js
@@ -1,9 +1,12 @@
 //created by swakil
+//Updated per Issue Tracker #8 JMP: 4/11/19
 
-if(wfTask=="Water Meter Set" && wfStatus=="Completed"){
+logDebug("Script #32 - Start");
+
+if(wfTask=="Application Submittal" && wfStatus=="Completed"){
 	var valid = false;
 	var taskstatustocheck = "Ready to Pay with Specific Contract Fees";
-	var newAppStatus = "Closed-Specific Contract Fees";
+	var newAppStatus = "Specific Contract Fees-Accepted";
 	var tasks = aa.workflow.getHistory(capId).getOutput();
 	for (i in tasks){
 		task = tasks[i];
@@ -13,7 +16,9 @@ if(wfTask=="Water Meter Set" && wfStatus=="Completed"){
 		}
 	}
 	if(valid){
-		updateAppStatus(newAppStatus, "Updated by EMSE");
+		updateAppStatus(newAppStatus, "Updated by EMSE Script #32");
 		logDebug("Updated App Status");
 	}
 }
+
+logDebug("Script #32 - End");
