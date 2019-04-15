0a678d7a97e5d421e4e7664ee6c4197aca6e240f
Martin <amartin@auroragov.org>
2019-04-15 10:02:06 -0600

    ajm

--- INCLUDES_SCRIPTS/func_5097_Enforcement_check4Dups.js
+++ INCLUDES_SCRIPTS/func_5097_Enforcement_check4Dups.js
@@ -1,46 +1,86 @@
-// SCRIPTNUMBER: 5097
-// SCRIPTFILENAME: 5097_Enforcement_check4Dups.js
-// PURPOSE: Checks for duplicates records based on address and type.
-// DATECREATED: 04/10/2019
-// BY: amartin
-// CHANGELOG: 
-//Script Tester header.  Comment this out when deploying.
-//var myCapId = "19-000022-CVM";
-//var myUserId = "AMARTIN";
-//var eventName = "";
-//var wfTask = "Renewal Registration";
-//var wfStatus = "Sent Renewal";
-//var wfComment = "";
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA_BEFORE_LOCATION.js
+| Event   : ACA_BEFORE_LOCATION
+|
+| Usage   : Master Script by Accela.  See accompanying documentation and release notes.
+|
+| Client  : N/A
+| Action# : N/A
+|
+| Notes   :
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+var useCustomScriptFile = true; // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
 
-//var useProductScript = true;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
-//var runEvent = true; // set to true to simulate the event and run all std choices/scripts for the record type.  
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+    useSA = true;
+    SA = bzr.getOutput().getDescription();
+    bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+    if (bzr.getSuccess()) {
+        SAScript = bzr.getOutput().getDescription();
+    }
+}
 
-///* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
+if (SA) {
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
+    eval(getScriptText(SAScript, SA));
+} else {
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
+}
 
-/*
-User code generally goes inside the try block below.
-*/
+//eval(getScriptText("INCLUDES_CUSTOM", null, useCustomScriptFile));
 
-//try 
-//{
-//your code here
-//End script Tester header 
-//functions cannot start with a number so I preceded it with func_
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+    if (!servProvCode)
+        servProvCode = aa.getServiceProviderCode();
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    try {
+        if (useProductScripts) {
+            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+        } else {
+            var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+        }
+        return emseScript.getScriptText() + "";
+    } catch (err) {
+        return "";
+    }
+}
 
-func_5097_Enforcement_check4Dups();
-
-function func_5097_Enforcement_check4Dups() {
-eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));	
-	var possibleDupAltIds = "";	
-	var cap = aa.env.getValue("CapModel");
-	var capId = cap.getCapID();
-	var parentId = cap.getParentCapID();
-	var appTypeResult = cap.getCapType();
-	var appTypeString = appTypeResult.toString();           // Convert application type to string ("Building/A/B/C")
-	var appTypeArray = appTypeString.split("/");            // Array of application type string	
-
-    try{
-    //if(ifTracer(appTypeString.startsWith("Enforcement/Incident/Vacant/"), '"Enforcement/Incident/Vacant/"')){
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+var parentId = cap.getParentCapID();
+var appTypeResult = cap.getCapType();
+var appTypeString = appTypeResult.toString();           // Convert application type to string ("Building/A/B/C")
+var appTypeArray = appTypeString.split("/");            // Array of application type string
+// page flow custom code begin
+try{
+    //Script 26
+    if(ifTracer(appTypeString.startsWith("Enforcement/Incident/Vacant/"), '"Enforcement/Incident/Vacant/"')){
         
         var capIdsArray = capIdsGetByAddr4ACA(); //Get all records for same address
         var forestryRecsOpen = false;
@@ -56,49 +96,39 @@ eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
 
             logDebug("sameAddrAltId:" + sameAddrAltId + ", status: " + sameAddrCapStatus);
             //if type is Forestry/ and a status match cancel
-            if(ifTracer(sameAddrAppTypeString.startsWith("Enforcement/Incident/Vacant/Master/") &&
+            if(ifTracer(sameAddrAppTypeString.startsWith("Enforcement/Incident/Vacant/") &&
                         matches(sameAddrCapStatus, "Application","Monitoring","Recorded","Recorded and Assessed","Recorded and Expired","Registered","Registered and Expired","Registered and Recorded"),
                         'Code Vacancy with correct status')){
                 message += "It appears someone has already submitted a request for this Address.  Please call the Enforcement Department at 303-739-XXXX if you wish to submit an additional request.<BR>";
-possibleDupAltIds +=  "Found1,"; 
- break;
+                break;
             }
         }
-    //}		
-		/*
-            var capAddResult = aa.cap.getCapListByDetailAddress(AddressStreetName,parseInt(AddressHouseNumber),AddressStreetSuffix,AddressZip,AddressStreetDirection,null);
-			logDebug("---------------------> var capAddResult is: " + capAddResult);		
-            if(!capAddResult.getSuccess()) return;
-            logDebug("---------------------> Found a cap.");
-            var capIdArray = capAddResult.getOutput();
-                        logDebug("---------------------> capIdArray: " + capIdArray);
-            for (cappy in capIdArray){
-                var relCapId = capIdArray[cappy].getCapID();
-			                        logDebug("---------------------> inside for loop.  relCapId is: " + relCapId);	
-                var relCap = aa.cap.getCap(relCapId).getOutput();
-                // get cap type
-                var relType = relCap.getCapType().toString();
-                var relStatus = relCap.getCapStatus();
-                //Add new blocks here for other records.
-                if(relType.startsWith("Enforcement/Incident/Vacant/Master") && matches(relStatus, "Application","Monitoring","Recorded","Recorded and Assessed","Recorded and Expired","Registered","Registered and Expired","Registered and Recorded")){
-                    possibleDupAltIds += relCapId.getCustomID() + ",";
-                        logDebug("---------------------> inside If that matches on record and status  " + relType.startsWith); 
-				}
-            }
-			*/
-            
-            if(possibleDupAltIds.length > 0){
-                cancel = true;
-                showMessage = true;
-                comment("Possible duplicates: " + possibleDupAltIds.substring(0, possibleDupAltIds.length -1));
-            }
-    }
-    catch(err){
-				logDebug("---------------------> caught error.");
-		cancel = true;
-        showMessage = true;
-        comment("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber);
-        logDebug("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber + ". Stack: " + err.stack);
+    }//END Script 26
+    showMessage = cancel = message.length ? true : false;
+}
+catch(err){
+    //cancel = true;
+    //showDebug = 3;
+    logDebug("Error on custom pageflow ACA_BEFORE_LOCATION. Err: " + err);
+}
+
+// page flow custom code end
+if (debug.indexOf("**ERROR") > 0) {
+    aa.env.setValue("ErrorCode", "1");
+    aa.env.setValue("ErrorMessage", debug);
+} else {
+    if (cancel) {
+        aa.env.setValue("ErrorCode", "-2");
+        if (showMessage)
+            aa.env.setValue("ErrorMessage", message);
+        if (showDebug)
+            aa.env.setValue("ErrorMessage", debug);
+    } else {
+        aa.env.setValue("ErrorCode", "0");
+        if (showMessage)
+            aa.env.setValue("ErrorMessage", message);
+        if (showDebug)
+            aa.env.setValue("ErrorMessage", debug);
     }
 }
 
@@ -167,32 +197,3 @@ function ifTracer (cond, msg) {
     logDebug((cond).toString().toUpperCase() + ': ' + msg);
     return cond;
 }
-
-function getScriptText(vScriptName, servProvCode, useProductScripts) {
-    if (!servProvCode)
-        servProvCode = aa.getServiceProviderCode();
-    vScriptName = vScriptName.toUpperCase();
-    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
-    try {
-        if (useProductScripts) {
-            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
-        } else {
-            var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
-        }
-        return emseScript.getScriptText() + "";
-    } catch (err) {
-        return "";
-    }
-}
-
-logDebug("---------------------> 5097_Enforcement_check4Dups.js ended.");
-
-//Script Tester footer.  Comment this out when deploying.
-//}	
-
-//catch (err) 
-//{
-//	logDebug("A JavaScript Error occured: " + err.message);
-//}
-//aa.env.setValue("ScriptReturnCode", "0");
-//aa.env.setValue("ScriptReturnMessage", debug)
