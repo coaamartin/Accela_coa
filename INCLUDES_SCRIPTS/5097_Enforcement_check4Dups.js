c00b8151a2d333529887186a945771a78ce2bfa4
Martin <amartin@auroragov.org>
2019-04-16 08:41:03 -0600

    ajm

--- INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
+++ INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
@@ -34,10 +34,11 @@ logGlobals(AInfo);
             if(!capAddResult.getSuccess()) {logDebug("the message was no data found. ");};
             logDebug("Found something so moving on. " + capAddResult);
             var capIdArray = capAddResult.getOutput();
-              logDebug("Found something so moving on. " + capIdArray);          
-            for (cappy in capIdArray){
+              logDebug("Found something so moving on. " + capIdArray); 
+y=0;			  
+            for (y in capIdArray){
 				            logDebug("inside for loop. ");
-                var relCapId = capIdArray[cappy].getCapID();
+                var relCapId = capIdArray[y].getCapID();
                 var relCap = aa.cap.getCap(relCapId).getOutput();
                 // get cap type
                 var relType = relCap.getCapType().toString();
