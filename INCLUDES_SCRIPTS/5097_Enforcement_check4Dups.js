53faa0d4bcce3664069c7450d04c20e1ddb1b3d7
Martin <amartin@auroragov.org>
2019-04-16 10:06:27 -0600

    ajm

--- INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
+++ INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
@@ -1,13 +1,10 @@
-//function 5097_Enforcement_check4Dups(){
-
+//ajm added for testing
+logDebug("Entering AJM TEST");
 
 /*------------------------------------------------------------------------------------------------------/
-| END User Configurable Parameters
+| BEGIN Event Specific Variables
 /------------------------------------------------------------------------------------------------------*/
-
-
-
-
+//Log All Environmental Variables as  globals
 var params = aa.env.getParamValues();
 var keys = params.keys();
 var key = null;
@@ -21,64 +18,70 @@ while (keys.hasMoreElements()) {
 | END Event Specific Variables
 /------------------------------------------------------------------------------------------------------*/
 
+//if (preExecute.length)
+//	doStandardChoiceActions(preExecute, true, 0); // run Pre-execution code
 
 logGlobals(AInfo);
 
-	//var vAddress;
-	//AddedAddressList = AddedAddressList.toArray();	
-	//logDebug("the message was " + AddedAddressList);
-	//x = 0;
-	//for (x in AddedAddressList) {
-	//	vAddress = AddedAddressList[x];
-	//logDebug("the address was " + AddedAddressList);		
-	//}
-    try{
-        var checkForDups = AInfo["Check for Duplicates"];
-        var possibleDupAltIds = "";
-        
-        //if(ifTracer(checkForDups == "CHECKED", 'Checking for duplicates')){
-			logDebug("1 " + AddressStreetName);
-			logDebug("1 " + AddressHouseNumber);
-			logDebug("1 " + AddressStreetSuffix);
-			logDebug("1 " + AddressZip);
-			logDebug("1 " + AddressStreetDirection);			
-            var capAddResult = aa.cap.getCapListByDetailAddress(AddressStreetName,parseInt(AddressHouseNumber),AddressStreetSuffix,AddressZip,AddressStreetDirection,null);
-            if(!capAddResult.getSuccess()) {logDebug("the message was no data found. ");};
-            logDebug("Found something so moving on. " + capAddResult);
-            var capIdArray = capAddResult.getOutput();
-              logDebug("Found something so moving on. " + capIdArray); 
-y=0;			  
-var relCapId = capIdArray[y].getCapID();
-			logDebug("relCapId " + relCapId);
-var relCap = aa.cap.getCap(relCapId).getOutput();
-			logDebug("relCap " + relCap);
-var relType = relCap.getCapType().toString();		
-			logDebug("relType " + relType);
-var relStatus = relCap.getCapStatus();	
-			logDebug("relStatus " + relStatus);
-            for (y in capIdArray){
-				            logDebug("inside for loop. ");
-                var relCapId = capIdArray[y].getCapID();
-                var relCap = aa.cap.getCap(relCapId).getOutput();
-                // get cap type
-                var relType = relCap.getCapType().toString();
-                var relStatus = relCap.getCapStatus();
-                
-                if(relType.startsWith("Enforcement/Incident/Vacant/Master") && matches(relStatus, "Monitoring","Assigned","Submitted","Working","No Plant","Planted","Removed","Complete Not Stake", "Complete Staked","Warranty Failed")){
-                    possibleDupAltIds += relCapId.getCustomID() + ",";
-                }
-            }
+deleteCadRows();
+
+function deleteCadRows()
+{
+	 var cadQuery = getRemovedCADAddresses();
+
+        var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
+        var ds = initialContext.lookup("java:/AA");
+        var conn = ds.getConnection();
+        var sStmt = conn.prepareStatement(cadQuery);
+		//sStmt.setString(1, 'APD Caution');
+        var rSet = sStmt.executeQuery();
+        var counter = 0;
+        while (rSet.next()) {
+			counter = counter + 1;
+			var foundDuplicate = rSet.getString("Found");
+
+			logDebug("Found a duplicate: " + foundDuplicate);			
+			//aa.addressCondition.removeAddressCondition(refAddrid, conId);
+        }
+        sStmt.close();
+        conn.close();
+	    logDebug("Done with this:" + counter);
+		//sendEmailToApplicant();
+}
+
+//Get addresses to be remove(It was removed from CAD).
+function getRemovedCADAddresses()
+{
+	var altId = capId.getCustomID();
+	var cadQuery = "exec coa_duplicate_for_address " + AddressValidatedNumber + ",'" + ApplicationTypeLevel1 + "','" + ApplicationTypeLevel2 + "','" + ApplicationTypeLevel3 + "','" + ApplicationTypeLevel4 + "'";
+	return cadQuery;
+}
+
+function sendEmailToApplicant(){
+  var contacts = "Applicant";
+  var template = "PW_LIC_AGR_REV";
+  var lictype = "Adrianlictype" + ""; //force string
+  var wireless = "Adrianwireless" + ""; //force string
+  var flagpole = "Adrianflagpoles" + ""; //force string
+  var emailparams = aa.util.newHashtable();
+  emailparams.put("$$PERMITID$$", lictype)
+  emailparams.put("$$TASKCOMMENTS$$", wireless);
+  emailparams.put("$$PERMITADDR$$", flagpole);
+  emailparams.put("$$PERMITWRKDESC$$", flagpole); 
+  emailContacts(contacts, template, emailparams, "", "", "N", "");
+}
+//end ajm add
             
-            if(possibleDupAltIds.length > 0){
-                cancel = true;
-                showMessage = true;
-                comment("Possible duplicates: " + possibleDupAltIds.substring(0, possibleDupAltIds.length -1));
-            }
+            //if(possibleDupAltIds.length > 0){
+            //    cancel = true;
+            //    showMessage = true;
+            //    comment("Possible duplicates: " + possibleDupAltIds.substring(0, possibleDupAltIds.length -1));
+            //}
         //}
-    }
-    catch(err){
-        showMessage = true;
-        comment("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber);
-        logDebug("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber + ". Stack: " + err.stack);
-    }
+    //}
+    //catch(err){
+    //    showMessage = true;
+    //    comment("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber);
+    //    logDebug("Error on custom function (). Please contact administrator. Err: " + err + ". Line: " + err.lineNumber + ". Stack: " + err.stack);
+    //}
 //}
