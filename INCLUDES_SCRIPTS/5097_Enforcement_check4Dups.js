1cbe747910fe69e82815903476589de6593ae941
Martin <amartin@auroragov.org>
2019-04-16 08:57:22 -0600

    ajm

--- INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
+++ INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
@@ -1,4 +1,86 @@
 //function 5097_Enforcement_check4Dups(){
+var triggerEvent = aa.env.getValue("EventName");
+var controlString = null;
+var documentOnly = false; // Document Only -- displays hierarchy of std choice steps
+
+
+var preExecute = "PreExecuteForAfterEvents"; //Assume after event unless before decected
+var eventType = "After"; //Assume after event
+if (triggerEvent != "") {
+	controlString = triggerEvent; // Standard choice for control
+	if (triggerEvent.indexOf("Before") > 0) {
+		preExecute = "PreExecuteForBeforeEvents";
+		eventType = "Before";
+	}
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var SCRIPT_VERSION = 3.0;
+var useCustomScriptFile = true; // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
+	}
+}
+
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, useCustomScriptFile));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", null, useCustomScriptFile));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM", null, useCustomScriptFile));
+
+if (documentOnly) {
+	doStandardChoiceActions(controlString, false, 0);
+	aa.env.setValue("ScriptReturnCode", "0");
+	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");
+	aa.abortScript();
+}
+
+var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", vEventName);
+
+var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
+var doStdChoices = true; // compatibility default
+var doScripts = false;
+var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
+if (bzr) {
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
+	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
+	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+}
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+	if (!servProvCode)
+		servProvCode = aa.getServiceProviderCode();
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScripts) {
+			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+
+
 var params = aa.env.getParamValues();
 var keys = params.keys();
 var key = null;
