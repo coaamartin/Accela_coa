9df75fbca1da23af5d88656d6b72779ff8b01b3d
Martin <amartin@auroragov.org>
2019-04-16 09:07:26 -0600

    ajm

--- INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
+++ INCLUDES_SCRIPTS/5097_Enforcement_check4Dups.js
@@ -4,44 +4,14 @@
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
 /------------------------------------------------------------------------------------------------------*/
-var SCRIPT_VERSION = 3.0;
-var useCustomScriptFile = true; // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
-var useSA = false;
-var SA = null;
-var SAScript = null;
-var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
-if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
-	useSA = true;
-	SA = bzr.getOutput().getDescription();
-	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
-	if (bzr.getSuccess()) {
-		SAScript = bzr.getOutput().getDescription();
-	}
-}
 
-if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, useCustomScriptFile));
-	eval(getScriptText(SAScript, SA));
-} else {
 	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
 	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", null, useCustomScriptFile));
-}
+
 
 eval(getScriptText("INCLUDES_CUSTOM", null, useCustomScriptFile));
 
-var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", vEventName);
 
-var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
-var doStdChoices = true; // compatibility default
-var doScripts = false;
-var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
-if (bzr) {
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
-	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
-	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
-}
 
 function getScriptText(vScriptName, servProvCode, useProductScripts) {
 	if (!servProvCode)
