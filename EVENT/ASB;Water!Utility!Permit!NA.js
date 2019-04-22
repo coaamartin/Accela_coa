3e75118e5736145977012a31ba48a8d3f65d2249
Porter <jmporter@auroragov.org>
2019-04-22 10:45:16 -0600

    JMP

--- EVENT/ASB;Water!Utility!Permit!NA.js
+++ EVENT/ASB;Water!Utility!Permit!NA.js
@@ -2,4 +2,4 @@ wtrScript131_checkASITbefore();
 
 //jmain 08-01-2018 included into this event per COA script tracker - this script
 //to be replaced by a page flow script, I think.  SWAKIL is on it.
-//include("11_WaterUtilityNeedPhaseWorkPlanForPermit");
+//include("5011_WaterUtilityNeedPhaseWorkPlanForPermit");
