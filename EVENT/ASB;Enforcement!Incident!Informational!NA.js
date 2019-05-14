5c7649798c5029aafd03223966dad8eb522106e0
Adrian Martin <amartin@auroragov.org>
2019-05-14 15:26:07 -0600

    ajm

--- EVENT/ASB;Enforcement!Incident!Informational!NA.js
+++ EVENT/ASB;Enforcement!Incident!Informational!NA.js
@@ -1,10 +1,4 @@
 logDebug("ASB:Enforcement/Incident/Informational/NA");
-//if (ApplicationTypeLevel1 == "Enforcement" && ApplicationTypeLevel2 == "Incident" && ApplicationTypeLevel3 == "Informational" && ApplicationTypeLevel4 = "NA") {
-//	if (AInfo["Type of Issue"] == "BANNERS") {		
-//		include("5097_Enforcement_check4Dups");
-//	}
-//}
-
 var checkForBanners = AInfo["Type of Issue"];
 if(ifTracer(checkForBanners == "BANNERS", 'Checking for duplicate BANNERS Events.')){
 	include("5097_Enforcement_check4Dups");
