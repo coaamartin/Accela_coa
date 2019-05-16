71ce979d179f1dfa529047137e66fe3c7e01b0c6
Porter <jmporter@auroragov.org>
2019-05-15 18:47:03 -0600

    JMP

--- EVENT/ASA;Licenses!Contractor!General!Application.js
+++ EVENT/ASA;Licenses!Contractor!General!Application.js
@@ -1,5 +1,8 @@
 
 // script 91
+
+updateTask("License Issuance", "Active", "Updated by Issue Log #60", "Updated by Issue Log #60");
+
 var applicantContact = getContactObj(capId,"Contractor Applicant");
 if (applicantContact) {
 	editAppName(applicantContact.people.getBusinessName());
