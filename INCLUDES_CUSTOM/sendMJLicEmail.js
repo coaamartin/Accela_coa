7b6fc8887cb29b1fcd3c94460cef7131effca307
Suhail Wakil <suhailwakil@gmail.com>
2019-05-18 15:46:28 -0700

    script 226 fix

--- INCLUDES_CUSTOM/sendMJLicEmail.js
+++ INCLUDES_CUSTOM/sendMJLicEmail.js
@@ -2,13 +2,32 @@
 function sendMJLicEmail(itemCap){
     logDebug("sendMJLicEmail() started.");
     try{
+        var asiValues = new Array();
+        loadAppSpecific(asiValues); 
+        var adResult = aa.address.getAddressByCapId(capId).getOutput(); 
+        for(x in adResult)
+        {
+            var adType = adResult[x].getAddressType(); 
+            var stNum = adResult[x].getHouseNumberStart();
+            var preDir =adResult[x].getStreetDirection();
+            var stName = adResult[x].getStreetName(); 
+            var stType = adResult[x].getStreetSuffix();
+            var city = adResult[x].getCity();
+            var state = adResult[x].getState();
+            var zip = adResult[x].getZip();
+        }
+        var primaryAddress = stNum + " " + preDir + " " + stName + " " + stType + " " + "," + city + " " + state + " " + zip;
+
         var vEmailTemplate = "LIC MJ APPROVAL OF LICENSE #226 - 230";
         var vReportTemplate = "MJ_License";
         var vEParams = aa.util.newHashtable();
         addParameter(vEParams, "$$ApplicationName$$", appTypeAlias);
         addParameter(vEParams, "$$recordAlias$$", appTypeAlias);
         addParameter(vEParams, "$$wfComment$$", wfComment);
-        
+        addParameter(VEParams, "$$StateLicenseNumber$$", asiValues["State License Number"]);  
+        addParameter(VEParams, "$$TradeName$$", asiValues["Trade Name"]);
+        addParameter(VEParams, "$$FullAddress$$", primaryAddress); 
+
         var vRParams = aa.util.newHashtable();
         addParameter(vRParams, "Record_ID", itemCap.getCustomID());
         
