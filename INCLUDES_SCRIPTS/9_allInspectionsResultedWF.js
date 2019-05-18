f804d6dc62a4935e93ea873dc5bd0267318446ea
Suhail Wakil <suhailwakil@gmail.com>
2019-05-17 20:11:30 -0700

    script 9

--- INCLUDES_SCRIPTS/9_allInspectionsResultedWF.js
+++ INCLUDES_SCRIPTS/9_allInspectionsResultedWF.js
@@ -11,32 +11,6 @@ if (wfTask == "License Issuance" && wfStatus == "Issued"){
     if(!resulted){
       cancel = true;
       showMessage = true;
-      logDebug("<h2 style='background-color:rgb(255, 0, 0);'>Inspections are not completed </h2>");
+      logDebug("<B>Inspections are not completed </B>");
     } 
-}
-
-function allInspectionsResulted(typeArray, statusArray) {
-    logDebug("---------------------> In the allInspectionsResulted function");       
-    var inspResultObj = aa.inspection.getInspections(capId);
-    if (inspResultObj.getSuccess()) {
-        inspList = inspResultObj.getOutput();
-        var processedInspTypes = [];
-
-        for (xx in inspList) {
-            if(exists(inspList[xx].getInspectionType(), typeArray) && exists(inspList[xx].getInspectionStatus(), statusArray)){
-                processedInspTypes.push(inspList[xx].getInspectionType());  
-            }else{
-                return false;
-            }
-        }
-        for (yy in typeArray){
-            if (!exists(typeArray[yy], processedInspTypes)){
-               return false; 
-            }
-        }
-       return true;
-    }else{
-        return false;
-    }
-    return false;
 }
