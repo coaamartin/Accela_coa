c7443c09e53f806dd69cdaa1a5ab33a5e76216cf
Suhail Wakil <suhailwakil@gmail.com>
2019-05-18 16:12:15 -0700

    updated

--- INCLUDES_CUSTOM/sendMJLicEmail.js
+++ INCLUDES_CUSTOM/sendMJLicEmail.js
@@ -17,7 +17,10 @@ function sendMJLicEmail(itemCap){
             var zip = adResult[x].getZip();
         }
         var primaryAddress = stNum + " " + preDir + " " + stName + " " + stType + " " + "," + city + " " + state + " " + zip;
-
+        var acaURLDefault = lookup("ACA_CONFIGS", "ACA_SITE");
+        acaURLDefault = acaURLDefault.substr(0, acaURLDefault.toUpperCase().indexOf("/ADMIN"));
+        var recordDeepUrl = getACARecordURL(acaURLDefault);
+    
         var vEmailTemplate = "LIC MJ APPROVAL OF LICENSE #226 - 230";
         var vReportTemplate = "MJ_License";
         var vEParams = aa.util.newHashtable();
@@ -27,11 +30,13 @@ function sendMJLicEmail(itemCap){
         addParameter(vEParams, "$$StateLicenseNumber$$", asiValues["State License Number"]);  
         addParameter(vEParams, "$$TradeName$$", asiValues["Trade Name"]);
         addParameter(vEParams, "$$FullAddress$$", primaryAddress); 
+        addParameter(vEParams, "$$acaRecordUrl$$", recordDeepUrl);
 
         var vRParams = aa.util.newHashtable();
         addParameter(vRParams, "Record_ID", itemCap.getCustomID());
         
         emailContactsWithReportLinkASync("Applicant,Responsible Party", vEmailTemplate, vEParams, vReportTemplate, vRParams, null, null, itemCap);
+        //emailContactsWithReportLinkASync("Applicant,Responsible Party", emailTemplate, eParams, "", "", "N", "");
     }
     catch(err){
         showMessage = true;
