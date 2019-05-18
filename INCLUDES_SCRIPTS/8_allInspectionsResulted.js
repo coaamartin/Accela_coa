ad621fd6185743c2881bd047e5b5103a7f3ff064
Suhail Wakil <suhailwakil@gmail.com>
2019-05-17 20:30:55 -0700

    script9

--- INCLUDES_SCRIPTS/8_allInspectionsResulted.js
+++ INCLUDES_SCRIPTS/8_allInspectionsResulted.js
@@ -1,3 +1,11 @@
+// SCRIPTNUMBER: 8
+// SCRIPTFILENAME: 8_allInspectionsResultedWF.js
+// PURPOSE: Once all 5 application inspection types have been been resulted "Passed" or "Passed - Minor Violations", 
+//              on a Licenses/Marijuana/*/Application, updates Workflow Task "Inspection Phase" with the Task Status "Complete"
+// DATECREATED: 05/17/2019
+// BY: SWAKIL
+// CHANGELOG: 05/17/2019 , SWAKIL Created this file. It calls allInspectionsResulted function, that is added to includes_custom
+
 var typeArray = ["MJ AMED Inspections",
                 "MJ Building Inspections",
                 "MJ Code Enforcement Inspections",
--- INCLUDES_SCRIPTS/9_allInspectionsResultedWF.js
+++ INCLUDES_SCRIPTS/9_allInspectionsResultedWF.js
@@ -1,3 +1,12 @@
+// SCRIPTNUMBER: 9
+// SCRIPTFILENAME: 9_allInspectionsResultedWF.js
+// PURPOSE: Prevent wfTask "License Issuance" from being statused "Issued" if:
+//              inspection types on an MJ Application has not yet been resulted "Passed" or "Passed - Minor Violations".
+//              there is a balance greater than 0 on any FEE ITEM in the "New" or "Invoiced" status
+// DATECREATED: 05/17/2019
+// BY: SWAKIL
+// CHANGELOG: 05/17/2019 , SWAKIL Created this file. It calls allInspectionsResulted function, that is added to includes_custom
+
 if (wfTask == "License Issuance" && wfStatus == "Issued"){
    var typeArray = ["MJ AMED Inspections",
                 "MJ Building Inspections",
@@ -13,4 +22,12 @@ if (wfTask == "License Issuance" && wfStatus == "Issued"){
       showMessage = true;
       logDebug("<B>Inspections are not completed </B>");
     } 
+
+    if (balanceDue > 0){
+      cancel = true;
+      showMessage = true;
+      logDebug("<B>There is balance due.</B>");
+    }
+
+
 }
