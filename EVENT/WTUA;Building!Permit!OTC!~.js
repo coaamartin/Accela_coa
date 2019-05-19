97c8edc85228240f73f8aa6392e2cdbe44e02ba0
khobday <khobday@truepointsolutions.com>
2019-05-18 19:21:13 -0700

    khobday

--- EVENT/WTUA;Building!Permit!OTC!~.js
+++ EVENT/WTUA;Building!Permit!OTC!~.js
@@ -16,6 +16,17 @@ var issuedEmlTemplate = "BLD PERMIT ISSUED # 35";
 var reportTemplate = "Building Permit"
 var reportParams = aa.util.newHashtable();
 addParameter(reportParams, "RecordID", capIDString);
+
+var reportTemplate = "Building Permit";
+var reportParams = aa.util.newHashtable();
+addParameter(reportParams, "RecordID", capIDString);
+
+var eParams = aa.util.newHashtable();
+addParameter(eParams, "$$altID$$", capIDString);
+addParameter(eParams, "$$ContactFullName$$", applicant.getFullName());
+addParameter(eParams, "$$recordAlias$$", appTypeAlias);
+addParameter(eParams, "$$acaRecordUrl$$", recordURL);
+
    
 if(wfTask == "Permit Issuance" && wfStatus == "Issued"){
 
