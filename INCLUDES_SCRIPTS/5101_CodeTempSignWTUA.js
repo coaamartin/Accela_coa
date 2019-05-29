a5bfc192f61b3ecfaab15ab91f4ae7078f217b55
Adrian Martin <amartin@auroragov.org>
2019-05-29 11:16:31 -0600

    ajm

--- INCLUDES_SCRIPTS/5101_CodeTempSignWTUA.js
+++ INCLUDES_SCRIPTS/5101_CodeTempSignWTUA.js
@@ -153,7 +153,7 @@ if (wfTask == "Final Approval 2" && wfStatus == "Approved") {
 		eParams.put("$$todayDate$$", todayDate);
 		eParams.put("$$altid$$",capId.getCustomID());
 		eParams.put("$$capAlias$$",cap.getCapType().getAlias());
-		eParams.put("$$goPay$$",goPay);		
+		//eParams.put("$$goPay$$",goPay);		
 		logDebug('Attempting to send email: ' + emailTemplate + " : " + capId.getCustomID());
 		emailContacts("Applicant", emailTemplate, eParams, null, null, "Y");
 	}		
