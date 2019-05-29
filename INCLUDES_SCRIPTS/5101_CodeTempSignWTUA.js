f4c11de74d3a0811a575c3de94a463e20358f1b9
Adrian Martin <amartin@auroragov.org>
2019-05-29 12:07:25 -0600

    ajm

--- INCLUDES_SCRIPTS/5101_CodeTempSignWTUA.js
+++ INCLUDES_SCRIPTS/5101_CodeTempSignWTUA.js
@@ -149,7 +149,6 @@ if (wfTask == "Final Approval 2" && wfStatus == "Approved") {
     acaURL += "/urlrouting.ashx?type=1009&module=Enforcement&capId1=" + capId.getID1() + "&capId2=" + capId.getID2() + "&capId3=" + capId.getID3() + "&AgencyCode=" + aa.getServiceProviderCode();
 	
 	//var goPay = "https://awebdev.aurora.city/CitizenAccess/urlrouting.ashx?type=1009&Module=" + cap.getCapModel().getModuleName() + "&capID1=" + capId.getID1() + "&capID2=" + capId.getID2() + "&capID3=" + capId.getID3() + "&agencyCode=AURORACO&HideHeader=false";
-	logDebug("goPay is." + goPay);	
 	if (emailTemplate != null && emailTemplate != "") {
 		logDebug("5101 sending generic invoice.  Defaulting to contact Applicant.");	
 		eParams = aa.util.newHashtable();
