809c9dbda5b490987be521617552b755e0e75222
Porter <jmporter@auroragov.org>
2019-05-29 11:02:34 -0600

    JMP

--- EVENT/WTUA;Licenses!Professional!General!Application.js
+++ EVENT/WTUA;Licenses!Professional!General!Application.js
@@ -1,19 +1,15 @@
 // Script 64
 // Script 106 JMP 
 
+if ("License Issuance".equals(wfTask) && "Issued".equals(wfStatus) && balanceDue == 0) {
+   logDebug("JMP - Trying to email");
+	include("64_CreateProfessionalLicenseAndLP");
+}
+
 if ("License Issuance".equals(wfTask) && "Ready to Pay".equals(wfStatus)) 
 {
 	// script 427
 	var theFee = 102;
-}
-	updateFee("LIC_110", "LIC_PROFESSIONAL_GENERAL", "FINAL", theFee, "N");
-	// 
-
-
-if ("License Issuance".equals(wfTask) && "Issued".equals(wfStatus) && balanceDue == 0) {
-	include("64_CreateProfessionalLicenseAndLP");
+   updateFee("LIC_110", "LIC_PROFESSIONAL_GENERAL", "FINAL", theFee, "N");
 }
 
-//SWAKIL
-
-//include("5050_ReadyToPayWF");
