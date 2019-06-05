7a558985bb716adc16b2e1d31f37f03fefce064d
Porter <jmporter@auroragov.org>
2019-06-05 08:16:07 -0600

    JMP

--- EVENT/WTUB;Licenses!Contractor!General!Application.js
+++ EVENT/WTUB;Licenses!Contractor!General!Application.js
@@ -1,6 +1,9 @@
 //SWAKIL - Email
 
-if (("License Issuance".equals(wfTask) && typeof wfStatus !== "Note")) // || ("License Issuance".equals(wfTask) && wfStatus !== "Denied") || ("License Issuance".equals(wfTask) && wfStatus !== "Additional Info Required"))
+if (("License Issuance".equals(wfTask) && wfStatus == "Note")) // || ("License Issuance".equals(wfTask) && wfStatus !== "Denied") || ("License Issuance".equals(wfTask) && wfStatus !== "Additional Info Required"))
+{
+}
+else
 {
   logDebug("Ready to check to stop due to payment - Calling 5050");
   include("5050_ReadyToPayWF");
