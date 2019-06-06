4b7b126576ec359b1c9e0d3a0fbae0a9b806e94b
Porter <jmporter@auroragov.org>
2019-06-06 07:13:14 -0600

    JMP

--- INCLUDES_SCRIPTS/111_CreateContractorLicenseAndLP.js
+++ INCLUDES_SCRIPTS/111_CreateContractorLicenseAndLP.js
@@ -49,7 +49,7 @@ var contact = getContactByType(contactType, capId);
 	//capId = createdApp;
    logDebug("emailing from #111 - JMP");
    
-	emailContacts(contactType,vEmailTemplate, vEParams, "", "","N", "");
+	emailContacts_JMP(contactType,vEmailTemplate, vEParams, "", "");
    
 	//capId = tmpCap;
 	
@@ -96,18 +96,85 @@ var contact = getContactByType(contactType, capId);
 		aa.expiration.editB1Expiration(rB1ExpResult.getB1Expiration());
 	}
 	
-//	var vEmailTemplate = "BLD CLL LICENSE ISSUANCE # 111 & 112";
-//	var vEParams = aa.util.newHashtable();
-//	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
-//	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(vNewExpDate,0));
-//	addParameter(vEParams, "$$ApplicationID$$", createdApp.getCustomID());
-//	addParameter(vEParams, "$$altID$$", createdApp.getCustomID());
+function emailContacts_JMP(sendEmailToContactTypes, emailTemplate, vEParams, reportTemplate, vRParams) {
+	var vChangeReportName = "";
+	var conTypeArray = [];
+	var validConTypes = getContactTypes();
+	var x = 0;
+	var vConType;
+	var vAsyncScript = "SEND_EMAIL_TO_CONTACTS_ASYNC";
+	var envParameters = aa.util.newHashMap();
+	var vAddAdHocTask = true;
 
-//	tmpCap = capId;
-//	capId = createdApp;
-//	emailContacts("All",vEmailTemplate, vEParams, null,null);
-//	capId = tmpCap;
+	//Ad-hoc Task Requested
+	if (arguments.length > 5) {
+		vAddAdHocTask = arguments[5]; // use provided prefrence for adding an ad-hoc task for manual notification
+		if (vAddAdHocTask == "N") {
+logDebug("No adhoc task");			
+			vAddAdHocTask = false;
+		}
+	}
 	
-//} else { //contact required exist on child (current) record
-//	logDebug("**WARN contact of type : " + contactType + " not found on record");
-//}
+	//Change Report Name Requested
+	if (arguments.length > 6) {
+		vChangeReportName = arguments[6]; // use provided report name
+	}
+
+logDebug("Provided contact types to send to: " + sendEmailToContactTypes);
+	
+	//Check to see if provided contact type(s) is/are valid
+	if (sendEmailToContactTypes != "All" && sendEmailToContactTypes != null && sendEmailToContactTypes != '') {
+		conTypeArray = sendEmailToContactTypes.split(",");
+	}
+	for (x in conTypeArray) {
+		//check all that are not "Primary"
+		vConType = conTypeArray[x];
+		if (vConType != "Primary" && !exists(vConType, validConTypes)) {
+			logDebug(vConType + " is not a valid contact type. No actions will be taken for this type.");
+			conTypeArray.splice(x, (x+1));
+		}
+	}
+	//Check if any types remain. If not, don't continue processing
+	if ((sendEmailToContactTypes != "All" && sendEmailToContactTypes != null && sendEmailToContactTypes != '') && conTypeArray.length <= 0) {
+		logDebug(vConType + " is not a valid contact type. No actions will be taken for this type.");
+		return false;	
+	}
+	else if((sendEmailToContactTypes != "All" && sendEmailToContactTypes != null && sendEmailToContactTypes != '') && conTypeArray.length > 0) {
+		sendEmailToContactTypes = conTypeArray.toString();
+	}
+	
+logDebug("Validated contact types to send to: " + sendEmailToContactTypes);	
+	//Save variables to the hash table and call sendEmailASync script. This allows for the email to contain an ACA deep link for the document
+	envParameters.put("sendEmailToContactTypes", sendEmailToContactTypes);
+	envParameters.put("emailTemplate", emailTemplate);
+	envParameters.put("vEParams", vEParams);
+	envParameters.put("reportTemplate", reportTemplate);
+	envParameters.put("vRParams", vRParams);
+	envParameters.put("vChangeReportName", vChangeReportName);
+	envParameters.put("CapId", capId);
+	envParameters.put("vAddAdHocTask", vAddAdHocTask);
+	
+	//Start modification to support batch script
+	var vEvntTyp = aa.env.getValue("eventType");
+	if (vEvntTyp == "Batch Process") {
+		aa.env.setValue("sendEmailToContactTypes", sendEmailToContactTypes);
+		aa.env.setValue("emailTemplate", emailTemplate);
+		aa.env.setValue("vEParams", vEParams);
+		aa.env.setValue("reportTemplate", reportTemplate);
+		aa.env.setValue("vRParams", vRParams);
+		aa.env.setValue("vChangeReportName", vChangeReportName);
+		aa.env.setValue("CapId", capId);
+		aa.env.setValue("vAddAdHocTask", vAddAdHocTask);		
+		//call sendEmailASync script
+		logDebug("Attempting to run Non-Async: " + vAsyncScript);
+		aa.includeScript(vAsyncScript);
+	}
+	else {
+		//call sendEmailASync script
+		logDebug("Attempting to run Async: " + vAsyncScript);
+		aa.runAsyncScript(vAsyncScript, envParameters);
+	}
+	//End modification to support batch script
+	
+	return true;
+}
