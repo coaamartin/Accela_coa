74b93e9d4342ec61641d4ccc2373eeeaea942e1e
Suhail Wakil <suhailwakil@gmail.com>
2019-06-16 20:10:48 -0700

    updated logic

--- INCLUDES_CUSTOM/allInspectionsResulted.js
+++ INCLUDES_CUSTOM/allInspectionsResulted.js
@@ -1,33 +1,21 @@
 function allInspectionsResulted(typeArray, statusArray) {
-    logDebug("---------------------> In the allInspectionsResulted function");       
+    logDebug("---------------------> In the allInspectionsResulted function"); 
     var inspResultObj = aa.inspection.getInspections(capId);
     if (inspResultObj.getSuccess()) {
         inspList = inspResultObj.getOutput();
         var processedInspTypes = [];
 
         for (xx in inspList) {
-            if(exists(inspList[xx].getInspectionType(), typeArray)){
-                if(exists(inspList[xx].getInspectionStatus(), statusArray)){
-                    processedInspTypes.push(inspList[xx].getInspectionType());  
-                }else{
-                    logDebug(inspList[xx].getInspectionType() + " is statused: "+inspList[xx].getInspectionStatus());
-                    return false;
-                }
-            }else{
-                logDebug("Insp not on required list: "+inspList[xx].getInspectionType()+" >>Skipping");
+            if(exists(inspList[xx].getInspectionType(), typeArray) && exists(inspList[xx].getInspectionStatus(), statusArray)){
+                    processedInspTypes.push(inspList[xx].getInspectionType());      
             }
         }
 
-        for (yy in typeArray){
-            if (!exists(typeArray[yy], processedInspTypes)){
-                logDebug(typeArray[yy] + " is not in the inspection List");
-                return false; 
+        for(kk in typeArray){
+            if(!exists(typeArray[kk], processedInspTypes)){
+                return false;
             }
         }
-       return true;
-    }else{
-        return false;
-        
+        return true;
     }
-    return false;
 }
