479774d99235d14820a6044ba071cbf1ff787815
Porter <jmporter@auroragov.org>
2019-06-19 15:00:43 -0600

    JMP

--- EVENT/WTUA;Licenses!Contractor!General!License.js
+++ EVENT/WTUA;Licenses!Contractor!General!License.js
@@ -9,7 +9,7 @@ if ("License Status".equals(wfTask) && "About to Expire".equals(wfStatus))
 	if ("Residential Building".equals(AInfo["Contractor Type"])) {
 		theFee = 180;
 	}
-	updateFee("LIC_020", "LIC_CONTRACTOR_GENERAL", "FINAL", theFee, "N");
+	updateFee("LIC_040", "LIC_CONTRACTOR_RENEWAL", "FINAL", theFee, "N");
    
 	include("5117_EMailAboutToExpire");
    
