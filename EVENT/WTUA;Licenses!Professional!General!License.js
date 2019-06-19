d2f0690eb672d83c0e4bf2b629247bcd819aa3fe
Porter <jmporter@auroragov.org>
2019-06-19 15:19:58 -0600

    JMP

--- EVENT/WTUA;Licenses!Professional!General!License.js
+++ EVENT/WTUA;Licenses!Professional!General!License.js
@@ -4,7 +4,7 @@ if ("License Status".equals(wfTask) && "About to Expire".equals(wfStatus))
    
 	include("5117_EMailAboutToExpire");
    
-   var theFee = 102;
+   var theFee = 1;
    updateFee("LIC_130", "LIC_PROFESSIONAL_GENERAL", "FINAL", theFee, "N");
    
 }
