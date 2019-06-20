1fb6ae0945f61463ed9b87dc1f79dc947440f628
Porter <jmporter@auroragov.org>
2019-06-20 09:18:39 -0600

    JMP

--- EVENT/WTUA;Licenses!Contractor!General!License.js
+++ EVENT/WTUA;Licenses!Contractor!General!License.js
@@ -1,15 +1,15 @@
 
 if ("License Status".equals(wfTask) && "About to Expire".equals(wfStatus)) 
-{
-   
-   var theFee = 125;
+{   
+   	// script 427
+	var theFee = 125;
 	if ("Commercial Building".equals(AInfo["Contractor Type"])) {
 		theFee = 300;
 	}
 	if ("Residential Building".equals(AInfo["Contractor Type"])) {
 		theFee = 180;
 	}
-	updateFee("LIC_040", "LIC_PRO_GEN", "FINAL", theFee, "N");
+	updateFee("LIC_020", "LIC_CONTRACTOR_GENERAL", "FINAL", theFee, "N");
    
 	include("5117_EMailAboutToExpire");
    
--- EVENT/WTUA;Licenses!Professional!General!License.js
+++ EVENT/WTUA;Licenses!Professional!General!License.js
@@ -4,7 +4,7 @@ if ("License Status".equals(wfTask) && "About to Expire".equals(wfStatus))
    
 	include("5117_EMailAboutToExpire");
    
-   var theFee = 1;
-   updateFee("LIC_130", "LIC_PROFESSIONAL_GENERAL", "FINAL", theFee, "N");
+	var theFee = 102;
+   updateFee("LIC_110", "LIC_PROFESSIONAL_GENERAL", "FINAL", theFee, "N");
    
 }
