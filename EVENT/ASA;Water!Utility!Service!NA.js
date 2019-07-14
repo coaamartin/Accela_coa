f95d240cdf38acdf4d2a923fd7445b3900f63a46
Suhail Wakil <suhailwakil@gmail.com>
2019-07-14 12:50:54 -0700

    script ID 486

---
+++ EVENT/ASA;Water!Utility!Service!NA.js
@@ -0,0 +1,2 @@
+//SWAKIL
+include("486_Assess_Water_Utility_Fees");
---
+++ INCLUDES_SCRIPTS/486_Assess_Water_Utility_Fees.js
@@ -0,0 +1,32 @@
+logDebug("486_Assess_Water_Utility_Fees");
+var tService = AInfo["Type of Service"];
+if (publicUser)
+{
+	if ("Fire Line Repair".equals(tService))
+	{
+		addFee("WAT_US_03", "WAT_US", 'FINAL', 1, 'Y');
+	}
+	else if ("Sewer Repair".equals(tService))
+	{
+		addFee("WAT_US_02", "WAT_US", 'FINAL', 1, 'Y');
+	}
+	else if ("Water Repair".equals(tService))
+	{
+		addFee("WAT_US_01", "WAT_US", 'FINAL', 1, 'Y');
+	}
+}
+else if (!"undefined".equals(typeof(wfTask)) && "Inspection".equals(wfTask) && "Ready to Pay".equals(wfStatus))
+{
+	if ("Fire Line Repair".equals(tService))
+	{
+		addFee("WAT_US_03", "WAT_US", 'FINAL', 1, 'Y');
+	}
+	else if ("Sewer Repair".equals(tService))
+	{
+		addFee("WAT_US_02", "WAT_US", 'FINAL', 1, 'Y');
+	}
+	else if ("Water Repair".equals(tService))
+	{
+		addFee("WAT_US_01", "WAT_US", 'FINAL', 1, 'Y');
+	}
+}
