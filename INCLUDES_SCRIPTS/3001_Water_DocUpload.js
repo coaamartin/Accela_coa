8fe595e306a42b7746a969fd9913441d9749f0b8
Suhail Wakil <suhailwakil@gmail.com>
2019-07-14 09:37:02 -0700

    script 3001

---
+++ INCLUDES_SCRIPTS/3001_Water_DocUpload.js
@@ -0,0 +1,23 @@
+//SWAKIL
+logDebug("3001_Water_DocUpload");
+if (appMatch("Water/Water/SWMP/Application"))
+{
+	if (isTaskActive("Application Submittal") && "Waiting on Documents".equals(capStatus))
+	{
+		updateAppStatus("Submitted", "Updated via script");
+	}
+}
+else if (appMatch("Water/Water/SWMP/Permit"))
+{
+	if (isTaskActive("Final Certification") && "Waiting on Re-Certs".equals(capStatus))
+	{
+		updateAppStatus("Re-Certs Received", "Updated via script");
+	}
+}
+else if (appMatch("Water/Water/PPBMP/NA"))
+{
+	if (isTaskActive("Annual Report") && "Active".equals(capStatus))
+	{
+		updateAppStatus("Annual Report Received", "Updated via script");
+	}
+}
