4593ea55085523de7cc4277479378810cb8a384e
Suhail Wakil <suhailwakil@gmail.com>
2019-07-23 21:12:41 -0700

    update 483

--- INCLUDES_SCRIPTS/483_addReInspectionFee.js
+++ INCLUDES_SCRIPTS/483_addReInspectionFee.js
@@ -7,6 +7,7 @@ inspCount = inspectionStatusCheck(typeArray, statusArray);
 if(inspCount >=3){
 	var thisFeeAmt = 138.00;
 	addFee("WAT_IP_03","WAT_IP","FINAL",thisFeeAmt,"Y");
+    updateAppStatus("Payment Pending", "Updated via script");
 }
 
 function inspectionStatusCheck(typeArray, statusArray) {
