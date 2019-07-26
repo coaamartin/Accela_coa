437de3c726252675103290d638c30156d3bee4aa
Suhail Wakil <suhailwakil@gmail.com>
2019-07-25 18:05:58 -0700

    Scripts 488 and 489

---
+++ EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
@@ -0,0 +1,5 @@
+//SWAKIL ID 488
+if ("City Application Intake".equals(wfTask) && "Complete".equals(wfStatus))
+{
+	include("488_Check_MJ_Renewal_Docs");
+}
---
+++ INCLUDES_SCRIPTS/489_Check_MJ_Application_Docs.js
@@ -0,0 +1,34 @@
+var docs = getDocumentList();
+if ("Corporation".equals(AInfo["Type of Ownership"] || "LLC".equals(AInfo["Type of Ownership"]) || "Partnership".equals(AInfo["Type of Ownership"])))
+{
+	var allGood = false;
+	var requiredDocs = new Array();
+	for (var d in docs)
+	{
+		requiredDocs[docs[d].getDocCategory()] = true;
+	}
+	if (!requiredDocs["Local - Articles of Incorporation"] || !requiredDocs["Local - Bylaws"])
+	{
+		cancel = true;
+		showMessage = true;
+		comment("Document types Local - Articles of Incorporation and Local - Bylaws are required for this Type of Ownership");
+	}
+}
+else if ("Individual".equals(AInfo["Type of Ownership"] || "Sole Proprietor".equals(AInfo["Type of Ownership"])))
+{
+	var allGood = false;
+	for (var d in docs)
+	{
+		if ("Local - Operating Agreement".equals(docs[d].getDocCategory()))
+		{
+			allGood = true;
+			break;
+		}
+	}
+	if (!allGood)
+	{
+		cancel = true;
+		showMessage = true;
+		comment("Document type Local - Operating Agreement is required for this Type of Ownership");
+	}	
+}
