7b6753ed287c2924e88220ab49e27deea2286208
Suhail Wakil <suhailwakil@gmail.com>
2019-07-27 20:49:38 -0700

    Script ID 493

--- INCLUDES_SCRIPTS/5012_SWMPPermitScheduleRoutine.js
+++ INCLUDES_SCRIPTS/5012_SWMPPermitScheduleRoutine.js
@@ -43,34 +43,6 @@ else if ("ECKO and Initial Inspection".equals(inspType) && "Failed".equals(inspR
     var failedStatusArray = ["Violation", "Needs", "Yes/Violation"];
     var newInspId = createPendingInspectionReturnId("WAT_SW_APP", "ECKO and Initial Inspection");
     copyGuideSheetItemsByGuideSheetStatus(inspId, newInspId, failedStatusArray);
-
-    //SWAKIL Water Eorsion Script ID 9
-    //add fee everytime after 3rd fail
-
-    var typeArray = ["ECKO and Initial Inspection"];
-    var statusArray = ["Failed"];
-    inspCount = inspectionStatusCheck(typeArray, statusArray);
-
-    if(inspCount >=3){
-        addFee("WAT_SWMP_28","WAT_SWMP_APP","FINAL", 1, "Y");
-        updateAppStatus("Payment Pending", "Updated via script");
-    }
-}
-function inspectionStatusCheck(typeArray, statusArray) {
-    var count = 0;
-    var inspResultObj = aa.inspection.getInspections(capId);
-    if (inspResultObj.getSuccess()) {
-        inspList = inspResultObj.getOutput();
-        var processedInspTypes = [];
-
-        for (xx in inspList) {
-            if(exists(inspList[xx].getInspectionType(), typeArray) && exists(inspList[xx].getInspectionStatus(), statusArray)){
-                processedInspTypes.push(inspList[xx].getInspectionType());      
-            }
-        }
-        count = processedInspTypes.length;
-    }
-    return count;
 }
 function copyGuideSheetItemsByGuideSheetStatus(fromInspId, toInspId, statusArray)
 {
