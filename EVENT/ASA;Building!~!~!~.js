c304d9051022cf9e75b481585a8625cbfd5fe50a
khobday <khobday@truepointsolutions.com>
2019-08-02 16:54:43 -0700

    khobday

--- EVENT/ASA;Building!~!~!~.js
+++ EVENT/ASA;Building!~!~!~.js
@@ -2,9 +2,10 @@ logDebug("Adding Fee");
 //updated 5/29/19 Keith - OTC permits should not be getting this Base Fee.
 //updated 7/25/19 Keith - Per Darcy stop Master from getting the Base Fee.
 //updated 8/2/19 Keith - Per Darcy stop Amendment from getting the Base Fee.
+//updated 8/2/19 Keith - Per Darcy stop With Plans from getting the Base Fee.
 
 //if(!appMatch("Building/Permit/New Building/NA") && !appMatch("Building/Permit/OTC/*"))
-if(!appMatch("Building/Permit/New Building/NA") && !appMatch("Building/Permit/OTC/*") && !appMatch("Building/Permit/Master/*")&& !appMatch("Building/Permit/New Building/Amendment"))
+if(!appMatch("Building/Permit/New Building/NA") && !appMatch("Building/Permit/OTC/*") && !appMatch("Building/Permit/Master/*")&& !appMatch("Building/Permit/New Building/Amendment")&& !appMatch("Building/Permit/Plans/NA"))
     addFee("BLD_PNP_06","BLD_PNP","FINAL",1,"Y");
 
 // 5/29/19 Keith - Why are we setting Debug to false here?? I should only be done in one place in the EMSE globals!
