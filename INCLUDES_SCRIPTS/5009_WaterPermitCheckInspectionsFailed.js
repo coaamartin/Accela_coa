feb07704226ac20bda3ee299799b5f61ce9e225b
Suhail Wakil <suhailwakil@gmail.com>
2019-08-20 20:37:24 -0700

    Update 5009_WaterPermitCheckInspectionsFailed.js

--- INCLUDES_SCRIPTS/5009_WaterPermitCheckInspectionsFailed.js
+++ INCLUDES_SCRIPTS/5009_WaterPermitCheckInspectionsFailed.js
@@ -1,13 +1,19 @@
 if ("Failed".equals(inspResult)){
 	var contact = "Applicant";
 	var template = "WAT_UTILITY_PERMIT_FAILED_INSP";
+	if(vEventName == "V360InspectionResultSubmitAfter")
+		inspectionComment = inspComment;
+	else
+		inspectionComment = inspResultComment;
+
+		
 	var emailParams = aa.util.newHashtable();
 	addParameter(emailParams, "$$altID$$", cap.getCapModel().getAltID());
 	addParameter(emailParams, "$$recordAlias$$", cap.getCapModel().getCapType().getAlias());
 	addParameter(emailParams, "$$recordStatus$$", cap.getCapModel().getCapStatus());
 	addParameter(emailParams, "$$inspID$$", inspId);
 	addParameter(emailParams, "$$inspResult$$", inspResult);
-	addParameter(emailParams, "$$inspComment$$", inspComment);
+	addParameter(emailParams, "$$inspComment$$", inspectionComment);
 	addParameter(emailParams, "$$inspResultDate$$", inspResultDate);
 	addParameter(emailParams, "$$inspGroup$$", inspGroup);
 	addParameter(emailParams, "$$inspType$$", inspType);
