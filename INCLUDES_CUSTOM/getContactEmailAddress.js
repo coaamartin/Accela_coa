4516ee1485e73bd2c39ecb55d82c2e5548971a9b
Suhail Wakil <suhailwakil@gmail.com>
2019-08-22 19:10:49 -0700

    Update getContactEmailAddress.js

--- INCLUDES_CUSTOM/getContactEmailAddress.js
+++ INCLUDES_CUSTOM/getContactEmailAddress.js
@@ -1,5 +1,6 @@
 function getContactEmailAddress(contType, itemCap){
 	var thisContact = getContactByType(contType, itemCap);
+	aa.print("Test:"+ thisContact)
 	var thisContEmailAddr = thisContact.getEmail();
 	
 	if(thisContEmailAddr) return thisContEmailAddr;
