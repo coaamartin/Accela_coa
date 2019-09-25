0ebcfb81f1b640f033b685ebfc8cd02cc951487e
SuhailWakil <suhailwakil@gmail.com>
2019-09-25 23:43:32 +0000

    New script to fire when payment applied

---
+++ EVENT/PAPA;Licenses!Marijuana!~!Application.js
@@ -0,0 +1,6 @@
+//PAPA PaymentAPplyAfter
+//sendEmailReceipt_MJApplication_PAPA();
+aa.print("invoiceSeqArr " + invoiceSeqArr[0].getClass());
+aa.print("feeSeqArr " + feeSeqArr[0].getClass());
+aa.print("appliedAmountArr " + appliedAmountArr[0].getClass());
+aa.print("invoiceSeqArr " + feeSeqNbrArr[0].getClass());
