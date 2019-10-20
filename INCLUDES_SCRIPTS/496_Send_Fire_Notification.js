037e9cf547ae3f535675b8886b2f805e6ac41e17
SuhailWakil <suhailwakil@gmail.com>
2019-10-20 09:51:14 -0500

    updates to emails sent

--- INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
+++ INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
@@ -1,10 +1,8 @@
 if (balanceDue > 0)
 {
   //email the applicant
-  var contacts = "Applicant,Contractor(s)";
-  var lptypes = "Contractor";
+  var contacts = "Individual";
   var emailtemplate = "JD_TEST_TEMPLATE";
 
   emailContactsWithReportLinkASync(contacts, emailtemplate, "", "", "", "N", "");
-  coa_emailLicenseProfessionals(lptypes, emailtemplate, "", "", "", capId);
 }
