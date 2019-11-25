ed001f5a67d7a3ea0270c76b6799be379b359b92
SuhailWakil <suhailwakil@gmail.com>
2019-11-25 17:05:09 -0600

    more fire validations

--- INCLUDES_SCRIPTS/497_FireValidation.js
+++ INCLUDES_SCRIPTS/497_FireValidation.js
@@ -10,6 +10,30 @@ if ("Complete".equals(inspResult))
 			cancel = true;
 			showMessage = true;
 			comment("Cannot Complete this inspection with open violations.");
+		}	
+	}
+
+	var gsi = getGuideSheetObjects(inspId);
+	if (gsi) 
+	{
+		for (var gs in gsi) 
+		{
+			var t = gsi[gs];
+			t.loadInfoTables();
+			for (var tb in t.infoTables)
+			{					
+				var g = (t.infoTables[tb] ? t.infoTables[tb] : []);
+				for (var fvi in g) 
+				{
+					var fvit = g[fvi];
+					if ("Non Compliance".equals(fvit["Violation Status"])) 
+					{
+						cancel = true;
+						showMessage = true;
+						comment("Cannot complete inspection while there are checklist items in non compliance");							
+					}
+				}	
+			}
 		}
 	}
 }
