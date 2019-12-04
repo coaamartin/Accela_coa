a9436d47ba3a1235dbffaf4bde9723668e060c90
Province <rprovinc@auroragov.org>
2019-12-04 14:47:00 -0700

    updated ASA FIRE

--- EVENT/ASA;Fire!~!~!~.js
+++ EVENT/ASA;Fire!~!~!~.js
@@ -1,1 +1,4 @@
-script16_FillApplicationNameWhenEmpty();
+// This was updated on 12/4/42019 by Raymond Province
+
+// script16_FillApplicationNameWhenEmpty();
+editAppName(getAppSpecific("Buisness Name"));
