1968084136235b5dee6d972dc03aa80388db4675
Cory Probasco <cprobasco@truepointsolutions.com>
2019-12-05 09:11:23 -0700

    fixed syntax/typo issues

--- EVENT/ASA;Fire!~!~!~.js
+++ EVENT/ASA;Fire!~!~!~.js
@@ -1,7 +1,6 @@
 // This was updated on 12/4/42019 by Raymond Province
 
 // script16_FillApplicationNameWhenEmpty();
-var newBuisnessName= getAppSpecific("Buisness Name");
-// editAppName(newBuisnessName); 
-logDebug(newBuisnessName);
-// logDebug(getAppSpecific);
+var newBusinessName= getAppSpecific("Business Name");
+logDebug("Updating Application Name field with Business Name of: " + newBusinessName);
+editAppName(newBusinessName); 
