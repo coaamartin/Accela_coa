b92a997ea4b305f5506436af2a28e15634bc9b16
Province <rprovinc@auroragov.org>
2020-01-08 14:28:35 -0700

    updated script 498

--- INCLUDES_SCRIPTS/498_ASB_FireValidation.js
+++ INCLUDES_SCRIPTS/498_ASB_FireValidation.js
@@ -1,5 +1,6 @@
+//This was updated by Raymond Provinc on 1/8/2019 to remove fire compliant from running this code.
 var contacts = getContactArrayBefore();
-if (contacts.length == 0)
+if (!appMatch("Fire/Complaint/NA/NA")(contacts.length == 0))
 {
 	cancel = true;
 	showMessage = true;
