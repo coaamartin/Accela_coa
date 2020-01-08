187184ee83b4455671780b7f86bf51061ea7b4f5
Province <rprovinc@auroragov.org>
2020-01-08 14:42:18 -0700

    ran another fix on 498 sscript for fire

--- INCLUDES_SCRIPTS/498_ASB_FireValidation.js
+++ INCLUDES_SCRIPTS/498_ASB_FireValidation.js
@@ -1,8 +1,13 @@
 //This was updated by Raymond Provinc on 1/8/2019 to remove fire compliant from running this code.
+
+if(!appMatch("Fire/Complaint/*/*")){
 var contacts = getContactArrayBefore();
-if (!appMatch("Fire/Complaint/NA/NA")(contacts.length == 0))
+if (contacts.length == 0)
 {
 	cancel = true;
 	showMessage = true;
 	comment("Must at least have 1 contact");
 }
+}
+
+//if (!appMatch("Fire/Complaint/*/*")(contacts.length == 0))
