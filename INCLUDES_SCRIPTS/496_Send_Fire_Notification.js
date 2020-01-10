1f3f1a9ddb569523e2036644d4e1629b5f09a119
Province <rprovinc@auroragov.org>
2020-01-10 08:53:45 -0700

    added log debugging to 496 script

--- INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
+++ INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
@@ -7,10 +7,11 @@ if (balanceDue > 0)
 	envParameters.put("invNbr", InvoiceNbrArray[0] + "");
 	aa.runAsyncScript(vAsyncScript, envParameters)
 	var capId = aa.env.getValue("capId");
-	var iContact = getContactByType("Individual", capId);
-	var fName = iContact.getFirstName();
-	var lName = iContact.getLastName();
-	var emailTo = iContact.getEmail();
+	// var iContact = getContactByType("Individual", capId);
+	// var fName = iContact.getFirstName();
+	// var lName = iContact.getLastName();
+	// var emailTo = iContact.getEmail();
+	logDebug("CapID info: " + capId)
 	logDebug("User Last name: " + fName)
 	logDebug("User First name: " + lName)
 	logDebug("User email: " + emailTo)
