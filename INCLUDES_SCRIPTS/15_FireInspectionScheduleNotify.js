171f05c508ffc9f0e02fe75476778b9a035130e0
Province <rprovinc@auroragov.org>
2020-01-14 08:50:00 -0700

    updated 15 to include FD complaint

--- INCLUDES_SCRIPTS/15_FireInspectionScheduleNotify.js
+++ INCLUDES_SCRIPTS/15_FireInspectionScheduleNotify.js
@@ -177,9 +177,9 @@ else if ("FD Complaint Follow-Up Inspection".equals(inspType) && "Violations Fou
 	envParameters.put("cap", cap);
 	envParameters.put("reportName", "Fire_Follow_Up_Inspection");
 	envParameters.put("InspActNumber", inspId);
-	envParameters.put("altId", ALTID);
+	envParameters.put("altId", altid);
 	logDebug("Parameter that is being passed: " + inspId + " & "+ envParameters);
-	logDebug("Sending Fire_Follow_Up_Inspection for Inspection type: " + ALTID);
+	logDebug("Sending Fire_Follow_Up_Inspection for Inspection type: " + altid);
 	var vAsyncScript = "SEND_FIRE_INSP_RESULT";
 	aa.runAsyncScript(vAsyncScript, envParameters);	
 }
