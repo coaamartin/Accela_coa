d1534d60c3aeff8f482d7ec876a84a1f76f20025
Province <rprovinc@auroragov.org>
2020-02-06 11:08:17 -0700

    updated invoice fee email

--- INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
+++ INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
@@ -14,5 +14,31 @@ if (balanceDue > 0)
 	//logDebug("Invoice ID = " + invNbr);
 	logDebug("End of 496_Fire_Notifications script");
 	logDebug("**END** FIRE_INVOICE_ASYNC kicks off from here");
+// Testing all of the parameters that are gonig to send fire invoice async
+	var capId = aa.env.getValue("capId");
+	var cap = aa.env.getValue("cap");
+	var invNbr = aa.env.getValue("INVOICEID");
+	var emailTo = getEmailString(); 
+	var capAlias = cap.getCapModel().getAppTypeAlias();
+	var today = new Date();
+	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+	var tParams = aa.util.newHashtable();
+	tParams.put("$$todayDate$$", thisDate);
+	tParams.put("$$altID$$", capId.getCustomID());
+	tParams.put("$$capAlias$$", capAlias);
+	tParams.put("$$FirstName$$", fName);
+	tParams.put("$$LastName$$", lName);
+	var rParams = aa.util.newHashtable();
+	rParams.put("AGENCYID", "AURORACO");
+	rParams.put("INVOICEID", invNbr);
+	var emailtemplate = "FIRE INVOICED FEES";
+	var report = generateReportFile("Invoice Report", rParams, aa.getServiceProviderCode());
+	logDebug("1" + capId);
+	logDebug("2" + invNbr);
+	logDebug("3" + emailTo);
+	logDebug("4" + tParams);
+	logDebug("5" + rParams);
+	logDebug("6" + emailtemplate);
+	logDebug("7" + report);
 }
 
