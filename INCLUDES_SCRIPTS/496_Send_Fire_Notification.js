fd576cc665edb7dd06e5d9e2e5c6d3dbde2fc3f3
Province <rprovinc@auroragov.org>
2020-02-07 09:49:08 -0700

    updated fire invoice script

--- INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
+++ INCLUDES_SCRIPTS/496_Send_Fire_Notification.js
@@ -1,20 +1,20 @@
 if (balanceDue > 0)
 {
 	logDebug("Starting 496_Fire_Notifications script");
+	var InvoiceNbr = InvoiceNbrArray[0] + "";
 	var envParameters = aa.util.newHashMap();
 	envParameters.put("capId", capId);
 	envParameters.put("cap", cap);
-	envParameters.put("INVOICEID", InvoiceNbrArray[0] + "");
+	envParameters.put("INVOICEID", InvoiceNbr);
 	envParameters.put("AGENCYID", "AURORACO");
 	var vAsyncScript = "SEND_FIRE_INVOICE_ASYNC";
 	aa.runAsyncScript(vAsyncScript, envParameters)
 	logDebug("CapID info: " + envParameters);
-	var InvoiceNbr = InvoiceNbrArray[0] + "";
 	logDebug ("Invoice NBR: " + InvoiceNbr);
 	logDebug("End of 496_Fire_Notifications script");
 	logDebug("**END** FIRE_INVOICE_ASYNC kicks off from here");
-	var errorLog = [];
-	logDebug("Errors from Send_fire_Invoice_Async =" + errorLog);
+	//var errorLog = [];
+	//logDebug("Errors from Send_fire_Invoice_Async =" + errorLog);
 
 	
 }
