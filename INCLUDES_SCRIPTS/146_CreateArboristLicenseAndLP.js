82cf1290c34133706fc9215c4fd80a5e725446ea
Cory Probasco <cprobasco@truepointsolutions.com>
2020-03-18 11:14:55 -0600

    updated to include proper ACA site link

--- INCLUDES_SCRIPTS/146_CreateArboristLicenseAndLP.js
+++ INCLUDES_SCRIPTS/146_CreateArboristLicenseAndLP.js
@@ -72,6 +72,8 @@ if (contact) {
 	var vEmailTemplate = "FT ARBORIST LICENSE ISSUANCE #146";
     var reportName = "JD_TEST_SSRS";
     var LicenseType = "Licenses/Contractor/Arborist/License";
+	var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+    acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
     var rptParams = aa.util.newHashtable();
     rptParams.put("Record_ID", licenseNbr);
 	var vEParams = aa.util.newHashtable();
@@ -79,6 +81,7 @@ if (contact) {
 	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(vNewExpDate,0));
 	addParameter(vEParams, "$$ApplicationID$$", licenseNbr);
 	addParameter(vEParams, "$$altID$$", licenseNbr);
+	addParameter(vEparams,"$$acaURL$$",acaSite);
 
 	tmpCap = capId;
 	capId = createdApp;
