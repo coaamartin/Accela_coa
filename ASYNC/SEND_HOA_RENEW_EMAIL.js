fd7305e2c840f263bb2fbadddbc3124c92c7a510
Province <rprovinc@auroragov.org>
2020-03-23 10:55:24 -0600

    updated 5082 script and moved the ASYNC even into it for testing

--- ASYNC/SEND_HOA_RENEW_EMAIL.js
+++ ASYNC/SEND_HOA_RENEW_EMAIL.js
@@ -1,11 +1,15 @@
 //SEND_HOA_RENEW_EMAIL
 logDebug("***** Starting SEND_HOA_RENEW_EMAIL from script *****");
+message = "";
 try
 {
 	var capId = aa.env.getValue("capId");
 	var cap = aa.env.getValue("cap");
-	var altId = aa.env.getValue("altID")
-	var emailTo = getEmailString(); 
+	message = invNbr;
+	var amount = aa.env.getValue("amount");
+	var contactsEmailToArray = getContactsEmailString();
+	var emailTo = "";
+	for (var e in contactsEmailToArray) emailTo += contactsEmailToArray[e] + ",";
 	var capAlias = cap.getCapModel().getAppTypeAlias();
 	var today = new Date();
 	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
@@ -13,38 +17,41 @@ try
 	tParams.put("$$todayDate$$", thisDate);
 	tParams.put("$$altID$$", capId.getCustomID());
 	tParams.put("$$capAlias$$", capAlias);
-	var rParams = aa.util.newHashtable();
+	tParams.put("$$amount$$", amount);
+	//var rParams = aa.util.newHashtable();
+	//rParams.put("INVOICEID", String(invNbr));
 	var emailtemplate = "HOA RENEWAL CONFIRMATION LETTER";
-    sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams);
+	//var report = generateReportFile("Receipt Report", rParams, aa.getServiceProviderCode());
+	sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams);
 }
 catch(e)
 {
 	email("rprovinc@auroragov.org", "aurora@gov.org", "Error", e.message);
 }
-function getEmailString(contactTypeArray)
+
+function getContactsEmailString(contactTypeArray)
 {
-	var emailString = "";
+	var itemCap = capId;
 	var result = new Array();
 	if (!contactTypeArray)
-		contactTypeArray = new Array();
+		contactTypeArray = new Array();		
 	var getAll = (contactTypeArray.length == 0)
 	if (arguments.length > 1)
 		itemCap = arguments[1];
-	var contactArray = getPeople(capId);
-
-	//need to add inspection contact below to this logic 
+	var emailString = "";
+	var contactArray = getPeople(itemCap);
 	for (var c in contactArray)
-	if (!(contactArray[c].getPeople().getEmail() && contactArray[c].getPeople().getEmail().length() > 0))
+	{
+		if (!(contactArray[c].getPeople().getEmail() && contactArray[c].getPeople().getEmail().length() > 0))
 			continue;
 		if (getAll || exists(contactArray[c].getPeople().getContactType(), contactTypeArray))
 		{
 			result.push(contactArray[c].getPeople().getEmail());
 		}
-	logDebug(emailString);
-	return emailString;
+	}
+	return result;
 }
-logDebug("Starting function getPeople")
-function getPeople(capId)
+ function getPeople(capId)
 {
 	capPeopleArr = null;
 	var s_result = aa.people.getCapContactByCapID(capId);
@@ -173,29 +180,29 @@ function email(pToEmail, pFromEmail, pSubject, pText)
 
 
 
-	// var result = null;
+	var result = null;
 
-	// result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel);
+	result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel, reportFile);
 
-	// if(result.getSuccess())
+	if(result.getSuccess())
 
-	// {
+	{
 
-	// 	logDebug("Sent email successfully!");
+		logDebug("Sent email successfully!");
 
-	// 	return true;
+		return true;
 
-	// }
+	}
 
-	// else
+	else
 
-	// {
+	{
 
-	// 	logDebug("Failed to send mail. - " + result.getErrorType());
+		logDebug("Failed to send mail. - " + result.getErrorType());
 
-	// 	return false;
+		return false;
 
-	// }
+	}
 
 }
  function convertContactAddressModelArr(contactAddressScriptModelArr)
@@ -223,3 +230,262 @@ function email(pToEmail, pFromEmail, pSubject, pText)
 	return contactAddressModelArr;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try
+// {
+// 	var capId = aa.env.getValue("capId");
+// 	var cap = aa.env.getValue("cap");
+// 	var altId = aa.env.getValue("altID")
+// 	var emailTo = getEmailString(); 
+// 	var capAlias = cap.getCapModel().getAppTypeAlias();
+// 	var today = new Date();
+// 	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+// 	var tParams = aa.util.newHashtable();
+// 	tParams.put("$$todayDate$$", thisDate);
+// 	tParams.put("$$altID$$", capId.getCustomID());
+// 	tParams.put("$$capAlias$$", capAlias);
+// 	var rParams = aa.util.newHashtable();
+// 	var emailtemplate = "HOA RENEWAL CONFIRMATION LETTER";
+//     sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams);
+// }
+// catch(e)
+// {
+// 	email("rprovinc@auroragov.org", "aurora@gov.org", "Error", e.message);
+// }
+// function getEmailString(contactTypeArray)
+// {
+// 	var emailString = "";
+// 	var result = new Array();
+// 	if (!contactTypeArray)
+// 		contactTypeArray = new Array();
+// 	var getAll = (contactTypeArray.length == 0)
+// 	if (arguments.length > 1)
+// 		itemCap = arguments[1];
+// 	var contactArray = getPeople(capId);
+
+// 	//need to add inspection contact below to this logic 
+// 	for (var c in contactArray)
+// 	if (!(contactArray[c].getPeople().getEmail() && contactArray[c].getPeople().getEmail().length() > 0))
+// 			continue;
+// 		if (getAll || exists(contactArray[c].getPeople().getContactType(), contactTypeArray))
+// 		{
+// 			result.push(contactArray[c].getPeople().getEmail());
+// 		}
+// 	logDebug(emailString);
+// 	return emailString;
+// }
+// logDebug("Starting function getPeople")
+// function getPeople(capId)
+// {
+// 	capPeopleArr = null;
+// 	var s_result = aa.people.getCapContactByCapID(capId);
+// 	if(s_result.getSuccess())
+// 	{
+// 		capPeopleArr = s_result.getOutput();
+// 		if(capPeopleArr != null || capPeopleArr.length > 0)
+// 		{
+// 			for (loopk in capPeopleArr)	
+// 			{
+// 				var capContactScriptModel = capPeopleArr[loopk];
+// 				var capContactModel = capContactScriptModel.getCapContactModel();
+// 				var peopleModel = capContactScriptModel.getPeople();
+// 				var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
+// 				if (contactAddressrs.getSuccess())
+// 				{
+// 					var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
+// 					peopleModel.setContactAddressList(contactAddressModelArr);    
+// 				}
+// 			}
+// 		}
+// 		else
+// 		{
+// 			aa.print("WARNING: no People on this CAP:" + capId);
+// 			capPeopleArr = null;
+// 		}
+// 	}
+// 	else
+// 	{
+// 		aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
+// 		capPeopleArr = null;	
+// 	}
+// 	return capPeopleArr;
+// }
+// function logDebug(str){aa.print(str);}
+// function logMessage(str){aa.print(str);}
+//  function getContactByType(conType,capId) {
+
+//     var contactArray = getPeople(capId);
+
+
+
+//     for(thisContact in contactArray) {
+
+//         if((contactArray[thisContact].getPeople().contactType).toUpperCase() == conType.toUpperCase())
+
+//             return contactArray[thisContact].getPeople();
+
+//     }
+
+
+
+//     return false;
+
+// }
+// function email(pToEmail, pFromEmail, pSubject, pText) 
+// 	{
+// 	//Sends email to specified address
+// 	//06SSP-00221
+// 	//
+// 	aa.sendMail(pFromEmail, pToEmail, "", pSubject, pText);
+// 	logDebug("Email sent to "+pToEmail);
+// 	return true;
+// 	}
+
+
+// // function generateReportFile(aaReportName,parameters,rModule) 
+// // {
+// // 	var reportName = aaReportName;
+
+// // 	report = aa.reportManager.getReportInfoModelByName(reportName);
+// // 	report = report.getOutput();
+
+
+// // 	report.setModule(rModule);
+// // 	report.setCapId(capId);
+// // 	report.setReportParameters(parameters);
+// // 	//Added
+// // 	vAltId = capId.getCustomID();
+// // 	report.getEDMSEntityIdModel().setAltId(vAltId);
+// // 	var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
+// // 	aa.print("---"+permit.getOutput().booleanValue());
+// // 	if(permit.getOutput().booleanValue()) 
+// // 	{
+// // 		var reportResult = aa.reportManager.getReportResult(report);
+
+// // 		if(reportResult) 
+// // 		{
+// // 			reportResult = reportResult.getOutput();
+// // 			var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+// // 			logMessage("Report Result: "+ reportResult);
+// // 			reportFile = reportFile.getOutput();
+// // 			return reportFile
+// // 		} else 
+// // 		{
+// // 			logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+// // 			return false;
+// // 		}
+// // 	} else 
+// // 	{
+// // 		logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+// // 		return false; 
+// // 	}
+// // }
+//  function sendNotification(emailFrom,emailTo,emailCC,templateName,params)
+
+// {
+
+// 	var itemCap = capId;
+
+// 	if (arguments.length == 7) itemCap = arguments[6]; // use cap ID specified in args
+
+
+
+// 	var id1 = itemCap.ID1;
+
+//  	var id2 = itemCap.ID2;
+
+//  	var id3 = itemCap.ID3;
+
+
+
+// 	var capIDScriptModel = aa.cap.createCapIDScriptModel(id1, id2, id3);
+
+
+
+
+
+// 	//var result = null;
+
+// 	result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel);
+
+// 	if(result.getSuccess())
+
+// 	{
+
+// 		logDebug("Sent email successfully!");
+
+// 		return true;
+
+// 	}
+
+// 	else
+
+// 	{
+
+// 		logDebug("Failed to send mail. - " + result.getErrorType());
+
+// 		return false;
+
+// 	}
+
+// }
+//  function convertContactAddressModelArr(contactAddressScriptModelArr)
+
+// {
+
+// 	var contactAddressModelArr = null;
+
+// 	if(contactAddressScriptModelArr != null && contactAddressScriptModelArr.length > 0)
+
+// 	{
+
+// 		contactAddressModelArr = aa.util.newArrayList();
+
+// 		for(loopk in contactAddressScriptModelArr)
+
+// 		{
+
+// 			contactAddressModelArr.add(contactAddressScriptModelArr[loopk].getContactAddressModel());
+
+// 		}
+
+// 	}	
+
+// 	return contactAddressModelArr;
+
+// }
