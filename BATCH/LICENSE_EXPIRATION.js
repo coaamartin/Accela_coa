7262940b32cf7ebc265df31ded4260d760ce23ea
Province <rprovinc@auroragov.org>
2020-03-25 14:38:29 -0600

    updating scripts to have hoa nam shown

--- BATCH/LICENSE_EXPIRATION.js
+++ BATCH/LICENSE_EXPIRATION.js
@@ -399,8 +399,8 @@ function mainProcess() {
 			eParams = aa.util.newHashtable();
 			eParams.put("$$expirationDate$$", b1ExpDate);
 			eParams.put("$$altID$$",capId.getCustomID());
-			eParams.put("$$capName$$",capName);
-			eParams.put("$$HOANAME$$",hoaName);
+			//eParams.put("$$capName$$",capName);
+			eParams.put("$$HOANAME$$",AInfo["Name of HOA"]);
 			eParams.put("$$acaRecordUrl$$",getACARecordURL(""));
 			if (reportName != null && reportName != "") {
 				var rParams = aa.util.newHashtable();
