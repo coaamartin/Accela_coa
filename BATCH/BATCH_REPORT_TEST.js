5bdfdafaa092c81baf64f1d35bc0b02fa43592c5
Province <rprovinc@auroragov.org>
2020-05-14 08:02:49 -0600

    added new script BATCH_REPORT_TEST

--- BATCH/BATCH_REPORT_TEST.js
+++ BATCH/BATCH_REPORT_TEST.js
@@ -1,29 +1,130 @@
-//SEND_REPORT_EMAIL from batch job.
-function getScriptText(vScriptName){
-    vScriptName = vScriptName.toUpperCase();
-    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
-    var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),vScriptName,"ADMIN");
-    return emseScript.getScriptText() + "";          
+/*------------------------------------------------------------------------------------------------------/
+| TEST PARAMETERS (Uncomment to use in the script tester)
+/------------------------------------------------------------------------------------------------------*/
+//aa.env.setValue("paramStdChoice","BATCH_CloseInactiveHOA");
+//aa.env.setValue("eventType","Batch Process");
+//aa.env.setValue("BatchJobName","BATCH_CloseInactiveHOA");
+/*------------------------------------------------------------------------------------------------------/
+| Program: BATCH_CloseInactiveHOA  
+| Trigger: Batch
+| Client: COA
+|
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| USER CONFIGURABLE PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+currentUserID = "ADMIN";
+useAppSpecificGroupName = false;
+/*------------------------------------------------------------------------------------------------------/
+| GLOBAL VARIABLES
+/------------------------------------------------------------------------------------------------------*/
+message = "";
+br = "<br>";
+debug = "";
+systemUserObj = aa.person.getUser(currentUserID).getOutput();
+publicUser = false;
+/*------------------------------------------------------------------------------------------------------/
+| INCLUDE SCRIPTS (Core functions, batch includes, custom functions)
+/------------------------------------------------------------------------------------------------------*/
+SCRIPT_VERSION = 3.0;
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
+	}
 }
 
-var SCRIPT_VERSION = 3.0
-aa.env.setValue("CurrentUserID", "ADMIN");
-eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
-eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
-eval(getScriptText("INCLUDES_CUSTOM"));
+if (SA) {
+	eval(getMasterScriptText("INCLUDES_ACCELA_FUNCTIONS", SA));
+	eval(getMasterScriptText(SAScript, SA));
+} else {
+	eval(getMasterScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+}
+
+eval(getScriptText("INCLUDES_BATCH"));
+eval(getMasterScriptText("INCLUDES_CUSTOM"));
+
+/*------------------------------------------------------------------------------------------------------/
+| CORE EXPIRATION BATCH FUNCTIONALITY
+/------------------------------------------------------------------------------------------------------*/
+try {
+	showMessage = false;
+	showDebug = true;
+	if (String(aa.env.getValue("showDebug")).length > 0) {
+		showDebug = aa.env.getValue("showDebug").substring(0, 1).toUpperCase().equals("Y");
+	}
+
+	sysDate = aa.date.getCurrentDate();
+	var startDate = new Date();
+	var startTime = startDate.getTime(); // Start timer
+	var systemUserObj = aa.person.getUser("ADMIN").getOutput();
+
+	sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(), sysDate.getDayOfMonth(), sysDate.getYear(), "");
+	batchJobResult = aa.batchJob.getJobID();
+	batchJobName = "" + aa.env.getValue("BatchJobName");
+	batchJobID = 0;
+
+	if (batchJobResult.getSuccess()) {
+		batchJobID = batchJobResult.getOutput();
+		logDebug("Batch Job " + batchJobName + " Job ID is " + batchJobID);
+	} else {
+		logDebug("Batch job ID not found " + batchJobResult.getErrorMessage());
+	}
 
 
-logDebug("***** Starting BATCH_REPORT_TEST *****");
-try
-{
-	var capId = aa.env.getValue("capId");
-	var cap = aa.env.getValue("cap");
-	//var hoaName = aa.env.getValue("HOANAME");
-	var emailTo = getEmailString(); 
-	//var emailTo = "rprovinc@auroragov.org";
-	//var capAlias = cap.getCapModel().getAppTypeAlias();
-	var today = new Date();
-	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+/*------------------------------------------------------------------------------------------------------/
+| <===========Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/
+	logDebug("Start of Job");
+
+	mainProcess();
+
+	logDebug("End of Job: Elapsed Time : " + elapsed() + " Seconds");
+	
+/*------------------------------------------------------------------------------------------------------/
+| <===========END=Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/	
+} catch (err) {
+	handleError(err,"Batch Job:" + batchJobName + " Job ID:" + batchJobID);
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| <=========== Errors and Reporting
+/------------------------------------------------------------------------------------------------------*/
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ScriptReturnCode", "1");
+	aa.env.setValue("ScriptReturnMessage", debug);
+} else {
+	aa.env.setValue("ScriptReturnCode", "0");
+	if (showMessage) {
+		aa.env.setValue("ScriptReturnMessage", message);
+	}
+	if (showDebug) {
+		aa.env.setValue("ScriptReturnMessage", debug);
+	}
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| FUNCTIONS (mainProcess is the core function for processing expiration records)
+/------------------------------------------------------------------------------------------------------*/
+function mainProcess() {  
+    /*----------------------------------------------------------------------------------------------------/
+	| BATCH PARAMETERS
+    /------------------------------------------------------------------------------------------------------*/
+    var paramStdChoice = aa.env.getValue("paramStdChoice");	// use this standard choice for parameters instead of batchjob params
+    var daysInactive = getJobParam("daysInactive");//this will be used to determine how often to run the report...will change var name
+    var today = new Date();
+    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var emailSendTo = getJobParam("emailSendTo");
+	var emailTitle = getJobParam("emailTitle");
+	var emailBodyMsg="";
 	var tParams = aa.util.newHashtable();
 	tParams.put("$$todayDate$$", thisDate);
 	//tParams.put("$$HOANAME$$", hoaName);
@@ -33,205 +134,56 @@ try
 	var emailtemplate = "Need to get the email template";
     //sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams);
     sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams, null);
-}
-
-catch(e)
-{
-	email("rprovinc@auroragov.org", "aurora@gov.org", "Error", e.message);
-}
-function getEmailString()
-{
-	var emailString = "";
-	var contactArray = getPeople(capId);
-
-	//need to add inspection contact below to this logic 
-	for (var c in contactArray)
-	{
-		if (contactArray[c].getPeople().getEmail() && contactArray[c].getPeople().contactType != "")
-		{
-			emailString += contactArray[c].getPeople().getEmail() + ";";
-		}
-	}
-	logDebug(emailString);
-	return emailString;
-}
-logDebug("Starting function getPeople")
- function getPeople(capId)
-{
-	capPeopleArr = null;
-	var s_result = aa.people.getCapContactByCapID(capId);
-	if(s_result.getSuccess())
-	{
-		capPeopleArr = s_result.getOutput();
-		if(capPeopleArr != null || capPeopleArr.length > 0)
-		{
-			for (loopk in capPeopleArr)	
-			{
-				var capContactScriptModel = capPeopleArr[loopk];
-				var capContactModel = capContactScriptModel.getCapContactModel();
-				var peopleModel = capContactScriptModel.getPeople();
-				var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
-				if (contactAddressrs.getSuccess())
-				{
-					var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
-					peopleModel.setContactAddressList(contactAddressModelArr);    
-				}
-			}
-		}
-		else
-		{
-			aa.print("WARNING: no People on this CAP:" + capId);
-			capPeopleArr = null;
-		}
-	}
-	else
-	{
-		aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
-		capPeopleArr = null;	
-	}
-	return capPeopleArr;
-}
-function logDebug(str){aa.print(str);}
-function logMessage(str){aa.print(str);}
- function getContactByType(conType,capId) {
-
-    var contactArray = getPeople(capId);
-
-
-
-    for(thisContact in contactArray) {
-
-        if((contactArray[thisContact].getPeople().contactType).toUpperCase() == conType.toUpperCase())
-
-            return contactArray[thisContact].getPeople();
-
-    }
-
-
-
-    return false;
-
-}
-function email(pToEmail, pFromEmail, pSubject, pText) 
-	{
-	//Sends email to specified address
-	//06SSP-00221
-	//
-	aa.sendMail(pFromEmail, pToEmail, "", pSubject, pText);
-	logDebug("Email sent to "+pToEmail);
-	return true;
-	}
-
-
-function generateReportFile(aaReportName,parameters,rModule) 
-{
-	var reportName = aaReportName;
-
-	report = aa.reportManager.getReportInfoModelByName(reportName);
-	report = report.getOutput();
-
-
-	report.setModule(rModule);
-	report.setCapId(capId);
-	report.setReportParameters(parameters);
-	//Added
-	vAltId = capId.getCustomID();
-	report.getEDMSEntityIdModel().setAltId(vAltId);
-	var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
-	aa.print("---"+permit.getOutput().booleanValue());
-	if(permit.getOutput().booleanValue()) 
-	{
-		var reportResult = aa.reportManager.getReportResult(report);
-
-		if(reportResult) 
-		{
-			reportResult = reportResult.getOutput();
-			var reportFile = aa.reportManager.storeReportToDisk(reportResult);
-			logMessage("Report Result: "+ reportResult);
-			reportFile = reportFile.getOutput();
-			return reportFile
-		} else 
-		{
-			logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
-			return false;
-		}
-	} else 
-	{
-		logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
-		return false; 
-	}
-}
- function sendNotification(emailFrom,emailTo,emailCC,templateName,params,reportFile)
-
-{
-
-	var itemCap = capId;
-
-	if (arguments.length == 7) itemCap = arguments[6]; // use cap ID specified in args
-
-
-
-	var id1 = itemCap.ID1;
-
- 	var id2 = itemCap.ID2;
-
- 	var id3 = itemCap.ID3;
-
-
-
-	var capIDScriptModel = aa.cap.createCapIDScriptModel(id1, id2, id3);
-
-
-
-
-
-	var result = null;
-
-	result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel, reportFile);
-
-	if(result.getSuccess())
-
-	{
-
-		logDebug("Sent email successfully!");
-
-		return true;
-
-	}
-
-	else
-
-	{
-
-		logDebug("Failed to send mail. - " + result.getErrorType());
-
-		return false;
-
-	}
-
-}
- function convertContactAddressModelArr(contactAddressScriptModelArr)
-
-{
-
-	var contactAddressModelArr = null;
-
-	if(contactAddressScriptModelArr != null && contactAddressScriptModelArr.length > 0)
-
-	{
-
-		contactAddressModelArr = aa.util.newArrayList();
-
-		for(loopk in contactAddressScriptModelArr)
-
-		{
-
-			contactAddressModelArr.add(contactAddressScriptModelArr[loopk].getContactAddressModel());
-
+    logDebug("Processing Batch_report_test.js. ")
+    /*----------------------------------------------------------------------------------------------------/
+	| Email Header
+	/------------------------------------------------------------------------------------------------------*/
+	emailBodyMsg+="Hello,"+br;
+	emailBodyMsg+=br;
+	emailBodyMsg+="Please see attached report." + br;
+    emailBodyMsg+=br;
+    emailBodyMsg+="Thank you and have a great day," + br;
+	emailBodyMsg+=br;
+	/*----------------------------------------------------------------------------------------------------/
+	| End Email Header
+	/------------------------------------------------------------------------------------------------------*/
+    	//generate email notices
+		if (emailSendTo && emailSendTo != "") {
+			logDebug("=================================================");
+			logDebug('Attempting to send email: ' + emailTemplate );
+				aa.sendMail("noreply@accela.com", emailSendTo, "", emailTitle, emailBodyMsg);
 		}
 
-	}	
-
-	return contactAddressModelArr;
-
+		logDebug("=================================================");
+		logDebug("Finished sending email" );
 }
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========internal functions - do not modify ================>
+/-----------------------------------------------------------------------------------------------------*/
+function getMasterScriptText(vScriptName) {
+	var servProvCode = aa.getServiceProviderCode();
+	if (arguments.length > 1)
+		servProvCode = arguments[1]; // use different serv prov code
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+
+function getScriptText(vScriptName) {
+	var servProvCode = aa.getServiceProviderCode();
+	if (arguments.length > 1)
+		servProvCode = arguments[1]; // use different serv prov code
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		var emseScript = emseBiz.getScriptByPK(servProvCode, vScriptName, "ADMIN");
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
