6babca7e82c8c0c17655354ca43a141cd1e304ea
Province <rprovinc@auroragov.org>
2020-05-27 07:56:35 -0600

    updating scripts

--- ASYNC/SEND_BATCH_REPORT_TESTv2.js
+++ ASYNC/SEND_BATCH_REPORT_TESTv2.js
@@ -1,218 +1,119 @@
-//SEND_FIRE_INSP_RESULT
-logDebug("***** Starting SEND_BATCH_REPORT_TESTv2 from script *****");
-try
-{
-	var reportName = aa.env.getValue("reportName");
-    var emailTo = aa.env.getValue("emailSendTo");
-    var emailtemplate = "Report_Test_Email";
-	//var today = new Date();
-	//var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-	// var tParams = aa.util.newHashtable();
-	// tParams.put("$$todayDate$$", thisDate);
-	// tParams.put("$$altID$$", capId.getCustomID());
-	// tParams.put("$$capAlias$$", capAlias);
-	// var rParams = aa.util.newHashtable();
-	// if ("Fire Primary Inspection".equals(reportName))
-	// {		
-	// 	rParams.put("RecordId", altId);
-	// }
-	// else if ("Fire_Follow_Up_Inspection".equals(reportName))
-	// {
-	// 	rParams.put("RecordID", altId);
-	// }
-	// else if ("Fire Order Notice".equals(reportName))
-	// {
-	// 	rParams.put("RecordID", capId.getCustomID());
-		
-	// }
+//Get environmental variables pass into the script
+var sendEmailToAddresses = getJobParam("emailSendTo");
+var emailTemplate = "Report_Test_Email";
+var vEParams = aa.env.getValue("vEParams");
+var reportTemplate = getJobParam("reportName");
+var vRParams = aa.env.getValue("vRParams");
+var vChangeReportName = aa.env.getValue("vChangeReportName");
+var capId = aa.env.getValue("CapId");
+var adHocTaskContactsList = aa.env.getValue("adHocTaskContactsList");
 
-    //var report = generateReportFile(reportName, rParams, aa.getServiceProviderCode());
-    var report = generateReportFile(reportName, "", aa.getServiceProviderCode());
-    // sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, tParams, [report]);
-    sendNotification("noreply@aurora.gov", emailTo, "", emailtemplate, "", [report]);
-}
-catch(e)
-{
-	email("debug@gmail.com", "aurora@gov.org", "Error", e.message);
-}
+//Constant variables used in the script
+var CONST_ADHOC_PROCESS = "ADHOC_WORKFLOW";
+var CONST_ADHOC_TASK = "Manual Notification";
 
-logDebug("Starting function getPeople")
-//  function getPeople(capId)
-// {
-// 	capPeopleArr = null;
-// 	var s_result = aa.people.getCapContactByCapID(capId);
-// 	if(s_result.getSuccess())
-// 	{
-// 		capPeopleArr = s_result.getOutput();
-// 		if(capPeopleArr != null || capPeopleArr.length > 0)
-// 		{
-// 			for (loopk in capPeopleArr)	
-// 			{
-// 				var capContactScriptModel = capPeopleArr[loopk];
-// 				var capContactModel = capContactScriptModel.getCapContactModel();
-// 				var peopleModel = capContactScriptModel.getPeople();
-// 				var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
-// 				if (contactAddressrs.getSuccess())
-// 				{
-// 					var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
-// 					peopleModel.setContactAddressList(contactAddressModelArr);    
-// 				}
-// 			}
-// 		}
-// 		else
-// 		{
-// 			aa.print("WARNING: no People on this CAP:" + capId);
-// 			capPeopleArr = null;
-// 		}
-// 	}
-// 	else
-// 	{
-// 		aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
-// 		capPeopleArr = null;	
-// 	}
-// 	return capPeopleArr;
-// }
-function logDebug(str){aa.print(str);}
-function logMessage(str){aa.print(str);}
-// function getContactByType(conType,capId) {
-
-//     var contactArray = getPeople(capId);
-
-
-
-//     for(thisContact in contactArray) {
-
-//         if((contactArray[thisContact].getPeople().contactType).toUpperCase() == conType.toUpperCase())
-
-//             return contactArray[thisContact].getPeople();
-
-//     }
-
-
-
-//     return false;
-
-// }
-function email(pToEmail, pFromEmail, pSubject, pText) 
-	{
-	//Sends email to specified address
-	//06SSP-00221
-	//
-	aa.sendMail(pFromEmail, pToEmail, "", pSubject, pText);
-	logDebug("Email sent to "+pToEmail);
-	return true;
-	}
-
-
-function generateReportFile(aaReportName,parameters,rModule) 
-{
-	var reportName = aaReportName;
-
-	report = aa.reportManager.getReportInfoModelByName(reportName);
-	report = report.getOutput();
-
-
-	report.setModule(rModule);
-	report.setCapId(capId);
-	report.setReportParameters(parameters);
-	//Added
-	vAltId = capId.getCustomID();
-	report.getEDMSEntityIdModel().setAltId(vAltId);
-	var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
-	aa.print("---"+permit.getOutput().booleanValue());
-	if(permit.getOutput().booleanValue()) 
-	{
-		var reportResult = aa.reportManager.getReportResult(report);
-
-		if(reportResult) 
-		{
-			reportResult = reportResult.getOutput();
-			var reportFile = aa.reportManager.storeReportToDisk(reportResult);
-			logMessage("Report Result: "+ reportResult);
-			reportFile = reportFile.getOutput();
-			return reportFile
-		} else 
-		{
-			logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
-			return false;
+try {
+	//Start modification to support batch script, if not batch then grab globals, if batch do not.
+	if (aa.env.getValue("eventType") != "Batch Process") {
+		// Begin Code needed to call master script functions ---------------------------------------------------
+		function getScriptText(vScriptName, servProvCode, useProductScripts) {
+			if (!servProvCode)
+				servProvCode = aa.getServiceProviderCode();
+			vScriptName = vScriptName.toUpperCase();
+			var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+			try {
+				if (useProductScripts) {
+					var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+				} else {
+					var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+				}
+				return emseScript.getScriptText() + "";
+			} catch (err) {
+				return "";
+			}
 		}
-	} else 
-	{
-		logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
-		return false; 
+		var SCRIPT_VERSION = 3.0;
+		aa.env.setValue("CurrentUserID", "ADMIN");
+		eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, true));
+		eval(getScriptText("INCLUDES_ACCELA_GLOBALS", null, true));
+		eval(getScriptText("INCLUDES_CUSTOM", null, true));
 	}
-}
- function sendNotification(emailFrom,emailTo,emailCC,templateName,params,reportFile)
+	//End Code needed to call master script functions -----------------------------------------------------
 
-{
+	logDebug("1) Here in SEND_EMAIL_ASYNC. Event Type: " + aa.env.getValue("eventType"));
+	logDebug("2) sendEmailToAddresses: " + sendEmailToAddresses);
+	logDebug("3) emailTemplate: " + emailTemplate);
+	logDebug("4) reportTemplate: " + reportTemplate);
+	logDebug("5) adHocTaskContactsList: " + adHocTaskContactsList);
 
-	var itemCap = capId;
-
-	if (arguments.length == 7) itemCap = arguments[6]; // use cap ID specified in args
-
-
-
-	var id1 = itemCap.ID1;
-
- 	var id2 = itemCap.ID2;
-
- 	var id3 = itemCap.ID3;
-
-
-
-	var capIDScriptModel = aa.cap.createCapIDScriptModel(id1, id2, id3);
-
-
-
-
-
-	var result = null;
-
-	result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel, reportFile);
-
-	if(result.getSuccess())
-
-	{
-
-		logDebug("Sent email successfully!");
-
-		return true;
-
-	}
-
-	else
-
-	{
-
-		logDebug("Failed to send mail. - " + result.getErrorType());
-
-		return false;
-
-	}
-
-}
- function convertContactAddressModelArr(contactAddressScriptModelArr)
-
-{
-
-	var contactAddressModelArr = null;
-
-	if(contactAddressScriptModelArr != null && contactAddressScriptModelArr.length > 0)
-
-	{
-
-		contactAddressModelArr = aa.util.newArrayList();
-
-		for(loopk in contactAddressScriptModelArr)
-
-		{
-
-			contactAddressModelArr.add(contactAddressScriptModelArr[loopk].getContactAddressModel());
-
+	//1. Handle report, if needed
+	if (reportTemplate != null && reportTemplate != '') {
+		//Generate report and get report name
+		if (vRParams == null) {
+			vRParams = aa.util.newHashtable();
 		}
+		var vReportName = generateReportForEmail(capId, reportTemplate, aa.getServiceProviderCode(), vRParams);
+		//logDebug("Generated report " + vReportName);
+		if (vReportName != null && vReportName != false) {
+			//Update the report name if one was provided.
+			if (vChangeReportName != null && vChangeReportName != "") {
+				logDebug("Renaming generated report document name from " + vReportName + " to " + vChangeReportName);
+				if (editDocumentName(vReportName, vChangeReportName) == true) {
+					vReportName = vChangeReportName;
+				}
+			}
+			//Get document deep link URL, add to email params
+			var vACAUrl = lookup("ACA_CONFIGS", "ACA_SITE");
+			vACAUrl = vACAUrl.substr(0, vACAUrl.toUpperCase().indexOf("/ADMIN"));
+			var vDocumentList = aa.document.getDocumentListByEntity(capId, "CAP").getOutput();
+			if (vDocumentList != null) {
+				for (y = 0; y < vDocumentList.size(); y++) {
+					var vDocumentModel = vDocumentList.get(y);
+					if (vDocumentModel.getFileName() == vReportName) {
+						//Add the document url to the email paramaters using the name: $$acaDocDownloadUrl$$
+						getACADocDownloadParam4Notification(vEParams, vACAUrl, vDocumentModel);
+						//logDebug("including document url: " + vEParams.get('$$acaDocDownloadUrl$$'));
+						break;
+					}
+				}
+			}
+		}
+	}
 
-	}	
+	//2. Send Email, if needed
+	if (sendEmailToAddresses && sendEmailToAddresses != '') {
+		//Get From email from template configuration
+		var mailFrom;
+		if (emailTemplate && emailTemplate != '') {
+			var tmpl = aa.communication.getNotificationTemplate(emailTemplate).getOutput();
+			mailFrom = tmpl.getEmailTemplateModel().getFrom();
+		}
+		logDebug("mailFrom = " + mailFrom);
 
-	return contactAddressModelArr;
+		//Add standard email variables from record information
+		vEParams = addStdVarsToEmail(vEParams, capId);
 
+		logDebug("added standard vars to email");
+
+		//Get the capId type needed for the email function
+		var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+
+		//Loop through the email addresses and send to each
+		var arEmails = sendEmailToAddresses.split(',');
+		logDebug("arEmails.length = " + arEmails.length);
+		var sentTo = []; //Prevent duplicates
+		for (var i = 0; i < arEmails.length; i++) {
+			var thisEmail = arEmails[i];
+			if (!exists(thisEmail.toUpperCase(), sentTo)) {
+				logDebug("SEND_EMAIL_ASYNC: " + capId.getCustomID() + ": Sending " + emailTemplate + " from " + mailFrom + " to " + thisEmail);
+				//**Note we won't have contact specific email parameters like contact name, since we have no contact object, just email address
+				//vEParamsToSend = vConObj.getEmailTemplateParams(vEParams);
+				logDebug("Email Sent: " + aa.document.sendEmailAndSaveAsDocument(mailFrom, thisEmail, "", emailTemplate, vEParams, capId4Email, null).getSuccess());
+				sentTo.push(thisEmail.toUpperCase());
+			}
+		}
+	}
+
+} catch (err) {
+	logDebug("Error in Send_Email_ASYNC.js");
 }
