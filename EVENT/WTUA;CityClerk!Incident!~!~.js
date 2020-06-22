88fa0b97d61139c10badf7dc56c7493c85fb6a2b
Province <rprovinc@auroragov.org>
2020-06-22 15:05:05 -0600

    added two new scripts

--- EVENT/WTUA;CityClerk!Incident!~!~.js
+++ EVENT/WTUA;CityClerk!Incident!~!~.js
@@ -1,2 +1,14 @@
-//feeseqnum =    addFee(feecode, feeschedule, 'FINAL', parseFloat(thefee), 'Y');
-updateFee(feecode, feeschedule, "FINAL", parseFloat(thefee), "Y", "N");
+//Written by rprovinc   
+//
+//include("5121_CityClerkWTUA.js");
+
+//*****************************************************************************
+//Script WTUA;CityClerk!Incident!~!~.js
+//Record Types:	CityClerk\Incident\*\* 
+//Event: 		WTUA
+//Desc:			Going to update the fee when ever a CityClerk record is open and Non-Profit is set to No.
+//
+//Created By: Rprovinc
+//******************************************************************************
+logDebug("Starting WTUA;CityClerk!Incident!~!~.js");
+include("5121_CityClerkWTUA");
