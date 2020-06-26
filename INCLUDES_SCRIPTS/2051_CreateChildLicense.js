dfcf6d2b80874a6085c51d6e48cd419e82c2050c
Acharlton <acharlton@truepointsolutions.com>
2020-06-26 15:52:40 -0600

    Update 2051_CreateChildLicense.js
    Added specific calls for each License Type child record getting created and expiration dates set.

--- INCLUDES_SCRIPTS/2051_CreateChildLicense.js
+++ INCLUDES_SCRIPTS/2051_CreateChildLicense.js
@@ -14,19 +14,63 @@ try {
 	newChildID = null;
 	// Replaced
 	if (wfStatus == 'Issue License'){
-		if (exists(appTypeArray[2],["General"])) {
+		if (exists(appTypeArray[2],["General"],["Class 2"],["Common Consumption"])) {
 			newChildID = createChildLic(appTypeArray[0], appTypeArray[1], appTypeArray[2], 'License', capName);
 			//default to 12 months from today
 			comment("Checking on what the renewal date should be set to");
 			numberOfMonths = 12;
 			tmpNewDate = dateAddMonths(null, numberOfMonths);
 			comment("TMP NEW Date Default = "+tmpNewDate);
-		} else if (appMatch('*/Liquor/Liquor License/Application')) {
+		} else if (exists(appTypeArray[2],["Cabaret"],["Liquor License"], ["Tasting License"], ["Tasting Permit"]))  {
 			newChildID = createChildLic(appTypeArray[0], appTypeArray[1], appTypeArray[2], 'License', capName);
 			//default to 12 months from today
 			comment("Checking on what the renewal date should be set to");
 			numberOfMonths = 12;
+			tmpNewDate = AInfo['State License Expiration Date'];	
+		} else if (exists(appTypeArray[2],["Class 1"])){
+			var licType = AInfo['Type of License'];
+			newChildID = createChildLic(appTypeArray[0], appTypeArray[1], appTypeArray[2], 'License', capName);
+			//default to 12 months from today
+			comment("Checking on what the renewal date should be set to");
+			if (matches(licType,"Trash Haulers License","Amusement Device Distributor")) {
+			numberOfMonths = 12;
+			today = new Date(); comment('Today = ' + today);
+			theMonth = today.getMonth(); comment('the month = ' + theMonth);
+			theYear = today.getFullYear(); comment('The Year = ' + theYear);
+			nextYear = theYear + 1; comment('Next Year = ' + nextYear);
+			if (matches(theMonth, '11')) {
+				tmpNewDate = '12/31/'+ nextYear;
+			} else {
+				tmpNewDate = '12/31/'+ theYear;
+			}
+			logDebug("New Date 12/31 will be: " + tmpNewDate);
+			}
+			if (matches(licType,"Door Sellers License","Manufactured Home Park License","Stable License")) {
+			numberOfMonths = 12;
 			tmpNewDate = dateAddMonths(null, numberOfMonths);
+			}
+		} else if (exists(appTypeArray[2],["Temporary Permit"])){
+			var licType = AInfo['Type of License'];
+			//default to 12 months from today
+			comment("Checking on what the renewal date should be set to");
+			if (matches(licType,"Christmas Tree Lot")) {
+			numberOfMonths = 12;
+			today = new Date(); comment('Today = ' + today);
+			theMonth = today.getMonth(); comment('the month = ' + theMonth);
+			theYear = today.getFullYear(); comment('The Year = ' + theYear);
+			nextYear = theYear + 1; comment('Next Year = ' + nextYear);
+			tmpNewDate = '12/31/'+ theYear;
+			logDebug("New Date 12/31 will be: " + tmpNewDate);
+			}
+			if (matches(licType,"Fireworks Stand License","Carnival/Circus (Amusement Enterprise)")) {
+			numberOfMonths = 12;
+			today = new Date(); comment('Today = ' + today);
+			theMonth = today.getMonth(); comment('the month = ' + theMonth);
+			theYear = today.getFullYear(); comment('The Year = ' + theYear);
+			nextYear = theYear + 1; comment('Next Year = ' + nextYear);
+			tmpNewDate = '12/31/'+ theYear;
+			logDebug("New Date 12/31 will be: " + tmpNewDate);
+			}
 		}
 
 	
