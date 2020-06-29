176a35be0845ceecaf311122b1e12aae40707c33
Province <rprovinc@auroragov.org>
2020-06-29 11:16:25 -0600

    WTUA for city clerk

---
+++ EVENT/WTUB;CityClerk!Incident!~!~.js
@@ -0,0 +1,20 @@
+//Written by rprovinc   
+//
+//include("5121_CityClerk.js");
+
+//*****************************************************************************
+//Script WTUA;CityClerk!Incident!~!~.js
+//Record Types:	CityClerk\Incident\*\* 
+//Event: 		ASA
+//Desc:			Going to update the fee when ever a CityClerk record is open and Non-Profit is set to No.
+//
+//Created By: Rprovinc
+//******************************************************************************
+logDebug("Starting WTUA;CityClerk!Incident!~!~.js");
+
+if (wfTask == "Planning Director Approval" && wfStatus == "Approved") {
+
+	// Script 5121_CityClerk
+    include("5121_CityClerk");
+	
+}
