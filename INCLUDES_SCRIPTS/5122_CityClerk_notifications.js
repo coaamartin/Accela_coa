d6c5696dd79c123ca412298a3cb10bbe44701f07
Province <rprovinc@auroragov.org>
2020-07-13 13:52:48 -0600

    updated 5122 for testing

--- INCLUDES_SCRIPTS/5122_CityClerk_notifications.js
+++ INCLUDES_SCRIPTS/5122_CityClerk_notifications.js
@@ -16,7 +16,7 @@ appTypeArray = appTypeString.split("/");
 logDebug("appType: " + appTypeString);
 
 
- //Donation Bins code
+//Donation Bins code
 if ("CityClerk/Incident/DonationBin/NA".equals(appTypeString)) {
     logDebug("Starting to send notifications");
     //var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org;jjking@auroragov.org";
@@ -30,19 +30,21 @@ if ("CityClerk/Incident/DonationBin/NA".equals(appTypeString)) {
     var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
     var altId = capId.getCustomID();
     logDebug("Testing to see if I get a temp record id before a new if statement. This is before. Altid: " + altId);
-    if (recordApplicant != null ){
-    var tParams = aa.util.newHashtable();
-    tParams.put("$$todayDate$$", thisDate);
-    tParams.put("$$altid$$", capId.getCustomID());
-    tParams.put("$$Record Type$$", "Donation Bin");
-    tParams.put("$$capAlias$$", capAlias);
-    tParams.put("$$FirstName$$", firstName);
-    tParams.put("$$LastName$$", lastName);
-    logDebug("EmailTo: " + emailTo);
-    logDebug("during if statement. AltId show below in tparams");
-    logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-    logDebug("End of Script 5122_CityClerk_notifications.js");
+    if (recordApplicant != null) {
+        var tParams = aa.util.newHashtable();
+        tParams.put("$$todayDate$$", thisDate);
+        tParams.put("$$altid$$", altId);
+        tParams.put("$$Record Type$$", "Donation Bin");
+        tParams.put("$$capAlias$$", capAlias);
+        tParams.put("$$FirstName$$", firstName);
+        tParams.put("$$LastName$$", lastName);
+        logDebug("EmailTo: " + emailTo);
+        logDebug("Table Parameters: " + tParams);
+        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+        logDebug("End of Script 5122_CityClerk_notifications.js");
+    }
+    else if (recordApplicant == null) {
+        logDebug("Email could not be sent as there is no Applicant email address.")
     }
 }
 
@@ -57,18 +59,25 @@ else if ("CityClerk/Incident/TempUse/NA".equals(appTypeString)) {
     var firstName = recordApplicant.getFirstName();
     var lastName = recordApplicant.getLastName();
     var today = new Date();
-	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var tParams = aa.util.newHashtable();
-    tParams.put("$$todayDate$$", thisDate);
-    tParams.put("$$altid$$", capId.getCustomID());
-    tParams.put("$$Record Type$$", "Temp Use Permit");
-    tParams.put("$$capAlias$$", capAlias);
-    tParams.put("$$FirstName$$", firstName);
-    tParams.put("$$LastName$$", lastName);
-    logDebug("EmailTo: " + emailTo);
-    logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-    logDebug("End of Script 5122_CityClerk_notifications.js");
+    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var altId = capId.getCustomID();
+    logDebug("Testing to see if I get a temp record id before a new if statement. This is before. Altid: " + altId);
+    if (recordApplicant != null) {
+        var tParams = aa.util.newHashtable();
+        tParams.put("$$todayDate$$", thisDate);
+        tParams.put("$$altid$$", altId);
+        tParams.put("$$Record Type$$", "Temp Use Permit");
+        tParams.put("$$capAlias$$", capAlias);
+        tParams.put("$$FirstName$$", firstName);
+        tParams.put("$$LastName$$", lastName);
+        logDebug("EmailTo: " + emailTo);
+        logDebug("Table Parameters: " + tParams);
+        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+        logDebug("End of Script 5122_CityClerk_notifications.js");
+    }
+    else if (recordApplicant == null) {
+        logDebug("Email could not be sent as there is no Applicant email address.")
+    }
 }
 
 //Temp Sign code
@@ -82,16 +91,23 @@ else if ("CityClerk/Incident/TempSign/NA".equals(appTypeString)) {
     var firstName = recordApplicant.getFirstName();
     var lastName = recordApplicant.getLastName();
     var today = new Date();
-	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var tParams = aa.util.newHashtable();
-    tParams.put("$$todayDate$$", thisDate);
-    tParams.put("$$altid$$", capId.getCustomID());
-    tParams.put("$$Record Type$$", "Temp Sign Permit");
-    tParams.put("$$capAlias$$", capAlias);
-    tParams.put("$$FirstName$$", firstName);
-    tParams.put("$$LastName$$", lastName);
-    logDebug("EmailTo: " + emailTo);
-    logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-    logDebug("End of Script 5122_CityClerk_notifications.js");
+    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var altId = capId.getCustomID();
+    logDebug("Testing to see if I get a temp record id before a new if statement. This is before. Altid: " + altId);
+    if (recordApplicant != null) {
+        var tParams = aa.util.newHashtable();
+        tParams.put("$$todayDate$$", thisDate);
+        tParams.put("$$altid$$", altId);
+        tParams.put("$$Record Type$$", "Temp Sign Permit");
+        tParams.put("$$capAlias$$", capAlias);
+        tParams.put("$$FirstName$$", firstName);
+        tParams.put("$$LastName$$", lastName);
+        logDebug("EmailTo: " + emailTo);
+        logDebug("Table Parameters: " + tParams);
+        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+        logDebug("End of Script 5122_CityClerk_notifications.js");
+    }
+    else if (recordApplicant == null) {
+        logDebug("Email could not be sent as there is no Applicant email address.")
+    }
 }
