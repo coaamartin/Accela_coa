59d1f454b524ca1c484877096c7ddba7811fb6f9
Acharlton <acharlton@truepointsolutions.com>
2020-07-31 12:01:46 -0600

    Update 2056_Send_License_Notification.js

--- INCLUDES_SCRIPTS/2056_Send_License_Notification.js
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -1,14 +1,7 @@
 if (balanceDue > 0) {
 logDebug("Starting _License_Notification script");
-var pEmailTemplate = 'LIC FEES INVOICED'
-var pEParams = aa.util.newHashMap();
-pEParams.put("capId", capId);
-pEParams.put("cap", cap);
-pEParams.put("AGENCYID", "AURORACO");
-//var vAsyncScript = "SEND_LICENSE_INVOICE_ASYNC";
-//aa.runAsyncScript(vAsyncScript, pEParams);
-emailContactsWithReportLinkASync('Primary', pEmailTemplate, pEParams)
-logDebug("CapID info: " + pEParams);
+var contacts = 'Primary,Applicant';
+var emailTemplate = 'LIC FEES INVOICED';
+emailContactsWithReportLinkASync(contacts, emailTemplate, "", "", "", "N", "")
 logDebug("End of 2056_License_Notification script");
-logDebug("**END** SEND_License_INVOICE_ASYNC kicks off from here");
 }
