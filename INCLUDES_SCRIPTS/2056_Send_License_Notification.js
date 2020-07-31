a165ff5c6fb16b6d3767f18060c73160f45e110e
Acharlton <acharlton@truepointsolutions.com>
2020-07-31 11:46:54 -0600

    Update 2056_Send_License_Notification.js
    Using a different function

--- INCLUDES_SCRIPTS/2056_Send_License_Notification.js
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -1,14 +1,15 @@
 if (balanceDue > 0) {
 logDebug("Starting _License_Notification script");
-var InvoiceNbr = InvoiceNbrArray[0] + "";
-var envParameters = aa.util.newHashMap();
-envParameters.put("capId", capId);
-envParameters.put("cap", cap);
-envParameters.put("INVOICEID", InvoiceNbr);
-envParameters.put("AGENCYID", "AURORACO");
-var vAsyncScript = "SEND_LICENSE_INVOICE_ASYNC";
-aa.runAsyncScript(vAsyncScript, envParameters);
-logDebug("CapID info: " + envParameters);
+var pEmailTemplate = 'LIC FEES INVOICED'
+var pEParams = aa.util.newHashMap();
+pEParams.put("capId", capId);
+pEParams.put("cap", cap);
+pEParams.put("INVOICEID", InvoiceNbr);
+pEParams.put("AGENCYID", "AURORACO");
+//var vAsyncScript = "SEND_LICENSE_INVOICE_ASYNC";
+//aa.runAsyncScript(vAsyncScript, pEParams);
+emailContactsWithReportLinkASync('Primary', pEmailTemplate, pEParams)
+logDebug("CapID info: " + pEParams);
 logDebug("Invoice NBR: " + InvoiceNbr);
 logDebug("End of 2056_License_Notification script");
 logDebug("**END** SEND_License_INVOICE_ASYNC kicks off from here");
