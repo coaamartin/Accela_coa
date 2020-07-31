09eacacec1c1707af885cf72f7f3b21560f6e384
Acharlton <acharlton@truepointsolutions.com>
2020-07-31 11:30:25 -0600

    Update WTUA;Licenses!~!~!~.js
    Updating WTUA logic

--- EVENT/WTUA;Licenses!~!~!~.js
+++ EVENT/WTUA;Licenses!~!~!~.js
@@ -2,6 +2,6 @@ if (wfStatus == 'About to Expire') {
 	include("2052_LicenseStatus");
 }
 
-if (appMatch('Licenses/Liquor/*/*') || appMatch('Licenses/Supplemental/*/*')) {
+if ((wfStatus == 'Fees Invoiced') && (appMatch('Licenses/Liquor/*/*') || appMatch('Licenses/Supplemental/*/*'))) {
 	include("2056_Send_License_Notification");
 }
