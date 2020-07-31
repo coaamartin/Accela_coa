bcea05d16fd88f51b4792ddcc2b3bd8e49f3fea0
Acharlton <acharlton@truepointsolutions.com>
2020-07-31 10:41:45 -0600

    Invoice Fee Scripts for Licenses

--- EVENT/WTUA;Licenses!~!~!~.js
+++ EVENT/WTUA;Licenses!~!~!~.js
@@ -1,3 +1,7 @@
 if (wfStatus == 'About to Expire') {
 	include("2052_LicenseStatus");
 }
+
+if (appMatch('Licenses/Liquor/*/*') || appMatch('Licenses/Supplemental/*/*')) {
+	include("2056_Send_License_Notification");
+}
---
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -0,0 +1,13 @@
+logDebug("Starting _License_Notification script");
+var InvoiceNbr = InvoiceNbrArray[0] + "";
+var envParameters = aa.util.newHashMap();
+envParameters.put("capId", capId);
+envParameters.put("cap", cap);
+envParameters.put("INVOICEID", InvoiceNbr);
+envParameters.put("AGENCYID", "AURORACO");
+var vAsyncScript = "SEND_LICENSE_INVOICE_ASYNC";
+aa.runAsyncScript(vAsyncScript, envParameters);
+logDebug("CapID info: " + envParameters);
+logDebug("Invoice NBR: " + InvoiceNbr);
+logDebug("End of 2056_License_Notification script");
+logDebug("**END** SEND_License_INVOICE_ASYNC kicks off from here");
