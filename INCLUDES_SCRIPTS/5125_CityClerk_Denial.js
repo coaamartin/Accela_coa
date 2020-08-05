039c56ad099d98c66c4883c67715f9ebfe88e661
Province <rprovinc@auroragov.org>
2020-08-05 07:34:21 -0600

    updated wtua and created 5125 script

--- INCLUDES_SCRIPTS/5125_CityClerk_Denial.js
+++ INCLUDES_SCRIPTS/5125_CityClerk_Denial.js
@@ -19,28 +19,27 @@ logDebug("appType: " + appTypeString);
 //Donation Bins code
 if ("CityClerk/Incident/DonationBin/NA".equals(appTypeString)) {
     logDebug("Starting to send notifications");
-    //var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org;jjking@auroragov.org";
-    var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org";
     var vEmailTemplate = "CC DENIAL";
     var capAlias = cap.getCapModel().getAppTypeAlias();
     var recordApplicant = getContactByType("Applicant", capId);
     var firstName = recordApplicant.getFirstName();
     var lastName = recordApplicant.getLastName();
+    var emailTo = recordApplicant.getEmail();
+    var wfcomment = wfcomment; 
     var today = new Date();
-    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var altId = capId.getCustomID();
-    if (recordApplicant != null) {
-        var tParams = aa.util.newHashtable();
-        tParams.put("$$todayDate$$", thisDate);
-        tParams.put("$$altid$$", altId);
-        tParams.put("$$Record Type$$", "Donation Bin");
-        tParams.put("$$capAlias$$", capAlias);
-        tParams.put("$$FirstName$$", firstName);
-        tParams.put("$$LastName$$", lastName);
-        logDebug("EmailTo: " + emailTo);
-        logDebug("Table Parameters: " + tParams);
-        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-        logDebug("End of Script 5125_CityClerk_denial.js");
+	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var tParams = aa.util.newHashtable();
+    tParams.put("$$todayDate$$", thisDate);
+    tParams.put("$$altid$$", capId.getCustomID());
+    tParams.put("$$Record Type$$", "Donation Bin");
+    tParams.put("$$capAlias$$", capAlias);
+    tParams.put("$$FirstName$$", firstName);
+    tParams.put("$$LastName$$", lastName);
+    tParams.put("$$wfComment$$", wfComment);
+    logDebug("EmailTo: " + emailTo);
+    logDebug("Table Parameters: " + tParams);
+    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+    logDebug("End of Script 5125-Denial")
     }
     else if (recordApplicant == null) {
         //logDebug("Email could not be sent as there is no Applicant email address.")
@@ -50,28 +49,27 @@ if ("CityClerk/Incident/DonationBin/NA".equals(appTypeString)) {
 //Temp Use code
 else if ("CityClerk/Incident/TempUse/NA".equals(appTypeString)) {
     logDebug("Starting to send notifications");
-    //var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org;JNAPPER@auroragov.org";
-    var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org";
     var vEmailTemplate = "CC DENIAL";
     var capAlias = cap.getCapModel().getAppTypeAlias();
     var recordApplicant = getContactByType("Applicant", capId);
     var firstName = recordApplicant.getFirstName();
     var lastName = recordApplicant.getLastName();
+    var emailTo = recordApplicant.getEmail();
+    var wfcomment = wfcomment; 
     var today = new Date();
-    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var altId = capId.getCustomID();
-    if (recordApplicant != null) {
-        var tParams = aa.util.newHashtable();
-        tParams.put("$$todayDate$$", thisDate);
-        tParams.put("$$altid$$", altId);
-        tParams.put("$$Record Type$$", "Temp Use Permit");
-        tParams.put("$$capAlias$$", capAlias);
-        tParams.put("$$FirstName$$", firstName);
-        tParams.put("$$LastName$$", lastName);
-        logDebug("EmailTo: " + emailTo);
-        logDebug("Table Parameters: " + tParams);
-        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-        logDebug("End of Script 5125_CityClerk_denial.js");
+	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var tParams = aa.util.newHashtable();
+    tParams.put("$$todayDate$$", thisDate);
+    tParams.put("$$altid$$", capId.getCustomID());
+    tParams.put("$$Record Type$$", "Temp Use Permit");
+    tParams.put("$$capAlias$$", capAlias);
+    tParams.put("$$FirstName$$", firstName);
+    tParams.put("$$LastName$$", lastName);
+    tParams.put("$$wfComment$$", wfComment);
+    logDebug("EmailTo: " + emailTo);
+    logDebug("Table Parameters: " + tParams);
+    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+    logDebug("End of Script 5125-Denial")
     }
     else if (recordApplicant == null) {
         logDebug("Email could not be sent as there is no Applicant email address.")
@@ -81,28 +79,27 @@ else if ("CityClerk/Incident/TempUse/NA".equals(appTypeString)) {
 //Temp Sign code
 else if ("CityClerk/Incident/TempSign/NA".equals(appTypeString)) {
     logDebug("Starting to send notifications");
-    //var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org;jjking@auroragov.org";
-    var emailTo = "rprovinc@auroragov.org;bwatkins@auroragov.org;cmariano@auroragov.org";
     var vEmailTemplate = "CC DENIAL";
     var capAlias = cap.getCapModel().getAppTypeAlias();
     var recordApplicant = getContactByType("Applicant", capId);
     var firstName = recordApplicant.getFirstName();
     var lastName = recordApplicant.getLastName();
+    var emailTo = recordApplicant.getEmail();
+    var wfcomment = wfcomment; 
     var today = new Date();
-    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var altId = capId.getCustomID();
-    if (recordApplicant != null) {
-        var tParams = aa.util.newHashtable();
-        tParams.put("$$todayDate$$", thisDate);
-        tParams.put("$$altid$$", altId);
-        tParams.put("$$Record Type$$", "Temp Sign Permit");
-        tParams.put("$$capAlias$$", capAlias);
-        tParams.put("$$FirstName$$", firstName);
-        tParams.put("$$LastName$$", lastName);
-        logDebug("EmailTo: " + emailTo);
-        logDebug("Table Parameters: " + tParams);
-        sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
-        logDebug("End of Script 5125_CityClerk_denial.js");
+	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+    var tParams = aa.util.newHashtable();
+    tParams.put("$$todayDate$$", thisDate);
+    tParams.put("$$altid$$", capId.getCustomID());
+    tParams.put("$$Record Type$$", "Temp Sign Permit");
+    tParams.put("$$capAlias$$", capAlias);
+    tParams.put("$$FirstName$$", firstName);
+    tParams.put("$$LastName$$", lastName);
+    tParams.put("$$wfComment$$", wfComment);
+    logDebug("EmailTo: " + emailTo);
+    logDebug("Table Parameters: " + tParams);
+    sendNotification("noreply@auroraco.gov", emailTo, "", vEmailTemplate, tParams, null);
+    logDebug("End of Script 5125-Denial")
     }
     else if (recordApplicant == null) {
         logDebug("Email could not be sent as there is no Applicant email address.")
