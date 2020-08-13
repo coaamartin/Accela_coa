27bd793821e861d2fb0666c17e8a729eacd46a17
Province <rprovinc@auroragov.org>
2020-08-13 13:12:34 -0600

    added a pra event and updated fee logic

--- EVENT/PRA;CityClerk!Incident!~!~.js
+++ EVENT/PRA;CityClerk!Incident!~!~.js
@@ -11,10 +11,15 @@
 //Created By: Rprovinc
 //******************************************************************************
 
-
+logDebug("Starting PRA;CityClerk!Incident!~!~.js ");
+logDebug("Checking to see if City Manager's Office Approval status = Approved");
 if(ifTracer(wfTask == "City Manager's Office Approval" && wfStatus == "Approved")){
     //Script to verify that the payment has been processed
+    logDebug("Checking balance")
     if(balanceDue == 0){
+        logDebug("Starting 5127_CityClerk_PRA.js");
         include("5127_CityClerk_PRA.js");
+        logDebug("End of 5127_CityClerk_PRA.js");
     }
 }
+logDebug("End of PRA;CityClerk!Incident!~!~.js ");
