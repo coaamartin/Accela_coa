1c8cf92056de4166895586709343bac73fe85557
Province <rprovinc@auroragov.org>
2020-08-17 07:40:53 -0600

    added logic for planning dir email and updated pra events

--- INCLUDES_SCRIPTS/5127_CityClerk_PRA.js
+++ INCLUDES_SCRIPTS/5127_CityClerk_PRA.js
@@ -17,29 +17,59 @@ logDebug("Check to see what type of record it running and then update app status
 
 
 //Donation Bins code
-if ("CityClerk/Incident/DonationBin/NA".equals(appTypeString)) {
+if ("Application Close".equals(wfTask)) {
+    var bal = getCapBalanceDue();
+    logDebug("Current balance: " + bal);
     logDebug("Starting DB approval email and updating statues");
+
+    //Check balance and update task
+    if (bal == 0){
+    updateAppStatus("Approved","Status updated via script 5127_CityClerk_PRA.js");
+    updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");
     include("5124_CityClerk_Approval");
-    activateTask("Application Close");
-    updateAppStatus("PERMIT ISSUED","Status updated via script 5127_CityClerk_PRA.js");
-    updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");				
+}				
 } 
 
-//Temp Use code
-else if ("CityClerk/Incident/TempUse/NA".equals(appTypeString)) {
-    logDebug("Starting TU approval email and updating statues");
-    include("5124_CityClerk_Approval");
-    activateTask("Application Close");
-    updateAppStatus("PERMIT ISSUED","Status updated via script 5127_CityClerk_PRA.js");
-    updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");	
-}
+// //Temp Use code
+// else if ("CityClerk/Incident/TempUse/NA".equals(appTypeString)) {
+//     logDebug("Starting TU approval email and updating statues");
+//     include("5124_CityClerk_Approval");
+//     activateTask("Application Close");
+//     updateAppStatus("PERMIT ISSUED","Status updated via script 5127_CityClerk_PRA.js");
+//     updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");	
+// }
 
-//Temp Sign code
-else if ("CityClerk/Incident/TempSign/NA".equals(appTypeString)) {
-    logDebug("Starting TS approval email and updating statues");
-    include("5124_CityClerk_Approval");
-    activateTask("Application Close");
-    updateAppStatus("PERMIT ISSUED","Status updated via script 5127_CityClerk_PRA.js");
-    updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");	
+// //Temp Sign code
+// else if ("CityClerk/Incident/TempSign/NA".equals(appTypeString)) {
+//     logDebug("Starting TS approval email and updating statues");
+//     include("5124_CityClerk_Approval");
+//     activateTask("Application Close");
+//     updateAppStatus("PERMIT ISSUED","Status updated via script 5127_CityClerk_PRA.js");
+//     updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");	
+// }
+
+function getCapBalanceDue() {
+    //Optional capId
+    itemCap = capId;
+    if (arguments.length == 1)
+        itemCap = arguments[0];
+
+    var feesArr = loadFees();
+    if (!feesArr)
+        return 0;
+
+    var tot = 0;
+    for (i in feesArr)
+    {
+    	//if (("INVOICED".equals(feesArr[i].status)) || (("NEW".equals(feesArr[i].status))))
+    	//{
+    		tot += (+feesArr[i].amount) - (+feesArr[i].amountPaid);
+         
+         
+    	//}
+        
+    }
+    
+    return tot;
 }
 logDebug("End of 5127_CityClerk_PRA script"); 
