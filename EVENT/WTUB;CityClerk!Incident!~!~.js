7d4460a59bcd13769904e1b80ec2b525e8f54622
Province <rprovinc@auroragov.org>
2020-08-18 11:26:23 -0600

    updated planning director email

--- EVENT/WTUB;CityClerk!Incident!~!~.js
+++ EVENT/WTUB;CityClerk!Incident!~!~.js
@@ -16,18 +16,3 @@ if (wfTask == "Planning Director Approval" && wfStatus == "In Progress") {
         include("5122_CityClerk_Notifications");
         logDebug("Finished sending notification to the Planning Director");
     }
-
-// if(ifTracer(wfTask == "City Manager's Office Approval" && wfStatus == "Approved")){
-//     //Script to verify that the payment has been processed
-//     if(balanceDue > 0){
-//         cancel = true;
-//         showMessage = true;
-//         comment("All fees need to be paid before issuing application");
-//     }
-// }
-// logDebug("Starting to send notification to the planning director");
-// if(wfTask == "Planning Director Approval") {
-//     logDebug("Starting to send notification to the Planning Director");
-//     include("5122_CityClerk_Notifications");
-//     logDebug("Finished sending notification to the Planning Director");
-// }
