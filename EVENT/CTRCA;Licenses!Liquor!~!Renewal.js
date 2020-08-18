6cb4d5cc636943e06674f0cd82370635daa3de60
Acharlton <acharlton@truepointsolutions.com>
2020-08-18 15:10:44 -0600

    Update CTRCA;Licenses!Liquor!~!Renewal.js

--- EVENT/CTRCA;Licenses!Liquor!~!Renewal.js
+++ EVENT/CTRCA;Licenses!Liquor!~!Renewal.js
@@ -1,2 +1,5 @@
 //Run script to set ALTID for child renewal record.
 include("2054_CreateLicenseRenewal");
+logDebug("Starting License Fees for Liquor License");
+include("2057_LicenseFees");
+logDebug("Ending License Fees for Liquor License");
