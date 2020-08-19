e494485442bc0d5a912ba6e36e1ee57b0ea14677
Province <rprovinc@auroragov.org>
2020-08-19 10:22:43 -0600

    Added a PRA event to handle payment processing

--- EVENT/PRA;CityClerk!Incident!~!~.js
+++ EVENT/PRA;CityClerk!Incident!~!~.js
@@ -11,7 +11,18 @@
 //Created By: Rprovinc
 //******************************************************************************
 
-logDebug("Starting PRA;CityClerk!Incident!~!~.js ");
+logDebug("Starting PPA;CityClerk!~!~!~.js ");
 logDebug("Starting 5127_CityClerk_PRA.js");
-include("5127_CityClerk_PRA.js");
-logDebug("End of PRA;CityClerk!Incident!~!~.js ");
+//include("5127_CityClerk_PRA.js");
+logDebug("Current balance: " + balanceDue);
+logDebug("Starting DB approval email and updating statues");
+//Check balance and update task
+if (balanceDue == 0){
+updateAppStatus("Approved","Status updated via script 5127_CityClerk_PRA.js");
+updateTask("Application Close", "Approved", "Updated via script 5127_CityClerk_PRA.js");
+include("5124_CityClerk_Approval");
+logDebug("End of 5127_CityClerk_PRA script"); 
+logDebug("---------------------> 5127_CityClerk_PRA.js ended.");
+aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
+}
+logDebug("End of PPA;CityClerk!Incident!~!~.js ");
