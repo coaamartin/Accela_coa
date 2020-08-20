553f2516a27c519fa2050399e847f644000323c9
Province <rprovinc@auroragov.org>
2020-08-20 09:27:00 -0600

    updated CTRCA evemt

--- EVENT/ASA;CityClerk!Incident!~!~.js
+++ EVENT/ASA;CityClerk!Incident!~!~.js
@@ -1,7 +1,7 @@
 logDebug("ASA:CityClerk/Incident/NA/NA");
 //include("5122_CityClerk_Notifications");
 //include("EMSE:ConvertToRealCapAfter");
-var altId = capId.getCustomID();
+var altId = capId;
 logDebug("Record ID: " + altId);
 if (altID.equals("%TMP%")) {
 logDebug("Going to have the CTRCA event trigger.");
