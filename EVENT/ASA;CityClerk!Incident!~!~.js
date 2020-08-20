f3a59ed952a5b2f260ba33bcd62cb41edb7bbd89
Province <rprovinc@auroragov.org>
2020-08-20 15:59:55 -0600

    updated asa cc script

--- EVENT/ASA;CityClerk!Incident!~!~.js
+++ EVENT/ASA;CityClerk!Incident!~!~.js
@@ -1,13 +1,13 @@
 logDebug("ASA:CityClerk/Incident/NA/NA");
+include("5110_CodeTempSignCTRCA");
+// var altId = capId;
+// var altId2 = capId.getCustomID();
+// logDebug("altId: " + altId);
+// logDebug("altId2: " + altId2);
 
-var altId = capId;
-var altId2 = capId.getCustomID();
-logDebug("altId: " + altId);
-logDebug("altId2: " + altId2);
-
-if (ifTracer(altId2.equals("*TMP*"))) {
-     logDebug("Going to have the CTRCA event trigger.");
-} else {
-    logDebug("Going to have 5110 script tirgger.")
-    include("5110_CodeTempSignCTRCA");
-}
+// if (ifTracer(altId2.equals("*TMP*"))) {
+//      logDebug("Going to have the CTRCA event trigger.");
+// } else {
+//     logDebug("Going to have 5110 script tirgger.")
+//     include("5110_CodeTempSignCTRCA");
+// }
