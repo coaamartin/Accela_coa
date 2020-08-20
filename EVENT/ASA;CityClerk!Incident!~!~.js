190b4744803c1590874a42ec703ae0333824e05a
Province <rprovinc@auroragov.org>
2020-08-20 15:20:31 -0600

    updated asa cc script

--- EVENT/ASA;CityClerk!Incident!~!~.js
+++ EVENT/ASA;CityClerk!Incident!~!~.js
@@ -1,7 +1,7 @@
 logDebug("ASA:CityClerk/Incident/NA/NA");
 //include("5122_CityClerk_Notifications");
 //include("EMSE:ConvertToRealCapAfter");
-var altId = capId;
+var altId = capId.getCustomID();
 var altId2 = capId.getCustomID();
 logDebug("altId: " + altId);
 logDebug("altId2: " + altId2);
