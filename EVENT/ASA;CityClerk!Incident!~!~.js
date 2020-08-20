0c40d5a92b5f252f4ffedab1b6b225b3de275a4c
Province <rprovinc@auroragov.org>
2020-08-20 09:35:05 -0600

    updated planning director email script

--- EVENT/ASA;CityClerk!Incident!~!~.js
+++ EVENT/ASA;CityClerk!Incident!~!~.js
@@ -1,9 +1,9 @@
 logDebug("ASA:CityClerk/Incident/NA/NA");
 //include("5122_CityClerk_Notifications");
 //include("EMSE:ConvertToRealCapAfter");
-var altId = capId;
-logDebug("Record ID: " + altId);
-if (altID.equals("%TMP%")) {
+//var altId = capId;
+logDebug("Record ID: " + capID);
+if (capID.equals("%TMP%")) {
 logDebug("Going to have the CTRCA event trigger.");
 }
 else {
