219dae1992cb01237c3a64ab491c673196550abc
Suhail Wakil <suhailwakil@gmail.com>
2020-10-11 14:13:12 -0700

    Update 489_Check_MJ_Application_Docs.js

--- INCLUDES_SCRIPTS/489_Check_MJ_Application_Docs.js
+++ INCLUDES_SCRIPTS/489_Check_MJ_Application_Docs.js
@@ -11,7 +11,7 @@ if ("Corporation".equals(AInfo["Type of Ownership"]) || "LLC".equals(AInfo["Type
 	{
 		cancel = true;
 		showMessage = true;
-		comment("Document types Local - Articles of Incorporation and Local - Bylaws are required for this Type of Ownership");
+		comment("Document types Local - Articles of Incorporation and Local - Bylaws are required for this Type of Ownership.");
 	}
 }
 else if ("Individual".equals(AInfo["Type of Ownership"]) || "Sole Proprietor".equals(AInfo["Type of Ownership"]) || "Partnership".equals(AInfo["Type of Ownership"]))
@@ -29,6 +29,6 @@ else if ("Individual".equals(AInfo["Type of Ownership"]) || "Sole Proprietor".eq
 	{
 		cancel = true;
 		showMessage = true;
-		comment("Document type Local - Operating Agreement is required for this Type of Ownership");
+		comment("Document type Local - Operating Agreement is required for this Type of Ownership.");
 	}	
 }
