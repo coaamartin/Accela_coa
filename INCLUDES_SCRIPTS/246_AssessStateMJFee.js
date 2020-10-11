0d414644b86bb0f0c814cd8b9c0fe1b6a591d8cc
Suhail Wakil <suhailwakil@gmail.com>
2020-10-11 11:57:52 -0700

    Update 246_AssessStateMJFee.js

--- INCLUDES_SCRIPTS/246_AssessStateMJFee.js
+++ INCLUDES_SCRIPTS/246_AssessStateMJFee.js
@@ -1,5 +1,6 @@
-
+//Commenting based on ID 454
 //assess state application fee based on application type
+/*
 if (appMatch("Licenses/Marijuana/Retail Cultivation/Application")) {
 	updateFee("LIC_MJRC_01", "LIC_MJ_RC", "FINAL", 1, "N");
 } else if (appMatch("Licenses/Marijuana/Retail Product Manufacturer/Application")) {
@@ -11,3 +12,4 @@ if (appMatch("Licenses/Marijuana/Retail Cultivation/Application")) {
 } else if (appMatch("Licenses/Marijuana/Retail Store/Application")) {
 	updateFee("LIC_MJRC_01", "LIC_MJ_RC", "FINAL", 1, "N");
 }
+*/
