a1aa30698416ff49072416e139dda834bce917fd
Province <rprovinc@auroragov.org>
2020-10-13 10:44:11 -0600

    updated 5126 script

--- INCLUDES_SCRIPTS/5126_Associations_Number.js
+++ INCLUDES_SCRIPTS/5126_Associations_Number.js
@@ -69,6 +69,8 @@ logDebug("Started to run script 5126");
 // logDebug("Finished running the async event??");
 //Associations/Neighborhood/Association
 logDebug("Starting SQL script");
+var altId = capId.getCustomID();
+logDebug("altID: " + altId);
 //SELECT cast (MAX(cast(b1_checklist_comment as int))as varchar (20))FROM BCHCKBOXWHERE B1_CHECKBOX_DESC like 'Neighborhood Group Number';
 var sql = "SELECT convert (varchar (20), MAX(convert(int, b1_checklist_comment))) as group_number " +
 	" FROM BCHCKBOX " +
@@ -95,7 +97,6 @@ function doSQL(sql) {
 			while (results.next()){
 				array.push( results.getString("group_number"));
 			}
-			//rSet.close();
 		sStmt.close();
 		conn.close();
 		if(array==null || array==undefined || array==""){
@@ -103,89 +104,15 @@ function doSQL(sql) {
 		}
 		logDebug("Array: " + array);
 		hoaNumber = ++array;
-		// for (var i = 0; i < hoaNumber.length; i++){
-		// 	hoaNumber[i] + 1;
-		// }
 		logDebug ("HOA number is: " + hoaNumber);
+		//new sql function would kick off to update the value on the record.
 		return array;
 		}
 	} catch (err) {
 		aa.print(err.message);
 	}
-	hoaNumber = array++;
-	logDebug ("HOA number is: " + hoaNumber);
-	//new sql function would kick off to update the value on the record.
+
+	
 }
 
-		// var rSet = sStmt.executeQuery();
-			// logDebug("SSTMT: " + rSet); 
-			// while (rSet) {
-			// var obj = {};
-			// var md = rSet.getMetaData();
-			// logDebug("MD value: " + md);
-			// var columns = md.getColumnCount();
-			// logDebug("Columns: "+ columns);
-			// for (i = 1; i <= columns; i++) {
-			// 	obj[md.getColumnName(i)] = String(rSet.getString(md.getColumnName(i)));
-			// }
-			// obj = rSet.getRow();
-			// array.push(obj);
-			// logDebug("Array: " + array);
-			// return array;
-			// }
-			// rSet.close();
-			// sStmt.close();
-			// conn.close();
-			// logDebug("The highest neighborhood number is: " + array);
-			// var hoaNumber = array++;
-			// logDebug("New HOA number is: " + hoaNumber);
 
-// logDebug("The highest neighborhood number is: " + array);
-// var hoaNumber = array++;
-// logDebug("New HOA number is: " + hoaNumber);
-
-//getNeighborhoodNumber();
-
-// function getNeighborhoodNumber(){
-
-//     try {
-
-//         //Associations/Neighborhood/Association
-//         logDebug("Starting SQL script");
-//         //SELECT cast (MAX(cast(b1_checklist_comment as int))as varchar (20))FROM BCHCKBOXWHERE B1_CHECKBOX_DESC like 'Neighborhood Group Number';
-//         var sql = "SELECT convert (varchar (20), MAX(convert(int, b1_checklist_comment))) as group_number " +
-//                   " FROM BCHCKBOX " +
-//                   " WHERE B1_CHECKBOX_DESC like 'Neighborhood Group Number'"
-
-//         var array = [];
-//         var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
-//         var ds = initialContext.lookup("java:/AA");
-//         var conn = ds.getConnection();
-//         var sStmt = conn.prepareStatement(sql);
-//         logDebug("SQL statement that is built: " + sql);
-//         logDebug("SQL results: " + array);
-//         //only execute select statements
-// 		if (sql.toUpperCase().indexOf("SELECT") == 0) {
-// 			sStmt.executeQuery();
-// 			results = sStmt.getResultSet()
-// 			while (results.next()){
-// 					array.push( results.getString("group_number"));
-// 				}
-// 		sStmt.close();
-// 		conn.close();
-// 		if(array==null || array==undefined || array==""){
-// 			return "";
-// 		}
-// 		return array;
-// 		}
-// 		} catch (err) {
-// 			logDebug("This is the error: " + err.message);
-
-// 	}
-//         logDebug("The highest neighborhood number is: " + array);
-//         var hoaNumber = array++;
-//         logDebug("New HOA number is: "+ hoaNumber);
-
-// }
-//push the new hoaNumber into the Neighborhood Group Number
-//This is a cutom field
