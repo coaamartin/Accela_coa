7424d68e82076e523584f2b54a170b3c713300d0
Suhail Wakil <suhailwakil@gmail.com>
2020-10-24 12:50:58 -0700

    Update ASA;Licenses!Marijuana!~!Application.js

--- EVENT/ASA;Licenses!Marijuana!~!Application.js
+++ EVENT/ASA;Licenses!Marijuana!~!Application.js
@@ -1,13 +1,12 @@
 showDebug = true;
 //JMAIN - MJ Application Submittal
+if(!publicUser){
 include("28_AMEDEmailApplicantAtRecordCreation");
+}
 
 //assess State MJ Licensing Fee on application submit
 include("246_AssessStateMJFee");
 
-//schedule pending inspections
-//include("404_AppAutomationMJ");
-
 //SWAKIL
 //required docs conditions
-include("495_Add_Required_Docs_Conditions");
+//include("495_Add_Required_Docs_Conditions");
