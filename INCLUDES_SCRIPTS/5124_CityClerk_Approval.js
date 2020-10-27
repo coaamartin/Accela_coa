ce0fe37b1391159e4c62ae2f54c222dc116a6f35
Province <rprovinc@auroragov.org>
2020-10-27 07:58:10 -0600

    updated 5124

--- INCLUDES_SCRIPTS/5124_CityClerk_Approval.js
+++ INCLUDES_SCRIPTS/5124_CityClerk_Approval.js
@@ -39,7 +39,7 @@ logDebug("appType: " + appTypeString);
     //tParams.put("$$wfComment$$", wfComment);
     logDebug("EmailTo: " + emailTo);
     logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@aurorgov.org", emailTo, "", vEmailTemplate, tParams, null);
+    sendNotification("noreply@auroagov.org", emailTo, "", vEmailTemplate, tParams, null);
     logDebug("End of Script 5124_CityClerk_Approval.js");
 }
 
@@ -65,7 +65,7 @@ logDebug("appType: " + appTypeString);
     //tParams.put("$$wfComment$$", wfComment);
     logDebug("EmailTo: " + emailTo);
     logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@aurorgov.org", emailTo, "", vEmailTemplate, tParams, null);
+    sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
     logDebug("End of Script 5124_CityClerk_Approval.js");
 }
 
@@ -91,6 +91,6 @@ logDebug("appType: " + appTypeString);
     //tParams.put("$$wfComment$$", wfComment);
     logDebug("EmailTo: " + emailTo);
     logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@aurorgov.org", emailTo, "", vEmailTemplate, tParams, null);
+    sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
     logDebug("End of Script 5124_CityClerk_Approval.js");
 }
