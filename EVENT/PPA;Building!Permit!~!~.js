aa2c63d952ef020368dc47dd51dfce32dff2ea09
Province <rprovinc@auroragov.org>
2020-10-27 07:35:52 -0600

    updated permits cert

--- EVENT/PPA;Building!Permit!~!~.js
+++ EVENT/PPA;Building!Permit!~!~.js
@@ -15,10 +15,10 @@ if (balanceDue == 0) {
 	aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 	//Start to generate the Certificate. This will attach to the record when ran.
 	logDebug("Starting to kick off event to attach cert to record");
-	CapId = capId;
+	CapId = capId.getCustomID();
 	var vAsyncScript = "RUN_PERMITS_CERT";
 	var envParameters = aa.util.newHashMap();
-	envParameters.put("CapId", CapId);
+	envParameters.put("CapId", capId);
 	logDebug("Starting to kick off ASYNC event. Params being passed: " + envParameters);
 	aa.runAsyncScript(vAsyncScript, envParameters);
 // 	if ("Building/Permit/DonationBin/NA".equals(appTypeString)) {
