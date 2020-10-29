de74492e996a07a6378d0cd2fbce58b5e4bf18f6
Province <rprovinc@auroragov.org>
2020-10-29 09:59:38 -0600

    updated wtua buidling

--- ASYNC/RUN_PERMITS_CERT.js
+++ ASYNC/RUN_PERMITS_CERT.js
@@ -20,7 +20,7 @@ try {
 
   //Start to generate the Certificate. This will attach to the record when ran.
   logDebug("Starting to kick off event to attach cert to record");
-  logDebug("altId = " + capId);
+  logDebug("altId = " + altId);
   logDebug("AppType = " + appTypeString);
   if ("Building/Permit/DonationBin/NA".equals(appTypeString)) {
     logDebug("Donation Bin app type. Starting to run report.");
@@ -52,41 +52,42 @@ try {
     report = null;
     report = generateReportFile(repName, reportParameters, module);
   }
+  function generateReportFile(aaReportName, parameters, rModule) {
+    var reportName = aaReportName
+    report = aa.reportManager.getReportInfoModelByName(reportName);
+    report = report.getOutput();
+    report.setModule(rModule);
+    report.setCapId(capId);
+    report.setReportParameters(parameters);
+    //Added
+    vAltId = capId.getCustomID();
+    report.getEDMSEntityIdModel().setAltId(vAltId);
+    var permit = aa.reportManager.hasPermission(reportName, "ADMIN");
+    aa.print("---" + permit.getOutput().booleanValue());
+    if (permit.getOutput().booleanValue()) {
+      var reportResult = aa.reportManager.getReportResult(report);
+  
+      if (reportResult) {
+        reportResult = reportResult.getOutput();
+        var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+        logMessage("Report Result: " + reportResult);
+        reportFile = reportFile.getOutput();
+        return reportFile
+      } else {
+        logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
+        return false;
+      }
+    } else {
+      logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
+      return false;
+    }
+  }
 }
 catch (e) {
   email("debug@gmail.com", "rprovinc@auroragov.org", "Error", e.message);
   aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 }
-function generateReportFile(aaReportName, parameters, rModule) {
-  var reportName = aaReportName
-  report = aa.reportManager.getReportInfoModelByName(reportName);
-  report = report.getOutput();
-  report.setModule(rModule);
-  report.setCapId(capId);
-  report.setReportParameters(parameters);
-  //Added
-  vAltId = capId.getCustomID();
-  report.getEDMSEntityIdModel().setAltId(vAltId);
-  var permit = aa.reportManager.hasPermission(reportName, "ADMIN");
-  aa.print("---" + permit.getOutput().booleanValue());
-  if (permit.getOutput().booleanValue()) {
-    var reportResult = aa.reportManager.getReportResult(report);
-
-    if (reportResult) {
-      reportResult = reportResult.getOutput();
-      var reportFile = aa.reportManager.storeReportToDisk(reportResult);
-      logMessage("Report Result: " + reportResult);
-      reportFile = reportFile.getOutput();
-      return reportFile
-    } else {
-      logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
-      return false;
-    }
-  } else {
-    logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
-    return false;
-  }
-} 
+ 
 aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 function wait(ms) {
   var start = new Date().getTime();
