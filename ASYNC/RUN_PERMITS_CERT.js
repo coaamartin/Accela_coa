8b8fe89cdca6eac1c4802e45243c1bfcb52a3144
Province <rprovinc@auroragov.org>
2020-10-29 10:59:33 -0600

    updated run permits cert

--- ASYNC/RUN_PERMITS_CERT.js
+++ ASYNC/RUN_PERMITS_CERT.js
@@ -29,6 +29,44 @@ if ("Building/Permit/DonationBin/NA".equals(appTypeString)) {
   logDebug("Rparams for envent" + reportParameters);
   report = null;
   report = generateReportFile(repName, reportParameters, module);
+  function generateReportFile(aaReportName,parameters,rModule) 
+{
+  var reportName = aaReportName;
+
+  report = aa.reportManager.getReportInfoModelByName(reportName);
+  report = report.getOutput();
+
+
+  report.setModule(rModule);
+  report.setCapId(capId);
+  report.setReportParameters(parameters);
+  //Added
+  vAltId = capId.getCustomID();
+  report.getEDMSEntityIdModel().setAltId(vAltId);
+  var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
+  aa.print("---"+permit.getOutput().booleanValue());
+  if(permit.getOutput().booleanValue()) 
+  {
+    var reportResult = aa.reportManager.getReportResult(report);
+
+    if(reportResult) 
+    {
+      reportResult = reportResult.getOutput();
+      var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+      logMessage("Report Result: "+ reportResult);
+      reportFile = reportFile.getOutput();
+      return reportFile
+    } else 
+    {
+      logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+      return false;
+    }
+  } else 
+  {
+    logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+    return false; 
+  }
+}
 }
 if ("Building/Permit/TempSigns/NA".equals(appTypeString)) {
   logDebug("Temp Sign app type. Starting to run report.");
@@ -39,6 +77,44 @@ if ("Building/Permit/TempSigns/NA".equals(appTypeString)) {
   logDebug("Rparams for envent" + reportParameters);
   report = null;
   report = generateReportFile(repName, reportParameters, module);
+  function generateReportFile(aaReportName,parameters,rModule) 
+{
+  var reportName = aaReportName;
+
+  report = aa.reportManager.getReportInfoModelByName(reportName);
+  report = report.getOutput();
+
+
+  report.setModule(rModule);
+  report.setCapId(capId);
+  report.setReportParameters(parameters);
+  //Added
+  vAltId = capId.getCustomID();
+  report.getEDMSEntityIdModel().setAltId(vAltId);
+  var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
+  aa.print("---"+permit.getOutput().booleanValue());
+  if(permit.getOutput().booleanValue()) 
+  {
+    var reportResult = aa.reportManager.getReportResult(report);
+
+    if(reportResult) 
+    {
+      reportResult = reportResult.getOutput();
+      var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+      logMessage("Report Result: "+ reportResult);
+      reportFile = reportFile.getOutput();
+      return reportFile
+    } else 
+    {
+      logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+      return false;
+    }
+  } else 
+  {
+    logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+    return false; 
+  }
+}
 }
 if ("Building/Permit/TempUse/NA".equals(appTypeString)) {
   logDebug("Temp Use app type. Starting to run report.");
@@ -49,6 +125,44 @@ if ("Building/Permit/TempUse/NA".equals(appTypeString)) {
   logDebug("Rparams for envent" + reportParameters);
   report = null;
   report = generateReportFile(repName, reportParameters, module);
+  function generateReportFile(aaReportName,parameters,rModule) 
+{
+  var reportName = aaReportName;
+
+  report = aa.reportManager.getReportInfoModelByName(reportName);
+  report = report.getOutput();
+
+
+  report.setModule(rModule);
+  report.setCapId(capId);
+  report.setReportParameters(parameters);
+  //Added
+  vAltId = capId.getCustomID();
+  report.getEDMSEntityIdModel().setAltId(vAltId);
+  var permit = aa.reportManager.hasPermission(reportName,"ADMIN");
+  aa.print("---"+permit.getOutput().booleanValue());
+  if(permit.getOutput().booleanValue()) 
+  {
+    var reportResult = aa.reportManager.getReportResult(report);
+
+    if(reportResult) 
+    {
+      reportResult = reportResult.getOutput();
+      var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+      logMessage("Report Result: "+ reportResult);
+      reportFile = reportFile.getOutput();
+      return reportFile
+    } else 
+    {
+      logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+      return false;
+    }
+  } else 
+  {
+    logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+    return false; 
+  }
+}
 }
 
 function generateReportFile(aaReportName,parameters,rModule) 
