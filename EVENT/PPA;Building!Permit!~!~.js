f1fbef995cb310c5c4141e7a0bc1d312fa6786ee
Province <rprovinc@auroragov.org>
2020-10-29 09:28:38 -0600

    updated RUN PERMITS ASYNC

--- EVENT/PPA;Building!Permit!~!~.js
+++ EVENT/PPA;Building!Permit!~!~.js
@@ -16,6 +16,7 @@ if (balanceDue == 0) {
 	//Start to generate the Certificate. This will attach to the record when ran.
 	logDebug("Starting to kick off event to attach cert to record");
 	CapId = capId.getCustomID();
+	appType = cap.getCapType().toString();
 	var vAsyncScript = "RUN_PERMITS_CERT";
 	var envParameters = aa.util.newHashMap();
 	envParameters.put("CapId", capId);
