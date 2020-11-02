80c4a528f721252ce613bce8c195f5837561c6ea
Province <rprovinc@auroragov.org>
2020-11-02 10:47:37 -0700

    updated 5121

--- INCLUDES_SCRIPTS/5121_CItyClerk.js
+++ INCLUDES_SCRIPTS/5121_CItyClerk.js
@@ -181,43 +181,41 @@ else if ("Yes".equals(vASIValue) && "Building/Permit/DonationBin/NA".equals(appT
     report = generateReportFile(repName, reportParameters, module);
     aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 
-    function generateReportFile(aaReportName, parameters, rModule) {
-        var reportName = aaReportName;
-
-        report = aa.reportManager.getReportInfoModelByName(reportName);
-        report = report.getOutput();
-
-
-        report.setModule(rModule);
-        report.setCapId(capID);
-        report.setReportParameters(parameters);
-        //Added
-        vAltId = capID;
-        report.getEDMSEntityIdModel().setAltId(vAltId);
-        var permit = aa.reportManager.hasPermission(reportName, "ADMIN");
-        aa.print("---" + permit.getOutput().booleanValue());
-        if (permit.getOutput().booleanValue()) {
-            var reportResult = aa.reportManager.getReportResult(report);
-
-            if (reportResult) {
-                reportResult = reportResult.getOutput();
-                var reportFile = aa.reportManager.storeReportToDisk(reportResult);
-                logMessage("Report Result: " + reportResult);
-                reportFile = reportFile.getOutput();
-                return reportFile
-            } else {
-                logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
-                return false;
-            }
-        } else {
-            logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
-            return false;
-        }
-    }
-
     include("5124_CityClerk_Approval");
 }
+function generateReportFile(aaReportName, parameters, rModule) {
+    var reportName = aaReportName;
 
+    report = aa.reportManager.getReportInfoModelByName(reportName);
+    report = report.getOutput();
+
+
+    report.setModule(rModule);
+    report.setCapId(capID);
+    report.setReportParameters(parameters);
+    //Added
+    vAltId = capID;
+    report.getEDMSEntityIdModel().setAltId(vAltId);
+    var permit = aa.reportManager.hasPermission(reportName, "ADMIN");
+    aa.print("---" + permit.getOutput().booleanValue());
+    if (permit.getOutput().booleanValue()) {
+        var reportResult = aa.reportManager.getReportResult(report);
+
+        if (reportResult) {
+            reportResult = reportResult.getOutput();
+            var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+            logMessage("Report Result: " + reportResult);
+            reportFile = reportFile.getOutput();
+            return reportFile
+        } else {
+            logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
+            return false;
+        }
+    } else {
+        logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
+        return false;
+    }
+}
 logDebug("End of tax exempt script");
 logDebug("End of script 5121_CityClerk.js");
 
