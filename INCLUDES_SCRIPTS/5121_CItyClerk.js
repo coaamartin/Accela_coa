eee06b57cfed64f081c723a6ce4d561484baa4e4
Province <rprovinc@auroragov.org>
2020-11-02 10:42:37 -0700

    updated run_tu_cert

--- INCLUDES_SCRIPTS/5121_CItyClerk.js
+++ INCLUDES_SCRIPTS/5121_CItyClerk.js
@@ -118,7 +118,7 @@ else if ("No".equals(vASIValue) && "Building/Permit/TempSigns/NA".equals(appType
     logDebug("Table Parameters: " + tParams);
     sendNotification("noreply@auroragov.org", emailTo, "", emailTemplate, tParams, null);
     logDebug("End of Script 5121_CityClerk.js");
-} 
+}
 //If Tax excempt is yes do the following.
 else if ("Yes".equals(vASIValue) && "Building/Permit/DonationBin/NA".equals(appTypeString)) {
     updateAppStatus("Approved", "Status updated via script 5127_CityClerk_PRA.js");
@@ -162,16 +162,59 @@ else if ("Yes".equals(vASIValue) && "Building/Permit/DonationBin/NA".equals(appT
     //aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
     //Start to generate the Certificate. This will attach to the record when ran.
     logDebug("Starting to kick off event to attach cert to record");
-    var altID = capId.getCustomID();
+    var capID = capId.getCustomID();
     var appType = cap.getCapType().toString();
     var serProvCode = aa.getServiceProviderCode();
     var vAsyncScript = "RUN_TU_CERT";
-    var envParameters = aa.util.newHashMap();
-    envParameters.put("CapId", altID);
-    envParameters.put("AppType", appType);
-    envParameters.put("ServProvCode", serProvCode);
-    logDebug("Starting to kick off ASYNC eventfor Temp Use. Params being passed: " + envParameters);
-    aa.runAsyncScript(vAsyncScript, envParameters);
+    // var envParameters = aa.util.newHashMap();
+    // envParameters.put("CapId", altID);
+    // envParameters.put("AppType", appType);
+    // envParameters.put("ServProvCode", serProvCode);
+    //logDebug("Starting to kick off ASYNC eventfor Temp Use. Params being passed: " + envParameters);
+    //aa.runAsyncScript(vAsyncScript, envParameters);
+    var module = "Building";
+    var repName = "Temp_Use_Permit_script";
+    reportParameters = aa.util.newHashMap(); 
+    reportParameters.put("RecordID", capID);
+    logDebug("Report Parameters: " + reportParameters);
+    report = null;
+    report = generateReportFile(repName, reportParameters, module);
+    aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
+
+    function generateReportFile(aaReportName, parameters, rModule) {
+        var reportName = aaReportName;
+
+        report = aa.reportManager.getReportInfoModelByName(reportName);
+        report = report.getOutput();
+
+
+        report.setModule(rModule);
+        report.setCapId(capID);
+        report.setReportParameters(parameters);
+        //Added
+        vAltId = capID;
+        report.getEDMSEntityIdModel().setAltId(vAltId);
+        var permit = aa.reportManager.hasPermission(reportName, "ADMIN");
+        aa.print("---" + permit.getOutput().booleanValue());
+        if (permit.getOutput().booleanValue()) {
+            var reportResult = aa.reportManager.getReportResult(report);
+
+            if (reportResult) {
+                reportResult = reportResult.getOutput();
+                var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+                logMessage("Report Result: " + reportResult);
+                reportFile = reportFile.getOutput();
+                return reportFile
+            } else {
+                logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
+                return false;
+            }
+        } else {
+            logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
+            return false;
+        }
+    }
+
     include("5124_CityClerk_Approval");
 }
 
