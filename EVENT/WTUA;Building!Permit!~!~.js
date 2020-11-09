bb79c6f79f4f6dd871bacea1b404097ed49873de
Province <rprovinc@auroragov.org>
2020-11-09 11:23:56 -0700

    updated wtua permit

--- EVENT/WTUA;Building!Permit!~!~.js
+++ EVENT/WTUA;Building!Permit!~!~.js
@@ -173,17 +173,17 @@ else if (wfTask == "Final Approval" && wfStatus == "Denied") {
     closeAllTasks(capId, "");
 }
 
-else if (wfStatus = "Void") {
-    //include("5125_CityClerk_Denial");
-    updateAppStatus("VOID", "Script 5125_CityClerk_Denial");
-    closeAllTasks(capId, "");
-}
+// else if (wfStatus = "Void") {
+//     //include("5125_CityClerk_Denial");
+//     updateAppStatus("VOID", "Script 5125_CityClerk_Denial");
+//     closeAllTasks(capId, "");
+// }
 
-else if (wfStatus = "Withdrawn") {
-    //include("5125_CityClerk_Denial");
-    updateAppStatus("Withdrawn", "Script 5125_CityClerk_Denial");
-    closeAllTasks(capId, "");
-}
+// else if (wfStatus = "Withdrawn") {
+//     //include("5125_CityClerk_Denial");
+//     updateAppStatus("Withdrawn", "Script 5125_CityClerk_Denial");
+//     closeAllTasks(capId, "");
+// }
 logDebug("End of WTUA;Building");
 
 // aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
