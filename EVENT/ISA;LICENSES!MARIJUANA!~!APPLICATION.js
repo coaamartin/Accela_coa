b1d219d55340231b9a7727f3b79545e85d0935a5
Suhail Wakil <suhailwakil@gmail.com>
2020-11-11 19:01:26 -0800

    Assigning inspections using std choice

---
+++ EVENT/ISA;LICENSES!MARIJUANA!~!APPLICATION.js
@@ -0,0 +1,15 @@
+//ISA:LICENSES/MARIJUANA/*/APPLICATION 
+//allow manual assignment override
+if(matches(inspInspector,null,undefined,"")){
+	if(inspType =="MJ Security Inspections - Police"){
+		//get assignment designation
+		var assignTo = lookup("MARIJUANA_INSPECTION_ASSIGNMENT",inspType);
+		var assignToFullName = assi
+		if(assignTo){
+			var assignToId = aa.person.getUser(firstName, middleName, lastName);
+			assignInspection(inspId,assignTo)
+		}
+		
+	}
+	
+}
