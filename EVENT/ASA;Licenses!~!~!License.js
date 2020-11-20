b840a39cdfab95b5cb138c2d60f7bd3242808f40
Acharlton <acharlton@truepointsolutions.com>
2020-11-20 05:18:25 -0700

    Update ASA;Licenses!~!~!License.js

--- EVENT/ASA;Licenses!~!~!License.js
+++ EVENT/ASA;Licenses!~!~!License.js
@@ -2,6 +2,6 @@
 if (appMatch("Licenses/Supplemental/Seasonal Licenses/License")){
 logDebug("Starting Script to Set Temporary Expiration Date");
 updateShortNotes(AInfo['Type of License']);
-include("2059_TmpPermitExpirationDate");
+include("2059_TmpPermitExpiraitionDate");
 logDebug("End of 2059_TmpPermitExpirationDate script");
 }
