67e3a41c46316a633242a4a83bee1eab7c0cb3d7
Acharlton <acharlton@truepointsolutions.com>
2020-12-04 10:43:22 -0700

    Updating GB script
    Modified GB exp code

--- EVENT/WTUA;Licenses!Business!General!License.js
+++ EVENT/WTUA;Licenses!Business!General!License.js
@@ -4,6 +4,9 @@ if (wfTask == 'License Status' && wfStatus == 'About to Expire') {
 	 lic = new licenseObject(capIDString);
 	 lic.setStatus('About to Expire');
 	 }
+if (wfStatus == "Issue License") {
+	include("2060_GBExpirationDateSet");
+}
 } catch (err) {
 	logDebug("A JavaScript Error occured: " + err.message + " In Line " + err.lineNumber);
 }
---
+++ INCLUDES_SCRIPTS/2060_GBExpirationDateSet.js
@@ -0,0 +1,14 @@
+//************************************************ >>  2060_GBExpirationDateSet.js  << ****************************
+// SCRIPTNUMBER: 2060
+// SCRIPTFILENAME: 2060_GBExpirationDateSet.js
+// PURPOSE: ​When General Business record is approved, and Issue License is selected, Set expiration date until gentax import is complete.
+// DATECREATED: 2020-07-25
+// BY: Alex Charlton TruePointSolutions
+
+// create child license
+numberOfMonths = 24;
+tmpNewDate = dateAddMonths(null, numberOfMonths);
+
+	if (wfStatus == 'Issue License'){
+		thisLic.setStatus("Active");  thisLic.setExpiration(dateAdd(tmpNewDate,0));
+	}
