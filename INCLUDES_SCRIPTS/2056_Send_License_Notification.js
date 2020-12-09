fceb4fe52a0981401776af2753735da14f8d2e8d
Acharlton <acharlton@truepointsolutions.com>
2020-12-09 00:56:30 -0700

    Updating scripts for Email notifications

--- INCLUDES_SCRIPTS/2056_Send_License_Notification.js
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -1,13 +1,20 @@
 if (balanceDue > 0) {
 logDebug("Starting _License_Notification script");
-var contacts = 'Primary';
-var emailTemplate = 'LIC FEES INVOICED';
-var reportName = 'Invoice Report';
+//var contacts = 'Primary';
+//var emailTemplate = 'LIC FEES INVOICED';
+//var reportName = 'Invoice Report';
 var altID = capId.getCustomID()
-var reportParam = altID;
-logDebug("REPORT PARAM IS ="+reportParam);
-emailContactsWithReportLinkASync(contacts, emailTemplate, "", "reportName", "reportParam", "N", "");
-logDebug("Finished email without report");
+appType = cap.getCapType().toString();
+var vAsyncScript = "SEND_EMAIL_TAXLIC_INVOICE_ASYNC";
+var envParameters = aa.util.newHashMap();
+envParameters.put("altID", altID);
+envParameters.put("capId", capId);
+envParameters.put("cap", cap);
+logDebug("Starting to kick off ASYNC event for Invoice. Params being passed: " + envParameters);
+aa.runAsyncScript(vAsyncScript, envParameters);
+//logDebug("REPORT PARAM IS ="+reportParam);
+//emailContactsWithReportLinkASync(contacts, emailTemplate, "", "reportName", "reportParam", "N", "");
+//logDebug("Finished email without report");
 //emailContactsWithReportAttachASync(contacts, emailTemplate, "", reportName, reportParam, "N", "")
 //logDebug("Finished Running email with Report");
 logDebug("End of 2056_License_Notification script");
