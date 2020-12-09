667523e7ef8ba680f11d9014961effbbe2e60b81
Acharlton <acharlton@truepointsolutions.com>
2020-12-09 00:27:32 -0700

    Update 2056_Send_License_Notification.js

--- INCLUDES_SCRIPTS/2056_Send_License_Notification.js
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -4,7 +4,10 @@ var contacts = 'Primary';
 var emailTemplate = 'LIC FEES INVOICED';
 var reportName = 'Invoice Report';
 var reportParam = capId;
-//emailContactsWithReportLinkASync(contacts, emailTemplate, "", "", "", "N", "")
+logDebug("REPORT PARAM IS ="+capId);
+emailContactsWithReportLinkASync(contacts, emailTemplate, "", "", "", "N", "");
+logDebug("Finished email without report");
 emailContactsWithReportAttachASync(contacts, emailTemplate, "", reportName, reportParam, "N", "")
+logDebug("Finished Running email with Report");
 logDebug("End of 2056_License_Notification script");
 }
