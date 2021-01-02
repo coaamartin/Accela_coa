f5faf655fd621a42ba533554994aca657fb9abfd
Suhail Wakil <suhailwakil@gmail.com>
2021-01-02 15:52:44 -0800

    Create CTRCA;Licenses!Marijuana!~!Renewal.js

---
+++ EVENT/CTRCA;Licenses!Marijuana!~!Renewal.js
@@ -0,0 +1,46 @@
+//CTRCA;Licenses!Marijuana!~!Renewal.js
+//send email 
+//******************************************************************************
+if(publicUser){
+	var emailTemplate = "LIC_MJ_RENEWALSUBMIT";
+	var capAlias = cap.getCapModel().getAppTypeAlias();
+	var recordApplicant = getContactByType("Applicant", capId);
+	var firstName = recordApplicant.getFirstName();
+	var lastName = recordApplicant.getLastName();
+	//var emailTo = getContactByType("Applicant", capId);
+	var emailTo = getAllContactsEmails()
+	
+	var today = new Date();
+	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+	var fullAddress = "";
+	var capAddresses = aa.address.getAddressByCapId(capId);
+		if (capAddresses.getSuccess()) {
+			capAddresses = capAddresses.getOutput();
+			if (capAddresses != null && capAddresses.length > 0) {
+				capAddresses = capAddresses[0];
+				
+				fullAddress = capAddresses.getHouseNumberStart() + " ";
+				fullAddress = fullAddress + capAddresses.getStreetName() + " ";
+				fullAddress = fullAddress + capAddresses.getCity() + " ";
+				fullAddress = fullAddress + capAddresses.getState() + " ";
+				fullAddress = fullAddress + capAddresses.getZip();
+			}
+	}
+	var eParams = aa.util.newHashtable();
+	if(fullAddress!="" || fullAddress!=undefined){
+		eParams.put("$$fullAddress$$", fullAddress);
+	}else{
+		eParams.put("$$fullAddress$$", "");
+	}
+	
+	
+	eParams.put("$$todayDate$$", thisDate);
+	eParams.put("$$altid$$", capId.getCustomID());
+	eParams.put("$$capAlias$$", capAlias);
+	eParams.put("$$FirstName$$", firstName);
+	eParams.put("$$LastName$$", lastName);
+	
+	sendNotification("noreply@auroragov.org", emailTo, "", emailTemplate, eParams, null);
+
+}
+
