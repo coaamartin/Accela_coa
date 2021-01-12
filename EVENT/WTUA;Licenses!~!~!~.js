ad0735e6079a89e962128ec9b893b5ba2c838c11
Acharlton <acharlton@truepointsolutions.com>
2021-01-12 15:33:49 -0700

    Update WTUA;Licenses!~!~!~.js

--- EVENT/WTUA;Licenses!~!~!~.js
+++ EVENT/WTUA;Licenses!~!~!~.js
@@ -9,6 +9,4 @@ if (appMatch('Licenses/Liquor/*/*') || appMatch('Licenses/Business/*/*') || appM
 if (wfTask == 'Application Received' && wfStatus == 'Complete'){
 	include("2057_LicenseFees");
 }
-if (wfStatus == 'Active') {
-	include("2061_Send_IssuedLicense_Email");
-}
+
