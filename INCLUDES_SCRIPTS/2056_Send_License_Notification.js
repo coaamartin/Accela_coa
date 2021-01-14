c30d2bb6dc87c06d0cb89e1170c934de74dd6299
Acharlton <acharlton@truepointsolutions.com>
2021-01-13 23:01:20 -0700

    Update 2056_Send_License_Notification.js

--- INCLUDES_SCRIPTS/2056_Send_License_Notification.js
+++ INCLUDES_SCRIPTS/2056_Send_License_Notification.js
@@ -37,6 +37,7 @@ if (matches(wfStatus,"Approved", "Denied", "Pending") && wfTask == "Zoning Revie
 		var today = new Date();
 		var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 		var tParams = aa.util.newHashtable();
+		getWorkflowParams4Notification(tParams);
 		tParams.put("$$todayDate$$", thisDate);
 		tParams.put("$$altID$$", capId.getCustomID());
 		tParams.put("$$capAlias$$", capAlias);
@@ -55,6 +56,7 @@ if (matches(wfStatus,"Approved", "Denied", "Pending") && wfTask == "Building Rev
 		var today = new Date();
 		var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 		var tParams = aa.util.newHashtable();
+		getWorkflowParams4Notification(tParams);
 		tParams.put("$$todayDate$$", thisDate);
 		tParams.put("$$altID$$", capId.getCustomID());
 		tParams.put("$$capAlias$$", capAlias);
@@ -62,4 +64,45 @@ if (matches(wfStatus,"Approved", "Denied", "Pending") && wfTask == "Building Rev
 		var emailtemplate = "LIC GB ZONING";
 		sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, null);
 	}
+}
+
+
+function getWorkflowParams4Notification(params) {
+    // pass in a hashtable and it will add the additional parameters to the table
+
+    // Load Parmeters - Workflow
+    var taskItem = null;
+    if (arguments.length > 1 && arguments[1] != null) {
+        taskItem = arguments[1]; // use cap ID specified in args
+    }
+
+    if (taskItem) {             // Load from taskItem
+        addParameter(params, "$$wfTask$$", taskItem.getTaskDescription());
+        addParameter(params, "$$wfProcess$$", taskItem.getProcessCode());
+        addParameter(params, "$$wfNote$$", taskItem.getDispositionNote());
+        addParameter(params, "$$wfStatus$$", taskItem.getDisposition());
+        addParameter(params, "$$wfDate$$", taskItem.getDispositionDateString());
+        addParameter(params, "$$wfDue$$", taskItem.getDueDate());
+        addParameter(params, "$$wfComment$$", taskItem.getDispositionComment());
+        addParameter(params, "$$wfStaffUserID$$", taskItem.getAsgnStaff());
+        addParameter(params, "$$wfStaffUserFullName$$", getUserFullName(taskItem.getAsgnStaff()));
+        addParameter(params, "$$wfTimeBillable$$", taskItem.getBillable());
+        addParameter(params, "$$wfTimeOT$$", taskItem.getOverTime());
+        addParameter(params, "$$wfHours$$", taskItem.getHoursSpent());
+    } else if (wfTask) {        // Load from Globals
+        if (typeof (wfStaffUserID) == "undefined") wfStaffUserID = currentUserID;
+        addParameter(params, "$$wfTask$$", wfTask);
+        addParameter(params, "$$wfProcess$$", wfProcess);
+        addParameter(params, "$$wfNote$$", wfNote);
+        addParameter(params, "$$wfStatus$$", wfStatus);
+        addParameter(params, "$$wfDate$$", wfDateMMDDYYYY);
+        addParameter(params, "$$wfDue$$", wfDue);
+        addParameter(params, "$$wfComment$$", wfComment);
+        addParameter(params, "$$wfStaffUserID$$", wfStaffUserID);
+        addParameter(params, "$$wfStaffUserFullName$$", getUserFullName(wfStaffUserID));
+        addParameter(params, "$$wfTimeBillable$$", wfTimeBillable);
+        addParameter(params, "$$wfTimeOT$$", wfTimeOT);
+        addParameter(params, "$$wfHours$$", wfHours);
+    }
+    return params;
 }
