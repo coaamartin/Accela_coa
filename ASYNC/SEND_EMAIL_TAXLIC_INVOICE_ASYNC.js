56b8145b20410ed792064cd9bf6a25b50b65bf7f
Acharlton <acharlton@truepointsolutions.com>
2021-01-15 16:38:47 -0700

    Update SEND_EMAIL_TAXLIC_INVOICE_ASYNC.js

--- ASYNC/SEND_EMAIL_TAXLIC_INVOICE_ASYNC.js
+++ ASYNC/SEND_EMAIL_TAXLIC_INVOICE_ASYNC.js
@@ -267,3 +267,15 @@ function lookup(stdChoice,stdValue)
 		}
 	return strControl;
 	}
+	
+function addParameter(pamaremeters, key, value)
+{
+	if(key != null)
+	{
+		if(value == null)
+		{
+			value = "";
+		}
+		pamaremeters.put(key, value);
+	}
+}
