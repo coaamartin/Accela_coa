e2764de4ff397033e5503341c6cbe4740b766f44
Suhail Wakil <suhailwakil@gmail.com>
2021-01-20 13:51:51 -0800

    Update WTUB;Licenses!Marijuana!~!Renewal.js

--- EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
+++ EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
@@ -3,3 +3,9 @@ if ("City Application Intake".equals(wfTask) && "Complete".equals(wfStatus))
 {
 	include("488_Check_MJ_Renewal_Docs");
 }
+
+if(wfTask.equals("Renewal Review") && wfStatus.equals("Complete") && balanceDue > 0){
+	cancel = true;
+	showMessage = true;
+	comment("Fee is not Paid yet");
+}
