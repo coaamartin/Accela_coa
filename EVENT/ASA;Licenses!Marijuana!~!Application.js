6647c4394c5b8fe82b57c43c95a6979a87d8e515
Suhail Wakil <suhailwakil@gmail.com>
2021-01-20 20:37:57 -0800

    Update ASA;Licenses!Marijuana!~!Application.js

--- EVENT/ASA;Licenses!Marijuana!~!Application.js
+++ EVENT/ASA;Licenses!Marijuana!~!Application.js
@@ -6,6 +6,18 @@ include("28_AMEDEmailApplicantAtRecordCreation");
 
 //ACA FEE Invoice
 if(publicUser){
+	if (appMatch("Licenses/Marijuana/Retail Cultivation/Application")) {
+		updateFee("LIC_MJRC_02", "LIC_MJ_RC", "FINAL", 1, "N");
+	} else if (appMatch("Licenses/Marijuana/Retail Product Manufacturer/Application")) {
+		updateFee("LIC_MJRPM_02", "LIC_MJ_RPM", "FINAL", 1, "N");
+	} else if (appMatch("Licenses/Marijuana/Retail Transporter/Application")) {
+		updateFee("LIC_MJTR_02", "LIC_MJ_TRANS", "FINAL", 1, "N");
+	} else if (appMatch("Licenses/Marijuana/Testing Facility/Application")) {
+		updateFee("LIC_MJTST_02", "LIC_MJ_TEST", "FINAL", 1, "N");
+	} else if (appMatch("Licenses/Marijuana/Retail Store/Application")) {
+		updateFee("LIC_MJST_01", "LIC_MJ_STORE", "FINAL", 1, "N");
+	}
+
 	_invoiceAllFees();
 }
 //assess State MJ Licensing Fee on application submit
@@ -41,3 +53,8 @@ function _invoiceAllFees() {
 		}
 	}
 }
+
+
+
+
+
