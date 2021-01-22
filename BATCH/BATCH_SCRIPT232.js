11528ab687934aeb725780e77e8a41a65cc22e34
Suhail Wakil <suhailwakil@gmail.com>
2021-01-22 13:29:19 -0800

    Update BATCH_SCRIPT232.js

--- BATCH/BATCH_SCRIPT232.js
+++ BATCH/BATCH_SCRIPT232.js
@@ -197,8 +197,8 @@ function scheduleNextInspections(cycleInspections) {
 			//for building inspections, update inspection status and wait for other building inspections to be completed
 			if (cycleInspections[i].getInspectionType().indexOf("MJ Building Inspections") != -1) {
 				scheduleInspectDate(inspType, nextInspDate, inspector);
-				//cycleInspections[i].setInspectionStatus("Passed - Notification Pending");
-				//aa.inspection.editInspection(cycleInspections[i]);
+				cycleInspections[i].setInspectionStatus("Passed - Notification Pending");
+				aa.inspection.editInspection(cycleInspections[i]);
 				inspCounter++;
 			} else {
 				scheduleInspectDate(inspType, nextInspDate, inspector);
@@ -281,7 +281,7 @@ var bldgInspSchedDate;
 		
 		//update inspection status to reflect that notification was sent
 		for (i in cycleInspections) {
-			if (cycleInspections[i].getInspectionStatus() == "Passed" || cycleInspections[i].getInspectionStatus() == "Passed - Minor Violations") {
+			if (cycleInspections[i].getInspectionStatus() == "Passed" || cycleInspections[i].getInspectionStatus() == "Passed - Minor Violations" || cycleInspections[i].getInspectionStatus() == "Passed - Notification Pending") {
 				if (cycleInspections[i].getInspectionType().indexOf("MJ Building Inspections") != -1) {
 					cycleInspections[i].setInspectionStatus("Passed - Notification Sent");
 					aa.inspection.editInspection(cycleInspections[i]);
