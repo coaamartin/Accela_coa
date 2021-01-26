fbcc053a7d27c4b209ad0195784748c196646020
Suhail Wakil <suhailwakil@gmail.com>
2021-01-26 14:45:26 -0800

    Update WTUB;Licenses!Marijuana!~!Renewal.js

--- EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
+++ EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
@@ -10,23 +10,29 @@ var vPayments;
 var vPaymentSeqNbr = 0;
 var vPaymentAmt = 0;
 
-// Get all payments on the record
-vPayments = aa.finance.getPaymentByCapID(capId, null);
-if (vPayments.getSuccess() == true) {
-    vPayments = vPayments.getOutput();
-    var y = 0;
-    // Loop through payments to get the latest by highest SEQ number
-    for (y in vPayments) {
-        vPayment = vPayments[y];
-        if (vPayment.getPaymentSeqNbr() > vPaymentSeqNbr) {
-            vPaymentSeqNbr = vPayment.getPaymentSeqNbr();
-            vPaymentAmt = vPayment.getPaymentAmount();
-        }
-    }
-}
+var totalPaid = totalPaidAmount();
 
-if(wfTask.equals("Renewal Review") && wfStatus.equals("Complete") && vPaymentAmt < 2500){
+if(wfTask.equals("Renewal Review") && wfStatus.equals("Complete") && totalPaid < 2500){
 	cancel = true;
 	showMessage = true;
 	comment("Fee is not Paid yet");
+}
+
+function totalPaidAmount(){
+    var vPayment;
+    var vPayments;
+    var vPaymentAmt = 0;
+
+// Get all payments on the record
+    vPayments = aa.finance.getPaymentByCapID(capId, null);
+    if (vPayments.getSuccess() == true) {
+        vPayments = vPayments.getOutput();
+        var y = 0;
+        // Loop through payments to get the latest by highest SEQ number
+        for (y in vPayments) {
+            vPayment = vPayments[y];
+            vPaymentAmt += vPayment.getPaymentAmount();
+        }
+    }
+    return vPaymentAmt;
 }
