cbc3e0cb7ecd940b092368df3bfbfd5b9064a194
Suhail Wakil <suhailwakil@gmail.com>
2021-01-26 14:11:07 -0800

    Update WTUB;Licenses!Marijuana!~!Renewal.js

--- EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
+++ EVENT/WTUB;Licenses!Marijuana!~!Renewal.js
@@ -4,7 +4,28 @@ if ("City Application Intake".equals(wfTask) && "Complete".equals(wfStatus))
 	include("488_Check_MJ_Renewal_Docs");
 }
 
-if(wfTask.equals("Renewal Review") && wfStatus.equals("Complete") && balanceDue > 0){
+
+var vPayment;
+var vPayments;
+var vPaymentSeqNbr = 0;
+var vPaymentAmt = 0;
+
+// Get all payments on the record
+vPayments = aa.finance.getPaymentByCapID(capId, null);
+if (vPayments.getSuccess() == true) {
+    vPayments = vPayments.getOutput();
+    var y = 0;
+    // Loop through payments to get the latest by highest SEQ number
+    for (y in vPayments) {
+        vPayment = vPayments[y];
+        if (vPayment.getPaymentSeqNbr() > vPaymentSeqNbr) {
+            vPaymentSeqNbr = vPayment.getPaymentSeqNbr();
+            vPaymentAmt = vPayment.getPaymentAmount();
+        }
+    }
+}
+
+if(wfTask.equals("Renewal Review") && wfStatus.equals("Complete") && vPaymentAmt >= 2500){
 	cancel = true;
 	showMessage = true;
 	comment("Fee is not Paid yet");
