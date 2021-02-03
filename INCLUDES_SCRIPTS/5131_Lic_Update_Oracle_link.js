927f67ce58e0ded40252d79583f3cfc559c7776b
Province <rprovinc@auroragov.org>
2021-02-03 10:48:02 -0700

    added script 5131 and updated script asa licenses application

--- INCLUDES_SCRIPTS/5131_Lic_Update_Oracle_link.js
+++ INCLUDES_SCRIPTS/5131_Lic_Update_Oracle_link.js
@@ -13,15 +13,15 @@
 
 //Below is code to create and then populate the oracle link when appliaction is submitted.
 
-logDebug("Start of script 5131_Lic_Update_Oracle_link.js");	 	 
-	// get general business license number
-	var envParameters = aa.util.newHashMap();
-    var genBusinessNumber = AInfo["General Business License Number"];
-    var oracleURL = "http://doccity/cs/idcplg?IdcService=GET_SEARCH_RESULTS_FORCELOGIN&QueryText=" + genBusinessNumber;
-    logDebug("General Buiness Number: " + genBusinessNumber);
-    logDebug("Oracle Link: " + oracleURL);
-    //setCustomField(vFieldName,vFieldValue);
-    setCustomField("Oracle Link",oracleURL);
-    var oracleLink = AInfo["Oracle Link"];
-    logDebug("Updated Oracle Link: " + oracleLink);
-	logDebug("End of script 5131_Lic_Update_Oracle_link.js");
+logDebug("Start of script 5131_Lic_Update_Oracle_link.js");
+// get general business license number
+var envParameters = aa.util.newHashMap();
+var genBusinessNumber = AInfo["General Business License Number"];
+var oracleURL = "http://doccity/cs/idcplg?IdcService=GET_SEARCH_RESULTS_FORCELOGIN&QueryText=" + genBusinessNumber;
+logDebug("General Buiness Number: " + genBusinessNumber);
+logDebug("Oracle Link: " + oracleURL);
+//setCustomField(vFieldName,vFieldValue);
+setCustomField("Oracle Link", oracleURL);
+var oracleLink = AInfo["Oracle Link"];
+logDebug("Updated Oracle Link: " + oracleLink);
+logDebug("End of script 5131_Lic_Update_Oracle_link.js");
