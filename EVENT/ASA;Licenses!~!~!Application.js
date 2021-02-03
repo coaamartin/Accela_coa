133a202032400e55ff553aa8bffe1b908d16e199
Province <rprovinc@auroragov.org>
2021-02-03 10:30:06 -0700

    added script 5131 and updated script asa licenses application

--- EVENT/ASA;Licenses!~!~!Application.js
+++ EVENT/ASA;Licenses!~!~!Application.js
@@ -1,8 +1,10 @@
 if (appMatch('Licenses/Liquor/*/*') || appMatch('Licenses/Supplemental/*/*')){
 	if (appMatch('Licenses/Liquor/Liquor License/*') || appMatch('Licenses/Liquor/Tasting License/*') || appMatch('Licenses/Supplemental/*/*')){
 		updateShortNotes(AInfo['Type of License']);
+		include(5131_Lic_Update_Oracle_link.js)
 	}
 	if (appMatch('Licenses/Liquor/Liquor Permit/*')){
 		updateShortNotes(AInfo['Type of Permit']);
 	}
 }
+
