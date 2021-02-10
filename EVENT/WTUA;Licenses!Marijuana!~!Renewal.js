47322fd33016b49855d3b2e28e315f6919fc84ab
Suhail Wakil <suhailwakil@gmail.com>
2021-02-09 17:21:39 -0800

    Update WTUA;Licenses!Marijuana!~!Renewal.js

--- EVENT/WTUA;Licenses!Marijuana!~!Renewal.js
+++ EVENT/WTUA;Licenses!Marijuana!~!Renewal.js
@@ -37,7 +37,7 @@ if (totalPaid >= 2500 && wfTask == "Renewal Review" && wfStatus == "Complete") {
 			renewalCapProject.setStatus("Complete");
 			aa.cap.updateProject(renewalCapProject);
 		}
-
+        emailOnRenewApproval();
 		/*
 		//This logic is in BATCH JOB
         var vAsyncScript = "SEND_MJ_LICENSE_ASYNC";
@@ -77,4 +77,60 @@ include("432_closeLicenseMJ");
 if (wfTask=="Renewal Review" && wfStatus=="Additional Info Required")
 {
     include("210_SendMJEmail");
+}
+
+function emailOnRenewApproval(){
+    var emailTemplate= "LIC_MJ_RENEWALAPPROVAL";
+    var applicant = getContactByType("Applicant", capId);
+    //var acaUrl = lookup("ACA_CONFIGS","OFFICIAL_WEBSITE_URL");
+    var acaURLDefault = lookup("ACA_CONFIGS", "ACA_SITE");
+    acaURLDefault = acaURLDefault.substr(0, acaURLDefault.toUpperCase().indexOf("/ADMIN"));
+    var recordDeepUrl = getACARecordURL(acaURLDefault);
+
+    var asiValues = new Array();
+    loadAppSpecific(asiValues);
+
+    if (!applicant || !applicant.getEmail())
+    {
+        logDebug("**WARN  - no applicant found or no email capId =" + capId);
+    }
+    else
+    {
+        var files = new Array();
+        // use the correct parameters related to the email template provided + wfComment
+        var adResult = aa.address.getAddressByCapId(capId).getOutput();
+        for(x in adResult)
+        {
+            var adType = adResult[x].getAddressType();
+            var stNum = adResult[x].getHouseNumberStart();
+            var preDir =adResult[x].getStreetDirection();
+            var stName = adResult[x].getStreetName();
+            var stType = adResult[x].getStreetSuffix();
+            var city = adResult[x].getCity();
+            var state = adResult[x].getState();
+            var zip = adResult[x].getZip();
+        }
+
+        var primaryAddress = stNum + " " + preDir + " " + stName + " " + stType + " " + "," + city + " " + state + " " + zip;
+        var appName = cap.getSpecialText();
+
+        var eParams = aa.util.newHashtable();
+
+        addParameter(eParams, "$$altID$$", cap.getCapModel().getAltID());
+        addParameter(eParams, "$$recordAlias$$", cap.getCapType().getAlias());
+        addParameter(eParams, "$$recordStatus$$", cap.getCapStatus());
+        addParameter(eParams, "$$wfTask$$", wfTask.toUpperCase());
+        addParameter(eParams, "$$wfStatus$$", wfStatus);
+        addParameter(eParams, "$$wfDate$$", wfDate);
+        addParameter(eParams, "$$wfComment$$", wfComment);
+        addParameter(eParams, "$$acaRecordUrl$$", recordDeepUrl);
+        addParameter(eParams, "$$FullAddress$$", primaryAddress);
+        addParameter(eParams, "$$ApplicationName$$", appName);
+        addParameter(eParams, "$$TradeName$$", appName);
+        //addParameter(eParams, "$$TradeName$$", asiValues["Trade Name"]);
+        addParameter(eParams, "$$StateLicenseNumber$$", asiValues["State License Number"]);
+
+        //send email to applicant, no report included
+        emailContactsWithReportLinkASync("Applicant,Responsible Party", emailTemplate, eParams, "", "", "N", "");
+    }
 }
