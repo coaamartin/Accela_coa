80bacc938af73482a3c424cb5676e5cd0598f433
Province <rprovinc@auroragov.org>
2021-02-16 07:24:03 -0700

    updated ctrca event for arborist

--- EVENT/CTRCA;Licenses!Contractor!Arborist!Renewal.js
+++ EVENT/CTRCA;Licenses!Contractor!Arborist!Renewal.js
@@ -1,6 +1,6 @@
 // script 148
 
-// var thisDate = new Date();
-// if ((thisDate.getMonth() == 1 && thisDate.getDate() > 1) || thisDate.getMonth() > 1) {
-// 	updateFee("LIC_CONT_A02", "LIC_CONTRACTOR_ARBORIST", "FINAL", 1, "Y");
-// }
+var thisDate = new Date();
+if ((thisDate.getMonth() == 1 && thisDate.getDate() > 1) || thisDate.getMonth() > 1) {
+	updateFee("LIC_CONT_A02", "LIC_CONTRACTOR_ARBORIST", "FINAL", 1, "Y");
+}
