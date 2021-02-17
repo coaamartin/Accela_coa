c9c9d41fceab638366a6ee0a1786e0f7783ba866
Province <rprovinc@auroragov.org>
2021-02-17 14:24:31 -0700

    updated ctrca for mj pre

--- EVENT/CTRCA;Licenses!Marijuana!Pre Application Meeting!NA.js
+++ EVENT/CTRCA;Licenses!Marijuana!Pre Application Meeting!NA.js
@@ -66,24 +66,43 @@ if(publicUser){
     assignTask("Pre Licensing Meeting",userObj.getUserID());
 }
 
-function getAllContactsEmailsLocal(){
-	var vConObjArry = getContactObjsByCap(capId);
-	var emailsString = "";
-	for(eachCont in vConObjArry){
-		var vConObj = vConObjArry[eachCont];
-		var vConRefSeqNbr = vConObj.refSeqNumber;
-		//Get contact email
-		if (vConObj) {
-			conEmail = vConObj.people.getEmail();
-			if (conEmail && conEmail != null && conEmail != "") {
-				conEmail = conEmail.toUpperCase();
-				emailsString += conEmail + ";";
-			}
-		}
-	}
-	
-	if(emailsString.length > 0) return emailsString.slice(0, -1)
-	
-    return false;
+
+function getEmailStringLocal()
+{
+   var emailString = "";
+   var contactArray = getPeople(capId);
+
+   for (var c in contactArray)
+   {
+      if (contactArray[c].getPeople().getEmail())
+      {
+         emailString += contactArray[c].getPeople().getEmail() + ";";
+
+      }
+   }
+   logDebug(emailString);
+   return emailString;
 }
+
+
+// function getAllContactsEmailsLocal(){
+// 	var vConObjArry = getContactObjsByCap(capId);
+// 	var emailsString = "";
+// 	for(eachCont in vConObjArry){
+// 		var vConObj = vConObjArry[eachCont];
+// 		var vConRefSeqNbr = vConObj.refSeqNumber;
+// 		//Get contact email
+// 		if (vConObj) {
+// 			conEmail = vConObj.people.getEmail();
+// 			if (conEmail && conEmail != null && conEmail != "") {
+// 				conEmail = conEmail.toUpperCase();
+// 				emailsString += conEmail + ";";
+// 			}
+// 		}
+// 	}
+	
+// 	if(emailsString.length > 0) return emailsString.slice(0, -1)
+	
+//     return false;
+// }
 
