df37a7bbad77edb21f8d1a111c7488c7a377c1fb
Province <rprovinc@auroragov.org>
2021-02-23 09:38:33 -0700

    updated 5043 script for bld

--- EVENT/IFA;Building!Permit!~!~.js
+++ EVENT/IFA;Building!Permit!~!~.js
@@ -2,8 +2,9 @@
 Invoice Fee After
 */
 
-
 //jmain 06-01-2018  
-if(!appMatch("Building/Permit/DonationBin/*") && !appMatch("Building/Permit/TempSigns/*") && !appMatch("Building/Permit/TempUse/*")) {
+if(appMatch("Building/Permit/OTC/*")) {
+logDubug("IFA event for Building Permits OTC")
 include("5043_BuildingEmailnvoicedFees");
+
 }
