85fca1abac890682f85f08eed06739d74247e951
Province <rprovinc@auroragov.org>
2021-02-23 10:30:13 -0700

    5043 script

--- EVENT/IFA;Building!Permit!~!~.js
+++ EVENT/IFA;Building!Permit!~!~.js
@@ -2,9 +2,8 @@
 Invoice Fee After
 */
 
-//jmain 06-01-2018  
-if(appMatch("Building/Permit/OTC/*")) {
-logDubug("IFA event for Building Permits OTC")
-include("5043_BuildingEmailnvoicedFees");
 
+//jmain 06-01-2018  
+if(!appMatch("Building/Permit/DonationBin/*") && !appMatch("Building/Permit/TempSigns/*") && !appMatch("Building/Permit/TempUse/*")) {
+include("5043_BuildingEmailnvoicedFees");
 }
