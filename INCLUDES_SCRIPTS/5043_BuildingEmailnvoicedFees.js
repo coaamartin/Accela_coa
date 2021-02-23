4b6d5051ad214b0a59305c50153d1cbe96c5dbcb
Province <rprovinc@auroragov.org>
2021-02-23 09:32:21 -0700

    updated 5043 script for bld

--- INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
+++ INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
@@ -5,14 +5,32 @@ that fees are owed and they can click the link to go online to login to their ac
 Written by JMAIN
 */
 logDebug("Kicking off 5043_BuildingEmailInvoicedFees.js");
-if (balanceDue > 0)
-{
+if (balanceDue > 0) {
   //email the applicant
-  var contacts = "Applicant,Contractor(s)";
-  var lptypes = "Contractor";
-  var emailtemplate = "BLD_INVOICEDFEES";
+  // var contacts = "Applicant,Contractor(s)";
+  // var lptypes = "Contractor";
+  // var emailtemplate = "BLD_INVOICEDFEES";
 
-  emailContactsWithReportLinkASync(contacts, emailtemplate, "", "", "", "N", "");
+  // emailContactsWithReportLinkASync(contacts, emailtemplate, "", "", "", "N", "");
   //coa_emailLicenseProfessionals(lptypes, emailtemplate, "", "", "", capId);
+  logDebug("Starting to send notifications");
+  var emailTo = getContactByType("Applicant", capId);
+  var vEmailTemplate = "BLD_INVOICEDFEES";
+  var capAlias = cap.getCapModel().getAppTypeAlias();
+  var firstName = recordApplicant.getFirstName();
+  var lastName = recordApplicant.getLastName();
+  var today = new Date();
+  var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+  var altId = capId.getCustomID();
+  var tParams = aa.util.newHashtable();
+  //tParams.put("$$todayDate$$", thisDate);
+  //tParams.put("$$altid$$", altId);
+  //tParams.put("$$capAlias$$", capAlias);
+  //tParams.put("$$FirstName$$", firstName);
+  //tParams.put("$$LastName$$", lastName);
+  //tParams.put("$$appDate$$", appDate);
+  logDebug("EmailTo: " + emailTo);
+  //logDebug("Table Parameters: " + tParams);
+  sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
 }
 logDebug("End off 5043_BuildingEmailInvoicedFees.js");
