50437347d4249d03db469804ce37eb7ddc98be51
Warthan <jwarthan@auroragov.org>
2021-03-05 13:44:55 -0700

    batch report

--- BATCH/BATCH_REPORT_WITH_NO_PARAM.js
+++ BATCH/BATCH_REPORT_WITH_NO_PARAM.js
@@ -159,7 +159,7 @@ function mainProcess() {
 	var rParams = aa.util.newHashtable();
 	//rParams.put("FromDate", "5/1/2020");
 	//rParams.put("ToDate", "5/31/2020");
-	logdebug("Gen report", reportName)
+	logDebug("Gen report", reportName)
 	var report = generateReportFile(reportName, rParams, aa.getServiceProviderCode());
 
 	var tParams = aa.util.newHashtable();
