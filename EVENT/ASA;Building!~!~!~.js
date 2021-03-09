4aa5840082d06a66222852138f1e0348e211432c
Province <rprovinc@auroragov.org>
2021-03-09 09:16:08 -0700

    updated ASA building event

--- EVENT/ASA;Building!~!~!~.js
+++ EVENT/ASA;Building!~!~!~.js
@@ -7,7 +7,7 @@ logDebug("Adding Fee");
 //if(!appMatch("Building/Permit/New Building/NA") && !appMatch("Building/Permit/OTC/*"))
 if(!appMatch("Building/Permit/New Building/NA") && !appMatch("Building/Permit/OTC/*") && !appMatch("Building/Permit/TempSigns/*")&& !appMatch("Building/Permit/TempUse/*")
 && !appMatch("Building/Permit/DonationBin/*") && !appMatch("Building/Permit/Master/*")&& !appMatch("Building/Permit/New Building/Amendment")
-&& !appMatch("Building/Permit/Plans/NA"))
+&& !appMatch("Building/Permit/Plans/NA")&& !appMatch("Building/Enforcement/Notice of Violation/NA"))
     addFee("BLD_PNP_06","BLD_PNP","FINAL",1,"Y");
 
 // 5/29/19 Keith - Why are we setting Debug to false here?? I should only be done in one place in the EMSE globals!
