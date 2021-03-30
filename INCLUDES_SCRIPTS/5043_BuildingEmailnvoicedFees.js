bdbaaeb9d3c644d4c4863d5cfb39b4210becb0e5
Province <rprovinc@auroragov.org>
2021-03-30 14:26:40 -0600

    updated 5043 script

--- INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
+++ INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
@@ -4,15 +4,42 @@ that fees are owed and they can click the link to go online to login to their ac
 
 Written by JMAIN
 */
+// if (balanceDue > 0) {
+//   //email the applicant
+//   // logDebug("Email to: " + emailTo);
+//   //   var emailTo1 = emailTo;
+//   var contacts = "Applicant";
+//   //var lptypes = "Contractor";
+//   var emailtemplate = "BLD_INVOICEDFEES";
+//   emailContactsWithReportLinkASync("Applicant", emailtemplate, "", "", "", "N", "");
+//   //coa_emailLicenseProfessionals(lptypes, emailtemplate, "", "", "", capId);
+//   logDebug("End of email to send");
+// }
+// logDebug("End of 5043_BuildingEmailInvoicedFees.js");
+
+
 if (balanceDue > 0) {
-  //email the applicant
-  // logDebug("Email to: " + emailTo);
-  //   var emailTo1 = emailTo;
-  var contacts = "Applicant";
-  //var lptypes = "Contractor";
-  var emailtemplate = "BLD_INVOICEDFEES";
-  emailContactsWithReportLinkASync("Applicant", emailtemplate, "", "", "", "N", "");
-  //coa_emailLicenseProfessionals(lptypes, emailtemplate, "", "", "", capId);
-  logDebug("End of email to send");
+  //Donation Bins code
+  logDebug("Starting to send emails to Applicant");
+  //Send Notification
+  logDebug("Starting to send notifications for fee processing");
+  var emailTemplate = "BLD_INVOICEDFEES";
+  var capAlias = cap.getCapModel().getAppTypeAlias();
+  var recordApplicant = getContactByType("Applicant", capId);
+  var firstName = recordApplicant.getFirstName();
+  var lastName = recordApplicant.getLastName();
+  var emailTo = recordApplicant.getEmail();
+  var altId = capId.getCustomID();
+  var today = new Date();
+  var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+  var tParams = aa.util.newHashtable();
+  tParams.put("$$todayDate$$", thisDate);
+  tParams.put("$$altID$$", altId);
+  tParams.put("$$capAlias$$", capAlias);
+  tParams.put("$$FirstName$$", firstName);
+  tParams.put("$$LastName$$", lastName);
+  logDebug("EmailTo: " + emailTo);
+  logDebug("Table Parameters: " + tParams);
+  sendNotification("noreply@auroragov.org", emailTo, "", emailTemplate, tParams, null);
+  logDebug("End of Script 5043_BuildingEmailInfoicedFees.js");
 }
-logDebug("End of 5043_BuildingEmailInvoicedFees.js");
