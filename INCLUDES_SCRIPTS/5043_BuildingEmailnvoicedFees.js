9fa98f5876fde753e50d531a111ac0b07d668f30
Province <rprovinc@auroragov.org>
2021-03-30 10:55:37 -0600

    updated ifa building permit

--- INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
+++ INCLUDES_SCRIPTS/5043_BuildingEmailnvoicedFees.js
@@ -8,25 +8,10 @@ if (balanceDue > 0) {
   //email the applicant
   // logDebug("Email to: " + emailTo);
   //   var emailTo1 = emailTo;
-  var capId = aa.env.getValue("capId");
-  var cap = aa.env.getValue("cap");
-  var recordID = aa.env.getValue("altID");
-  var emailTo = getEmailString();
-  var recordApplicant = getContactByType("Applicant", capId);
-  var firstName = recordApplicant.getFirstName();
-  var lastName = recordApplicant.getLastName();
-  //var contacts = "Applicant";
+  var contacts = "Applicant";
   //var lptypes = "Contractor";
   var emailtemplate = "BLD_INVOICEDFEES";
-  var tParams = aa.util.newHashtable();
-  tParams.put("$$todayDate$$", thisDate);
-  tParams.put("$$altid$$", recordID);
-  tParams.put("$$Record Type$$", capAlias);
-  tParams.put("$$capAlias$$", capAlias);
-  tParams.put("$$FirstName$$", firstName);
-  tParams.put("$$LastName$$", lastName);
-  //emailContactsWithReportLinkASync(contacts, emailtemplate, "", "", "", "N", "");
-  sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, "");
+  emailContactsWithReportLinkASync(contacts, emailtemplate, "", "", "", "N", "");
   //coa_emailLicenseProfessionals(lptypes, emailtemplate, "", "", "", capId);
   logDebug("End of email to send");
 }
