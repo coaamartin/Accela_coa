97ee92d2ef60dd15b0d3bbdd0986cb67d1671c4f
Province <rprovinc@auroragov.org>
2021-03-31 08:37:06 -0600

    updated ifa for building

--- EVENT/IFA;Building!Permit!~!~.js
+++ EVENT/IFA;Building!Permit!~!~.js
@@ -4,6 +4,6 @@ Invoice Fee After
 
 
 //jmain 06-01-2018  
-if(!appMatch("Building/Permit/DonationBin/*") && !appMatch("Building/Permit/TempSigns/*") && !appMatch("Building/Permit/TempUse/*")) {
+if(!appMatch("Building/Permit/DonationBin/*") && !appMatch("Building/Permit/TempSigns/*") && !appMatch("Building/Permit/TempUse/*") && !appMatch("Building/Permit/OTC/*")) {
 include("5043_BuildingEmailnvoicedFees");
 }
