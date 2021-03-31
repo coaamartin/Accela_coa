2951a3da41bfd7bccd59aa2e3de1c959939cde30
Province <rprovinc@auroragov.org>
2021-03-31 07:14:16 -0600

    updated ppa and PRA for otc

--- EVENT/PPA;Building!Permit!OTC!~.js
+++ EVENT/PPA;Building!Permit!OTC!~.js
@@ -2,19 +2,13 @@ if (!appMatch("Building/Permit/TempSigns/*") && !appMatch("Building/Permit/TempU
 !appMatch("Building/Permit/DonationBin/*")){
 logDebug("Starting PPA;Building!Permit!OTC!~.js ");
 logDebug("Current balance: " + balanceDue);
-
 //Check balance and update task
 if (balanceDue == 0) {
-	// updateAppStatus("Approved", "Status updated via script PPA;Building!Permit.js");
-	// closeTask("Application Close", "Approved", "", "");
-	// closeAllTasks(capId, "Approved");
-	//include("5124_CityClerk_Approval");
-	
-	logDebug("---------------------> PPA;Building!Permit!OTC!~.js has ended");
-	//aa.sendMail("rprovinc@auroragov.org", "rprovinc@auroragov.org", "", "Log", "Debug: <br>" + debug + "<br>Message: <br>" + message);
 	//Start to generate the Certificate. This will attach to the record when ran.
 	logDebug("Starting to kick off event to attach cert to record");
 		var altID = capId.getCustomID();
+		var invoiceNbrObj = getLastInvoice({});
+		var invNbr = invoiceNbrObj.getInvNbr()
 		appType = cap.getCapType().toString();
 		var vAsyncScript = "SEND_EMAIL_BLD_OTC";
 		var envParameters = aa.util.newHashMap();
@@ -22,7 +16,8 @@ if (balanceDue == 0) {
 		envParameters.put("capId", capId);
 		envParameters.put("cap", cap);
         envParameters.put("appType", appType);
-		logDebug("Starting to kick off ASYNC event for DB. Params being passed: " + envParameters);
+		envParameters.put("INVOICEID", String(invNbr));
+		logDebug("Starting to kick off ASYNC event for OTC. Params being passed: " + envParameters);
 		aa.runAsyncScript(vAsyncScript, envParameters);
 	
 }
