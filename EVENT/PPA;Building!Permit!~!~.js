c8aa762bae4cbd1f4ee5fb8bc22b25ca2f75c1f0
Province <rprovinc@auroragov.org>
2021-04-06 07:57:20 -0600

    updated PPA for master

--- EVENT/PPA;Building!Permit!~!~.js
+++ EVENT/PPA;Building!Permit!~!~.js
@@ -65,34 +65,3 @@ if (balanceDue == 0) {
 		logDebug("---------------------> PPA;Building!Permit.js ended.");
 	}
 }
-
-if (appMatch("Building/Permit/Master/*")) {
-	if (!publicUser) {
-		logDebug("Starting PPA;Building!Permit!OTC!~.js ");
-		logDebug("Current balance: " + balanceDue);
-		//Check balance and update task
-		if (balanceDue == 0) {
-			//Start to generate the Certificate. This will attach to the record when ran.
-			logDebug("Starting to kick off event to attach cert to record");
-			var altID = capId.getCustomID();
-			var invoiceNbrObj = getLastInvoice({});
-			var invNbr = invoiceNbrObj.getInvNbr()
-			appType = cap.getCapType().toString();
-			var vAsyncScript = "SEND_EMAIL_BLD_OTC";
-			var envParameters = aa.util.newHashMap();
-			envParameters.put("altID", altID);
-			envParameters.put("capId", capId);
-			envParameters.put("cap", cap);
-			envParameters.put("appType", appType);
-			envParameters.put("INVOICEID", String(invNbr));
-			logDebug("Starting to kick off ASYNC event for OTC. Params being passed: " + envParameters);
-			aa.runAsyncScript(vAsyncScript, envParameters);
-
-		}
-
-		logDebug("End of PPA;Building!Permit!OTC!~.js");
-		if (publicUser) {
-			logDebug("PRA event will not run as payment is being processed in ACA");
-		}
-	}
-}
