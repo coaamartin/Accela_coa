76a6bf40db103b3ca6b1481cd5f0be9b761453e7
Province <rprovinc@auroragov.org>
2021-04-08 10:42:27 -0600

    updated WTUA for NOV and updated script332

--- EVENT/WTUA;Building!Enforcement!Notice of Violation!NA.js
+++ EVENT/WTUA;Building!Enforcement!Notice of Violation!NA.js
@@ -3,4 +3,6 @@ var $iTrc = ifTracer;
 if($iTrc(wfTask == "Pre Hearing Inspection" && wfStatus == "Hearing Scheduled", 'wfTask == "Pre Hearing Inspection" && wfStatus == "Hearing Scheduled"')){
 	Script332_scheduleInspectionTSI();
 }
-
+if($iTrc(wfTask == "Complaint intake" && wfStatus == "Assign", 'wfTask == "Complaint intake" && wfStatus == "Assign"')){
+	Script332_scheduleInspectionTSI();
+}
