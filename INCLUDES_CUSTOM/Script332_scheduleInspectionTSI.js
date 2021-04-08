afcbbf5b5802926b8fd9d16857cbc36dd54d9a96
Province <rprovinc@auroragov.org>
2021-04-08 10:57:31 -0600

    updated WTUA for NOV and updated script332

--- INCLUDES_CUSTOM/Script332_scheduleInspectionTSI.js
+++ INCLUDES_CUSTOM/Script332_scheduleInspectionTSI.js
@@ -13,15 +13,17 @@ function Script332_scheduleInspectionTSI() {
             scheduleInspection("Pre Court Investigation", noOfDays, inspectorID, null, workDescGet(capId));
     }
     if (wfTask == "Complaint Intake" && wfStatus == "Assigned") {
-        var today = aa.util.parseDate(dateAdd(null, 0));
+        //var today = aa.util.parseDate(dateAdd(null, 0));
+        var today = new Date();
+        var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
         //var tsiArray = new Array();
 
         //loadTaskSpecific(tsiArray);
         //var pPreHearingDate = tsiArray["Pre hearing inspection date"];
         //var preHearingDateMinus1 = getPrevWorkingDays(new Date(pPreHearingDate), 1);
         var inspectorID = getInspectorID();
-        var noOfDays = today;
+        var noOfDays = thisDate;
 
-        scheduleInspection("Investigation", noOfDays, inspectorID, null, workDescGet(capId));
+        scheduleInspection("Investigation", 0, inspectorID, null, workDescGet(capId));
     }
 }
