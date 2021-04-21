c1972b034e1840a6f0169c66bea86410811978fe
Province <rprovinc@auroragov.org>
2021-04-21 08:24:53 -0600

    updated wtua building permit and 5132

--- INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
+++ INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
@@ -16,29 +16,25 @@ appTypeString = appTypeResult.toString();
 appTypeArray = appTypeString.split("/");
 logDebug("appType: " + appTypeString);
 
-
-
-if(wfTask == "Planning Review" && wfStatus == "Resubmittal Requested") {
-    logDebug("Starting to send notifications");
-    var vEmailTemplate = "BLD_PLANNING_RESUBMITTAL";
-    var capAlias = cap.getCapModel().getAppTypeAlias();
-    var recordApplicant = getContactByType("Applicant", capId);
-    var firstName = recordApplicant.getFirstName();
-    var lastName = recordApplicant.getLastName();
-    var emailTo = recordApplicant.getEmail();
-    var wfcomment = wfComment; 
-    var today = new Date();
-	var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var tParams = aa.util.newHashtable();
-    tParams.put("$$todayDate$$", thisDate);
-    tParams.put("$$altid$$", capId.getCustomID());
-    tParams.put("$$Record Type$$", "Master Plan");
-    tParams.put("$$capAlias$$", capAlias);
-    tParams.put("$$FirstName$$", firstName);
-    tParams.put("$$LastName$$", lastName);
-    tParams.put("$$wfComment$$", wfComment);
-    logDebug("EmailTo: " + emailTo);
-    logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
-    logDebug("End of Script 5132_BLD_ResubmitEmail.js");
-}
+logDebug("Starting to send notifications");
+var vEmailTemplate = "BLD_PLANNING_RESUBMITTAL";
+var capAlias = cap.getCapModel().getAppTypeAlias();
+var recordApplicant = getContactByType("Applicant", capId);
+var firstName = recordApplicant.getFirstName();
+var lastName = recordApplicant.getLastName();
+var emailTo = recordApplicant.getEmail();
+var wfcomment = wfComment;
+var today = new Date();
+var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+var tParams = aa.util.newHashtable();
+tParams.put("$$todayDate$$", thisDate);
+tParams.put("$$altid$$", capId.getCustomID());
+tParams.put("$$Record Type$$", "Master Plan");
+tParams.put("$$capAlias$$", capAlias);
+tParams.put("$$FirstName$$", firstName);
+tParams.put("$$LastName$$", lastName);
+tParams.put("$$wfComment$$", wfComment);
+logDebug("EmailTo: " + emailTo);
+logDebug("Table Parameters: " + tParams);
+sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
+logDebug("End of Script 5132_BLD_ResubmitEmail.js");
