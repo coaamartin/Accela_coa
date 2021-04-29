879e4bafc6c1f90b718027b477bf766bef112714
Province <rprovinc@auroragov.org>
2021-04-29 15:11:07 -0600

    updated ASA oda pre app

--- EVENT/ASA;ODA!Pre App!NA!NA.js
+++ EVENT/ASA;ODA!Pre App!NA!NA.js
@@ -1,1 +1,25 @@
-if(!publicUser) script220_ApplicationReceivedEmailForPreApp();
+// if(!publicUser) script220_ApplicationReceivedEmailForPreApp();
+
+logDebug("Starting ASA;ODA!Pre App!NA!NA");
+var vEmailTemplate = "ODA PRE APP SUBMITAL EMAIL # 220";
+   var capAlias = cap.getCapModel().getAppTypeAlias();
+   var recordApplicant = getContactByType("Applicant", capId);
+   var recordCC = getContactByType("Responsible Party", capId);
+   var firstName = recordApplicant.getFirstName();
+   var lastName = recordApplicant.getLastName();
+   var emailTo = recordApplicant.getEmail();
+   var emailCC = recordCC.getEmail();
+   var wfcomment = wfComment;
+   var today = new Date();
+   var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+   var tParams = aa.util.newHashtable();
+   tParams.put("$$todayDate$$", thisDate);
+   tParams.put("$$altid$$", capId.getCustomID());
+   tParams.put("$$capAlias$$", capAlias);
+   tParams.put("$$FirstName$$", firstName);
+   tParams.put("$$LastName$$", lastName);
+   tParams.put("$$wfComment$$", wfComment);
+   logDebug("EmailTo: " + emailTo);
+   logDebug("Table Parameters: " + tParams);
+   sendNotification("noreply@auroragov.org", emailTo, emailCC, vEmailTemplate, tParams, null);
+   logDebug("End of Script ASA;ODA!Pre App!NA!NA");
