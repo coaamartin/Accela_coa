be11206ad1f5ec8fa21bd7bf626cc39642a75890
Province <rprovinc@auroragov.org>
2021-04-29 15:22:35 -0600

    updated ASA oda pre app

--- EVENT/ASA;ODA!Pre App!NA!NA.js
+++ EVENT/ASA;ODA!Pre App!NA!NA.js
@@ -5,11 +5,13 @@ var vEmailTemplate = "ODA PRE APP SUBMITAL EMAIL # 220";
    var capAlias = cap.getCapModel().getAppTypeAlias();
    var recordApplicant = getContactByType("Applicant", capId);
    var recordCC = getContactByType("Consultant", capId);
+   if (recordCC == null) {
+       recordCC = "";
+   }
    var firstName = recordApplicant.getFirstName();
    var lastName = recordApplicant.getLastName();
    var emailTo = recordApplicant.getEmail();
    var emailCC = recordCC.getEmail();
-   var wfcomment = wfComment;
    var today = new Date();
    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
    var tParams = aa.util.newHashtable();
@@ -18,7 +20,6 @@ var vEmailTemplate = "ODA PRE APP SUBMITAL EMAIL # 220";
    tParams.put("$$capAlias$$", capAlias);
    tParams.put("$$FirstName$$", firstName);
    tParams.put("$$LastName$$", lastName);
-   tParams.put("$$wfComment$$", wfComment);
    logDebug("EmailTo: " + emailTo);
    logDebug("Table Parameters: " + tParams);
    sendNotification("noreply@auroragov.org", emailTo, emailCC, vEmailTemplate, tParams, null);
