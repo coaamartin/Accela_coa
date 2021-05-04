d6713c8a279e78a4c21f8369456acb57ddbf60da
Province <rprovinc@auroragov.org>
2021-05-04 14:57:41 -0600

    updated ASYNC for temp liq and tasting

--- ASYNC/SEND_EMAIL_TAXLIC_TMPLIC_ASYNC.js
+++ ASYNC/SEND_EMAIL_TAXLIC_TMPLIC_ASYNC.js
@@ -22,7 +22,7 @@ try
 }
 catch(e)
 {
-	email("acharlton@truepointsolutions.com", "acharlton@truepointsolutions.com", "Error in TMP Lic Async", e.message);
+	email("acharlton@truepointsolutions.com", "rprovinc@auroragov.org", "Error in TMP Lic Async", e.message);
 }
 function getEmailString()
 {
