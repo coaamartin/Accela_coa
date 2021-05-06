84658e341bb3e14a50817d6abffd2a960863139b
Province <rprovinc@auroragov.org>
2021-05-06 11:09:43 -0600

    5134 and WTUA for renewal

--- EVENT/WTUA;Licenses!Contractor!General!Renewal.js
+++ EVENT/WTUA;Licenses!Contractor!General!Renewal.js
@@ -1,3 +1,4 @@
-if ("License Review".equals(wfTask) && "Additional Info Needed".equals(wfStatus)) {
+if (wfStatus == "Additional Info Needed") {
+    logDebug("Starting email communication");
 	include("5134_BLD_Resubmittal");
 }
