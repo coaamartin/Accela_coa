d37fe9a9e259d5ffab7e2e640c67563442baf6ea
Province <rprovinc@auroragov.org>
2021-05-06 11:00:45 -0600

    5134 and WTUA for renewal

---
+++ EVENT/WTUA;Licenses!Contractor!General!Renewal.js
@@ -0,0 +1,3 @@
+if ("License Review".equals(wfTask) && "Additional Info Needed".equals(wfStatus)) {
+	include("5134_BLD_Resubmittal");
+}
---
+++ INCLUDES_SCRIPTS/5134_BLD_Resubmittal.js
@@ -0,0 +1,40 @@
+//Written by rprovinc   
+//
+//include("5134_BLD_Resubmittal.js");
+
+//*****************************************************************************
+//Script WTUA;Licenses!Contractor!General!Renewal.js
+//Record Types:	
+//Event: 		WTUA
+//Desc:			Going to send email communication to ciziten when resubmittal is required of them.
+//
+//Created By: Rprovinc
+
+//******************************************************************************
+appTypeResult = cap.getCapType(); //create CapTypeModel object
+appTypeString = appTypeResult.toString();
+appTypeArray = appTypeString.split("/");
+logDebug("appType: " + appTypeString);
+
+logDebug("Starting to send notifications");
+var vEmailTemplate = "BLD_RESUBMITTAL";
+var capAlias = cap.getCapModel().getAppTypeAlias();
+var recordApplicant = getContactByType("Applicant", capId);
+var firstName = recordApplicant.getFirstName();
+var lastName = recordApplicant.getLastName();
+var emailTo = recordApplicant.getEmail();
+var wfcomment = wfComment;
+var today = new Date();
+var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+var tParams = aa.util.newHashtable();
+tParams.put("$$todayDate$$", thisDate);
+tParams.put("$$altid$$", capId.getCustomID());
+tParams.put("$$Record Type$$", "Contractor");
+tParams.put("$$capAlias$$", capAlias);
+tParams.put("$$FirstName$$", firstName);
+tParams.put("$$LastName$$", lastName);
+tParams.put("$$wfComment$$", wfComment);
+logDebug("EmailTo: " + emailTo);
+logDebug("Table Parameters: " + tParams);
+sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
+logDebug("End of Script 5134_BLD_Resubmittal.js");
