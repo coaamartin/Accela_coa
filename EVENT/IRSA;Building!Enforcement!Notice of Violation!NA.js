192da2fb5cf959a66966c9c51d5df3f03c1d2776
Province <rprovinc@auroragov.org>
2021-05-17 14:47:46 -0600

    updated IRSA event

--- EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
+++ EVENT/IRSA;Building!Enforcement!Notice of Violation!NA.js
@@ -6,18 +6,22 @@ if(inspType == "Notice of Violation Inspection"){
     processNotOfViolInsp("Notice of Violation Inspection", "First Notice", true, "Notice of Violation Inspection", true, "Notice of Violation", "First Notice");
     processNotOfViolInsp("Notice of Violation Inspection", "Second Notice", true, "Notice of Violation Inspection", true, "Notice of Violation", "Second Notice");
     processNotOfViolInsp("Notice of Violation Inspection", "Third Notice", true, "Notice of Violation Inspection", true, "Notice of Violation", "Third Notice");
-    
+    inspComment = inspObj.getInspection().getResultComment();
+    wfComment = inspComment;
     if(inspResult == "Compliance"){
         var wfTsk2Update = "Notice of Violation", wfSt2Update = "Compliance";
         if(!isTaskActive(wfTsk2Update))
             activateTask(wfTsk2Update);
 		
 		resultWorkflowTask(wfTsk2Update, wfSt2Update);
-		
+        closeTask("Notice of Violation", "Compliance", wfComment, "Resulted via Script IRSA");
         updateAppStatus("Closed", "Updated via IRSA");
         var wfProcess = getWfProcessCodeByCapId(capId);
         logDebug("Record is Closed.  Closing all active tasks for process code: " + wfProcess);
         if(wfProcess) deactivateActiveTasks(wfProcess);
+    }
+    if(inspResult == "Non compliance"){
+      closeTask("Notice of Violation", "Non Compliance", wfComment, "Resulted via Script IRSA");
     }
     
     if(inspResult == "Issue Summons"){
