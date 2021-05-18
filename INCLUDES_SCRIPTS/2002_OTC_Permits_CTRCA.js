7a39a600c777e0cbb81a8476c7e68fbb69a8faac
Province <rprovinc@auroragov.org>
2021-05-18 07:50:41 -0600

    updated ppa and pra for otc permits

--- INCLUDES_SCRIPTS/2002_OTC_Permits_CTRCA.js
+++ INCLUDES_SCRIPTS/2002_OTC_Permits_CTRCA.js
@@ -4,89 +4,102 @@ var asiValues = new Array();
 loadAppSpecific(asiValues);
 
 comment("CTRCA Pending Inspections - UPDATED 2/21 Keith- to Script #501C");
-      if (appMatch('*/*/*/AC Only')) {
-  
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         }
+if (appMatch('*/*/*/AC Only')) {
 
-      if (appMatch('*/*/*/Furnace')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         if (asiValues['Electric Service Upgrade Required'] == 'CHECKED'){
-              createPendingInspection("BLD_OTC", "Electrical Final");
-              }
-         }
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+}
 
-      if (appMatch('*/*/*/Furnace and AC')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         }
+if (appMatch('*/*/*/Furnace')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   if (asiValues['Electric Service Upgrade Required'] == 'CHECKED') {
+      createPendingInspection("BLD_OTC", "Electrical Final");
+   }
+}
 
-      if (appMatch('*/*/*/Furnace AC and Water Heater')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         createPendingInspection("BLD_OTC", "Plumbing Final");
-         }
+if (appMatch('*/*/*/Furnace and AC')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+}
 
-      if (appMatch('*/*/*/Egress Window')) {
-         createPendingInspection("BLD_OTC", "Framing Final");
-         }
+if (appMatch('*/*/*/Furnace AC and Water Heater')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+   createPendingInspection("BLD_OTC", "Plumbing Final");
+}
 
-      if (appMatch('*/*/*/Gas Pipe')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         }
+if (appMatch('*/*/*/Egress Window')) {
+   createPendingInspection("BLD_OTC", "Framing Final");
+}
 
-      if (appMatch('*/*/*/Commercial Roof')) {
-         createPendingInspection("BLD_OTC", "Reroof Final");
-         }
+if (appMatch('*/*/*/Gas Pipe')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+}
 
-
-      if (appMatch('*/*/*/Residential Roof')) {
-         createPendingInspection("BLD_OTC", "Reroof Final");
-         }
+if (appMatch('*/*/*/Commercial Roof')) {
+   createPendingInspection("BLD_OTC", "Reroof Final");
+}
 
 
-      if (appMatch('*/*/*/Residential Electrical Service')) {
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         }
+if (appMatch('*/*/*/Residential Roof')) {
+   createPendingInspection("BLD_OTC", "Reroof Final");
+}
 
-      if (appMatch('*/*/*/Siding')) {
-         createPendingInspection("BLD_OTC", "Framing Final");
-         }
 
-	 if (appMatch('*/*/*/Tankless Water Heater')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         createPendingInspection("BLD_OTC", "Plumbing Final");
-         }
-  
-     if (appMatch('*/*/*/Water Heater')) {
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         createPendingInspection("BLD_OTC", "Plumbing Final");
-         }
+if (appMatch('*/*/*/Residential Electrical Service')) {
+   createPendingInspection("BLD_OTC", "Electrical Final");
+}
 
-      if (appMatch('*/*/*/Water Heater and AC')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         createPendingInspection("BLD_OTC", "Plumbing Final");
-         }
+if (appMatch('*/*/*/Siding')) {
+   createPendingInspection("BLD_OTC", "Framing Final");
+}
 
-      if (appMatch('*/*/*/Water Heater and Furnace')) {
-         createPendingInspection("BLD_OTC", "Mechanical Final");
-         createPendingInspection("BLD_OTC", "Electrical Final");
-         createPendingInspection("BLD_OTC", "Plumbing Final");
-         }
+if (appMatch('*/*/*/Tankless Water Heater')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+   createPendingInspection("BLD_OTC", "Plumbing Final");
+}
+if (appMatch('*/*/*/Boiler New Record')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+   createPendingInspection("BLD_OTC", "Plumbing Final");
+}
+if (appMatch('*/*/*/Residential Roofing and Siding')) {
+   createPendingInspection("BLD_OTC", "Framing Final");
+   createPendingInspection("BLD_OTC", "Reroof Final");
+}
+
+if (appMatch('*/*/*/Water Heater')) {
+   if (asiValues['Water Heater Type'] == 'Electic') {
+      createPendingInspection("BLD_OTC", "Electrical Final");
+      createPendingInspection("BLD_OTC", "Plumbing Final");
+   }
+   if (asiValues['Water Heater Type'] == 'Gas') {
+      createPendingInspection("BLD_OTC", "Plumbing Final");
+   }
+}
+if (appMatch('*/*/*/Water Heater and AC')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+   createPendingInspection("BLD_OTC", "Plumbing Final");
+}
+
+if (appMatch('*/*/*/Water Heater and Furnace')) {
+   createPendingInspection("BLD_OTC", "Mechanical Final");
+   createPendingInspection("BLD_OTC", "Electrical Final");
+   createPendingInspection("BLD_OTC", "Plumbing Final");
+}
 comment("Finished CTRCA Pending Inspections to Add");
 
 
-editAppSpecific("Permit Issued Date",dateAdd(null,0));
-editAppSpecific("Permit Expiration Date",dateAdd(null,180));
+editAppSpecific("Permit Issued Date", dateAdd(null, 0));
+editAppSpecific("Permit Expiration Date", dateAdd(null, 180));
 
 
 //script16_FillApplicationNameWhenEmpty();  Removed as this was requested 5/21/19 to be removed
 
 
-closeTask("Permit Issuance","Issued","Issued on Payment in ACA"); 
+closeTask("Permit Issuance", "Issued", "Issued on Payment in ACA");
 activateTask("Inspection Phase");
 //updateAppStatus("Issued","Issued on Payment in ACA")
 
