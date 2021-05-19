a9a2a9fdc9b72c86ca4cc2aa73702494145b0ff1
Province <rprovinc@auroragov.org>
2021-05-19 11:38:12 -0600

    updated WTUA building permit master

--- INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
+++ INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
@@ -17,7 +17,7 @@ appTypeArray = appTypeString.split("/");
 logDebug("appType: " + appTypeString);
 
 logDebug("Starting to send notifications");
-var vEmailTemplate = "BLD ACCEPTPLANS";// BLD ACCEPTPLANS
+var vEmailTemplate = "BLD ACCEPTPLANS RESUBMITTAL REQUESTED";// BLD ACCEPTPLANS
 var capAlias = cap.getCapModel().getAppTypeAlias();
 var recordApplicant = getContactByType("Applicant", capId);
 var firstName = recordApplicant.getFirstName();
