b424d676bed3990537b80172b38826fc0759f9e2
Province <rprovinc@auroragov.org>
2021-05-26 08:04:04 -0600

    updated 5134 and created 5135

--- EVENT/WTUA;Building!Permit!New Building!Amendment.js
+++ EVENT/WTUA;Building!Permit!New Building!Amendment.js
@@ -1,2 +1,10 @@
 // written by SWAKIL
 include("5046_DueDatesBuildingAmend");
+
+if($iTrc(wfStatus == "Resubmittal Requested", 'wfStatus:Resubmittal Requested')){
+    include("5134_BLD_Resubmittal");
+}
+
+if($iTrc(wfStatus == "Withdrawn", 'wfStatus:Withdrawn')){
+    include("5135_BLD_Withdrawn");
+}
