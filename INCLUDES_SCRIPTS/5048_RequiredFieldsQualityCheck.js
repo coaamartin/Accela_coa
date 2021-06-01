33bf18abd65c05b8e9c2477abefda4c0db65afaa
Province <rprovinc@auroragov.org>
2021-06-01 08:09:06 -0600

    updated WTUB for new building and added 5136

--- INCLUDES_SCRIPTS/5048_RequiredFieldsQualityCheck.js
+++ INCLUDES_SCRIPTS/5048_RequiredFieldsQualityCheck.js
@@ -7,7 +7,7 @@ if ("Quality Check".equals(wfTask) && "Approved".equals(wfStatus))
 //7-24-19 Keith - Removed Construction Type and Occupancy Group per request from Darcy
 //	var reqFieldsArray = ["Total Finished Area Sq Ft", "Project Category", "Construction Type", "Occupancy Group", "Maximum Occupancy", 
 	var reqFieldsArray = ["Total Finished Area Sq Ft", "Project Category", "Maximum Occupancy", 
-	"# of Residential Units", "Single Family Detached Home", "Special Inspections", "Materials Cost", "Valuation", "Homeowner acting as Contractor"];
+	"# of Residential Units", "Single Family Detached Home", "Special Inspections", "Materials Cost", "Valuation", "Homeowner acting as Contractor", "Code Reference"];
 
 	for (var x in reqFieldsArray)
 	{
@@ -22,7 +22,7 @@ if ("Quality Check".equals(wfTask) && "Approved".equals(wfStatus))
 			missingFields = missingFields.substring(0, missingFields.length - 2);
 			cancel=true;
 			showMessage=true;
-	        comment("Not all Required Custom fields are populated for QC Approval. The following fields are missing: " + missingFields);
+	        comment("Not all Required Custom fields are populated for the Quailty Check Approval. The following fields are missing: " + missingFields);
 			
 		}
 }
