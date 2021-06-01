6f106b400ec9c45d8d27bb109efefa2e5ecfc888
Province <rprovinc@auroragov.org>
2021-06-01 08:11:00 -0600

    updated WTUB for new building and added 5136

--- INCLUDES_SCRIPTS/5048_RequiredFieldsQualityCheck.js
+++ INCLUDES_SCRIPTS/5048_RequiredFieldsQualityCheck.js
@@ -6,6 +6,7 @@ if ("Quality Check".equals(wfTask) && "Approved".equals(wfStatus))
 
 //7-24-19 Keith - Removed Construction Type and Occupancy Group per request from Darcy
 //	var reqFieldsArray = ["Total Finished Area Sq Ft", "Project Category", "Construction Type", "Occupancy Group", "Maximum Occupancy", 
+//6/1/2021 Raymond Province - Added Code Reference per Darcy SS #995
 	var reqFieldsArray = ["Total Finished Area Sq Ft", "Project Category", "Maximum Occupancy", 
 	"# of Residential Units", "Single Family Detached Home", "Special Inspections", "Materials Cost", "Valuation", "Homeowner acting as Contractor", "Code Reference"];
 
