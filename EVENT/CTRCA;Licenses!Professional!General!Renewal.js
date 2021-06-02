e59731eed553843cd25d55a643920766aefbc380
Province <rprovinc@auroragov.org>
2021-06-02 13:14:29 -0600

    updated CTRCA pro renewal

--- EVENT/CTRCA;Licenses!Professional!General!Renewal.js
+++ EVENT/CTRCA;Licenses!Professional!General!Renewal.js
@@ -1,5 +1,5 @@
 // script 427
 //updateFee("LIC_130", "LIC_PROFESSIONAL_GENERAL", "FINAL", 1, "N");
 //update fee to auto invoice SS #942
-updateFee("LIC_130", "LIC_PROFESSIONAL_GENERAL", "FINAL", 1, "N");
+updateFee("LIC_130", "LIC_PROFESSIONAL_GENERAL", "FINAL", 1, "Y");
 // end Script 427
