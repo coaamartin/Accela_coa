1fa0b1e6fc09a13d76d5843110c40c0b5093723f
Province <rprovinc@auroragov.org>
2021-06-15 09:27:34 -0600

    updated WTUA permit plans and amendments

--- INCLUDES_SCRIPTS/5134_BLD_Resubmittal.js
+++ INCLUDES_SCRIPTS/5134_BLD_Resubmittal.js
@@ -16,9 +16,13 @@ appTypeString = appTypeResult.toString();
 appTypeArray = appTypeString.split("/");
 logDebug("appType: " + appTypeString);
 
-if(!wfTask == "Accept Plans") {
 logDebug("Starting to send notifications");
-var vEmailTemplate = "BLD RESUBMITTAL NOTIFICATION";
+var vEmailTemplate = "";
+if (wfTask == "Accept Plans"){
+    var vEmailTemplate = "BLD_ACCEPTPLANS_RESUBMITTAL REQUESTED";
+} else {
+    var vEmailTemplate = "BLD RESUBMITTAL NOTIFICATION";
+}
 var capAlias = cap.getCapModel().getAppTypeAlias();
 var recordApplicant = getContactByType("Applicant", capId);
 var firstName = recordApplicant.getFirstName();
@@ -41,31 +45,3 @@ logDebug("EmailTo: " + emailTo);
 logDebug("Table Parameters: " + tParams);
 sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
 logDebug("End of Script 5134_BLD_Resubmittal.js");
-}
-
-if(wfTask == "Accept Plans"){
-    logDebug("Starting to send notifications");
-    var vEmailTemplate = "BLD_ACCEPTPLANS_RESUBMITTAL REQUESTED";
-    var capAlias = cap.getCapModel().getAppTypeAlias();
-    var recordApplicant = getContactByType("Applicant", capId);
-    var firstName = recordApplicant.getFirstName();
-    var lastName = recordApplicant.getLastName();
-    var emailTo = recordApplicant.getEmail();
-    var wfcomment = wfComment;
-    var wfTask = aa.env.getValue("WorkflowTask");
-    var today = new Date();
-    var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-    var tParams = aa.util.newHashtable();
-    tParams.put("$$wfTask$$", wfTask);
-    tParams.put("$$todayDate$$", thisDate);
-    tParams.put("$$altid$$", capId.getCustomID());
-    tParams.put("$$Record Type$$", capAlias);
-    tParams.put("$$capAlias$$", capAlias);
-    tParams.put("$$FirstName$$", firstName);
-    tParams.put("$$LastName$$", lastName);
-    tParams.put("$$wfComment$$", wfComment);
-    logDebug("EmailTo: " + emailTo);
-    logDebug("Table Parameters: " + tParams);
-    sendNotification("noreply@auroragov.org", emailTo, "", vEmailTemplate, tParams, null);
-    logDebug("End of Script 5134_BLD_Resubmittal.js");
-}
