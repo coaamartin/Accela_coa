054dcb31e0a4dd14b1df8b9d298736e05bff671e
Province <rprovinc@auroragov.org>
2021-06-22 08:51:59 -0600

    updated IRSA mj event

--- EVENT/IRSA;Licenses!Marijuana!~!~.js
+++ EVENT/IRSA;Licenses!Marijuana!~!~.js
@@ -129,14 +129,20 @@ function _failedMJInspectionAutomation(vCapType){
 		var reportTemplate = "MJ_Compliance_Corrections_Letter";
 		var reportParams = aa.util.newHashtable();
 		addParameter(reportParams, "InspActNumber", inspId);
-		
+		var inspectionEmail = "";
+		if (inspectionEmail = "jbeuthel") {
+			inspectionEmail = "jbeuthel@Auroragov.onmicrosoft.com"
+		} else {
+			inspectionEmail = vInspector + "auroragov.org";
+		}
+		logDebug ("Inspection Email: " + inspectionEmail);
         var emails = _getContactEmailNoDupEmail(capId,"Inspection Contact");
 		emails = emails.join(";");
 		logDebug("Email send to: " + emails)
 		var reportFiles = new Array();
         var report = _generateReportFile(reportTemplate, reportParams, aa.getServiceProviderCode());
 		reportFiles.push(report);
-        _sendNotification("noreply@auroragov.org", emails, "jbeuthel@Auroragov.onmicrosoft.com", emailTemplate, eParams, reportFiles);
+        _sendNotification("noreply@auroragov.org", emails, inspectionEmail, emailTemplate, eParams, reportFiles);
 	}
 
 }
@@ -204,7 +210,12 @@ function _passedMJInspectionEmailNotification(vCapType){
 		var reportTemplate = "MJ_Compliance_Corrections_Letter";
 		var reportParams = aa.util.newHashtable();
 		addParameter(reportParams, "InspActNumber", inspId);
-		
+		if (inspectionEmail = "jbeuthel") {
+			inspectionEmail = "jbeuthel@Auroragov.onmicrosoft.com"
+		} else {
+			inspectionEmail = vInspector + "auroragov.org";
+		}
+		logDebug ("Inspection Email: " + inspectionEmail);
 		//send email with report attachment     
         var emails = _getContactEmailNoDupEmail(capId,"Inspection Contact");
 		emails = emails.join(";");
@@ -212,7 +223,7 @@ function _passedMJInspectionEmailNotification(vCapType){
 		var reportFiles = new Array();
         var report = _generateReportFile(reportTemplate, reportParams, aa.getServiceProviderCode());
 		reportFiles.push(report);
-        _sendNotification("noreply@auroragov.org", emails, "jbeuthel@Auroragov.onmicrosoft.com", emailTemplate, eParams, reportFiles);
+        _sendNotification("noreply@auroragov.org", emails, inspectionEmail, emailTemplate, eParams, reportFiles);
 	}
 
 }
