037de8da1dd857db0b4bd122c9f4d2a7775505ad
6charlton <65371934+6charlton@users.noreply.github.com>
2021-06-23 17:59:25 -0600

    Update IRSA;PublicWorks!Public Improvement!Permit!NA.js

--- EVENT/IRSA;PublicWorks!Public Improvement!Permit!NA.js
+++ EVENT/IRSA;PublicWorks!Public Improvement!Permit!NA.js
@@ -1,5 +1,11 @@
-/*
-if(inspType == "PI Inspection" && inspResult == "Final"){
-	activateTask("PI Inspection");
+if (inspType == "PI Inspection" && inspResult == "Final") {
+var w = AInfo["Warranty Work?"];
+logDebug("Warranty check =" +w);
+	if (w == 'Yes') {
+		closeTask("PI Inspection", "Completed", "", "");
+	}
+	if (w == 'No') {
+		closeTask("PI Inspection", "Completed", "", "");
+		closeTask("PI Inspection", "Not Required Warranty", "", "");
+		}
 }
-*/
