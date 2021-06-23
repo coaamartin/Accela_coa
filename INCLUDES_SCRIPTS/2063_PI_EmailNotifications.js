83252ba9737731349088845d2926e3774f5646e2
6charlton <65371934+6charlton@users.noreply.github.com>
2021-06-23 16:26:16 -0600

    Update 2063_PI_EmailNotifications.js
    Changing email applicant to solo.

--- INCLUDES_SCRIPTS/2063_PI_EmailNotifications.js
+++ INCLUDES_SCRIPTS/2063_PI_EmailNotifications.js
@@ -3,28 +3,29 @@ if ((wfStatus == 'Accepted') && appMatch('PublicWorks/Public Improvement/Permit/
 logDebug("Starting of 2063_PI_Email Notification Script");
 var recordID = capId.getCustomID()
 appType = cap.getCapType().toString();
-    var recordApplicants = getContactByType("Applicant", capId);
-    for (var i in recordApplicants) {
-        var recordApplicant = recordApplicants[i];
-        var firstName = recordApplicant.getFirstName();
-        var lastName = recordApplicant.getLastName();
-        var emailTo = recordApplicant.getEmail();
-        var capAlias = cap.getCapModel().getAppTypeAlias();
-        var today = new Date();
-        var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-        var tParms = aa.util.newHashtable();
-        addParameter(tParms, "$$todayDate$$", thisDate);
-        addParameter(tParms, "$$altid$$", recordID);
-        addParameter(tParms, "$$capAlias$$", capAlias);
-        addParameter(tParms, "$$FirstName$$", firstName);
-        addParameter(tParms, "$$LastName$$", lastName);
-        var rParams = aa.util.newHashtable();
-        rParams.put("RecordID", recordID);
-        logDebug("rParams: " + rParams);
-        var emailtemplate = "PI INITIAL ACCEPTANCE # 167";
-        var report = generateReportFile("PI_Initial_Acceptance_Script", rParams, aa.getServiceProviderCode());
-        sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParms, [report]);
-    }
+    var recordApplicant = getContactByType("Applicant", capId);
+    //for (var i in recordApplicants) {
+        //var recordApplicant = recordApplicants[i];
+		if recordApplicant {
+			var firstName = recordApplicant.getFirstName();
+			var lastName = recordApplicant.getLastName();
+			var emailTo = recordApplicant.getEmail();
+			var capAlias = cap.getCapModel().getAppTypeAlias();
+			var today = new Date();
+			var thisDate = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+			var tParms = aa.util.newHashtable();
+			addParameter(tParms, "$$todayDate$$", thisDate);
+			addParameter(tParms, "$$altid$$", recordID);
+			addParameter(tParms, "$$capAlias$$", capAlias);
+			addParameter(tParms, "$$FirstName$$", firstName);
+			addParameter(tParms, "$$LastName$$", lastName);
+			var rParams = aa.util.newHashtable();
+			rParams.put("RecordID", recordID);
+			logDebug("rParams: " + rParams);
+			var emailtemplate = "PI INITIAL ACCEPTANCE # 167";
+			var report = generateReportFile("PI_Initial_Acceptance_Script", rParams, aa.getServiceProviderCode());
+			sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParms, [report]);
+		}	
     if (showDebug) {
         email("acharlton@truepointsolutions.com", "acharlton@truepointsolutions.com", "DEBUG PI Acceptance Async for " +recordID, e.message + " in Line " + e.lineNumber + br + "Stack: " + e.stack + br + "Debug: " + debug);
     }
