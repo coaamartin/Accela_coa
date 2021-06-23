ec59de24d09dbd60f85e283c9e64a2f71e6a0ec5
6charlton <65371934+6charlton@users.noreply.github.com>
2021-06-23 16:52:05 -0600

    Update PRA;PublicWorks!Public Improvement!Permit!NA.js

--- EVENT/PRA;PublicWorks!Public Improvement!Permit!NA.js
+++ EVENT/PRA;PublicWorks!Public Improvement!Permit!NA.js
@@ -3,7 +3,7 @@ logDebug("Current balance: " + balanceDue);
 //Check balance and update task
 if (appMatch("PublicWorks/Public Improvement/Permit/*")) {
 	if (balanceDue == 0) {
-		branchTask("Fee Processing", "Complete", "", "");
+		updateTask("Fee Processing", "Complete", "", "");
 	}
 }
 logDebug("Ending PRA Public Improvement");
