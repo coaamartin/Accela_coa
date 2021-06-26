38c057e058359d1f9eef13834ff23d25e23bc84e
Suhail Wakil <suhailwakil@gmail.com>
2021-06-26 14:36:03 -0700

    Update 313_AddExpiredRenewalLateFeeMJ.js

--- INCLUDES_SCRIPTS/313_AddExpiredRenewalLateFeeMJ.js
+++ INCLUDES_SCRIPTS/313_AddExpiredRenewalLateFeeMJ.js
@@ -1,47 +1,48 @@
 var vLicenseId = getParentLicenseCapID(capId);
 if (vLicenseId != null && vLicenseId != false) {
-	// Assess the Delinquent Fee
-	var vLicenseObj;
-	var vExpDate;
-	var vExpDateString;
-	var vLicExp_mm;
-	var vLicExp_dd;
-	var vLicExp_yyyy
-	var vCompareDate;
-	// Get expiration date as MM/DD/YYYY
-	vLicenseObj = new licenseObject(null, vLicenseId);
-	vExpDate = vLicenseObj.b1ExpDate;
-	vExpDate = new Date(vExpDate);
-	vLicExp_mm = vExpDate.getMonth() + 1;
-	vLicExp_mm = (vLicExp_mm < 10) ? '0' + vLicExp_mm : vLicExp_mm;
-	vLicExp_dd = vExpDate.getDate();
-	vLicExp_dd = (vLicExp_dd < 10) ? '0' + vLicExp_dd : vLicExp_dd;
-	vLicExp_yyyy = vExpDate.getFullYear();
-	vExpDateString = vLicExp_mm + "/" + vLicExp_dd + "/" + vLicExp_yyyy;
-	vExpDateString = dateAdd(vExpDateString, 7);
-	vExpDate = new Date(vExpDateString);
-	// Get the date of submittal to compare against license expiration.
-	// For ACA submitted records it will be the Application Acceptance - Submitted date.
-	// For non ACA submitted records it will be the Application Acceptance - Application Received date.
-	var vCompareDate = new Date();;
-	logDebug("Delinquent fee check. ACA submittal: " + cap.isCreatedByACA() + " Expiration Date: " + vExpDateString + " Compare Date: " + vCompareDate);
-	if (vCompareDate != null && vCompareDate != false) {
-		//vCompareDate = new Date(vCompareDateString);
-		if (vExpDate < vCompareDate) {
-			logDebug("Assessing delinquent fee. ACA submittal: " + cap.isCreatedByACA() + " Expiration Date: " + vExpDateString + " Compare Date: " + vCompareDate);
-			if (appMatch("Licenses/Marijuana/Retail Cultivation/Renewal")) {
-				updateFee("LIC_MJRC_03", "LIC_MJ_RC", "FINAL", 1, "Y");				
-			} else if (appMatch("Licenses/Marijuana/Retail Product Manufacturer/Renewal")) {
-				updateFee("LIC_MJRPM_03", "LIC_MJ_RPM", "FINAL", 1, "Y");				
-			} else if (appMatch("Licenses/Marijuana/Retail Store/Renewal")) {
-				updateFee("LIC_MJST_02", "LIC_MJ_STORE", "FINAL", 1, "Y");				
-			} else if (appMatch("Licenses/Marijuana/Retail Transporter/Renewal")) {
-				updateFee("LIC_MJTR_03", "LIC_MJ_TRANS", "FINAL", 1, "Y");			
-			} else if (appMatch("Licenses/Marijuana/Testing Facility/Renewal")) {
-				updateFee("LIC_MJTST_03", "LIC_MJ_TEST", "FINAL", 1, "Y");				
-			} else {
-				logDebug("Invalid renewal record type");
-			}
-		}
-	}
+    // Assess the Delinquent Fee
+    var vLicenseObj;
+    var vExpDate;
+    var vExpDateString;
+    var vLicExp_mm;
+    var vLicExp_dd;
+    var vLicExp_yyyy
+    var vCompareDate;
+    // Get expiration date as MM/DD/YYYY
+    vLicenseObj = new licenseObject(null, vLicenseId);
+    vExpDate = vLicenseObj.b1ExpDate;
+    vExpDate = new Date(vExpDate);
+    vLicExp_mm = vExpDate.getMonth(); //Month less (no +1 for month)
+    vLicExp_mm = (vLicExp_mm < 10) ? '0' + vLicExp_mm : vLicExp_mm;
+    vLicExp_dd = vExpDate.getDate();
+    vLicExp_dd = (vLicExp_dd < 10) ? '0' + vLicExp_dd : vLicExp_dd;
+    vLicExp_yyyy = vExpDate.getFullYear();
+    vExpDateString = vLicExp_mm + "/" + vLicExp_dd + "/" + vLicExp_yyyy;
+
+    vExpDateString = dateAdd(vExpDateString, 7);
+    vExpDate = new Date(vExpDateString);
+    // Get the date of submittal to compare against license expiration.
+    // For ACA submitted records it will be the Application Acceptance - Submitted date.
+    // For non ACA submitted records it will be the Application Acceptance - Application Received date.
+    var vCompareDate = new Date();;
+    logDebug("Delinquent fee check. ACA submittal: " + cap.isCreatedByACA() + " Del Fee Date: " + vExpDateString + " Compare Date: " + vCompareDate);
+    if (vCompareDate != null && vCompareDate != false) {
+        //vCompareDate = new Date(vCompareDateString);
+        if (vExpDate < vCompareDate) {
+            logDebug("Assessing delinquent fee. ACA submittal: " + cap.isCreatedByACA() + " Del Fee Date: " + vExpDateString + " Compare Date: " + vCompareDate);
+            if (appMatch("Licenses/Marijuana/Retail Cultivation/Renewal")) {
+                updateFee("LIC_MJRC_03", "LIC_MJ_RC", "FINAL", 1, "Y");
+            } else if (appMatch("Licenses/Marijuana/Retail Product Manufacturer/Renewal")) {
+                updateFee("LIC_MJRPM_03", "LIC_MJ_RPM", "FINAL", 1, "Y");
+            } else if (appMatch("Licenses/Marijuana/Retail Store/Renewal")) {
+                updateFee("LIC_MJST_02", "LIC_MJ_STORE", "FINAL", 1, "Y");
+            } else if (appMatch("Licenses/Marijuana/Retail Transporter/Renewal")) {
+                updateFee("LIC_MJTR_03", "LIC_MJ_TRANS", "FINAL", 1, "Y");
+            } else if (appMatch("Licenses/Marijuana/Testing Facility/Renewal")) {
+                updateFee("LIC_MJTST_03", "LIC_MJ_TEST", "FINAL", 1, "Y");
+            } else {
+                logDebug("Invalid renewal record type");
+            }
+        }
+    }
 }
