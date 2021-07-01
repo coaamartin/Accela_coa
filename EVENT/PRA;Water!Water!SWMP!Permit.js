4bfb4599633a7ae66ec5f348243341b5a98906da
Suhail Wakil <suhailwakil@gmail.com>
2021-07-01 02:33:23 -0600

    1393

--- EVENT/PRA;Water!Water!SWMP!Permit.js
+++ EVENT/PRA;Water!Water!SWMP!Permit.js
@@ -2,6 +2,7 @@
 if (balanceDue <= 0 && isTaskActive("Active Permit"))
 {
 	//updateAppStatus("Issued", "Updated via script");
+    var rB1ExpResult = aa.expiration.getLicensesByCapID(capId).getOutput();
     rB1ExpResult.setExpDate(aa.date.parseDate(dateAdd(null, 365)));
     rB1ExpResult.setExpStatus("Active");
     aa.expiration.editB1Expiration(rB1ExpResult.getB1Expiration());
