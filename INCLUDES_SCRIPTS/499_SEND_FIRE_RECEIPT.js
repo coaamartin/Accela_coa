58283a2e72f1f2415f4e4a97de72726b049a983d
Province <rprovinc@auroragov.org>
2021-07-06 08:25:02 -0600

    updated 499

--- INCLUDES_SCRIPTS/499_SEND_FIRE_RECEIPT.js
+++ INCLUDES_SCRIPTS/499_SEND_FIRE_RECEIPT.js
@@ -1,9 +1,8 @@
-var invoiceNbrObj = getLastInvoice({});
-var invNbr = invoiceNbrObj.getInvNbr();
+var InvoiceNbr = InvoiceNbrArray[0] + "";
 var envParameters = aa.util.newHashMap();
 envParameters.put("capId", capId);
 envParameters.put("cap", cap);
 envParameters.put("amount", PaymentTotalPaidAmount);
-envParameters.put("INVOICEID", String(invNbr));
+envParameters.put("INVOICEID", InvoiceNbr);
 var vAsyncScript = "SEND_FIRE_RECEIPT_ASYNC";
 aa.runAsyncScript(vAsyncScript, envParameters);
