62ce7d180b9fb1a6a1dcc4a07e3f8038d60f625c
Province <rprovinc@auroragov.org>
2021-07-06 11:55:37 -0600

    updated wtua for new building

--- EVENT/CTRCA;Licenses!Contractor!General!Renewal.js
+++ EVENT/CTRCA;Licenses!Contractor!General!Renewal.js
@@ -2,11 +2,11 @@
 var theFee = 125;
 
 if ("Commercial Building".equals(AInfo["Contractor Type"])) {
-	theFee = 300;
+	theFee = 325;
 }
 
 if ("Residential Building".equals(AInfo["Contractor Type"])) {
-	theFee = 180;
+	theFee = 195;
 }
 
 updateFee("LIC_030", "LIC_CONTRACTOR_GENERAL", "FINAL", theFee, "N");
--- EVENT/WTUA;Licenses!Contractor!General!Application.js
+++ EVENT/WTUA;Licenses!Contractor!General!Application.js
@@ -5,10 +5,10 @@ if ("License Issuance".equals(wfTask) && "Ready to Pay".equals(wfStatus)) {
 	// script 427
 	var theFee = 125;
 	if ("Commercial Building".equals(AInfo["Contractor Type"])) {
-		theFee = 300;
+		theFee = 325;
 	}
 	if ("Residential Building".equals(AInfo["Contractor Type"])) {
-		theFee = 180;
+		theFee = 195;
 	}
 	updateFee("LIC_020", "LIC_CONTRACTOR_GENERAL", "FINAL", theFee, "N");
 	// end Script 427
