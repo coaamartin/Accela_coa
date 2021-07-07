9dd9cc4bb639db0fdc214174bdff971d6318127b
Province <rprovinc@auroragov.org>
2021-07-07 08:14:45 -0600

    updated 5132

--- INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
+++ INCLUDES_SCRIPTS/5132_BLD_ResubmitEmail.js
@@ -17,7 +17,7 @@ appTypeArray = appTypeString.split("/");
 logDebug("appType: " + appTypeString);
 
 logDebug("Starting to send notifications");
-var vEmailTemplate = "BLD_ACCEPTPLANS_RESUBMITTAL REQUESTED";// BLD ACCEPTPLANS
+var vEmailTemplate = "BLD_PLANNING_RESUBMITTAL";// BLD ACCEPTPLANS
 var capAlias = cap.getCapModel().getAppTypeAlias();
 var recordApplicant = getContactByType("Applicant", capId);
 var firstName = recordApplicant.getFirstName();
