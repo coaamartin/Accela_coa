03f3171cc1444b6da20c4cdab1dee8edaaf2c7b7
Province <rprovinc@auroragov.org>
2021-07-08 09:50:23 -0600

    updated WTUB for permit plans

--- EVENT/WTUB;Building!Permit!Plans!~.js
+++ EVENT/WTUB;Building!Permit!Plans!~.js
@@ -29,15 +29,15 @@ if ($iTrc(wfTask == "Accept Plans" && wfStatus == "Accepted")) {
 	for (TSIm in wfTSI) {
 		var tsiCheck = [wfTSI[TSIm].getChecklistComment()];
 		logDebug("TSICHECK: " + tsiCheck);
-
-		if (tsiCheck != "") {
-			AInfo["Updated1." + wfTSI[TSIm].getCheckboxDesc()] = wfTSI[TSIm].getChecklistComment();
-		}
-		if (tsiCheck == "") {
-			showMessage = true;
-			comment("<h2 style='background-color:rgb(255, 0, 0);'>At least one TSI field needs to be selected before the workflow can be completed.</h2>");
-			cancel = true;
-		}
+	}
+	
+	if (tsiCheck != "") {
+		AInfo["Updated1." + wfTSI[TSIm].getCheckboxDesc()] = wfTSI[TSIm].getChecklistComment();
+	}
+	if (tsiCheck == "") {
+		showMessage = true;
+		comment("<h2 style='background-color:rgb(255, 0, 0);'>At least one TSI field needs to be selected before the workflow can be completed.</h2>");
+		cancel = true;
 	}
 }
 logGlobals(AInfo);
