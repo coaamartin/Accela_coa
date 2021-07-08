cdc9b6925ee6124afcd85da8f86ae88693f6e737
Province <rprovinc@auroragov.org>
2021-07-08 07:53:33 -0600

    updated WTUB for permit plans

--- EVENT/WTUB;Building!Permit!Plans!~.js
+++ EVENT/WTUB;Building!Permit!Plans!~.js
@@ -15,44 +15,40 @@ Sample Call : checkSpecialInspections()
 checkSpecialInspections();
 //Adding code to verify that a TSI field has been selected
 if (wfTask == "Accept Plans" && wfStatus == "Approved") {
-	var foundCheckBox = false;   
+	var foundCheckBox = false;
 	var workflowResult = aa.workflow.getTasks(capId);
-   var wfObj = workflowResult.getOutput();
-   
-   for (i in wfObj) 
-   {
-      var fTask = wfObj[i];
+	var wfObj = workflowResult.getOutput();
+	logDebug("WFOBJ: " + wfObj);
+	logDebug("WorkflowResult: " + workflowResult);
+
+	for (i in wfObj) {
+		var fTask = wfObj[i];
 		var stepnumber = fTask.getStepNumber();
 		var processID = fTask.getProcessID();
-      
-      
-         var TSIResult = aa.taskSpecificInfo.getTaskSpecificInfoByTask(capId, processID, stepnumber)  // 
-			if (TSIResult.getSuccess()) 
-         {
-            
-            var TSI = TSIResult.getOutput();
-            if (TSI != null) 
-               
-            for (dmyIttr in TSI)
-            {          
-              
-              if (TSI[dmyIttr].getChecklistComment() != null)
-              {   
-                if (TSI[dmyIttr].getChecklistComment() == "CHECKED") 
-                {
-                   foundCheckBox = true;
-                }
-              }  
-            }
-            
-         }   
-	
-   }
-   
-   if (!foundCheckBox)
-   {
-      showMessage = true;
-	   comment("<h2 style='background-color:rgb(255, 0, 0);'>Accepted Plans requires at least one TSI field type to be checked for the workflow to continue.</h2>");
-      cancel = true;
-   }
-} 
+
+
+		var TSIResult = aa.taskSpecificInfo.getTaskSpecificInfoByTask(capId, processID, stepnumber) // 
+		if (TSIResult.getSuccess()) {
+
+			var TSI = TSIResult.getOutput();
+			if (TSI != null)
+
+				for (dmyIttr in TSI) {
+
+					if (TSI[dmyIttr].getChecklistComment() != null) {
+						if (TSI[dmyIttr].getChecklistComment() == "CHECKED") {
+							foundCheckBox = true;
+						}
+					}
+				}
+
+		}
+
+	}
+
+	if (!foundCheckBox) {
+		showMessage = true;
+		comment("<h2 style='background-color:rgb(255, 0, 0);'>Accepted Plans requires at least one TSI field type to be checked for the workflow to continue.</h2>");
+		cancel = true;
+	}
+}
