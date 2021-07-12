13b31608ff834ecf8161fe7f21408dd2194e293c
Province <rprovinc@auroragov.org>
2021-07-12 13:30:07 -0600

    updated 5141

--- INCLUDES_SCRIPTS/5141_Check_Special_Inspection_BLDPWP.js
+++ INCLUDES_SCRIPTS/5141_Check_Special_Inspection_BLDPWP.js
@@ -14,7 +14,9 @@ if (asiValues == "Yes"){
 if (asiValues == "No"){
     logDebug("Special Inspections Value was set to No. Going to close Workflow step Special Inspections.");
     //Need to close Special Inspeciton workflow step below
-    deactivateTask("Special Inspections Check");
+    //deactivateTask("Special Inspections Check");
+    //may use the updateTask function
+    updateTask("Special Inspections Check", "Not Required", "", "Resulted via Script 5141");
 
 }
 logDebug("---------------------> 5141_Check_Special_Inspection_BLDPWP.js ended.");
