42e07579fb9039fc8431afc1141c423639c54fd0
Province <rprovinc@auroragov.org>
2021-07-12 09:36:55 -0600

    updated LIC_CONT email send

--- ASYNC/SEND_EMAIL_LIC_CONT_LIC.js
+++ ASYNC/SEND_EMAIL_LIC_CONT_LIC.js
@@ -42,8 +42,8 @@ try
 	//need to run the license report below
 	var report = generateReportFile("Contractor_License_Script", rParams, aa.getServiceProviderCode());
 	//sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, [report]);
-    sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, [report]);
-	//sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, null);
+    //sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, [report]);
+	sendNotification("noreply@auroragov.org", emailTo, "", emailtemplate, tParams, null);
 }
 catch(e)
 {
