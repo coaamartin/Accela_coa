a23d12b2e89f9beece2d5fb637a1283180136455
Province <rprovinc@auroragov.org>
2021-07-12 09:30:48 -0600

    updated LIC_CONT email send

--- ASYNC/SEND_EMAIL_LIC_CONT_LIC.js
+++ ASYNC/SEND_EMAIL_LIC_CONT_LIC.js
@@ -1,4 +1,16 @@
 logDebug("***** Starting SEND_EMAIL_LIC_CONT_LIC *****");
+function getScriptText(vScriptName){
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),vScriptName,"ADMIN");
+    return emseScript.getScriptText() + "";          
+}
+
+var SCRIPT_VERSION = 3.0
+aa.env.setValue("CurrentUserID", "ADMIN");
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
+eval(getScriptText("COMMON_RUN_REPORT_AND_NOTIFICATION"));
 
 try
 {
