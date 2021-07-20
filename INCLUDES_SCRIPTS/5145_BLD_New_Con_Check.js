d9f9397167d03e77d214436eeeba39b139c18529
Province <rprovinc@auroragov.org>
2021-07-20 10:28:07 -0600

    updated IRSA for NOV

--- INCLUDES_SCRIPTS/5145_BLD_New_Con_Check.js
+++ INCLUDES_SCRIPTS/5145_BLD_New_Con_Check.js
@@ -52,142 +52,130 @@ asiCheckValues.push(asiRadioRec);
 logDebug("ASI Values: " + asiValues);
 logDebug("ASI ARRAY: " + asiCheckValues);
 
-if (asiValues == "Yes" || asiValues == null){
-    if(asiCheckValues.toString().indexOf("CHECKED") !== -1){
-    logDebug("Special Inspections Value was set to Yes. Going to keep the Special Inspections workflow task open.");
-}
-else {
-    logDebug("Value does not exists")
-		showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>At least one Special Inspection must be checked before the workflow can be completed.</h2>");
-		cancel = true
-}
-}
+if (asiValues == "Yes" || asiValues == null) {
+    if (asiCheckValues.toString().indexOf("CHECKED") !== -1) {
+        logDebug("Special Inspections Value was set to Yes. Going to keep the Special Inspections workflow task open.");
+    } else {
+        logDebug("Value does not exists")
+        showMessage = true;
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>At least one Special Inspection must be checked before the workflow can be completed.</h2>");
+        cancel = true
+    }
+
 //Below I need to check the special inspections to the required field and to the matching received field. If the required field has a check then so should the recieved field.
 //Concrete below
 if (asiConcreteReq == "CHECKED") {
-    if(asiConcreteReq == asiConCreteRec){
+    if (asiConcreteReq == asiConCreteRec) {
         logDebug("Concrete Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Concrete Report is required and has not been Received. Ensure that the Concrete report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Concrete Report is required and has not been Received. Ensure that the Concrete report is received.</h2>");
+        cancel = true
     }
 }
 //EIFS below
 if (asiEIFSReq == "CHECKED") {
-    if(asiEIFSReq == asiEIFSRec){
+    if (asiEIFSReq == asiEIFSRec) {
         logDebug("EIFS Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>EIFS Report is required and has not been Received. Ensure that the EIFS report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>EIFS Report is required and has not been Received. Ensure that the EIFS report is received.</h2>");
+        cancel = true
     }
 }
 //Firestopping Required 
 if (asiFireStoppingReq == "CHECKED") {
-    if(asiFireStoppingReq == asiFireStoppingRec){
+    if (asiFireStoppingReq == asiFireStoppingRec) {
         logDebug("Firestopping Required Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Firestopping Report is required and has not been Received. Ensure that the Firestopping report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Firestopping Report is required and has not been Received. Ensure that the Firestopping report is received.</h2>");
+        cancel = true
     }
 }
 //Med Gas Required
 if (asiMedGasReq == "CHECKED") {
-    if(asiMedGasReq == asiMedGasRec){
+    if (asiMedGasReq == asiMedGasRec) {
         logDebug("Med Gas Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Med Gas Report is required and has not been Received. Ensure that the Med Gas report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Med Gas Report is required and has not been Received. Ensure that the Med Gas report is received.</h2>");
+        cancel = true
     }
 }
 //Pier and Pile Foundations Required
 if (asiPeirReq == "CHECKED") {
-    if(asiPeirReq == asiPierRec){
+    if (asiPeirReq == asiPierRec) {
         logDebug("Pier and Pile Foundations Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Pier and Pile Foundations Report is required and has not been Received. Ensure that the Pier and Pile Foundations report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Pier and Pile Foundations Report is required and has not been Received. Ensure that the Pier and Pile Foundations report is received.</h2>");
+        cancel = true
     }
 }
 //Smoke Control Required  
 if (asiSmokeContReq == "CHECKED") {
-    if(asiSmokeContReq == asiSmokeContRec){
+    if (asiSmokeContReq == asiSmokeContRec) {
         logDebug("Smoke Control Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Smoke Control Report is required and has not been Received. Ensure that the Smoke Control report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Smoke Control Report is required and has not been Received. Ensure that the Smoke Control report is received.</h2>");
+        cancel = true
     }
 }
 //Sprayed FR Materials Required
 if (asiSprayedFrReq == "CHECKED") {
-    if(asiSprayedFrReq == asiSprayedFrRec){
+    if (asiSprayedFrReq == asiSprayedFrRec) {
         logDebug("Sprayed FR Materials Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Sprayed FR Materials Report is required and has not been Received. Ensure that the Sprayed FR Materials report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Sprayed FR Materials Report is required and has not been Received. Ensure that the Sprayed FR Materials report is received.</h2>");
+        cancel = true
     }
 }
 //Steel Required
 if (asiSteelReq == "CHECKED") {
-    if(asiSteelReq == asiStructRec){
+    if (asiSteelReq == asiStructRec) {
         logDebug("Steel Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Steel Report is required and has not been Received. Ensure that the Steel report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Steel Report is required and has not been Received. Ensure that the Steel report is received.</h2>");
+        cancel = true
     }
 }
 //Structural Masonry Required
 if (asiStructReq == "CHECKED") {
-    if(asiStructReq == asiStructRec){
+    if (asiStructReq == asiStructRec) {
         logDebug("Structural Masonry Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Structural Masonry Report is required and has not been Received. Ensure that the Structural Masonry report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Structural Masonry Report is required and has not been Received. Ensure that the Structural Masonry report is received.</h2>");
+        cancel = true
     }
 }
 //Structural Wood Required  
 if (asiStructWoodReq == "CHECKED") {
-    if(asiStructWoodReq == asiStructWoodRec){
+    if (asiStructWoodReq == asiStructWoodRec) {
         logDebug("Structural Wood Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Structural Wood is required and has not been Received. Ensure that the Structural Wood report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Structural Wood is required and has not been Received. Ensure that the Structural Wood report is received.</h2>");
+        cancel = true
     }
 }
 //Radio Freq Required
 if (asiRadioReq == "CHECKED") {
-    if(asiRadioReq == asiRadioRec){
+    if (asiRadioReq == asiRadioRec) {
         logDebug("Radio Freq Report was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Radio Freq Report is required and has not been Received. Ensure that the Radio Freq report is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Radio Freq Report is required and has not been Received. Ensure that the Radio Freq report is received.</h2>");
+        cancel = true
     }
 }
 
 
-if (asiValues == "No"){
+if (asiValues == "No") {
     logDebug("Special Inspections Value was set to No. Going to close Workflow step Special Inspections.");
     //Need to close Special Inspeciton workflow step below
     //may use the updateTask function
@@ -195,7 +183,7 @@ if (asiValues == "No"){
     // deactivateTask("Special Inspections Check");
 
 }
-
+}
 
 //below we need to run through checks of the Engineer letters. If One is required then the 
 //same required document needs to be received. 
@@ -225,57 +213,52 @@ logDebug("ASI Engineering Letters: " + asiLetterValues);
 //Drain Letter
 logDebug("Drain Letter Req: " + asiDrainLetterReq);
 if (asiDrainLetterReq == "CHECKED") {
-    if(asiDrainLetterReq == asiDrainLetterRec){
+    if (asiDrainLetterReq == asiDrainLetterRec) {
         logDebug("Drain Letter was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Drain Letter is required and has not been Received. Ensure that the Drain Letter is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Drain Letter is required and has not been Received. Ensure that the Drain Letter is received.</h2>");
+        cancel = true
     }
 }
 //Footing - Pier - Cassion Letter
 if (asiFootingReq == "CHECKED") {
-    if(asiFootingReq == asiFootingRec){
+    if (asiFootingReq == asiFootingRec) {
         logDebug("Footing - Pier - Cassion Letter was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Footing - Pier - Cassion Letter is required and has not been Received. Ensure that the Footing - Pier - Cassion Letter is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Footing - Pier - Cassion Letter is required and has not been Received. Ensure that the Footing - Pier - Cassion Letter is received.</h2>");
+        cancel = true
     }
 }
 //Foundation Letter
 if (asiFoundationLetterReq == "CHECKED") {
-    if(asiFoundationLetterReq == asiFoundationLetterRec){
+    if (asiFoundationLetterReq == asiFoundationLetterRec) {
         logDebug("Foundation Letter was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Foundation Letter is required and has not been Received. Ensure that the Foundation Letter is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Foundation Letter is required and has not been Received. Ensure that the Foundation Letter is received.</h2>");
+        cancel = true
     }
 }
 //ILC Letter
 if (asiILCLetterReq == "CHECKED") {
-    if(asiILCLetterReq == asiILCLetterRec){
+    if (asiILCLetterReq == asiILCLetterRec) {
         logDebug("ILC Letter was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>ILC Letter is required and has not been Received. Ensure that the ILC Letter is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>ILC Letter is required and has not been Received. Ensure that the ILC Letter is received.</h2>");
+        cancel = true
     }
 }
 //Waterproofing Letter
 if (asiWaterLetterReq == "CHECKED") {
-    if(asiWaterLetterReq == asiWaterLetterRec){
+    if (asiWaterLetterReq == asiWaterLetterRec) {
         logDebug("Waterproofing Letter was Required and was Received. Moving forward.")
-    }
-    else {
+    } else {
         showMessage = true;
-		comment("<h2 style='background-color:rgb(255, 0, 0);'>Waterproofing Letter is required and has not been Received. Ensure that the Waterproofing Letter is received.</h2>");
-		cancel = true
+        comment("<h2 style='background-color:rgb(255, 0, 0);'>Waterproofing Letter is required and has not been Received. Ensure that the Waterproofing Letter is received.</h2>");
+        cancel = true
     }
 }
 
